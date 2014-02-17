a434b81345d85be44086220f82c7cc073920ae83
Manuel de la Peña <manuel.delapenya@liferay.com>
2014-02-17 10:59:52 +0100

    LPS-44302 Split into several test methods, increasing readability

--- portal-impl/test/unit/com/liferay/portal/util/HtmlImplTest.java
+++ portal-impl/test/unit/com/liferay/portal/util/HtmlImplTest.java
@@ -75,32 +75,60 @@ public class HtmlImplTest {
 	}
 
 	@Test
-	public void testStripBetween() {
+	public void testStripBetweenNull() {
 		Assert.assertNull(_htmlImpl.stripBetween(null, "test"));
+	}
+
+	@Test
+	public void testStripBetweenHtmlElement() {
 		Assert.assertEquals(
 			"test--test",
 			_htmlImpl.stripBetween(
 				"test-<honk>thiswillbestripped</honk>-test", "honk"));
+	}
+
+	@Test
+	public void testStripBetweenHtmlElementWithAttribute() {
 		Assert.assertEquals(
 			"test--test",
 			_htmlImpl.stripBetween(
 				"test-<honk attribute=\"value\">thiswillbestripped</honk>-test",
 				"honk"));
+	}
+
+	@Test
+	public void testStripBetween() {
 		Assert.assertEquals(
 			"test-test-test", _htmlImpl.stripBetween("test-test-test", "test"));
+	}
+
+	@Test
+	public void testStripBetweenHtmlElementAcrossLines() {
 		Assert.assertEquals(
 			"works across  lines",
 			_htmlImpl.stripBetween(
 				"works across <honk>\r\n a number of </honk> lines", "honk"));
+	}
+
+	@Test
+	public void testStripBetweenMultipleOcurrencesOfHtmlElement() {
 		Assert.assertEquals(
 			"multiple occurrences, multiple indeed",
 			_htmlImpl.stripBetween(
 				"multiple <a>many</a>occurrences, multiple <a>HONK</a>indeed",
 				"a"));
+	}
+
+	@Test
+	public void testStripBetweenSelfClosedHtmlElement() {
 		Assert.assertEquals(
 			"self-closing <test/> is unhandled",
 			_htmlImpl.stripBetween(
 				"self-closing <test/> is unhandled", "test"));
+	}
+
+	@Test
+	public void testStripBetweenSelfClosedHtmlElementWithWhitespaceEnding() {
 		Assert.assertEquals(
 			"self-closing <test /> is unhandled",
 			_htmlImpl.stripBetween(
@@ -108,18 +136,46 @@ public class HtmlImplTest {
 	}
 
 	@Test
-	public void testStripComments() {
+	public void testStripNullComments() {
 		Assert.assertNull(_htmlImpl.stripComments(null));
+	}
+
+	@Test
+	public void testStripComments() {
 		Assert.assertEquals("", _htmlImpl.stripComments("<!-- bla -->"));
+	}
+
+	@Test
+	public void testStripEmptyComments() {
 		Assert.assertEquals("", _htmlImpl.stripComments("<!---->"));
+	}
+
+	@Test
+	public void testStripMultipleComments() {
 		Assert.assertEquals(
 			"test",
 			_htmlImpl.stripComments("te<!--  bla -->s<!-- bla bla -->t"));
+	}
+
+	@Test
+	public void testStripCommentsAccrossLines() {
 		Assert.assertEquals("test", _htmlImpl.stripComments("te<!-- \n -->st"));
+	}
+
+	@Test
+	public void testStripCommentsBefore() {
 		Assert.assertEquals(
 			"test", _htmlImpl.stripComments("<!--  bla -->test"));
+	}
+
+	@Test
+	public void testStripCommentsAfter() {
 		Assert.assertEquals(
 			"test", _htmlImpl.stripComments("test<!--  bla -->"));
+	}
+
+	@Test
+	public void testStripMultipleEmptyComments() {
 		Assert.assertEquals(
 			"test", _htmlImpl.stripComments("te<!-- --><!-- -->st"));
 	}
