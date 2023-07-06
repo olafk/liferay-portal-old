7dca790344db734a50319109f0b13fca6c1f91a0
mahmoud.azzam <mahmoud.azzam@liferay.com>
2023-07-06 13:12:29 +0100

    COMMERCE-10421 commerce-product-service Adding SortFieldNameTranslator for CPTaxCategory

---
+++ modules/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/internal/search/contributor/sort/CPTaxCategorySortFieldNameTranslator.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.product.internal.search.contributor.sort;
+
+import com.liferay.commerce.product.model.CPTaxCategory;
+import com.liferay.portal.search.contributor.sort.SortFieldNameTranslator;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Mahmoud Azzam
+ */
+@Component(service = SortFieldNameTranslator.class)
+public class CPTaxCategorySortFieldNameTranslator
+	implements SortFieldNameTranslator {
+
+	@Override
+	public Class<?> getEntityClass() {
+		return CPTaxCategory.class;
+	}
+
+	@Override
+	public String getSortFieldName(String orderByCol) {
+		return orderByCol;
+	}
+
+}
