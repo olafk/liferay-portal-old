5dc3d5541f5abac59ce2fdcd403db7bcc2e49361
Julius Lee <meng-huan.lee@liferay.com>
2023-09-08 15:30:16 -0700

    LPS-195732 Inline interface constants and impl logic into Util

--- portal-kernel/src/com/liferay/portal/kernel/servlet/BrowserSniffer.java
+++ portal-kernel/src/com/liferay/portal/kernel/servlet/BrowserSniffer.java
@@ -17,16 +17,6 @@ import org.osgi.annotation.versioning.ProviderType;
 @ProviderType
 public interface BrowserSniffer {
 
-	public static final String BROWSER_ID_CHROME = "chrome";
-
-	public static final String BROWSER_ID_EDGE = "edge";
-
-	public static final String BROWSER_ID_FIREFOX = "firefox";
-
-	public static final String BROWSER_ID_IE = "ie";
-
-	public static final String BROWSER_ID_OTHER = "other";
-
 	public boolean acceptsGzip(HttpServletRequest httpServletRequest);
 
 	public String getBrowserId(HttpServletRequest httpServletRequest);
@@ -34,54 +24,10 @@ public interface BrowserSniffer {
 	public BrowserMetadata getBrowserMetadata(
 		HttpServletRequest httpServletRequest);
 
-	/**
-	 * Returns the browser's version number as a float. This differs from {@link
-	 * BrowserSniffer#getVersion(HttpServletRequest)}, which returns the version
-	 * number as a String.
-	 *
-	 * <p>
-	 * Note that the version returned is defined as the real version of the
-	 * browser software, not the one used to render the page. For example, the
-	 * browser can be IE10 but it may be using a compatibility view emulating
-	 * IE8 to render the page. In such a case, this method would return
-	 * <code>10.0</code>, not <code>8.0</code>.
-	 * </p>
-	 *
-	 * @param  httpServletRequest the servlet request
-	 * @return a float representing the version number
-	 */
 	public float getMajorVersion(HttpServletRequest httpServletRequest);
 
-	/**
-	 * Returns the browser's revision.
-	 *
-	 * <p>
-	 * Note that the revision returned is defined as the real revision of the
-	 * browser software, not the one used to render the page. For example, the
-	 * browser can be IE10 but it may be using a compatibility view emulating
-	 * IE8 to render the page. In such a case, this method would return
-	 * <code>10.0</code>, not <code>8.0</code>.
-	 * </p>
-	 *
-	 * @param  httpServletRequest the servlet request
-	 * @return a String containing the revision number
-	 */
 	public String getRevision(HttpServletRequest httpServletRequest);
 
-	/**
-	 * Returns the browser's version.
-	 *
-	 * <p>
-	 * Note that the version returned is defined as the real version of the
-	 * browser software, not the one used to render the page. For example, the
-	 * browser can be an IE10 but it may be using a compatibility view emulating
-	 * IE8 to render the page. In such a case, this method would return
-	 * <code>10.0</code>, not <code>8.0</code>.
-	 * </p>
-	 *
-	 * @param  httpServletRequest the servlet request
-	 * @return a String containing the version number
-	 */
 	public String getVersion(HttpServletRequest httpServletRequest);
 
 	public boolean isAir(HttpServletRequest httpServletRequest);
--- portal-kernel/src/com/liferay/portal/kernel/servlet/BrowserSnifferUtil.java
+++ portal-kernel/src/com/liferay/portal/kernel/servlet/BrowserSnifferUtil.java
@@ -5,6 +5,14 @@
 
 package com.liferay.portal.kernel.servlet;
 
+import com.liferay.petra.string.CharPool;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.kernel.util.WebKeys;
+
 import javax.servlet.http.HttpServletRequest;
 
 /**
@@ -15,120 +23,443 @@ import javax.servlet.http.HttpServletRequest;
  */
 public class BrowserSnifferUtil {
 
+	public static final String BROWSER_ID_CHROME = "chrome";
+
+	public static final String BROWSER_ID_EDGE = "edge";
+
+	public static final String BROWSER_ID_FIREFOX = "firefox";
+
+	public static final String BROWSER_ID_IE = "ie";
+
+	public static final String BROWSER_ID_OTHER = "other";
+
+	public static final String[] REVISION_LEADINGS = {
+		"rv", "it", "ra", "trident", "ie"
+	};
+
+	public static final char[] REVISION_SEPARATORS = {
+		CharPool.BACK_SLASH, CharPool.COLON, CharPool.SLASH, CharPool.SPACE
+	};
+
+	public static final String[] VERSION_LEADINGS = {
+		"edge", "chrome", "firefox", "version", "minefield", "trident"
+	};
+
+	public static final char[] VERSION_SEPARATORS = {
+		CharPool.BACK_SLASH, CharPool.SLASH
+	};
+
 	public static boolean acceptsGzip(HttpServletRequest httpServletRequest) {
-		return _browserSniffer.acceptsGzip(httpServletRequest);
+		String acceptEncoding = httpServletRequest.getHeader(
+			HttpHeaders.ACCEPT_ENCODING);
+
+		if ((acceptEncoding != null) && acceptEncoding.contains("gzip")) {
+			return true;
+		}
+
+		return false;
+	}
+
+	public static String getAccept(HttpServletRequest httpServletRequest) {
+		String accept = StringPool.BLANK;
+
+		if (httpServletRequest == null) {
+			return accept;
+		}
+
+		accept = String.valueOf(
+			httpServletRequest.getAttribute(HttpHeaders.ACCEPT));
+
+		if (Validator.isNotNull(accept)) {
+			return accept;
+		}
+
+		accept = httpServletRequest.getHeader(HttpHeaders.ACCEPT);
+
+		if (accept != null) {
+			accept = StringUtil.toLowerCase(accept);
+		}
+		else {
+			accept = StringPool.BLANK;
+		}
+
+		httpServletRequest.setAttribute(HttpHeaders.ACCEPT, accept);
+
+		return accept;
 	}
 
 	public static String getBrowserId(HttpServletRequest httpServletRequest) {
-		return _browserSniffer.getBrowserId(httpServletRequest);
+		BrowserMetadata browserMetadata = getBrowserMetadata(
+			httpServletRequest);
+
+		if (browserMetadata.isChrome()) {
+			return BROWSER_ID_CHROME;
+		}
+		else if (browserMetadata.isEdge()) {
+			return BROWSER_ID_EDGE;
+		}
+		else if (browserMetadata.isIe()) {
+			return BROWSER_ID_IE;
+		}
+		else if (browserMetadata.isFirefox()) {
+			return BROWSER_ID_FIREFOX;
+		}
+
+		return BROWSER_ID_OTHER;
 	}
 
 	public static BrowserMetadata getBrowserMetadata(
 		HttpServletRequest httpServletRequest) {
 
-		return _browserSniffer.getBrowserMetadata(httpServletRequest);
+		return new BrowserMetadata(_getUserAgent(httpServletRequest));
 	}
 
-	public static BrowserSniffer getBrowserSniffer() {
-		return _browserSniffer;
-	}
-
+	/**
+	 * Returns the browser's version number as a float. This differs from {@link
+	 * BrowserSniffer#getVersion(HttpServletRequest)}, which returns the version
+	 * number as a String.
+	 *
+	 * <p>
+	 * Note that the version returned is defined as the real version of the
+	 * browser software, not the one used to render the page. For example, the
+	 * browser can be IE10 but it may be using a compatibility view emulating
+	 * IE8 to render the page. In such a case, this method would return
+	 * <code>10.0</code>, not <code>8.0</code>.
+	 * </p>
+	 *
+	 * @param  httpServletRequest the servlet request
+	 * @return a float representing the version number
+	 */
 	public static float getMajorVersion(HttpServletRequest httpServletRequest) {
-		return _browserSniffer.getMajorVersion(httpServletRequest);
+		return GetterUtil.getFloat(getVersion(httpServletRequest));
 	}
 
+	/**
+	 * Returns the browser's revision.
+	 *
+	 * <p>
+	 * Note that the revision returned is defined as the real revision of the
+	 * browser software, not the one used to render the page. For example, the
+	 * browser can be IE10 but it may be using a compatibility view emulating
+	 * IE8 to render the page. In such a case, this method would return
+	 * <code>10.0</code>, not <code>8.0</code>.
+	 * </p>
+	 *
+	 * @param  httpServletRequest the servlet request
+	 * @return a String containing the revision number
+	 */
 	public static String getRevision(HttpServletRequest httpServletRequest) {
-		return _browserSniffer.getRevision(httpServletRequest);
+		String revision = (String)httpServletRequest.getAttribute(
+			WebKeys.BROWSER_SNIFFER_REVISION);
+
+		if (revision != null) {
+			return revision;
+		}
+
+		revision = parseVersion(
+			_getUserAgent(httpServletRequest), REVISION_LEADINGS,
+			REVISION_SEPARATORS);
+
+		httpServletRequest.setAttribute(
+			WebKeys.BROWSER_SNIFFER_REVISION, revision);
+
+		return revision;
 	}
 
+	/**
+	 * Returns the browser's version.
+	 *
+	 * <p>
+	 * Note that the version returned is defined as the real version of the
+	 * browser software, not the one used to render the page. For example, the
+	 * browser can be an IE10 but it may be using a compatibility view emulating
+	 * IE8 to render the page. In such a case, this method would return
+	 * <code>10.0</code>, not <code>8.0</code>.
+	 * </p>
+	 *
+	 * @param  httpServletRequest the servlet request
+	 * @return a String containing the version number
+	 */
 	public static String getVersion(HttpServletRequest httpServletRequest) {
-		return _browserSniffer.getVersion(httpServletRequest);
+		String version = (String)httpServletRequest.getAttribute(
+			WebKeys.BROWSER_SNIFFER_VERSION);
+
+		if (version != null) {
+			return version;
+		}
+
+		String userAgent = _getUserAgent(httpServletRequest);
+
+		version = parseVersion(userAgent, VERSION_LEADINGS, VERSION_SEPARATORS);
+
+		if (version.isEmpty()) {
+			version = parseVersion(
+				userAgent, REVISION_LEADINGS, REVISION_SEPARATORS);
+		}
+
+		httpServletRequest.setAttribute(
+			WebKeys.BROWSER_SNIFFER_VERSION, version);
+
+		return version;
 	}
 
 	public static boolean isAir(HttpServletRequest httpServletRequest) {
-		return _browserSniffer.isAir(httpServletRequest);
+		BrowserMetadata browserMetadata = getBrowserMetadata(
+			httpServletRequest);
+
+		return browserMetadata.isAir();
 	}
 
 	public static boolean isAndroid(HttpServletRequest httpServletRequest) {
-		return _browserSniffer.isAndroid(httpServletRequest);
+		BrowserMetadata browserMetadata = getBrowserMetadata(
+			httpServletRequest);
+
+		return browserMetadata.isAndroid();
 	}
 
 	public static boolean isChrome(HttpServletRequest httpServletRequest) {
-		return _browserSniffer.isChrome(httpServletRequest);
+		BrowserMetadata browserMetadata = getBrowserMetadata(
+			httpServletRequest);
+
+		return browserMetadata.isChrome();
 	}
 
 	public static boolean isEdge(HttpServletRequest httpServletRequest) {
-		return _browserSniffer.isEdge(httpServletRequest);
+		BrowserMetadata browserMetadata = getBrowserMetadata(
+			httpServletRequest);
+
+		return browserMetadata.isEdge();
 	}
 
 	public static boolean isFirefox(HttpServletRequest httpServletRequest) {
-		return _browserSniffer.isFirefox(httpServletRequest);
+		BrowserMetadata browserMetadata = getBrowserMetadata(
+			httpServletRequest);
+
+		return browserMetadata.isFirefox();
 	}
 
 	public static boolean isGecko(HttpServletRequest httpServletRequest) {
-		return _browserSniffer.isGecko(httpServletRequest);
+		BrowserMetadata browserMetadata = getBrowserMetadata(
+			httpServletRequest);
+
+		return browserMetadata.isGecko();
 	}
 
 	public static boolean isIe(HttpServletRequest httpServletRequest) {
-		return _browserSniffer.isIe(httpServletRequest);
+		BrowserMetadata browserMetadata = getBrowserMetadata(
+			httpServletRequest);
+
+		return browserMetadata.isIe();
 	}
 
 	public static boolean isIeOnWin32(HttpServletRequest httpServletRequest) {
-		return _browserSniffer.isIeOnWin32(httpServletRequest);
+		BrowserMetadata browserMetadata = getBrowserMetadata(
+			httpServletRequest);
+
+		return browserMetadata.isIeOnWin32();
 	}
 
 	public static boolean isIeOnWin64(HttpServletRequest httpServletRequest) {
-		return _browserSniffer.isIeOnWin64(httpServletRequest);
+		BrowserMetadata browserMetadata = getBrowserMetadata(
+			httpServletRequest);
+
+		return browserMetadata.isIeOnWin64();
 	}
 
 	public static boolean isIphone(HttpServletRequest httpServletRequest) {
-		return _browserSniffer.isIphone(httpServletRequest);
+		BrowserMetadata browserMetadata = getBrowserMetadata(
+			httpServletRequest);
+
+		return browserMetadata.isIphone();
 	}
 
 	public static boolean isLinux(HttpServletRequest httpServletRequest) {
-		return _browserSniffer.isLinux(httpServletRequest);
+		BrowserMetadata browserMetadata = getBrowserMetadata(
+			httpServletRequest);
+
+		return browserMetadata.isLinux();
 	}
 
 	public static boolean isMac(HttpServletRequest httpServletRequest) {
-		return _browserSniffer.isMac(httpServletRequest);
+		BrowserMetadata browserMetadata = getBrowserMetadata(
+			httpServletRequest);
+
+		return browserMetadata.isMac();
 	}
 
 	public static boolean isMobile(HttpServletRequest httpServletRequest) {
-		return _browserSniffer.isMobile(httpServletRequest);
+		BrowserMetadata browserMetadata = getBrowserMetadata(
+			httpServletRequest);
+
+		return browserMetadata.isMobile();
 	}
 
 	public static boolean isMozilla(HttpServletRequest httpServletRequest) {
-		return _browserSniffer.isMozilla(httpServletRequest);
+		BrowserMetadata browserMetadata = getBrowserMetadata(
+			httpServletRequest);
+
+		return browserMetadata.isMozilla();
 	}
 
 	public static boolean isOpera(HttpServletRequest httpServletRequest) {
-		return _browserSniffer.isOpera(httpServletRequest);
+		BrowserMetadata browserMetadata = getBrowserMetadata(
+			httpServletRequest);
+
+		return browserMetadata.isOpera();
 	}
 
 	public static boolean isRtf(HttpServletRequest httpServletRequest) {
-		return _browserSniffer.isRtf(httpServletRequest);
+		BrowserMetadata browserMetadata = getBrowserMetadata(
+			httpServletRequest);
+
+		return browserMetadata.isRtf(getVersion(httpServletRequest));
 	}
 
 	public static boolean isSafari(HttpServletRequest httpServletRequest) {
-		return _browserSniffer.isSafari(httpServletRequest);
+		BrowserMetadata browserMetadata = getBrowserMetadata(
+			httpServletRequest);
+
+		return browserMetadata.isSafari();
 	}
 
 	public static boolean isSun(HttpServletRequest httpServletRequest) {
-		return _browserSniffer.isSun(httpServletRequest);
+		BrowserMetadata browserMetadata = getBrowserMetadata(
+			httpServletRequest);
+
+		return browserMetadata.isSun();
 	}
 
 	public static boolean isWebKit(HttpServletRequest httpServletRequest) {
-		return _browserSniffer.isWebKit(httpServletRequest);
+		BrowserMetadata browserMetadata = getBrowserMetadata(
+			httpServletRequest);
+
+		return browserMetadata.isWebKit();
 	}
 
 	public static boolean isWindows(HttpServletRequest httpServletRequest) {
-		return _browserSniffer.isWindows(httpServletRequest);
+		BrowserMetadata browserMetadata = getBrowserMetadata(
+			httpServletRequest);
+
+		return browserMetadata.isWindows();
 	}
 
-	public void setBrowserSniffer(BrowserSniffer browserSniffer) {
-		_browserSniffer = browserSniffer;
+	public static String parseVersion(
+		String userAgent, String[] leadings, char[] separators) {
+
+		leading:
+		for (String leading : leadings) {
+			int index = 0;
+
+			version:
+			while (true) {
+				index = userAgent.indexOf(leading, index);
+
+				if ((index < 0) ||
+					(((index += leading.length()) + 2) > userAgent.length())) {
+
+					continue leading;
+				}
+
+				char c1 = userAgent.charAt(index);
+				char c2 = userAgent.charAt(++index);
+
+				if (((c2 >= '0') && (c2 <= '9')) || (c2 == '.')) {
+					for (char separator : separators) {
+						if (c1 == separator) {
+							break version;
+						}
+					}
+				}
+			}
+
+			// Major
+
+			int majorStart = index;
+			int majorEnd = index + 1;
+
+			for (int i = majorStart; i < userAgent.length(); i++) {
+				char c = userAgent.charAt(i);
+
+				if ((c < '0') || (c > '9')) {
+					majorEnd = i;
+
+					break;
+				}
+			}
+
+			String major = userAgent.substring(majorStart, majorEnd);
+
+			if ((majorEnd >= userAgent.length()) ||
+				(userAgent.charAt(majorEnd) != '.')) {
+
+				return major;
+			}
+
+			// Minor
+
+			int minorStart = majorEnd + 1;
+			int minorEnd = userAgent.length();
+
+			for (int i = minorStart; i < userAgent.length(); i++) {
+				char c = userAgent.charAt(i);
+
+				if ((c < '0') || (c > '9')) {
+					minorEnd = i;
+
+					break;
+				}
+			}
+
+			String minor = userAgent.substring(minorStart, minorEnd);
+
+			String version = StringBundler.concat(
+				major, StringPool.PERIOD, minor);
+
+			if (leading.equals("trident")) {
+				if (version.equals("7.0")) {
+					version = "11.0";
+				}
+				else if (version.equals("6.0")) {
+					version = "10.0";
+				}
+				else if (version.equals("5.0")) {
+					version = "9.0";
+				}
+				else if (version.equals("4.0")) {
+					version = "8.0";
+				}
+			}
+
+			return version;
+		}
+
+		return StringPool.BLANK;
 	}
 
-	private static BrowserSniffer _browserSniffer;
+	private static String _getUserAgent(HttpServletRequest httpServletRequest) {
+		if (httpServletRequest == null) {
+			return StringPool.BLANK;
+		}
+
+		Object userAgentObject = httpServletRequest.getAttribute(
+			HttpHeaders.USER_AGENT);
+
+		if (userAgentObject != null) {
+			return userAgentObject.toString();
+		}
+
+		String userAgent = httpServletRequest.getHeader(HttpHeaders.USER_AGENT);
+
+		if (userAgent != null) {
+			userAgent = StringUtil.toLowerCase(userAgent);
+		}
+		else {
+			userAgent = StringPool.BLANK;
+		}
+
+		httpServletRequest.setAttribute(HttpHeaders.USER_AGENT, userAgent);
+
+		return userAgent;
+	}
 
 }
