igor-franca/liferay-portal

LPS-194440 Create new reducer function to add a new field to a specific node

modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ModelBuilder/ModelBuilderContext/objectFolderReducer.ts
@@ -22,7 +22,7 @@ import {
 } from './objectFolderReducerUtil';
 import {TYPES} from './typesEnum';
 
-export function ObjectFolderReducer(state: TState, action: TAction) {
+export function ObjectFolderReducer(state: TState, action: TAction): TState {
 	const store = useStore();
 
 	switch (action.type) {
@@ -120,11 +120,7 @@ export function ObjectFolderReducer(state: TState, action: TAction) {
 					return {
 						businessType: field.businessType,
 						externalReferenceCode: field.externalReferenceCode,
-						label: getLocalizableLabel(
-							newObjectDefinition.defaultLanguageId,
-							field.label,
-							field.name
-						),
+						label: field.label,
 						name: field.name,
 						primaryKey: field.name === 'id',
 						required: field.required,
@@ -209,6 +205,71 @@ export function ObjectFolderReducer(state: TState, action: TAction) {
 			};
 		}
 
+		case TYPES.ADD_NEW_OBJECT_FIELD: {
+			const {
+				edges,
+				newObjectField,
+				nodes,
+				objectDefinitionExternalReferenceCode,
+			} = action.payload;
+
+			const newNodes = nodes.map((node) => {
+				if (
+					node.data?.externalReferenceCode ===
+					objectDefinitionExternalReferenceCode
+				) {
+					const {objectFields} = node.data;
+					const newObjectFields = objectFields.map((objectField) => ({
+						...objectField,
+						selected: false,
+					}));
+
+					const newObjectFieldNode: ObjectFieldNode = {
+						businessType: newObjectField.businessType,
+						externalReferenceCode:
+							newObjectField.externalReferenceCode,
+						label: newObjectField.label,
+						name: newObjectField.name,
+						primaryKey: false,
+						required: newObjectField.required,
+						selected: true,
+					};
+
+					newObjectFields.push(newObjectFieldNode);
+
+					return {
+						...node,
+						data: {
+							...node.data,
+							nodeSelected: true,
+							objectFields: newObjectFields,
+						},
+					};
+				}
+
+				const unselectedFields = node.data?.objectFields.map(
+					(objectField) => ({
+						...objectField,
+						selected: false,
+					})
+				);
+
+				return {
+					...node,
+					data: {
+						...node.data,
+						nodeSelected: false,
+						objectFields: unselectedFields,
+					},
+				};
+			}) as Node<ObjectDefinitionNodeData>[];
+
+			return {
+				...state,
+				elements: [...newNodes, ...edges],
+			};
+		}
+
 		case TYPES.BULK_CHANGE_NODE_VIEW: {
 			const {hiddenFolderNodes, leftSidebarItem} = action.payload;
 			const {edges, nodes} = store.getState();
@@ -222,7 +283,7 @@ export function ObjectFolderReducer(state: TState, action: TAction) {
 						isHidden: !hiddenFolderNodes,
 					};
 				}
-			);
+			) as Node<ObjectDefinitionNodeData>[];
 
 			const updatedEdges = edges.map(
 				(edge: Edge<ObjectRelationshipEdgeData>) => {
@@ -231,7 +292,7 @@ export function ObjectFolderReducer(state: TState, action: TAction) {
 						isHidden: !hiddenFolderNodes,
 					};
 				}
-			);
+			) as Edge<ObjectRelationshipEdgeData>[];
 
 			const updatedLeftSidebarItems = leftSidebarItems.map(
 				(sidebarItem: LeftSidebarItemType) => {
@@ -572,7 +633,7 @@ export function ObjectFolderReducer(state: TState, action: TAction) {
 							selectedObjectRelationshipId,
 					},
 				})
-			);
+			) as Edge<ObjectRelationshipEdgeData>[];
 
 			const selectedNode = nodes.find(
 				(definitionNode) => definitionNode.data?.nodeSelected
@@ -612,7 +673,7 @@ export function ObjectFolderReducer(state: TState, action: TAction) {
 					nodeSelected:
 						definitionNode.id === selectedObjectDefinitionId,
 				},
-			}));
+			})) as Node<ObjectDefinitionNodeData>[];
 
 			const newLeftSidebarItems = leftSidebarItems.map((sidebarItem) => {
 				const newLeftSidebarDefinitions = sidebarItem.objectDefinitions?.map(

olafk/liferay-portal-old

modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ModelBuilder/ModelBuilderContext/objectFolderReducer.ts
@@ -18,8 +18,9 @@ import {
 } from '../types';
 import {updateURLParam} from '../utils';
 import {
-	fieldsCustomSort,
+	convertAllObjectFieldsToUnselected,
 	getNonOverlappingEdges,
+	objectFieldsCustomSort,
 } from './objectFolderReducerUtil';
 import {TYPES} from './typesEnum';
 
@@ -27,6 +28,66 @@ export function ObjectFolderReducer(state: TState, action: TAction): TState {
 	const store = useStore();
 
 	switch (action.type) {
+		case TYPES.ADD_NEW_OBJECT_FIELD: {
+			const {
+				edges,
+				newObjectField,
+				nodes,
+				objectDefinitionExternalReferenceCode,
+			} = action.payload;
+
+			const newNodes = nodes.map((node) => {
+				if (
+					node.data?.externalReferenceCode ===
+					objectDefinitionExternalReferenceCode
+				) {
+					const {objectFields} = node.data;
+
+					const newObjectFields = convertAllObjectFieldsToUnselected(
+						objectFields
+					);
+
+					newObjectFields.push({
+						businessType: newObjectField.businessType,
+						externalReferenceCode:
+							newObjectField.externalReferenceCode,
+						label: newObjectField.label,
+						name: newObjectField.name,
+						primaryKey: false,
+						required: newObjectField.required,
+						selected: true,
+					});
+
+					return {
+						...node,
+						data: {
+							...node.data,
+							objectFields: newObjectFields,
+							selected: true,
+						},
+					};
+				}
+
+				const unselectedObjectFields = convertAllObjectFieldsToUnselected(
+					node.data?.objectFields as ObjectFieldNode[]
+				);
+
+				return {
+					...node,
+					data: {
+						...node.data,
+						objectFields: unselectedObjectFields,
+						selected: false,
+					},
+				};
+			}) as Node<ObjectDefinitionNodeData>[];
+
+			return {
+				...state,
+				elements: [...newNodes, ...edges],
+			};
+		}
+
 		case TYPES.ADD_OBJECT_DEFINITION_TO_OBJECT_FOLDER: {
 			const {
 				newObjectDefinition,
@@ -133,11 +194,7 @@ export function ObjectFolderReducer(state: TState, action: TAction): TState {
 						businessType: objectField.businessType,
 						externalReferenceCode:
 							objectField.externalReferenceCode,
-						label: getLocalizableLabel(
-							newObjectDefinition.defaultLanguageId,
-							objectField.label,
-							objectField.name
-						),
+						label: objectField.label,
 						name: objectField.name,
 						primaryKey: objectField.name === 'id',
 						required: objectField.required,
@@ -202,7 +259,7 @@ export function ObjectFolderReducer(state: TState, action: TAction): TState {
 							newObjectDefinition.name
 						),
 						linkedObjectDefinition: false,
-						objectFields: fieldsCustomSort(objectFields),
+						objectFields: objectFieldsCustomSort(objectFields),
 						selected: true,
 					},
 					id: newObjectDefinition.id.toString(),
@@ -526,7 +583,7 @@ export function ObjectFolderReducer(state: TState, action: TAction): TState {
 								...objectDefinition,
 								hasSelfObjectRelationships:
 									selfObjectRelationships?.length > 0,
-								objectFields: fieldsCustomSort(
+								objectFields: objectFieldsCustomSort(
 									objectDefinition.objectFields
 								),
 							},