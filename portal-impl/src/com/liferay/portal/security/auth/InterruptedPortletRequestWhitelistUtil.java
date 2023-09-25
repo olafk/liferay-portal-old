edcc8a8b326f71fc6f1299460d685bfe541b8e14
Julius Lee <meng-huan.lee@liferay.com>
2023-09-25 14:24:37 -0700

    LPS-197236 Inline InterruptedPortletRequestWhiteListImpl into Util

--- portal-impl/src/com/liferay/portal/security/auth/InterruptedPortletRequestWhitelistUtil.java
+++ portal-impl/src/com/liferay/portal/security/auth/InterruptedPortletRequestWhitelistUtil.java
@@ -5,8 +5,16 @@
 
 package com.liferay.portal.security.auth;
 
-import com.liferay.portal.kernel.security.auth.PortletRequestWhitelist;
+import com.liferay.petra.string.CharPool;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.Portlet;
+import com.liferay.portal.kernel.service.PortletLocalServiceUtil;
+import com.liferay.portal.kernel.util.SetUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.util.PropsValues;
 
+import java.util.Collections;
 import java.util.Set;
 
 /**
@@ -15,45 +23,107 @@ import java.util.Set;
  */
 public class InterruptedPortletRequestWhitelistUtil {
 
-	public static PortletRequestWhitelist
-		getInterruptedPortletRequestWhitelist() {
-
-		return _interruptedPortletRequestWhitelist;
-	}
-
 	public static Set<String> getPortletInvocationWhitelist() {
-		return _interruptedPortletRequestWhitelist.
-			getPortletInvocationWhitelist();
+		return _portletInvocationWhitelist;
 	}
 
 	public static Set<String> getPortletInvocationWhitelistActions() {
-		return _interruptedPortletRequestWhitelist.
-			getPortletInvocationWhitelistActions();
+		return _portletInvocationWhitelistActions;
+	}
+
+	public static String[] getWhitelistActionsPropsValues() {
+		return PropsValues.PORTLET_INTERRUPTED_REQUEST_WHITELIST_ACTIONS;
+	}
+
+	public static String[] getWhitelistPropsValues() {
+		return PropsValues.PORTLET_INTERRUPTED_REQUEST_WHITELIST;
 	}
 
 	public static boolean isPortletInvocationWhitelisted(
 		long companyId, String portletId, String strutsAction) {
 
-		return _interruptedPortletRequestWhitelist.
-			isPortletInvocationWhitelisted(companyId, portletId, strutsAction);
+		if (_portletInvocationWhitelist.contains(portletId)) {
+			return true;
+		}
+
+		if (Validator.isNotNull(strutsAction) &&
+			_portletInvocationWhitelistActions.contains(strutsAction) &&
+			_isValidStrutsAction(companyId, portletId, strutsAction)) {
+
+			return true;
+		}
+
+		return false;
 	}
 
 	public static Set<String> resetPortletInvocationWhitelist() {
-		return _interruptedPortletRequestWhitelist.
-			resetPortletInvocationWhitelist();
+		_portletInvocationWhitelist = SetUtil.fromArray(
+			getWhitelistPropsValues());
+
+		if (_portletInvocationWhitelist.isEmpty()) {
+			_portletInvocationWhitelist = Collections.emptySet();
+		}
+		else {
+			_portletInvocationWhitelist = Collections.unmodifiableSet(
+				_portletInvocationWhitelist);
+		}
+
+		return _portletInvocationWhitelist;
 	}
 
 	public static Set<String> resetPortletInvocationWhitelistActions() {
-		return _interruptedPortletRequestWhitelist.
-			resetPortletInvocationWhitelistActions();
+		_portletInvocationWhitelistActions = SetUtil.fromArray(
+			getWhitelistActionsPropsValues());
+
+		if (_portletInvocationWhitelistActions.isEmpty()) {
+			_portletInvocationWhitelistActions = Collections.emptySet();
+		}
+		else {
+			_portletInvocationWhitelistActions = Collections.unmodifiableSet(
+				_portletInvocationWhitelistActions);
+		}
+
+		return _portletInvocationWhitelistActions;
 	}
 
-	public void setInterruptedPortletRequestWhitelist(
-		PortletRequestWhitelist whitelist) {
+	private static boolean _isValidStrutsAction(
+		long companyId, String portletId, String strutsAction) {
 
-		_interruptedPortletRequestWhitelist = whitelist;
+		try {
+			Portlet portlet = PortletLocalServiceUtil.getPortletById(
+				companyId, portletId);
+
+			if (portlet == null) {
+				return false;
+			}
+
+			String strutsPath = strutsAction.substring(
+				1, strutsAction.lastIndexOf(CharPool.SLASH));
+
+			if (strutsPath.equals(portlet.getStrutsPath()) ||
+				strutsPath.equals(portlet.getParentStrutsPath())) {
+
+				return true;
+			}
+		}
+		catch (Exception exception) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(exception);
+			}
+		}
+
+		return false;
 	}
 
-	private static PortletRequestWhitelist _interruptedPortletRequestWhitelist;
+	private static final Log _log = LogFactoryUtil.getLog(
+		InterruptedPortletRequestWhitelistUtil.class);
+
+	private static Set<String> _portletInvocationWhitelist;
+	private static Set<String> _portletInvocationWhitelistActions;
+
+	static {
+		resetPortletInvocationWhitelist();
+		resetPortletInvocationWhitelistActions();
+	}
 
 }
