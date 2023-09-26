liferay-search/liferay-portal

LPS-192640 LPS-193200 search-experiences: gradle buildService

modules/dxp/apps/search-experiences/search-experiences-api/src/main/java/com/liferay/search/experiences/service/SXPBlueprintLocalService.java
@@ -319,8 +319,8 @@ public interface SXPBlueprintLocalService
 	public SXPBlueprint updateSXPBlueprint(
 			long userId, long sxpBlueprintId, String configurationJSON,
 			Map<Locale, String> descriptionMap, String elementInstancesJSON,
-			String schemaVersion, Map<Locale, String> titleMap,
-			ServiceContext serviceContext)
+			String externalReferenceCode, String schemaVersion,
+			Map<Locale, String> titleMap, ServiceContext serviceContext)
 		throws PortalException;
 
 	/**

modules/dxp/apps/search-experiences/search-experiences-api/src/main/java/com/liferay/search/experiences/service/SXPElementLocalServiceUtil.java
@@ -359,14 +359,16 @@ public class SXPElementLocalServiceUtil {
 	public static SXPElement updateSXPElement(
 			long userId, long sxpElementId,
 			Map<java.util.Locale, String> descriptionMap,
-			String elementDefinitionJSON, boolean hidden, String schemaVersion,
+			String elementDefinitionJSON, String externalReferenceCode,
+			boolean hidden, String schemaVersion,
 			Map<java.util.Locale, String> titleMap,
 			com.liferay.portal.kernel.service.ServiceContext serviceContext)
 		throws PortalException {
 
 		return getService().updateSXPElement(
-			userId, sxpElementId, descriptionMap, elementDefinitionJSON, hidden,
-			schemaVersion, titleMap, serviceContext);
+			userId, sxpElementId, descriptionMap, elementDefinitionJSON,
+			externalReferenceCode, hidden, schemaVersion, titleMap,
+			serviceContext);
 	}
 
 	/**

modules/dxp/apps/search-experiences/search-experiences-api/src/main/java/com/liferay/search/experiences/service/SXPElementLocalServiceWrapper.java
@@ -409,14 +409,16 @@ public class SXPElementLocalServiceWrapper
 	public com.liferay.search.experiences.model.SXPElement updateSXPElement(
 			long userId, long sxpElementId,
 			java.util.Map<java.util.Locale, String> descriptionMap,
-			String elementDefinitionJSON, boolean hidden, String schemaVersion,
+			String elementDefinitionJSON, String externalReferenceCode,
+			boolean hidden, String schemaVersion,
 			java.util.Map<java.util.Locale, String> titleMap,
 			com.liferay.portal.kernel.service.ServiceContext serviceContext)
 		throws com.liferay.portal.kernel.exception.PortalException {
 
 		return _sxpElementLocalService.updateSXPElement(
-			userId, sxpElementId, descriptionMap, elementDefinitionJSON, hidden,
-			schemaVersion, titleMap, serviceContext);
+			userId, sxpElementId, descriptionMap, elementDefinitionJSON,
+			externalReferenceCode, hidden, schemaVersion, titleMap,
+			serviceContext);
 	}
 
 	/**

olafk/liferay-portal-old

modules/dxp/apps/search-experiences/search-experiences-api/src/main/java/com/liferay/search/experiences/service/SXPBlueprintLocalService.java
@@ -317,10 +317,10 @@ public interface SXPBlueprintLocalService
 
 	@Indexable(type = IndexableType.REINDEX)
 	public SXPBlueprint updateSXPBlueprint(
-			long userId, long sxpBlueprintId, String configurationJSON,
-			Map<Locale, String> descriptionMap, String elementInstancesJSON,
-			String schemaVersion, Map<Locale, String> titleMap,
-			ServiceContext serviceContext)
+			String externalReferenceCode, long userId, long sxpBlueprintId,
+			String configurationJSON, Map<Locale, String> descriptionMap,
+			String elementInstancesJSON, String schemaVersion,
+			Map<Locale, String> titleMap, ServiceContext serviceContext)
 		throws PortalException;
 
 	/**

modules/dxp/apps/search-experiences/search-experiences-api/src/main/java/com/liferay/search/experiences/service/SXPElementLocalServiceUtil.java
@@ -357,7 +357,7 @@ public class SXPElementLocalServiceUtil {
 	}
 
 	public static SXPElement updateSXPElement(
-			long userId, long sxpElementId,
+			String externalReferenceCode, long userId, long sxpElementId,
 			Map<java.util.Locale, String> descriptionMap,
 			String elementDefinitionJSON, boolean hidden, String schemaVersion,
 			Map<java.util.Locale, String> titleMap,
@@ -365,8 +365,9 @@ public class SXPElementLocalServiceUtil {
 		throws PortalException {
 
 		return getService().updateSXPElement(
-			userId, sxpElementId, descriptionMap, elementDefinitionJSON, hidden,
-			schemaVersion, titleMap, serviceContext);
+			externalReferenceCode, userId, sxpElementId, descriptionMap,
+			elementDefinitionJSON, hidden, schemaVersion, titleMap,
+			serviceContext);
 	}
 
 	/**

modules/dxp/apps/search-experiences/search-experiences-api/src/main/java/com/liferay/search/experiences/service/SXPElementLocalServiceWrapper.java
@@ -407,7 +407,7 @@ public class SXPElementLocalServiceWrapper
 
 	@Override
 	public com.liferay.search.experiences.model.SXPElement updateSXPElement(
-			long userId, long sxpElementId,
+			String externalReferenceCode, long userId, long sxpElementId,
 			java.util.Map<java.util.Locale, String> descriptionMap,
 			String elementDefinitionJSON, boolean hidden, String schemaVersion,
 			java.util.Map<java.util.Locale, String> titleMap,
@@ -415,8 +415,9 @@ public class SXPElementLocalServiceWrapper
 		throws com.liferay.portal.kernel.exception.PortalException {
 
 		return _sxpElementLocalService.updateSXPElement(
-			userId, sxpElementId, descriptionMap, elementDefinitionJSON, hidden,
-			schemaVersion, titleMap, serviceContext);
+			externalReferenceCode, userId, sxpElementId, descriptionMap,
+			elementDefinitionJSON, hidden, schemaVersion, titleMap,
+			serviceContext);
 	}
 
 	/**