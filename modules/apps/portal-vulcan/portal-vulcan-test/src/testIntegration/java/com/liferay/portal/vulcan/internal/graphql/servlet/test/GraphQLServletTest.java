ccorreagg/liferay-portal

LPS-191341 Add tests

modules/apps/portal-vulcan/portal-vulcan-test/src/testIntegration/java/com/liferay/portal/vulcan/internal/graphql/servlet/test/GraphQLServletTest.java
@@ -6,9 +6,14 @@
 package com.liferay.portal.vulcan.internal.graphql.servlet.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.util.HashMapDictionaryBuilder;
 import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.vulcan.graphql.servlet.ServletData;
 
@@ -22,6 +27,8 @@ import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.FrameworkUtil;
 import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.cm.Configuration;
+import org.osgi.service.cm.ConfigurationAdmin;
 
 /**
  * @author Luis Miguel Barcos
@@ -64,5 +71,69 @@ public class GraphQLServletTest extends BaseGraphQLServlet {
 
 		serviceRegistration.unregister();
 	}
+
+	@Test
+	public void testWithQueryDepthLimit() throws Exception {
+		Bundle bundle = FrameworkUtil.getBundle(GraphQLServletTest.class);
+
+		BundleContext bundleContext = bundle.getBundleContext();
+
+		ServiceRegistration<ServletData> serviceRegistration =
+			bundleContext.registerService(
+				ServletData.class, new TestServletData(), null);
+
+		Configuration[] configurations = _configurationAdmin.listConfigurations(
+			StringBundler.concat(
+				"(&(service.factoryPid=com.liferay.portal.vulcan.internal.",
+				"configuration.GraphQLCompanyConfiguration.scoped)(companyId=",
+				TestPropsValues.getCompanyId(), "))"));
+
+		Configuration factoryConfiguration =
+			_configurationAdmin.createFactoryConfiguration(
+				"com.liferay.portal.vulcan.internal.configuration." +
+					"GraphQLCompanyConfiguration.scoped",
+				StringPool.QUESTION);
+
+		try {
+			factoryConfiguration.update(
+				HashMapDictionaryBuilder.<String, Object>put(
+					"companyId", TestPropsValues.getCompanyId()
+				).put(
+					"queryDepthLimit", 1
+				).build());
+
+			String testDTOSimpleClassName = StringUtil.lowerCaseFirstLetter(
+				TestDTO.class.getSimpleName());
+
+			GraphQLField graphQLField = new GraphQLField(
+				testDTOSimpleClassName, new GraphQLField("field"),
+				new GraphQLField("_id"));
+
+			JSONObject jsonObject = invoke(graphQLField);
+
+			Assert.assertNull(
+				JSONUtil.getValueAsJSONObject(jsonObject, "JSONObject/data"));
+			Assert.assertEquals(
+				"Maximum query depth exceeded 2 > 1",
+				JSONUtil.getValueAsString(
+					jsonObject, "JSONArray/errors", "Object/0",
+					"Object/message"));
+
+			serviceRegistration.unregister();
+		}
+		finally {
+			if (configurations == null) {
+				factoryConfiguration.delete();
+			}
+			else {
+				Configuration configuration = configurations[0];
+
+				factoryConfiguration.update(configuration.getProperties());
+			}
+		}
+	}
+
+	@Inject
+	private ConfigurationAdmin _configurationAdmin;
 
 }

olafk/liferay-portal-old

modules/apps/portal-vulcan/portal-vulcan-test/src/testIntegration/java/com/liferay/portal/vulcan/internal/graphql/servlet/test/GraphQLServletTest.java
@@ -6,9 +6,14 @@
 package com.liferay.portal.vulcan.internal.graphql.servlet.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.util.HashMapDictionaryBuilder;
 import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.vulcan.graphql.servlet.ServletData;
 
@@ -22,6 +27,8 @@ import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.FrameworkUtil;
 import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.cm.Configuration;
+import org.osgi.service.cm.ConfigurationAdmin;
 
 /**
  * @author Luis Miguel Barcos
@@ -64,5 +71,69 @@ public class GraphQLServletTest extends BaseGraphQLServlet {
 
 		serviceRegistration.unregister();
 	}
+
+	@Test
+	public void testWithQueryDepthLimit() throws Exception {
+		Bundle bundle = FrameworkUtil.getBundle(GraphQLServletTest.class);
+
+		BundleContext bundleContext = bundle.getBundleContext();
+
+		ServiceRegistration<ServletData> serviceRegistration =
+			bundleContext.registerService(
+				ServletData.class, new TestServletData(), null);
+
+		Configuration[] configurations = _configurationAdmin.listConfigurations(
+			StringBundler.concat(
+				"(&(service.factoryPid=com.liferay.portal.vulcan.internal.",
+				"configuration.HeadlessAPICompanyConfiguration.scoped)(companyId=",
+				TestPropsValues.getCompanyId(), "))"));
+
+		Configuration factoryConfiguration =
+			_configurationAdmin.createFactoryConfiguration(
+				"com.liferay.portal.vulcan.internal.configuration." +
+					"HeadlessAPICompanyConfiguration.scoped",
+				StringPool.QUESTION);
+
+		try {
+			factoryConfiguration.update(
+				HashMapDictionaryBuilder.<String, Object>put(
+					"companyId", TestPropsValues.getCompanyId()
+				).put(
+					"queryDepthLimit", 1
+				).build());
+
+			String testDTOSimpleClassName = StringUtil.lowerCaseFirstLetter(
+				TestDTO.class.getSimpleName());
+
+			GraphQLField graphQLField = new GraphQLField(
+				testDTOSimpleClassName, new GraphQLField("field"),
+				new GraphQLField("_id"));
+
+			JSONObject jsonObject = invoke(graphQLField);
+
+			Assert.assertNull(
+				JSONUtil.getValueAsJSONObject(jsonObject, "JSONObject/data"));
+			Assert.assertEquals(
+				"Maximum query depth exceeded 2 > 1",
+				JSONUtil.getValueAsString(
+					jsonObject, "JSONArray/errors", "Object/0",
+					"Object/message"));
+
+			serviceRegistration.unregister();
+		}
+		finally {
+			if (configurations == null) {
+				factoryConfiguration.delete();
+			}
+			else {
+				Configuration configuration = configurations[0];
+
+				factoryConfiguration.update(configuration.getProperties());
+			}
+		}
+	}
+
+	@Inject
+	private ConfigurationAdmin _configurationAdmin;
 
 }