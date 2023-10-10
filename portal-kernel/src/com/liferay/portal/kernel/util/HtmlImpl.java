b6a69c02081fae30a9126ebdbb85a13f6245d91d
Julius Lee <meng-huan.lee@liferay.com>
2023-10-10 11:41:55 -0700

    LPS-198792 Move HtmlImpl into portal-kernel prepping to merge with Util

--- portal-kernel/src/com/liferay/portal/kernel/util/HtmlImpl.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/HtmlImpl.java
@@ -3,16 +3,11 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portal.util;
+package com.liferay.portal.kernel.util;
 
 import com.liferay.petra.string.CharPool;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
-import com.liferay.portal.kernel.util.HashMapBuilder;
-import com.liferay.portal.kernel.util.Html;
-import com.liferay.portal.kernel.util.StringUtil;
-import com.liferay.portal.kernel.util.URLCodec;
-import com.liferay.portal.kernel.util.Validator;
 
 import java.util.Map;
 import java.util.regex.Matcher;
