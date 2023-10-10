047cc616cd28c42c7dc36e61ccddd2446665ab97
Evan Thibodeau <evan.thibodeau@liferay.com>
2023-10-10 14:04:58 +0400

    LPS-198664 - When the staging indicator is shown, add a class to the body so that static elements can be properly positioned.

---
+++ modules/apps/depot/depot-web/src/main/java/com/liferay/depot/web/internal/template/StagingIndicatorTemplateContextContributor.java
@@ -0,0 +1,64 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.depot.web.internal.template;
+
+import com.liferay.depot.web.internal.util.StagingIndicatorUtil;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.service.permission.PortletPermission;
+import com.liferay.portal.kernel.template.TemplateContextContributor;
+import com.liferay.portal.kernel.util.GetterUtil;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Evan Thibodeau
+ */
+@Component(
+	property = "type=" + TemplateContextContributor.TYPE_THEME,
+	service = TemplateContextContributor.class
+)
+public class StagingIndicatorTemplateContextContributor
+	implements TemplateContextContributor {
+
+	@Override
+	public void prepare(
+		Map<String, Object> contextObjects,
+		HttpServletRequest httpServletRequest) {
+
+		try {
+			if (StagingIndicatorUtil.isShowStagingIndicator(
+					httpServletRequest, _portletPermission)) {
+
+				StringBundler sb = new StringBundler(2);
+
+				sb.append(
+					GetterUtil.getString(contextObjects.get("bodyCssClass")));
+
+				sb.append(" has-staging-indicator");
+
+				contextObjects.put("bodyCssClass", sb.toString());
+			}
+		}
+		catch (PortalException portalException) {
+			_log.error(portalException);
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		StagingIndicatorTemplateContextContributor.class);
+
+	@Reference
+	private PortletPermission _portletPermission;
+
+}
