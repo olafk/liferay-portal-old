brianchandotcom/liferay-portal

LPS-197820 buildService

modules/apps/saved-content/saved-content-api/src/main/java/com/liferay/saved/content/model/SavedContentEntryTable.java
@@ -1,0 +1,59 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.saved.content.model;
+
+import com.liferay.petra.sql.dsl.Column;
+import com.liferay.petra.sql.dsl.base.BaseTable;
+
+import java.sql.Types;
+
+import java.util.Date;
+
+/**
+ * The table class for the &quot;SavedContentEntry&quot; database table.
+ *
+ * @author Brian Wing Shun Chan
+ * @see SavedContentEntry
+ * @generated
+ */
+public class SavedContentEntryTable extends BaseTable<SavedContentEntryTable> {
+
+	public static final SavedContentEntryTable INSTANCE =
+		new SavedContentEntryTable();
+
+	public final Column<SavedContentEntryTable, Long> mvccVersion =
+		createColumn(
+			"mvccVersion", Long.class, Types.BIGINT, Column.FLAG_NULLITY);
+	public final Column<SavedContentEntryTable, Long> ctCollectionId =
+		createColumn(
+			"ctCollectionId", Long.class, Types.BIGINT, Column.FLAG_PRIMARY);
+	public final Column<SavedContentEntryTable, Long> savedContentId =
+		createColumn(
+			"savedContentId", Long.class, Types.BIGINT, Column.FLAG_PRIMARY);
+	public final Column<SavedContentEntryTable, Long> groupId = createColumn(
+		"groupId", Long.class, Types.BIGINT, Column.FLAG_DEFAULT);
+	public final Column<SavedContentEntryTable, Long> companyId = createColumn(
+		"companyId", Long.class, Types.BIGINT, Column.FLAG_DEFAULT);
+	public final Column<SavedContentEntryTable, Long> userId = createColumn(
+		"userId", Long.class, Types.BIGINT, Column.FLAG_DEFAULT);
+	public final Column<SavedContentEntryTable, String> userName = createColumn(
+		"userName", String.class, Types.VARCHAR, Column.FLAG_DEFAULT);
+	public final Column<SavedContentEntryTable, Date> createDate = createColumn(
+		"createDate", Date.class, Types.TIMESTAMP, Column.FLAG_DEFAULT);
+	public final Column<SavedContentEntryTable, Date> modifiedDate =
+		createColumn(
+			"modifiedDate", Date.class, Types.TIMESTAMP, Column.FLAG_DEFAULT);
+	public final Column<SavedContentEntryTable, Long> classNameId =
+		createColumn(
+			"classNameId", Long.class, Types.BIGINT, Column.FLAG_DEFAULT);
+	public final Column<SavedContentEntryTable, Long> classPK = createColumn(
+		"classPK", Long.class, Types.BIGINT, Column.FLAG_DEFAULT);
+
+	private SavedContentEntryTable() {
+		super("SavedContentEntry", SavedContentEntryTable::new);
+	}
+
+}

modules/apps/saved-content/saved-content-service/src/main/java/com/liferay/saved/content/model/impl/SavedContentEntryCacheModel.java
@@ -1,0 +1,204 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.saved.content.model.impl;
+
+import com.liferay.petra.lang.HashUtil;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.model.CacheModel;
+import com.liferay.portal.kernel.model.MVCCModel;
+import com.liferay.saved.content.model.SavedContentEntry;
+
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+
+import java.util.Date;
+
+/**
+ * The cache model class for representing SavedContentEntry in entity cache.
+ *
+ * @author Brian Wing Shun Chan
+ * @generated
+ */
+public class SavedContentEntryCacheModel
+	implements CacheModel<SavedContentEntry>, Externalizable, MVCCModel {
+
+	@Override
+	public boolean equals(Object object) {
+		if (this == object) {
+			return true;
+		}
+
+		if (!(object instanceof SavedContentEntryCacheModel)) {
+			return false;
+		}
+
+		SavedContentEntryCacheModel savedContentEntryCacheModel =
+			(SavedContentEntryCacheModel)object;
+
+		if ((savedContentId == savedContentEntryCacheModel.savedContentId) &&
+			(mvccVersion == savedContentEntryCacheModel.mvccVersion)) {
+
+			return true;
+		}
+
+		return false;
+	}
+
+	@Override
+	public int hashCode() {
+		int hashCode = HashUtil.hash(0, savedContentId);
+
+		return HashUtil.hash(hashCode, mvccVersion);
+	}
+
+	@Override
+	public long getMvccVersion() {
+		return mvccVersion;
+	}
+
+	@Override
+	public void setMvccVersion(long mvccVersion) {
+		this.mvccVersion = mvccVersion;
+	}
+
+	@Override
+	public String toString() {
+		StringBundler sb = new StringBundler(23);
+
+		sb.append("{mvccVersion=");
+		sb.append(mvccVersion);
+		sb.append(", ctCollectionId=");
+		sb.append(ctCollectionId);
+		sb.append(", savedContentId=");
+		sb.append(savedContentId);
+		sb.append(", groupId=");
+		sb.append(groupId);
+		sb.append(", companyId=");
+		sb.append(companyId);
+		sb.append(", userId=");
+		sb.append(userId);
+		sb.append(", userName=");
+		sb.append(userName);
+		sb.append(", createDate=");
+		sb.append(createDate);
+		sb.append(", modifiedDate=");
+		sb.append(modifiedDate);
+		sb.append(", classNameId=");
+		sb.append(classNameId);
+		sb.append(", classPK=");
+		sb.append(classPK);
+		sb.append("}");
+
+		return sb.toString();
+	}
+
+	@Override
+	public SavedContentEntry toEntityModel() {
+		SavedContentEntryImpl savedContentEntryImpl =
+			new SavedContentEntryImpl();
+
+		savedContentEntryImpl.setMvccVersion(mvccVersion);
+		savedContentEntryImpl.setCtCollectionId(ctCollectionId);
+		savedContentEntryImpl.setSavedContentId(savedContentId);
+		savedContentEntryImpl.setGroupId(groupId);
+		savedContentEntryImpl.setCompanyId(companyId);
+		savedContentEntryImpl.setUserId(userId);
+
+		if (userName == null) {
+			savedContentEntryImpl.setUserName("");
+		}
+		else {
+			savedContentEntryImpl.setUserName(userName);
+		}
+
+		if (createDate == Long.MIN_VALUE) {
+			savedContentEntryImpl.setCreateDate(null);
+		}
+		else {
+			savedContentEntryImpl.setCreateDate(new Date(createDate));
+		}
+
+		if (modifiedDate == Long.MIN_VALUE) {
+			savedContentEntryImpl.setModifiedDate(null);
+		}
+		else {
+			savedContentEntryImpl.setModifiedDate(new Date(modifiedDate));
+		}
+
+		savedContentEntryImpl.setClassNameId(classNameId);
+		savedContentEntryImpl.setClassPK(classPK);
+
+		savedContentEntryImpl.resetOriginalValues();
+
+		return savedContentEntryImpl;
+	}
+
+	@Override
+	public void readExternal(ObjectInput objectInput) throws IOException {
+		mvccVersion = objectInput.readLong();
+
+		ctCollectionId = objectInput.readLong();
+
+		savedContentId = objectInput.readLong();
+
+		groupId = objectInput.readLong();
+
+		companyId = objectInput.readLong();
+
+		userId = objectInput.readLong();
+		userName = objectInput.readUTF();
+		createDate = objectInput.readLong();
+		modifiedDate = objectInput.readLong();
+
+		classNameId = objectInput.readLong();
+
+		classPK = objectInput.readLong();
+	}
+
+	@Override
+	public void writeExternal(ObjectOutput objectOutput) throws IOException {
+		objectOutput.writeLong(mvccVersion);
+
+		objectOutput.writeLong(ctCollectionId);
+
+		objectOutput.writeLong(savedContentId);
+
+		objectOutput.writeLong(groupId);
+
+		objectOutput.writeLong(companyId);
+
+		objectOutput.writeLong(userId);
+
+		if (userName == null) {
+			objectOutput.writeUTF("");
+		}
+		else {
+			objectOutput.writeUTF(userName);
+		}
+
+		objectOutput.writeLong(createDate);
+		objectOutput.writeLong(modifiedDate);
+
+		objectOutput.writeLong(classNameId);
+
+		objectOutput.writeLong(classPK);
+	}
+
+	public long mvccVersion;
+	public long ctCollectionId;
+	public long savedContentId;
+	public long groupId;
+	public long companyId;
+	public long userId;
+	public String userName;
+	public long createDate;
+	public long modifiedDate;
+	public long classNameId;
+	public long classPK;
+
+}

modules/apps/saved-content/saved-content-service/src/main/resources/META-INF/sql/tables.sql
@@ -1,0 +1,14 @@
+create table SavedContentEntry (
+	mvccVersion LONG default 0 not null,
+	ctCollectionId LONG default 0 not null,
+	savedContentId LONG not null,
+	groupId LONG,
+	companyId LONG,
+	userId LONG,
+	userName VARCHAR(75) null,
+	createDate DATE null,
+	modifiedDate DATE null,
+	classNameId LONG,
+	classPK LONG,
+	primary key (savedContentId, ctCollectionId)
+);

olafk/liferay-portal-old

modules/apps/saved-content/saved-content-api/src/main/java/com/liferay/saved/content/model/SavedContentEntryTable.java
@@ -1,0 +1,60 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.saved.content.model;
+
+import com.liferay.petra.sql.dsl.Column;
+import com.liferay.petra.sql.dsl.base.BaseTable;
+
+import java.sql.Types;
+
+import java.util.Date;
+
+/**
+ * The table class for the &quot;SavedContentEntry&quot; database table.
+ *
+ * @author Brian Wing Shun Chan
+ * @see SavedContentEntry
+ * @generated
+ */
+public class SavedContentEntryTable extends BaseTable<SavedContentEntryTable> {
+
+	public static final SavedContentEntryTable INSTANCE =
+		new SavedContentEntryTable();
+
+	public final Column<SavedContentEntryTable, Long> mvccVersion =
+		createColumn(
+			"mvccVersion", Long.class, Types.BIGINT, Column.FLAG_NULLITY);
+	public final Column<SavedContentEntryTable, Long> ctCollectionId =
+		createColumn(
+			"ctCollectionId", Long.class, Types.BIGINT, Column.FLAG_PRIMARY);
+	public final Column<SavedContentEntryTable, Long> savedContentEntryId =
+		createColumn(
+			"savedContentEntryId", Long.class, Types.BIGINT,
+			Column.FLAG_PRIMARY);
+	public final Column<SavedContentEntryTable, Long> groupId = createColumn(
+		"groupId", Long.class, Types.BIGINT, Column.FLAG_DEFAULT);
+	public final Column<SavedContentEntryTable, Long> companyId = createColumn(
+		"companyId", Long.class, Types.BIGINT, Column.FLAG_DEFAULT);
+	public final Column<SavedContentEntryTable, Long> userId = createColumn(
+		"userId", Long.class, Types.BIGINT, Column.FLAG_DEFAULT);
+	public final Column<SavedContentEntryTable, String> userName = createColumn(
+		"userName", String.class, Types.VARCHAR, Column.FLAG_DEFAULT);
+	public final Column<SavedContentEntryTable, Date> createDate = createColumn(
+		"createDate", Date.class, Types.TIMESTAMP, Column.FLAG_DEFAULT);
+	public final Column<SavedContentEntryTable, Date> modifiedDate =
+		createColumn(
+			"modifiedDate", Date.class, Types.TIMESTAMP, Column.FLAG_DEFAULT);
+	public final Column<SavedContentEntryTable, Long> classNameId =
+		createColumn(
+			"classNameId", Long.class, Types.BIGINT, Column.FLAG_DEFAULT);
+	public final Column<SavedContentEntryTable, Long> classPK = createColumn(
+		"classPK", Long.class, Types.BIGINT, Column.FLAG_DEFAULT);
+
+	private SavedContentEntryTable() {
+		super("SavedContentEntry", SavedContentEntryTable::new);
+	}
+
+}

modules/apps/saved-content/saved-content-service/src/main/java/com/liferay/saved/content/model/impl/SavedContentEntryCacheModel.java
@@ -1,0 +1,205 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.saved.content.model.impl;
+
+import com.liferay.petra.lang.HashUtil;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.model.CacheModel;
+import com.liferay.portal.kernel.model.MVCCModel;
+import com.liferay.saved.content.model.SavedContentEntry;
+
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+
+import java.util.Date;
+
+/**
+ * The cache model class for representing SavedContentEntry in entity cache.
+ *
+ * @author Brian Wing Shun Chan
+ * @generated
+ */
+public class SavedContentEntryCacheModel
+	implements CacheModel<SavedContentEntry>, Externalizable, MVCCModel {
+
+	@Override
+	public boolean equals(Object object) {
+		if (this == object) {
+			return true;
+		}
+
+		if (!(object instanceof SavedContentEntryCacheModel)) {
+			return false;
+		}
+
+		SavedContentEntryCacheModel savedContentEntryCacheModel =
+			(SavedContentEntryCacheModel)object;
+
+		if ((savedContentEntryId ==
+				savedContentEntryCacheModel.savedContentEntryId) &&
+			(mvccVersion == savedContentEntryCacheModel.mvccVersion)) {
+
+			return true;
+		}
+
+		return false;
+	}
+
+	@Override
+	public int hashCode() {
+		int hashCode = HashUtil.hash(0, savedContentEntryId);
+
+		return HashUtil.hash(hashCode, mvccVersion);
+	}
+
+	@Override
+	public long getMvccVersion() {
+		return mvccVersion;
+	}
+
+	@Override
+	public void setMvccVersion(long mvccVersion) {
+		this.mvccVersion = mvccVersion;
+	}
+
+	@Override
+	public String toString() {
+		StringBundler sb = new StringBundler(23);
+
+		sb.append("{mvccVersion=");
+		sb.append(mvccVersion);
+		sb.append(", ctCollectionId=");
+		sb.append(ctCollectionId);
+		sb.append(", savedContentEntryId=");
+		sb.append(savedContentEntryId);
+		sb.append(", groupId=");
+		sb.append(groupId);
+		sb.append(", companyId=");
+		sb.append(companyId);
+		sb.append(", userId=");
+		sb.append(userId);
+		sb.append(", userName=");
+		sb.append(userName);
+		sb.append(", createDate=");
+		sb.append(createDate);
+		sb.append(", modifiedDate=");
+		sb.append(modifiedDate);
+		sb.append(", classNameId=");
+		sb.append(classNameId);
+		sb.append(", classPK=");
+		sb.append(classPK);
+		sb.append("}");
+
+		return sb.toString();
+	}
+
+	@Override
+	public SavedContentEntry toEntityModel() {
+		SavedContentEntryImpl savedContentEntryImpl =
+			new SavedContentEntryImpl();
+
+		savedContentEntryImpl.setMvccVersion(mvccVersion);
+		savedContentEntryImpl.setCtCollectionId(ctCollectionId);
+		savedContentEntryImpl.setSavedContentEntryId(savedContentEntryId);
+		savedContentEntryImpl.setGroupId(groupId);
+		savedContentEntryImpl.setCompanyId(companyId);
+		savedContentEntryImpl.setUserId(userId);
+
+		if (userName == null) {
+			savedContentEntryImpl.setUserName("");
+		}
+		else {
+			savedContentEntryImpl.setUserName(userName);
+		}
+
+		if (createDate == Long.MIN_VALUE) {
+			savedContentEntryImpl.setCreateDate(null);
+		}
+		else {
+			savedContentEntryImpl.setCreateDate(new Date(createDate));
+		}
+
+		if (modifiedDate == Long.MIN_VALUE) {
+			savedContentEntryImpl.setModifiedDate(null);
+		}
+		else {
+			savedContentEntryImpl.setModifiedDate(new Date(modifiedDate));
+		}
+
+		savedContentEntryImpl.setClassNameId(classNameId);
+		savedContentEntryImpl.setClassPK(classPK);
+
+		savedContentEntryImpl.resetOriginalValues();
+
+		return savedContentEntryImpl;
+	}
+
+	@Override
+	public void readExternal(ObjectInput objectInput) throws IOException {
+		mvccVersion = objectInput.readLong();
+
+		ctCollectionId = objectInput.readLong();
+
+		savedContentEntryId = objectInput.readLong();
+
+		groupId = objectInput.readLong();
+
+		companyId = objectInput.readLong();
+
+		userId = objectInput.readLong();
+		userName = objectInput.readUTF();
+		createDate = objectInput.readLong();
+		modifiedDate = objectInput.readLong();
+
+		classNameId = objectInput.readLong();
+
+		classPK = objectInput.readLong();
+	}
+
+	@Override
+	public void writeExternal(ObjectOutput objectOutput) throws IOException {
+		objectOutput.writeLong(mvccVersion);
+
+		objectOutput.writeLong(ctCollectionId);
+
+		objectOutput.writeLong(savedContentEntryId);
+
+		objectOutput.writeLong(groupId);
+
+		objectOutput.writeLong(companyId);
+
+		objectOutput.writeLong(userId);
+
+		if (userName == null) {
+			objectOutput.writeUTF("");
+		}
+		else {
+			objectOutput.writeUTF(userName);
+		}
+
+		objectOutput.writeLong(createDate);
+		objectOutput.writeLong(modifiedDate);
+
+		objectOutput.writeLong(classNameId);
+
+		objectOutput.writeLong(classPK);
+	}
+
+	public long mvccVersion;
+	public long ctCollectionId;
+	public long savedContentEntryId;
+	public long groupId;
+	public long companyId;
+	public long userId;
+	public String userName;
+	public long createDate;
+	public long modifiedDate;
+	public long classNameId;
+	public long classPK;
+
+}

modules/apps/saved-content/saved-content-service/src/main/resources/META-INF/sql/tables.sql
@@ -1,0 +1,14 @@
+create table SavedContentEntry (
+	mvccVersion LONG default 0 not null,
+	ctCollectionId LONG default 0 not null,
+	savedContentEntryId LONG not null,
+	groupId LONG,
+	companyId LONG,
+	userId LONG,
+	userName VARCHAR(75) null,
+	createDate DATE null,
+	modifiedDate DATE null,
+	classNameId LONG,
+	classPK LONG,
+	primary key (savedContentEntryId, ctCollectionId)
+);