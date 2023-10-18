faf5e212ff2633584fb102e3d6020378a5dd94df
Tamyris Torres <tamyris.bernardo@liferay.com>
2023-10-18 14:07:46 -0300

    LPS-199457 Delete UpgradeJavaCaptchaUtilCheck

--- modules/util/source-formatter/src/test/java/com/liferay/source/formatter/processor/UpgradeSourceProcessorTest.java
+++ modules/util/source-formatter/src/test/java/com/liferay/source/formatter/processor/UpgradeSourceProcessorTest.java
@@ -111,11 +111,6 @@ public class UpgradeSourceProcessorTest extends BaseSourceProcessorTestCase {
 	}
 
 	@Test
-	public void testUpgradeJavaCaptchaUtilCheck() throws Exception {
-		test("upgrade/UpgradeJavaCaptchaUtilCheck.testjava");
-	}
-
-	@Test
 	public void testUpgradeJavaCheck() throws Exception {
 		test("upgrade/UpgradeJavaCheck.testjava");
 	}
