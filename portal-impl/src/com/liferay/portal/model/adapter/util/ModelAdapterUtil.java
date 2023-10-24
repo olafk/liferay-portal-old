014a11d5b09fc705afbb571a0a22ba3e3a1c8c92
Julius Lee <meng-huan.lee@liferay.com>
2023-10-23 18:30:41 -0700

    LPS-199825 Move ModelAdapterUtil to portal-impl
    # breaking_change_report
    ## What portal-impl/src/com/liferay/portal/model/adapter/ModelAdapterUtil.java
    ModelAdapterUtil is moving from portal-kernel into portal-impl.
    ## Why
    We are merging portal-kernel into portal-impl as a long term goal to simplify portal structure.
    ## Alternatives
    Add portal-impl in build dependency and use the same class.
    ----

--- portal-impl/src/com/liferay/portal/model/adapter/util/ModelAdapterUtil.java
+++ portal-impl/src/com/liferay/portal/model/adapter/util/ModelAdapterUtil.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portal.kernel.model.adapter;
+package com.liferay.portal.model.adapter.util;
 
 import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
 import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapFactory;
