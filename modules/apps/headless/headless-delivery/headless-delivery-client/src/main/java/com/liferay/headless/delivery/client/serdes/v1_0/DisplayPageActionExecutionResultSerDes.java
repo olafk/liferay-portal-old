ruben-pulido/liferay-portal

LPS-200245 buildREST

modules/apps/headless/headless-delivery/headless-delivery-client/src/main/java/com/liferay/headless/delivery/client/serdes/v1_0/DisplayPageActionExecutionResultSerDes.java
@@ -1,0 +1,198 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.headless.delivery.client.serdes.v1_0;
+
+import com.liferay.headless.delivery.client.dto.v1_0.DisplayPageActionExecutionResult;
+import com.liferay.headless.delivery.client.json.BaseJSONParser;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.TreeMap;
+
+import javax.annotation.Generated;
+
+/**
+ * @author Javier Gamarra
+ * @generated
+ */
+@Generated("")
+public class DisplayPageActionExecutionResultSerDes {
+
+	public static DisplayPageActionExecutionResult toDTO(String json) {
+		DisplayPageActionExecutionResultJSONParser
+			displayPageActionExecutionResultJSONParser =
+				new DisplayPageActionExecutionResultJSONParser();
+
+		return displayPageActionExecutionResultJSONParser.parseToDTO(json);
+	}
+
+	public static DisplayPageActionExecutionResult[] toDTOs(String json) {
+		DisplayPageActionExecutionResultJSONParser
+			displayPageActionExecutionResultJSONParser =
+				new DisplayPageActionExecutionResultJSONParser();
+
+		return displayPageActionExecutionResultJSONParser.parseToDTOs(json);
+	}
+
+	public static String toJSON(
+		DisplayPageActionExecutionResult displayPageActionExecutionResult) {
+
+		if (displayPageActionExecutionResult == null) {
+			return "null";
+		}
+
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("{");
+
+		if (displayPageActionExecutionResult.getItemReference() != null) {
+			if (sb.length() > 1) {
+				sb.append(", ");
+			}
+
+			sb.append("\"itemReference\": ");
+
+			sb.append(
+				String.valueOf(
+					displayPageActionExecutionResult.getItemReference()));
+		}
+
+		sb.append("}");
+
+		return sb.toString();
+	}
+
+	public static Map<String, Object> toMap(String json) {
+		DisplayPageActionExecutionResultJSONParser
+			displayPageActionExecutionResultJSONParser =
+				new DisplayPageActionExecutionResultJSONParser();
+
+		return displayPageActionExecutionResultJSONParser.parseToMap(json);
+	}
+
+	public static Map<String, String> toMap(
+		DisplayPageActionExecutionResult displayPageActionExecutionResult) {
+
+		if (displayPageActionExecutionResult == null) {
+			return null;
+		}
+
+		Map<String, String> map = new TreeMap<>();
+
+		if (displayPageActionExecutionResult.getItemReference() == null) {
+			map.put("itemReference", null);
+		}
+		else {
+			map.put(
+				"itemReference",
+				String.valueOf(
+					displayPageActionExecutionResult.getItemReference()));
+		}
+
+		return map;
+	}
+
+	public static class DisplayPageActionExecutionResultJSONParser
+		extends BaseJSONParser<DisplayPageActionExecutionResult> {
+
+		@Override
+		protected DisplayPageActionExecutionResult createDTO() {
+			return new DisplayPageActionExecutionResult();
+		}
+
+		@Override
+		protected DisplayPageActionExecutionResult[] createDTOArray(int size) {
+			return new DisplayPageActionExecutionResult[size];
+		}
+
+		@Override
+		protected void setField(
+			DisplayPageActionExecutionResult displayPageActionExecutionResult,
+			String jsonParserFieldName, Object jsonParserFieldValue) {
+
+			if (Objects.equals(jsonParserFieldName, "itemReference")) {
+				if (jsonParserFieldValue != null) {
+					displayPageActionExecutionResult.setItemReference(
+						ClassFieldsReferenceSerDes.toDTO(
+							(String)jsonParserFieldValue));
+				}
+			}
+		}
+
+	}
+
+	private static String _escape(Object object) {
+		String string = String.valueOf(object);
+
+		for (String[] strings : BaseJSONParser.JSON_ESCAPE_STRINGS) {
+			string = string.replace(strings[0], strings[1]);
+		}
+
+		return string;
+	}
+
+	private static String _toJSON(Map<String, ?> map) {
+		StringBuilder sb = new StringBuilder("{");
+
+		@SuppressWarnings("unchecked")
+		Set set = map.entrySet();
+
+		@SuppressWarnings("unchecked")
+		Iterator<Map.Entry<String, ?>> iterator = set.iterator();
+
+		while (iterator.hasNext()) {
+			Map.Entry<String, ?> entry = iterator.next();
+
+			sb.append("\"");
+			sb.append(entry.getKey());
+			sb.append("\": ");
+
+			Object value = entry.getValue();
+
+			Class<?> valueClass = value.getClass();
+
+			if (value instanceof Map) {
+				sb.append(_toJSON((Map)value));
+			}
+			else if (valueClass.isArray()) {
+				Object[] values = (Object[])value;
+
+				sb.append("[");
+
+				for (int i = 0; i < values.length; i++) {
+					sb.append("\"");
+					sb.append(_escape(values[i]));
+					sb.append("\"");
+
+					if ((i + 1) < values.length) {
+						sb.append(", ");
+					}
+				}
+
+				sb.append("]");
+			}
+			else if (value instanceof String) {
+				sb.append("\"");
+				sb.append(_escape(entry.getValue()));
+				sb.append("\"");
+			}
+			else {
+				sb.append(String.valueOf(entry.getValue()));
+			}
+
+			if (iterator.hasNext()) {
+				sb.append(", ");
+			}
+		}
+
+		sb.append("}");
+
+		return sb.toString();
+	}
+
+}

olafk/liferay-portal-old

modules/apps/headless/headless-delivery/headless-delivery-client/src/main/java/com/liferay/headless/delivery/client/serdes/v1_0/DisplayPageActionExecutionResultSerDes.java
@@ -1,0 +1,195 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.headless.delivery.client.serdes.v1_0;
+
+import com.liferay.headless.delivery.client.dto.v1_0.DisplayPageActionExecutionResult;
+import com.liferay.headless.delivery.client.json.BaseJSONParser;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.TreeMap;
+
+import javax.annotation.Generated;
+
+/**
+ * @author Javier Gamarra
+ * @generated
+ */
+@Generated("")
+public class DisplayPageActionExecutionResultSerDes {
+
+	public static DisplayPageActionExecutionResult toDTO(String json) {
+		DisplayPageActionExecutionResultJSONParser
+			displayPageActionExecutionResultJSONParser =
+				new DisplayPageActionExecutionResultJSONParser();
+
+		return displayPageActionExecutionResultJSONParser.parseToDTO(json);
+	}
+
+	public static DisplayPageActionExecutionResult[] toDTOs(String json) {
+		DisplayPageActionExecutionResultJSONParser
+			displayPageActionExecutionResultJSONParser =
+				new DisplayPageActionExecutionResultJSONParser();
+
+		return displayPageActionExecutionResultJSONParser.parseToDTOs(json);
+	}
+
+	public static String toJSON(
+		DisplayPageActionExecutionResult displayPageActionExecutionResult) {
+
+		if (displayPageActionExecutionResult == null) {
+			return "null";
+		}
+
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("{");
+
+		if (displayPageActionExecutionResult.getMapping() != null) {
+			if (sb.length() > 1) {
+				sb.append(", ");
+			}
+
+			sb.append("\"mapping\": ");
+
+			sb.append(
+				String.valueOf(displayPageActionExecutionResult.getMapping()));
+		}
+
+		sb.append("}");
+
+		return sb.toString();
+	}
+
+	public static Map<String, Object> toMap(String json) {
+		DisplayPageActionExecutionResultJSONParser
+			displayPageActionExecutionResultJSONParser =
+				new DisplayPageActionExecutionResultJSONParser();
+
+		return displayPageActionExecutionResultJSONParser.parseToMap(json);
+	}
+
+	public static Map<String, String> toMap(
+		DisplayPageActionExecutionResult displayPageActionExecutionResult) {
+
+		if (displayPageActionExecutionResult == null) {
+			return null;
+		}
+
+		Map<String, String> map = new TreeMap<>();
+
+		if (displayPageActionExecutionResult.getMapping() == null) {
+			map.put("mapping", null);
+		}
+		else {
+			map.put(
+				"mapping",
+				String.valueOf(displayPageActionExecutionResult.getMapping()));
+		}
+
+		return map;
+	}
+
+	public static class DisplayPageActionExecutionResultJSONParser
+		extends BaseJSONParser<DisplayPageActionExecutionResult> {
+
+		@Override
+		protected DisplayPageActionExecutionResult createDTO() {
+			return new DisplayPageActionExecutionResult();
+		}
+
+		@Override
+		protected DisplayPageActionExecutionResult[] createDTOArray(int size) {
+			return new DisplayPageActionExecutionResult[size];
+		}
+
+		@Override
+		protected void setField(
+			DisplayPageActionExecutionResult displayPageActionExecutionResult,
+			String jsonParserFieldName, Object jsonParserFieldValue) {
+
+			if (Objects.equals(jsonParserFieldName, "mapping")) {
+				if (jsonParserFieldValue != null) {
+					displayPageActionExecutionResult.setMapping(
+						MappingSerDes.toDTO((String)jsonParserFieldValue));
+				}
+			}
+		}
+
+	}
+
+	private static String _escape(Object object) {
+		String string = String.valueOf(object);
+
+		for (String[] strings : BaseJSONParser.JSON_ESCAPE_STRINGS) {
+			string = string.replace(strings[0], strings[1]);
+		}
+
+		return string;
+	}
+
+	private static String _toJSON(Map<String, ?> map) {
+		StringBuilder sb = new StringBuilder("{");
+
+		@SuppressWarnings("unchecked")
+		Set set = map.entrySet();
+
+		@SuppressWarnings("unchecked")
+		Iterator<Map.Entry<String, ?>> iterator = set.iterator();
+
+		while (iterator.hasNext()) {
+			Map.Entry<String, ?> entry = iterator.next();
+
+			sb.append("\"");
+			sb.append(entry.getKey());
+			sb.append("\": ");
+
+			Object value = entry.getValue();
+
+			Class<?> valueClass = value.getClass();
+
+			if (value instanceof Map) {
+				sb.append(_toJSON((Map)value));
+			}
+			else if (valueClass.isArray()) {
+				Object[] values = (Object[])value;
+
+				sb.append("[");
+
+				for (int i = 0; i < values.length; i++) {
+					sb.append("\"");
+					sb.append(_escape(values[i]));
+					sb.append("\"");
+
+					if ((i + 1) < values.length) {
+						sb.append(", ");
+					}
+				}
+
+				sb.append("]");
+			}
+			else if (value instanceof String) {
+				sb.append("\"");
+				sb.append(_escape(entry.getValue()));
+				sb.append("\"");
+			}
+			else {
+				sb.append(String.valueOf(entry.getValue()));
+			}
+
+			if (iterator.hasNext()) {
+				sb.append(", ");
+			}
+		}
+
+		sb.append("}");
+
+		return sb.toString();
+	}
+
+}