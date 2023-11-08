adddfcc2420d0dd28b0eb150d6d7d005fb921bf4
clara izquierdo <claraizquierdosanz@gmail.com>
2023-11-08 12:26:36 +0100

    LPS-200973 Add types

---
+++ modules/apps/segments/segments-web/types/src/main/resources/META-INF/resources/js/utils/keyboardCodes.d.ts
@@ -0,0 +1,20 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+export declare const ARROW_DOWN_KEY_CODE = 'ArrowDown';
+export declare const ARROW_LEFT_KEY_CODE = 'ArrowLeft';
+export declare const ARROW_RIGHT_KEY_CODE = 'ArrowRight';
+export declare const ARROW_UP_KEY_CODE = 'ArrowUp';
+export declare const BACKSPACE_KEY_CODE = 'Backspace';
+export declare const D_KEY_CODE = 'KeyD';
+export declare const END_KEY_CODE = 'End';
+export declare const ENTER_KEY_CODE = 'Enter';
+export declare const ESCAPE_KEY_CODE = 'Escape';
+export declare const HOME_KEY_CODE = 'Home';
+export declare const PERIOD_KEY_CODE = 'Period';
+export declare const S_KEY_CODE = 'KeyS';
+export declare const SPACE_KEY_CODE = 'Space';
+export declare const TAB_KEY_CODE = 'Tab';
+export declare const Z_KEY_CODE = 'KeyZ';
