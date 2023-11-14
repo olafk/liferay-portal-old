seltong/liferay-portal

LPS-194293 Test system object fields in notification templates

modules/apps/notification/notification-test/src/testIntegration/java/com/liferay/notification/internal/type/test/BaseNotificationTypeTest.java
@@ -125,6 +125,8 @@ public class BaseNotificationTypeTest {
 
 		parentObjectEntryValues = LinkedHashMapBuilder.<String, Object>put(
 			"textObjectField", RandomTestUtil.randomString()
+		).put(
+			"systemObjectField", RandomTestUtil.randomString()
 		).build();
 
 		user1 = TestPropsValues.getUser();
@@ -222,6 +224,17 @@ public class BaseNotificationTypeTest {
 						"textObjectField"
 					).objectFieldSettings(
 						Collections.emptyList()
+					).build(),
+					new TextObjectFieldBuilder(
+					).labelMap(
+						LocalizedMapUtil.getLocalizedMap(
+							RandomTestUtil.randomString())
+					).name(
+						"systemObjectField"
+					).objectFieldSettings(
+						Collections.emptyList()
+					).system(
+						true
 					).build()));
 
 		parentObjectDefinition =
@@ -380,7 +393,8 @@ public class BaseNotificationTypeTest {
 				getTermName("integerObjectField"),
 				getTermName("picklistObjectField"),
 				getTermName("textObjectField"),
-				getTermName(true, "textObjectField")));
+				getTermName(true, "textObjectField"),
+				getTermName(true, "systemObjectField")));
 	}
 
 	protected List<Object> getTermValues() {

olafk/liferay-portal-old

modules/apps/notification/notification-test/src/testIntegration/java/com/liferay/notification/internal/type/test/BaseNotificationTypeTest.java
@@ -131,6 +131,8 @@ public class BaseNotificationTypeTest {
 		).build();
 
 		parentObjectEntryValues = LinkedHashMapBuilder.<String, Object>put(
+			"systemObjectField", RandomTestUtil.randomString()
+		).put(
 			"textObjectField", RandomTestUtil.randomString()
 		).build();
 
@@ -228,6 +230,17 @@ public class BaseNotificationTypeTest {
 				false, ObjectDefinitionConstants.SCOPE_COMPANY,
 				ObjectDefinitionConstants.STORAGE_TYPE_DEFAULT,
 				Arrays.asList(
+					new TextObjectFieldBuilder(
+					).labelMap(
+						LocalizedMapUtil.getLocalizedMap(
+							RandomTestUtil.randomString())
+					).name(
+						"systemObjectField"
+					).objectFieldSettings(
+						Collections.emptyList()
+					).system(
+						true
+					).build(),
 					new TextObjectFieldBuilder(
 					).labelMap(
 						LocalizedMapUtil.getLocalizedMap(
@@ -403,6 +416,7 @@ public class BaseNotificationTypeTest {
 				getTermName("integerObjectField"),
 				getTermName("picklistObjectField"),
 				getTermName("textObjectField"),
+				getTermName(true, "systemObjectField"),
 				getTermName(true, "textObjectField")));
 	}
 