54e31c598f40374b54d31ce11226dbcb105aac54
Rubén Pulido <ruben.pulido@liferay.com>
2023-11-14 02:30:39 +0100

    LPS-201091 Keep previous logic for isShowInfoButton

--- modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/display/context/JournalDisplayContext.java
+++ modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/display/context/JournalDisplayContext.java
@@ -1377,7 +1377,10 @@ public class JournalDisplayContext {
 	}
 
 	public boolean isShowInfoButton() {
-		if (isNavigationMine() || isNavigationRecent() || isSearch()) {
+		if (isNavigationMine() || isNavigationRecent() || isSearch() ||
+			ArrayUtil.isNotEmpty(_getAssetCategoryIds()) ||
+			ArrayUtil.isNotEmpty(_getAssetTagNames())) {
+
 			return false;
 		}
 
