6d6e05d90628d96f6476ab6fbaea82d46b96ff58
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2023-11-21 09:53:08 +0100

    LPS-201821 Adds an integration tests

--- modules/apps/layout/layout-content-page-editor-web-test/src/testIntegration/java/com/liferay/layout/content/page/editor/web/internal/portlet/action/test/PublishLayoutMVCActionCommandTest.java
+++ modules/apps/layout/layout-content-page-editor-web-test/src/testIntegration/java/com/liferay/layout/content/page/editor/web/internal/portlet/action/test/PublishLayoutMVCActionCommandTest.java
@@ -13,22 +13,36 @@ import com.liferay.layout.page.template.model.LayoutPageTemplateStructure;
 import com.liferay.layout.page.template.service.LayoutPageTemplateStructureLocalService;
 import com.liferay.layout.test.util.ContentLayoutTestUtil;
 import com.liferay.layout.test.util.LayoutTestUtil;
+import com.liferay.layout.util.BulkLayoutConverter;
 import com.liferay.layout.util.structure.DeletedLayoutStructureItem;
 import com.liferay.layout.util.structure.LayoutStructure;
 import com.liferay.layout.util.structure.LayoutStructureItem;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.model.LayoutConstants;
+import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.portlet.PortletIdCodec;
+import com.liferay.portal.kernel.security.permission.PermissionCheckerFactory;
 import com.liferay.portal.kernel.service.CompanyLocalService;
 import com.liferay.portal.kernel.service.LayoutLocalService;
 import com.liferay.portal.kernel.service.PortletPreferencesLocalService;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
+import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.test.util.UserTestUtil;
 import com.liferay.portal.kernel.util.PortletKeys;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
@@ -174,6 +188,78 @@ public class PublishLayoutMVCActionCommandTest {
 				layoutStructureItem3.getItemId()));
 	}
 
+	@Test
+	public void testPublishConversionDraftCreatedByDeletedUser()
+		throws Exception {
+
+		User user = UserTestUtil.addCompanyAdminUser(
+			_companyLocalService.getCompany(_group.getCompanyId()));
+
+		try {
+			ServiceContext serviceContext =
+				ServiceContextTestUtil.getServiceContext(
+					_group.getGroupId(), user.getUserId());
+
+			ServiceContextThreadLocal.pushServiceContext(serviceContext);
+
+			Layout originalLayout = _layoutLocalService.addLayout(
+				user.getUserId(), _group.getGroupId(), false,
+				LayoutConstants.DEFAULT_PARENT_LAYOUT_ID,
+				RandomTestUtil.randomString(), StringPool.BLANK,
+				StringPool.BLANK, LayoutConstants.TYPE_PORTLET, false,
+				StringPool.BLANK, serviceContext);
+
+			Assert.assertEquals(user.getUserId(), originalLayout.getUserId());
+
+			Assert.assertTrue(originalLayout.isTypePortlet());
+
+			serviceContext = ServiceContextTestUtil.getServiceContext(
+				_group.getGroupId(), TestPropsValues.getUserId());
+
+			ServiceContextThreadLocal.pushServiceContext(serviceContext);
+
+			_deleteUser(user, serviceContext);
+
+			_bulkLayoutConverter.convertLayout(originalLayout.getPlid());
+
+			originalLayout = _layoutLocalService.getLayout(
+				originalLayout.getPlid());
+
+			Assert.assertTrue(originalLayout.isTypePortlet());
+
+			Layout conversionDraftLayout = originalLayout.fetchDraftLayout();
+
+			Assert.assertNotNull(conversionDraftLayout);
+
+			Assert.assertEquals(
+				TestPropsValues.getUserId(), conversionDraftLayout.getUserId());
+
+			Assert.assertTrue(conversionDraftLayout.isTypeContent());
+
+			ContentLayoutTestUtil.publishLayout(
+				conversionDraftLayout, originalLayout);
+
+			originalLayout = _layoutLocalService.getLayout(
+				originalLayout.getPlid());
+
+			Assert.assertTrue(originalLayout.isTypeContent());
+
+			Assert.assertTrue(originalLayout.isPublished());
+
+			Layout draftLayout = originalLayout.fetchDraftLayout();
+
+			Assert.assertNotNull(draftLayout);
+
+			Assert.assertEquals(
+				conversionDraftLayout.getPlid(), draftLayout.getPlid());
+
+			Assert.assertTrue(draftLayout.isApproved());
+		}
+		finally {
+			ServiceContextThreadLocal.popServiceContext();
+		}
+	}
+
 	private FragmentEntryLink _addPortletToLayout(Layout layout)
 		throws Exception {
 
@@ -230,6 +316,18 @@ public class PublishLayoutMVCActionCommandTest {
 				PortletKeys.PREFS_OWNER_TYPE_LAYOUT, plid, encodePortletId));
 	}
 
+	private void _deleteUser(User user, ServiceContext serviceContext)
+		throws PortalException {
+
+		_userLocalService.updateStatus(
+			user.getUserId(), WorkflowConstants.STATUS_INACTIVE,
+			serviceContext);
+
+		_userLocalService.deleteUser(user.getUserId());
+
+		Assert.assertNull(_userLocalService.fetchUser(user.getUserId()));
+	}
+
 	private LayoutStructure _getLayoutStructure(Layout layout)
 		throws Exception {
 
@@ -241,6 +339,9 @@ public class PublishLayoutMVCActionCommandTest {
 		return LayoutStructure.of(
 			layoutPageTemplateStructure.getDefaultSegmentsExperienceData());
 	}
+
+	@Inject
+	private BulkLayoutConverter _bulkLayoutConverter;
 
 	private Company _company;
 
@@ -261,9 +362,15 @@ public class PublishLayoutMVCActionCommandTest {
 		_layoutPageTemplateStructureLocalService;
 
 	@Inject
+	private PermissionCheckerFactory _permissionCheckerFactory;
+
+	@Inject
 	private PortletPreferencesLocalService _portletPreferencesLocalService;
 
 	@Inject
 	private SegmentsExperienceLocalService _segmentsExperienceLocalService;
+
+	@Inject
+	private UserLocalService _userLocalService;
 
 }
