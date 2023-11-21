766e8bfcf96976c51b2c9007b16525f77307def9
Daniel Couso <daniel.couso@liferay.com>
2023-11-21 11:43:27 +0100

    LPS-201930 Avoid 404 error when rendered a widget page with blank master page

--- modules/apps/layout/layout-taglib/src/main/java/com/liferay/layout/taglib/internal/servlet/taglib/LayoutStructureCommonStylesCSSTopHeadDynamicInclude.java
+++ modules/apps/layout/layout-taglib/src/main/java/com/liferay/layout/taglib/internal/servlet/taglib/LayoutStructureCommonStylesCSSTopHeadDynamicInclude.java
@@ -57,7 +57,7 @@ public class LayoutStructureCommonStylesCSSTopHeadDynamicInclude
 		Layout layout = themeDisplay.getLayout();
 
 		if (!layout.isTypeAssetDisplay() && !layout.isTypeContent() &&
-			!layout.isTypePortlet()) {
+			(!layout.isTypePortlet() || (layout.getMasterLayoutPlid() == 0))) {
 
 			return;
 		}
