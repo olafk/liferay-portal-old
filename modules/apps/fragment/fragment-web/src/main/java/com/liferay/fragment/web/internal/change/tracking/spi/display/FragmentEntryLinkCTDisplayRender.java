liferay-site-management/liferay-portal

LPS-198917 Hide fragment duplicates that are added to the draft page

modules/apps/fragment/fragment-web/src/main/java/com/liferay/fragment/web/internal/change/tracking/spi/display/FragmentEntryLinkCTDisplayRender.java
@@ -56,7 +56,10 @@ public class FragmentEntryLinkCTDisplayRender
 
 	@Override
 	public boolean isHideable(FragmentEntryLink fragmentEntryLink) {
-		if (fragmentEntryLink.getOriginalFragmentEntryLinkId() == 0) {
+		Layout layout = _layoutLocalService.fetchLayout(
+			fragmentEntryLink.getPlid());
+
+		if (!layout.isDraft() && !layout.isDraftLayout()) {
 			return false;
 		}
 

olafk/liferay-portal-old

modules/apps/fragment/fragment-web/src/main/java/com/liferay/fragment/web/internal/change/tracking/spi/display/FragmentEntryLinkCTDisplayRender.java
@@ -56,7 +56,10 @@ public class FragmentEntryLinkCTDisplayRender
 
 	@Override
 	public boolean isHideable(FragmentEntryLink fragmentEntryLink) {
-		if (fragmentEntryLink.getOriginalFragmentEntryLinkId() == 0) {
+		Layout layout = _layoutLocalService.fetchLayout(
+			fragmentEntryLink.getClassPK());
+
+		if (!layout.isDraft() && !layout.isDraftLayout()) {
 			return false;
 		}
 