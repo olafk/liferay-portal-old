3af69ed634e4bdbdc0ee69eb0c2661642f7b2970
Janis Zhang <janis.zhang@liferay.com>
2023-11-23 15:47:54 +0800

    LPS-201364 Make ImageProcessorMessageListener as a component and register Destination in activate method.

--- modules/apps/document-library/document-library-preview-image/src/main/java/com/liferay/document/library/preview/image/internal/messaging/ImageProcessorMessageListener.java
+++ modules/apps/document-library/document-library-preview-image/src/main/java/com/liferay/document/library/preview/image/internal/messaging/ImageProcessorMessageListener.java
@@ -6,14 +6,49 @@
 package com.liferay.document.library.preview.image.internal.messaging;
 
 import com.liferay.document.library.kernel.util.ImageProcessorUtil;
+import com.liferay.portal.kernel.messaging.Destination;
+import com.liferay.portal.kernel.messaging.DestinationConfiguration;
+import com.liferay.portal.kernel.messaging.DestinationFactory;
+import com.liferay.portal.kernel.messaging.DestinationNames;
+import com.liferay.portal.kernel.messaging.MessageListener;
 import com.liferay.portal.kernel.repository.model.FileVersion;
+import com.liferay.portal.kernel.util.MapUtil;
 import com.liferay.portlet.documentlibrary.messaging.BaseProcessorMessageListener;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Sergio González
  */
+@Component(
+	property = "destination.name=" + DestinationNames.DOCUMENT_LIBRARY_IMAGE_PROCESSOR,
+	service = MessageListener.class
+)
 public class ImageProcessorMessageListener
 	extends BaseProcessorMessageListener {
+
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		Destination destination = _destinationFactory.createDestination(
+			new DestinationConfiguration(
+				DestinationConfiguration.DESTINATION_TYPE_SERIAL,
+				DestinationNames.DOCUMENT_LIBRARY_IMAGE_PROCESSOR));
+
+		_serviceRegistration = bundleContext.registerService(
+			Destination.class, destination,
+			MapUtil.singletonDictionary(
+				"destination.name", destination.getName()));
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		_serviceRegistration.unregister();
+	}
 
 	@Override
 	protected void generate(
@@ -23,5 +58,10 @@ public class ImageProcessorMessageListener
 		ImageProcessorUtil.generateImages(
 			sourceFileVersion, destinationFileVersion);
 	}
+
+	@Reference
+	private DestinationFactory _destinationFactory;
+
+	private ServiceRegistration<Destination> _serviceRegistration;
 
 }
