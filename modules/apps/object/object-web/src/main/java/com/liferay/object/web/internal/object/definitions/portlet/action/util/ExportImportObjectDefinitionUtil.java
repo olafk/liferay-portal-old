06ec73fd2119293f29831a89fcff5d4c1538042b
Guilherme Sa <guilherme.sa@liferay.com>
2023-11-24 10:49:21 -0300

    LPS-201519 Remove unused code
    Signed-off-by: Guilherme Sa <guilherme.sa@liferay.com>

--- modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/object/definitions/portlet/action/util/ExportImportObjectDefinitionUtil.java
+++ modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/object/definitions/portlet/action/util/ExportImportObjectDefinitionUtil.java
@@ -1,5 +1,5 @@
 /**
- * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
@@ -7,17 +7,12 @@ package com.liferay.object.web.internal.object.definitions.portlet.action.util;
 
 import com.liferay.object.admin.rest.dto.v1_0.ObjectAction;
 import com.liferay.object.admin.rest.dto.v1_0.ObjectDefinition;
-import com.liferay.petra.function.UnsafeFunction;
-import com.liferay.portal.kernel.json.JSONArray;
 import com.liferay.portal.kernel.json.JSONFactory;
-import com.liferay.portal.kernel.json.JSONFactoryUtil;
-import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.Validator;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -25,16 +20,6 @@ import java.util.Map;
  * @author Gabriel Albuquerque
  */
 public class ExportImportObjectDefinitionUtil {
-
-	public static void apply(
-		JSONObject objectDefinitionJSONObject,
-		UnsafeFunction<JSONObject, JSONObject, Exception> unsafeFunction) {
-
-		_apply(
-			objectDefinitionJSONObject, unsafeFunction, "objectLayouts",
-			"objectLayoutTabs", "objectLayoutBoxes", "objectLayoutRows",
-			"objectLayoutColumns");
-	}
 
 	public static void prepareObjectDefinitionForExport(
 		JSONFactory jsonFactory, ObjectDefinition objectDefinition) {
@@ -61,34 +46,6 @@ public class ExportImportObjectDefinitionUtil {
 				objectDefinition.getObjectFields(),
 				objectField -> Validator.isNull(
 					objectField.getRelationshipType())));
-	}
-
-	private static void _apply(
-		JSONObject jsonObject,
-		UnsafeFunction<JSONObject, JSONObject, Exception> unsafeFunction,
-		String... properties) {
-
-		JSONArray jsonArray = (JSONArray)jsonObject.get(properties[0]);
-
-		if (properties.length != 1) {
-			for (int i = 0; i < jsonArray.length(); i++) {
-				_apply(
-					(JSONObject)jsonArray.get(i), unsafeFunction,
-					Arrays.copyOfRange(properties, 1, properties.length));
-			}
-
-			return;
-		}
-
-		JSONArray newJSONArray = JSONFactoryUtil.createJSONArray();
-
-		for (int i = 0; i < jsonArray.length(); i++) {
-			JSONObject internalJSONObject = (JSONObject)jsonArray.get(i);
-
-			newJSONArray.put(() -> unsafeFunction.apply(internalJSONObject));
-		}
-
-		jsonObject.put(properties[0], newJSONArray);
 	}
 
 }
