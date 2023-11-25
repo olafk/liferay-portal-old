bd8463553c8105fdbd50aa549465db7319f47ecf
Janis Zhang <janis.zhang@liferay.com>
2023-11-25 11:20:10 +0800

    LPS-201423 Make ConvertProcessMessageListener as a component and register Destination in activate method.

--- modules/apps/document-library/document-library-service/src/main/java/com/liferay/document/library/internal/messaging/ConvertProcessMessageListener.java
+++ modules/apps/document-library/document-library-service/src/main/java/com/liferay/document/library/internal/messaging/ConvertProcessMessageListener.java
@@ -8,13 +8,29 @@ package com.liferay.document.library.internal.messaging;
 import com.liferay.portal.convert.ConvertProcess;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.messaging.Destination;
+import com.liferay.portal.kernel.messaging.DestinationConfiguration;
+import com.liferay.portal.kernel.messaging.DestinationFactory;
+import com.liferay.portal.kernel.messaging.DestinationNames;
 import com.liferay.portal.kernel.messaging.Message;
 import com.liferay.portal.kernel.messaging.MessageListener;
+import com.liferay.portal.kernel.util.MapUtil;
 import com.liferay.portal.util.ShutdownUtil;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Alexander Chow
  */
+@Component(
+	property = "destination.name=" + DestinationNames.CONVERT_PROCESS,
+	service = MessageListener.class
+)
 public class ConvertProcessMessageListener implements MessageListener {
 
 	@Override
@@ -29,6 +45,24 @@ public class ConvertProcessMessageListener implements MessageListener {
 		}
 	}
 
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		Destination destination = _destinationFactory.createDestination(
+			new DestinationConfiguration(
+				DestinationConfiguration.DESTINATION_TYPE_SERIAL,
+				DestinationNames.CONVERT_PROCESS));
+
+		_serviceRegistration = bundleContext.registerService(
+			Destination.class, destination,
+			MapUtil.singletonDictionary(
+				"destination.name", destination.getName()));
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		_serviceRegistration.unregister();
+	}
+
 	protected void doReceive(Message message) throws Exception {
 		ConvertProcess convertProcess = (ConvertProcess)message.getPayload();
 
@@ -37,5 +71,10 @@ public class ConvertProcessMessageListener implements MessageListener {
 
 	private static final Log _log = LogFactoryUtil.getLog(
 		ConvertProcessMessageListener.class);
+
+	@Reference
+	private DestinationFactory _destinationFactory;
+
+	private ServiceRegistration<Destination> _serviceRegistration;
 
 }
