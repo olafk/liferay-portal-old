veroglez/liferay-portal

LPS-202697 Add label for the translation status

modules/apps/layout/layout-js-components-web/src/main/resources/META-INF/resources/js/components/translation_selector/StatusLabel.tsx
@@ -1,0 +1,78 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import {DisplayType} from '@clayui/alert';
+import ClayLabel from '@clayui/label';
+import ClayLayout from '@clayui/layout';
+import {sub} from 'frontend-js-web';
+import React from 'react';
+
+import {Locale, Translations} from './TranslationSelector';
+
+const LABEL_STATUS = {
+	translated: {
+		displayType: 'success',
+		label: Liferay.Language.get('translated'),
+	},
+	default: {
+		displayType: 'info',
+		label: Liferay.Language.get('default'),
+	},
+	notTranslated: {
+		displayType: 'warning',
+		label: Liferay.Language.get('not-translated'),
+	},
+	translating: {
+		displayType: 'secondary',
+		label: '',
+	},
+} as const;
+
+interface Props {
+	defaultLocaleId: string;
+	item: Locale;
+	translations: Translations;
+}
+
+interface Status {
+	displayType: DisplayType;
+	label: string;
+}
+
+export default function StatusLabel({
+	defaultLocaleId,
+	item,
+	translations,
+}: Props) {
+	let status: Status = LABEL_STATUS.notTranslated;
+
+	if (item.id === defaultLocaleId) {
+		status = LABEL_STATUS.default;
+	}
+	else if (item.translations) {
+		const totalTranslations = Object.keys(translations).length;
+
+		if (item.translations === totalTranslations) {
+			status = LABEL_STATUS.translated;
+		}
+		else {
+			status = LABEL_STATUS.translating;
+			status.label = sub(Liferay.Language.get('translating-x-x'), [
+				item.translations,
+				totalTranslations,
+			]);
+		}
+	}
+
+	return (
+		<ClayLayout.ContentCol containerElement="span">
+			<ClayLayout.ContentSection>
+				<ClayLabel displayType={status.displayType}>
+					{status.label}
+				</ClayLabel>
+			</ClayLayout.ContentSection>
+		</ClayLayout.ContentCol>
+	);
+}

olafk/liferay-portal-old

modules/apps/layout/layout-js-components-web/src/main/resources/META-INF/resources/js/components/translation_selector/StatusLabel.tsx
@@ -1,0 +1,78 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import {DisplayType} from '@clayui/alert';
+import ClayLabel from '@clayui/label';
+import ClayLayout from '@clayui/layout';
+import {sub} from 'frontend-js-web';
+import React from 'react';
+
+import {Language, Translations} from './TranslationSelector';
+
+const LABEL_STATUS = {
+	default: {
+		displayType: 'info',
+		label: Liferay.Language.get('default'),
+	},
+	notTranslated: {
+		displayType: 'warning',
+		label: Liferay.Language.get('not-translated'),
+	},
+	translated: {
+		displayType: 'success',
+		label: Liferay.Language.get('translated'),
+	},
+	translating: {
+		displayType: 'secondary',
+		label: '',
+	},
+} as const;
+
+interface Props {
+	defaultLanguageId: Liferay.Language.Locale;
+	item: Language;
+	translations: Translations;
+}
+
+interface Status {
+	displayType: DisplayType;
+	label: string;
+}
+
+export default function StatusLabel({
+	defaultLanguageId,
+	item,
+	translations,
+}: Props) {
+	let status: Status = LABEL_STATUS.notTranslated;
+
+	if (item.id === defaultLanguageId) {
+		status = LABEL_STATUS.default;
+	}
+	else if (item.translations) {
+		const totalTranslations = Object.keys(translations).length;
+
+		if (item.translations === totalTranslations) {
+			status = LABEL_STATUS.translated;
+		}
+		else {
+			status = LABEL_STATUS.translating;
+			status.label = sub(Liferay.Language.get('translating-x-x'), [
+				item.translations,
+				totalTranslations,
+			]);
+		}
+	}
+
+	return (
+		<ClayLayout.ContentCol containerElement="span">
+			<ClayLayout.ContentSection>
+				<ClayLabel displayType={status.displayType}>
+					{status.label}
+				</ClayLabel>
+			</ClayLayout.ContentSection>
+		</ClayLayout.ContentCol>
+	);
+}