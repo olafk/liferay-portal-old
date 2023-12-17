veroglez/liferay-portal

LPS-202697 Add new parameter for input localized to hide the translation manager

portal-web/docroot/html/taglib/aui/input/init.jsp
@@ -32,6 +32,7 @@ boolean first = GetterUtil.getBoolean(String.valueOf(request.getAttribute("aui:i
 java.lang.String formName = GetterUtil.getString((java.lang.String)request.getAttribute("aui:input:formName"));
 java.lang.String helpMessage = GetterUtil.getString((java.lang.String)request.getAttribute("aui:input:helpMessage"));
 java.lang.String helpTextCssClass = GetterUtil.getString((java.lang.String)request.getAttribute("aui:input:helpTextCssClass"), "input-group-addon");
+boolean hideTranslationManager = GetterUtil.getBoolean(String.valueOf(request.getAttribute("aui:input:hideTranslationManager")));
 java.lang.String iconOff = GetterUtil.getString((java.lang.String)request.getAttribute("aui:input:iconOff"));
 java.lang.String iconOn = GetterUtil.getString((java.lang.String)request.getAttribute("aui:input:iconOn"));
 java.lang.String id = GetterUtil.getString((java.lang.String)request.getAttribute("aui:input:id"));

portal-web/docroot/html/taglib/ui/input_localized/init.jsp
@@ -26,6 +26,7 @@ String editorName = GetterUtil.getString((String)request.getAttribute("liferay-u
 String fieldPrefix = GetterUtil.getString((String)request.getAttribute("liferay-ui:input-localized:fieldPrefix"));
 String fieldPrefixSeparator = GetterUtil.getString((String)request.getAttribute("liferay-ui:input-localized:fieldPrefixSeparator"));
 String helpMessage = GetterUtil.getString((String)request.getAttribute("liferay-ui:input-localized:helpMessage"));
+boolean hideTranslationManager = GetterUtil.getBoolean((String)request.getAttribute("liferay-ui:input-localized:hideTranslationManager"));
 String id = HtmlUtil.getAUICompatibleId((String)request.getAttribute("liferay-ui:input-localized:id"));
 boolean ignoreRequestValue = GetterUtil.getBoolean((String)request.getAttribute("liferay-ui:input-localized:ignoreRequestValue"));
 String inputAddon = (String)request.getAttribute("liferay-ui:input-localized:inputAddon");

olafk/liferay-portal-old

portal-web/docroot/html/taglib/aui/input/init.jsp
@@ -42,6 +42,7 @@ java.lang.String label = GetterUtil.getString((java.lang.String)request.getAttri
 java.lang.String labelCssClass = GetterUtil.getString((java.lang.String)request.getAttribute("aui:input:labelCssClass"));
 java.lang.String languageId = GetterUtil.getString((java.lang.String)request.getAttribute("aui:input:languageId"));
 java.lang.String languagesDropdownDirection = GetterUtil.getString((java.lang.String)request.getAttribute("aui:input:languagesDropdownDirection"));
+boolean languagesDropdownVisible = GetterUtil.getBoolean(String.valueOf(request.getAttribute("aui:input:languagesDropdownVisible")));
 boolean last = GetterUtil.getBoolean(String.valueOf(request.getAttribute("aui:input:last")));
 boolean localized = GetterUtil.getBoolean(String.valueOf(request.getAttribute("aui:input:localized")));
 boolean localizeLabel = GetterUtil.getBoolean(String.valueOf(request.getAttribute("aui:input:localizeLabel")), true);

portal-web/docroot/html/taglib/ui/input_localized/init.jsp
@@ -31,6 +31,7 @@ boolean ignoreRequestValue = GetterUtil.getBoolean((String)request.getAttribute(
 String inputAddon = (String)request.getAttribute("liferay-ui:input-localized:inputAddon");
 String languageId = (String)request.getAttribute("liferay-ui:input-localized:languageId");
 String languagesDropdownDirection = GetterUtil.getString((String)request.getAttribute("liferay-ui:input-localized:languagesDropdownDirection"));
+boolean languagesDropdownVisible = GetterUtil.getBoolean((String)request.getAttribute("liferay-ui:input-localized:languagesDropdownVisible"));
 String maxLength = GetterUtil.getString((String)request.getAttribute("liferay-ui:input-localized:maxLength"));
 String name = (String)request.getAttribute("liferay-ui:input-localized:name");
 String placeholder = (String)request.getAttribute("liferay-ui:input-localized:placeholder");