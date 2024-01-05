2db2b495b6a3244bfc98230b4f62230a6d54b4c4
Michael Hashimoto <michael.hashimoto@liferay.com>
2024-01-04 16:19:09 -0800

    LRCI-3939 Add logic to handle ci:reevaluate github comments

--- workspaces/liferay-jethr0-workspace/client-extensions/liferay-jethr0-etc-spring-boot/src/main/java/com/liferay/jethr0/event/github/GitHubEventHandlerFactory.java
+++ workspaces/liferay-jethr0-workspace/client-extensions/liferay-jethr0-etc-spring-boot/src/main/java/com/liferay/jethr0/event/github/GitHubEventHandlerFactory.java
@@ -48,6 +48,10 @@ public class GitHubEventHandlerFactory extends BaseEventHandlerFactory {
 						return new HelpGitHubCommentEventHandler(
 							eventHandlerContext, messageJSONObject);
 					}
+					else if (body.startsWith("ci:reevaluate")) {
+						return new ReevaluateGitHubCommentEventHandler(
+							eventHandlerContext, messageJSONObject);
+					}
 					else if (body.startsWith("ci:reopen")) {
 						return new ReopenGitHubCommentEventHandler(
 							eventHandlerContext, messageJSONObject);
