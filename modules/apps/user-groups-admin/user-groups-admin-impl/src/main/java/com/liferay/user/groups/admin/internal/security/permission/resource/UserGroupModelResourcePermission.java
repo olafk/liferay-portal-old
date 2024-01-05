96492791e708caa5964d45e1e1a271dd44bf9497
Riccardo Alberti <riccardo.alberti@liferay.com>
2024-01-05 11:17:41 +0100

    COMMERCE-13129 Remove unused @Reference

--- modules/apps/user-groups-admin/user-groups-admin-impl/src/main/java/com/liferay/user/groups/admin/internal/security/permission/resource/UserGroupModelResourcePermission.java
+++ modules/apps/user-groups-admin/user-groups-admin-impl/src/main/java/com/liferay/user/groups/admin/internal/security/permission/resource/UserGroupModelResourcePermission.java
@@ -10,11 +10,9 @@ import com.liferay.portal.kernel.model.UserGroup;
 import com.liferay.portal.kernel.security.permission.PermissionChecker;
 import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
 import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermission;
-import com.liferay.portal.kernel.service.UserGroupLocalService;
 import com.liferay.portal.service.permission.UserGroupPermissionUtil;
 
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Igor Fabiano Nazar
@@ -74,8 +72,5 @@ public class UserGroupModelResourcePermission
 	public PortletResourcePermission getPortletResourcePermission() {
 		return null;
 	}
-
-	@Reference
-	private UserGroupLocalService _userGroupLocalService;
 
 }
