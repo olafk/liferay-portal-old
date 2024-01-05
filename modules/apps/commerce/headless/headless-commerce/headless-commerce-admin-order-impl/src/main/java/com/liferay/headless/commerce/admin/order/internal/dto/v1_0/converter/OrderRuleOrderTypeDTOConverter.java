86b44b958f80a9c2c269f209c4dec99bea35e25e
Riccardo Alberti <riccardo.alberti@liferay.com>
2024-01-05 11:11:40 +0100

    COMMERCE-13123 Remove unused @Reference

--- modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-order-impl/src/main/java/com/liferay/headless/commerce/admin/order/internal/dto/v1_0/converter/OrderRuleOrderTypeDTOConverter.java
+++ modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-order-impl/src/main/java/com/liferay/headless/commerce/admin/order/internal/dto/v1_0/converter/OrderRuleOrderTypeDTOConverter.java
@@ -9,7 +9,6 @@ import com.liferay.commerce.model.CommerceOrderType;
 import com.liferay.commerce.order.rule.model.COREntry;
 import com.liferay.commerce.order.rule.model.COREntryRel;
 import com.liferay.commerce.order.rule.service.COREntryRelService;
-import com.liferay.commerce.order.rule.service.COREntryService;
 import com.liferay.commerce.service.CommerceOrderTypeService;
 import com.liferay.headless.commerce.admin.order.dto.v1_0.OrderRuleOrderType;
 import com.liferay.portal.vulcan.dto.converter.DTOConverter;
@@ -64,8 +63,5 @@ public class OrderRuleOrderTypeDTOConverter
 
 	@Reference
 	private COREntryRelService _corEntryRelService;
-
-	@Reference
-	private COREntryService _corEntryService;
 
 }
