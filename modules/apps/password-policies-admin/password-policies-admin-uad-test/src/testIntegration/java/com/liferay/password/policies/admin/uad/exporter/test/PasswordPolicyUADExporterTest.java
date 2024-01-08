b31563ae91b977248ba316aa63850bc239d030df
Qi Zhang <qi.zhang@liferay.com>
2024-01-08 18:10:05 +0800

    LPS-204924 Move package, *TestUtil.java should be in test.util

--- modules/apps/password-policies-admin/password-policies-admin-uad-test/src/testIntegration/java/com/liferay/password/policies/admin/uad/exporter/test/PasswordPolicyUADExporterTest.java
+++ modules/apps/password-policies-admin/password-policies-admin-uad-test/src/testIntegration/java/com/liferay/password/policies/admin/uad/exporter/test/PasswordPolicyUADExporterTest.java
@@ -6,7 +6,7 @@
 package com.liferay.password.policies.admin.uad.exporter.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
-import com.liferay.password.policies.admin.uad.test.PasswordPolicyUADTestUtil;
+import com.liferay.password.policies.admin.uad.test.util.PasswordPolicyUADTestUtil;
 import com.liferay.portal.kernel.model.PasswordPolicy;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
--- modules/apps/password-policies-admin/password-policies-admin-uad-test/src/testIntegration/java/com/liferay/password/policies/admin/uad/test/util/PasswordPolicyUADTestUtil.java
+++ modules/apps/password-policies-admin/password-policies-admin-uad-test/src/testIntegration/java/com/liferay/password/policies/admin/uad/test/util/PasswordPolicyUADTestUtil.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.password.policies.admin.uad.test;
+package com.liferay.password.policies.admin.uad.test.util;
 
 import com.liferay.portal.kernel.model.PasswordPolicy;
 import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
