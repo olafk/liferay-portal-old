BryanEngler/liferay-portal

LPD-1995 portal-search-tuning-rankings-web: Add upgrade process to update the classNameId in rankings entries
https://liferay.atlassian.net/browse/LPD-1995

modules/dxp/apps/portal-search-tuning/portal-search-tuning-rankings-web/src/main/java/com/liferay/portal/search/tuning/rankings/web/internal/upgrade/registry/PortalSearchTuningRankingsWebUpgradeStepRegistrator.java
@@ -1,0 +1,34 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.search.tuning.rankings.web.internal.upgrade.registry;
+
+import com.liferay.portal.kernel.service.ClassNameLocalService;
+import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Almir Ferreira
+ */
+@Component(service = UpgradeStepRegistrator.class)
+public class PortalSearchTuningRankingsWebUpgradeStepRegistrator
+	implements UpgradeStepRegistrator {
+
+	@Override
+	public void register(Registry registry) {
+		registry.registerInitialization();
+
+		registry.register(
+			"0.0.1", "0.0.2",
+			new com.liferay.portal.search.tuning.rankings.web.internal.upgrade.
+				v0_0_2.RankingUpgradeProcess(_classNameLocalService));
+	}
+
+	@Reference
+	private ClassNameLocalService _classNameLocalService;
+
+}

modules/dxp/apps/portal-search-tuning/portal-search-tuning-rankings-web/src/main/java/com/liferay/portal/search/tuning/rankings/web/internal/upgrade/v0_0_2/RankingUpgradeProcess.java
@@ -1,0 +1,51 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.search.tuning.rankings.web.internal.upgrade.v0_0_2;
+
+import com.liferay.portal.kernel.model.ClassName;
+import com.liferay.portal.kernel.service.ClassNameLocalService;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.search.tuning.rankings.index.Ranking;
+
+import java.sql.PreparedStatement;
+
+/**
+ * @author Almir Ferreira
+ */
+public class RankingUpgradeProcess extends UpgradeProcess {
+
+	public RankingUpgradeProcess(ClassNameLocalService classNameLocalService) {
+		_classNameLocalService = classNameLocalService;
+	}
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		try (PreparedStatement preparedStatement = connection.prepareStatement(
+				"update jsonStorageEntry set classNameId = ? where " +
+					"classNameId = ?")) {
+
+			ClassName className = _classNameLocalService.fetchClassName(
+				"com.liferay.portal.search.tuning.rankings.web.internal." +
+					"index.Ranking");
+
+			if (className != null) {
+				_classNameLocalService.deleteClassName(className);
+
+				ClassName newClassName = _classNameLocalService.getClassName(
+					Ranking.class.getName());
+
+				preparedStatement.setLong(1, newClassName.getClassNameId());
+
+				preparedStatement.setLong(2, className.getClassNameId());
+
+				preparedStatement.execute();
+			}
+		}
+	}
+
+	private final ClassNameLocalService _classNameLocalService;
+
+}

olafk/liferay-portal-old

modules/dxp/apps/portal-search-tuning/portal-search-tuning-rankings-web/src/main/java/com/liferay/portal/search/tuning/rankings/web/internal/upgrade/registry/PortalSearchTuningRankingsWebUpgradeStepRegistrator.java
@@ -1,0 +1,32 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.search.tuning.rankings.web.internal.upgrade.registry;
+
+import com.liferay.portal.kernel.service.ClassNameLocalService;
+import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Almir Ferreira
+ */
+@Component(service = UpgradeStepRegistrator.class)
+public class PortalSearchTuningRankingsWebUpgradeStepRegistrator
+	implements UpgradeStepRegistrator {
+
+	@Override
+	public void register(Registry registry) {
+		registry.register(
+			"0.0.0", "0.0.1",
+			new com.liferay.portal.search.tuning.rankings.web.internal.upgrade.
+				v0_0_1.RankingUpgradeProcess(_classNameLocalService));
+	}
+
+	@Reference
+	private ClassNameLocalService _classNameLocalService;
+
+}

modules/dxp/apps/portal-search-tuning/portal-search-tuning-rankings-web/src/main/java/com/liferay/portal/search/tuning/rankings/web/internal/upgrade/v0_0_1/RankingUpgradeProcess.java
@@ -1,0 +1,51 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.search.tuning.rankings.web.internal.upgrade.v0_0_1;
+
+import com.liferay.portal.kernel.model.ClassName;
+import com.liferay.portal.kernel.service.ClassNameLocalService;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.search.tuning.rankings.index.Ranking;
+
+import java.sql.PreparedStatement;
+
+/**
+ * @author Almir Ferreira
+ */
+public class RankingUpgradeProcess extends UpgradeProcess {
+
+	public RankingUpgradeProcess(ClassNameLocalService classNameLocalService) {
+		_classNameLocalService = classNameLocalService;
+	}
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		try (PreparedStatement preparedStatement = connection.prepareStatement(
+				"update JSONStorageEntry set classNameId = ? where " +
+					"classNameId = ?")) {
+
+			ClassName className = _classNameLocalService.fetchClassName(
+				"com.liferay.portal.search.tuning.rankings.web.internal." +
+					"index.Ranking");
+
+			if (className != null) {
+				_classNameLocalService.deleteClassName(className);
+
+				ClassName newClassName = _classNameLocalService.getClassName(
+					Ranking.class.getName());
+
+				preparedStatement.setLong(1, newClassName.getClassNameId());
+
+				preparedStatement.setLong(2, className.getClassNameId());
+
+				preparedStatement.execute();
+			}
+		}
+	}
+
+	private final ClassNameLocalService _classNameLocalService;
+
+}