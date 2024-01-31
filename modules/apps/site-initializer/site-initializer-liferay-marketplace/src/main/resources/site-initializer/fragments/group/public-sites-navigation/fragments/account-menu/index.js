fed10c894612fc357bce2bba41214f374869a7a4
Keven Leone <keven.leone@liferay.com>
2024-01-30 23:22:00 -0300

    LPD-15510 Add Okta Create Account Link

--- modules/apps/site-initializer/site-initializer-liferay-marketplace/src/main/resources/site-initializer/fragments/group/public-sites-navigation/fragments/account-menu/index.js
+++ modules/apps/site-initializer/site-initializer-liferay-marketplace/src/main/resources/site-initializer/fragments/group/public-sites-navigation/fragments/account-menu/index.js
@@ -3,8 +3,3 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-const createAnAccount = document.querySelector('#createAccountBtn');
-
-createAnAccount.onclick = () => {
-	window.location.href = 'https://login.liferay.com/signin/register';
-};
