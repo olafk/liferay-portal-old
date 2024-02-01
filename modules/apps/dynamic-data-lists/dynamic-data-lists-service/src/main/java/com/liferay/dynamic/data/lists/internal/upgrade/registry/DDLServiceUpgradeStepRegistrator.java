de360271af18c866d6b36ca875ebba63507063f8
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2024-02-01 07:18:51 +0000

    LPD-15997 Wait for the minimum version to allow upgrade process starts earlier rather than later

--- modules/apps/dynamic-data-lists/dynamic-data-lists-service/src/main/java/com/liferay/dynamic/data/lists/internal/upgrade/registry/DDLServiceUpgradeStepRegistrator.java
+++ modules/apps/dynamic-data-lists/dynamic-data-lists-service/src/main/java/com/liferay/dynamic/data/lists/internal/upgrade/registry/DDLServiceUpgradeStepRegistrator.java
@@ -88,7 +88,7 @@ public class DDLServiceUpgradeStepRegistrator
 	private CounterLocalService _counterLocalService;
 
 	@Reference(
-		target = "(&(release.bundle.symbolic.name=com.liferay.dynamic.data.mapping.service)(&(release.schema.version>=1.0.0)))"
+		target = "(&(release.bundle.symbolic.name=com.liferay.dynamic.data.mapping.service)(&(release.schema.version>=0.0.2)))"
 	)
 	private Release _release;
 
