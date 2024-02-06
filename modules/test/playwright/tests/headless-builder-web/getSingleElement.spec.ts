8838ea1cda78bc75ed1280274bd273e2e10cab04
jalber786 <alberto.moreno@liferay.com>
2024-02-06 10:35:06 +0100

    LPD-15245 Substitution of Poshi test with a Playwright one

--- modules/test/playwright/tests/headless-builder-web/getSingleElement.spec.ts
+++ modules/test/playwright/tests/headless-builder-web/getSingleElement.spec.ts
@@ -109,3 +109,109 @@ test('can see path parameter property with map details', async ({
 	await headlessBuilderPage.goto();
 	await headlessBuilderPage.deleteApplicationThroughUI('My-app');
 });
+
+test('can list site scoped endpoint', async ({
+	apiApplicationPage,
+	apiHelpers,
+	headlessBuilderPage,
+	page,
+}) => {
+	await waitForHeadlessBuilderReady(apiHelpers, page);
+	const studentSiteDefinition =
+		await apiHelpers.objectAdmin.postObjectDefinition({
+			active: true,
+			externalReferenceCode: 'site-student-definition',
+			label: {
+				en_US: 'Student',
+			},
+			name: 'Student',
+			objectFields: [
+				{
+					DBType: 'String',
+					businessType: 'Text',
+					externalReferenceCode: 'student-name-field',
+					indexed: true,
+					indexedAsKeyword: false,
+					indexedLanguageId: 'en_US',
+					label: {
+						en_US: 'Student name',
+					},
+					listTypeDefinitionId: 0,
+					name: 'studentName',
+					required: true,
+					state: false,
+					system: false,
+					type: 'String',
+				},
+			],
+			pluralLabel: {
+				en_US: 'Students',
+			},
+			portlet: true,
+			restContextPath: '/o/c/students',
+			scope: 'site',
+			status: {
+				code: 0,
+			},
+		});
+
+	const studentApplication = await apiHelpers.object.postObjectEntry(
+		{
+			apiApplicationToAPISchemas: [
+				{
+					apiSchemaToAPIProperties: [
+						{
+							description: 'Name of the student',
+							externalReferenceCode: 'student-name-property',
+							name: 'studentName',
+							objectFieldERC: 'student-name-field',
+						},
+					],
+					description: 'Test site-schema',
+					externalReferenceCode: 'testSchema',
+					mainObjectDefinitionERC: 'site-student-definition',
+					name: 'testSchema',
+				},
+			],
+			applicationStatus: 'unpublished',
+			baseURL: 'my-app',
+			description: 'Retrive the data from the different students.',
+			externalReferenceCode: 'my-application',
+			title: 'My-app',
+		},
+		'headless-builder/applications'
+	);
+
+	await headlessBuilderPage.goto();
+	await headlessBuilderPage.goToEditAPIApplication(studentApplication.title);
+	await apiApplicationPage.createSingleElementApiEndpoint(
+		'Site',
+		'gettest',
+		'entryerc'
+	);
+
+	await apiApplicationPage.goToEndpointConfigurationTab();
+	await page.getByLabel('Response Body Schema').click();
+	await page
+		.getByRole('menuitem', {
+			name: studentApplication.apiApplicationToAPISchemas[0].name,
+		})
+		.click();
+	await page.getByRole('button', {name: 'Select an Option'}).click();
+	await page.getByRole('menuitem', {name: 'External Reference Code'}).click();
+	await apiApplicationPage.publishButton.click();
+
+	await headlessBuilderPage.goto();
+	await headlessBuilderPage.goToEditAPIApplication(studentApplication.title);
+	await apiApplicationPage.goToEndpointsTab();
+	await apiApplicationPage.goToEditEndpoint('/gettest/{entryerc}/');
+
+	await page.goto('/');
+	await apiHelpers.object.deleteObjectEntryByExternalReferenceCode(
+		'headless-builder/applications',
+		studentApplication.externalReferenceCode
+	);
+	await apiHelpers.objectAdmin.deleteObjectDefinition(
+		studentSiteDefinition.id
+	);
+});
