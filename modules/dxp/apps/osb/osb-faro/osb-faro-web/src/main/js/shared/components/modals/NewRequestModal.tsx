d7fb633c810aaa291bfbb3b570a7475518f06f11
Adriano Interaminense <adriano.interaminense@gmail.com>
2024-02-07 18:04:02 -0300

    LPD-17115 Use ClayMultiSelect to add individuals instead

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/shared/components/modals/NewRequestModal.tsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/shared/components/modals/NewRequestModal.tsx
@@ -1,17 +1,19 @@
 import * as API from 'shared/api';
 import ClayButton from '@clayui/button';
 import ClayLink from '@clayui/link';
+import ClayMultiSelect from '@clayui/multi-select';
 import FileDropTarget from 'shared/components/FileDropTarget';
 import Form from 'shared/components/form';
 import getCN from 'classnames';
 import Input from 'shared/components/Input';
 import Loading, {Align} from 'shared/components/Loading';
 import Modal from 'shared/components/modal';
-import React, {useRef, useState} from 'react';
-import SearchInputList from 'shared/components/SearchInputList';
+import React, {useEffect, useRef, useState} from 'react';
 import {Formik, FormikValues} from 'formik';
+import {NetworkStatus} from '@clayui/data-provider';
 import {paginationDefaults} from 'shared/util/pagination';
 import {sub} from 'shared/util/lang';
+import {useDebounce} from 'shared/hooks';
 
 const SAMPLE_CSV = 'user@example.com\nuser1@example.com\nuser2@example.com';
 
@@ -47,22 +49,40 @@ const NewRequestModal: React.FC<INewRequestModalProps> = ({
 	onClose,
 	onSubmit
 }) => {
-	const [emails, setEmails] = useState([]);
+	const [items, setItems] = useState([]);
 	const [fileName, setFileName] = useState(null);
+	const [email, setEmail] = useState('');
+	const [networkStatus, setNetworkStatus] = useState(NetworkStatus.Unused);
+	const [emails, setEmails] = useState([]);
 
-	const _formRef = useRef<Formik>();
+	const debouncedEmail = useDebounce(email, 500);
 
-	const fetchIndividuals = (inputValue: string): Promise<any> =>
-		API.individuals
-			.search({
+	useEffect(() => {
+		setNetworkStatus(NetworkStatus.Loading);
+
+		async function fetchIndividuals() {
+			const {items} = await API.individuals.search({
 				delta: AUTOCOMPLETE_DELTA,
-				filter: inputValue
-					? `contains(demographics/email/value, '${inputValue}')`
+				filter: debouncedEmail
+					? `contains(demographics/email/value, '${debouncedEmail}')`
 					: '',
 				groupId,
 				page
-			})
-			.then(({items}) => items.map(({properties}) => properties.email));
+			});
+
+			setItems(
+				items.map(({id, properties: {email}}) => ({
+					label: email,
+					value: id
+				}))
+			);
+			setNetworkStatus(NetworkStatus.Unused);
+		}
+
+		fetchIndividuals();
+	}, [debouncedEmail]);
+
+	const _formRef = useRef<Formik>();
 
 	const handleAccessClick = event => {
 		const {checked} = event.target;
@@ -120,7 +140,7 @@ const NewRequestModal: React.FC<INewRequestModalProps> = ({
 
 		if (subjectIdType === SubjectIdType.ByEmail) {
 			onSubmit({
-				emailAddresses: emails,
+				emailAddresses: emails.map(({label}) => label),
 				types
 			});
 		} else {
@@ -254,21 +274,20 @@ const NewRequestModal: React.FC<INewRequestModalProps> = ({
 
 										<Form.RadioGroup.Subsection>
 											<Input.Group>
-												<SearchInputList
-													clearOnAdd
-													containerClass='new-request-modal-container'
-													dataSourceFn={
-														fetchIndividuals
-													}
+												<ClayMultiSelect
+													allowsCustomLabel={false}
 													disabled={
 														values.subjectIdType ===
 														SubjectIdType.ByFile
 													}
-													items={emails}
+													loadingState={networkStatus}
+													onChange={setEmail}
 													onItemsChange={setEmails}
 													placeholder={Liferay.Language.get(
 														'example-email'
 													)}
+													sourceItems={items}
+													value={email}
 												/>
 											</Input.Group>
 										</Form.RadioGroup.Subsection>
