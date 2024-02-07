85cae907e7cf70d5be0be3890cf509bf91e32873
Veronica Gonzalez <veronica.gonzalez@liferay.com>
2024-02-07 10:29:06 +0100

    LPD-17105 Remove tooltip from viewport buttons

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/ViewportSizeSelector.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/ViewportSizeSelector.js
@@ -5,60 +5,11 @@
 
 import {ClayButtonWithIcon, default as ClayButton} from '@clayui/button';
 import classNames from 'classnames';
-import {useId} from 'frontend-js-components-web';
 import PropTypes from 'prop-types';
 import React from 'react';
 
-import {PopoverTooltip} from '../../common/components/PopoverTooltip';
 import {VIEWPORT_SIZES} from '../config/constants/viewportSizes';
 import {config} from '../config/index';
-
-const VIEWPORT_DESCRIPTIONS = {
-	desktop: Liferay.Language.get(
-		'the-styles-you-define-in-the-desktop-viewport-apply-to-all-other-viewports-unless-you-specify-a-style-in-another-viewport'
-	),
-	landscapeMobile: Liferay.Language.get(
-		'the-styles-you-define-in-the-landscape-phone-viewport-apply-to-the-portrait-phone-viewport-unless-you-specify-a-style-in-the-portrait-phone-viewport'
-	),
-	portraitMobile: Liferay.Language.get(
-		'the-portrait-phone-viewport-reflects-the-style-changes-you-make-in-any-other-viewport-unless-you-specify-a-style-in-the-portrait-phone-viewport'
-	),
-	tablet: Liferay.Language.get(
-		'the-styles-you-define-in-the-tablet-viewport-apply-to-all-the-phone-viewports-unless-you-specify-a-style-in-the-landscape-or-portrait-phone-viewports'
-	),
-};
-
-const SelectorButton = ({icon, label, onSelect, selectedSize, sizeId}) => {
-	const tooltipId = useId();
-
-	return (
-		<PopoverTooltip
-			alignPosition="bottom"
-			content={VIEWPORT_DESCRIPTIONS[sizeId]}
-			header={
-				sizeId === VIEWPORT_SIZES.desktop &&
-				Liferay.Language.get('default-viewport')
-			}
-			id={tooltipId}
-			showTooltipOnClick={false}
-			trigger={
-				<ClayButtonWithIcon
-					aria-label={label}
-					aria-pressed={selectedSize === sizeId}
-					className={classNames({
-						'page-editor__viewport-size-selector--default':
-							sizeId === VIEWPORT_SIZES.desktop,
-					})}
-					displayType="secondary"
-					key={sizeId}
-					onClick={() => onSelect(sizeId)}
-					size="sm"
-					symbol={icon}
-				/>
-			}
-		/>
-	);
-};
 
 export default function ViewportSizeSelector({onSizeSelected, selectedSize}) {
 	const {availableViewportSizes} = config;
@@ -67,13 +18,18 @@ export default function ViewportSizeSelector({onSizeSelected, selectedSize}) {
 		<ClayButton.Group className="flex-nowrap flex-shrink-0">
 			{Object.values(availableViewportSizes).map(
 				({icon, label, sizeId}) => (
-					<SelectorButton
-						icon={icon}
+					<ClayButtonWithIcon
+						aria-label={label}
+						aria-pressed={selectedSize === sizeId}
+						className={classNames({
+							'page-editor__viewport-size-selector--default':
+								sizeId === VIEWPORT_SIZES.desktop,
+						})}
+						displayType="secondary"
 						key={sizeId}
-						label={label}
-						onSelect={onSizeSelected}
-						selectedSize={selectedSize}
-						sizeId={sizeId}
+						onClick={() => onSizeSelected(sizeId)}
+						size="sm"
+						symbol={icon}
 					/>
 				)
 			)}
