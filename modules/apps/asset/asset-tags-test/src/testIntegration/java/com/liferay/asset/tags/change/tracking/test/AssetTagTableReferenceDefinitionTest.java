558088eede06ff66bbf650102cb07c93b260211a
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2024-02-14 13:25:29 +0100

    LPD-17663 Creates new asset-tags-test module and moves relevant tests

--- modules/apps/asset/asset-tags-test/src/testIntegration/java/com/liferay/asset/tags/change/tracking/test/AssetTagTableReferenceDefinitionTest.java
+++ modules/apps/asset/asset-tags-test/src/testIntegration/java/com/liferay/asset/tags/change/tracking/test/AssetTagTableReferenceDefinitionTest.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.asset.change.tracking.test;
+package com.liferay.asset.tags.change.tracking.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.asset.test.util.AssetTestUtil;
