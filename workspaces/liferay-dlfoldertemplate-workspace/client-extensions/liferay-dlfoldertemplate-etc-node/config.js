03e67e099abbed2f3e4c854520f73f9dc8158c4c
Mahmoud Hussein Tayem <tayemmahmoud87@gmail.com>
2024-02-26 17:49:34 +0400

    LPD-5166 name changes and remove extra esline config file

--- workspaces/liferay-dlfoldertemplate-workspace/client-extensions/liferay-dlfoldertemplate-etc-node/config.js
+++ workspaces/liferay-dlfoldertemplate-workspace/client-extensions/liferay-dlfoldertemplate-etc-node/config.js
@@ -12,18 +12,18 @@ export default {
 		'process.env.LIFERAY_ROUTES_DXP',
 	],
 	'folder.template.nodes.end.point': '/o/c/t4t14foldertemplatenodes/',
-	'liferay.dlfoldertemplate.oauth.application.server.oauth2.headless.server.client.id':
+	'liferay.dlfoldertemplate.etc.node.oauth.application.server.oauth2.headless.server.client.id':
 		'id-6455f1f2-f15c-07b1-cbc8-3e267524546',
-	'liferay.dlfoldertemplate.oauth.application.server.oauth2.headless.server.client.secret':
+	'liferay.dlfoldertemplate.etc.node.oauth.application.server.oauth2.headless.server.client.secret':
 		'secret-e585633a-07c8-13dd-b654-86adead8e6',
-	'liferay.dlfoldertemplate.oauth.application.server.oauth2.token.uri':
+	'liferay.dlfoldertemplate.etc.node.oauth.application.server.oauth2.token.uri':
 		'/o/oauth2/token',
-	'liferay.dlfoldertemplate.oauth.application.user.agent.oauth2.jwks.uri':
+	'liferay.dlfoldertemplate.etc.node.oauth.application.user.agent.oauth2.jwks.uri':
 		'/o/oauth2/jwks',
-	'liferay.dlfoldertemplate.oauth.application.user.agent.oauth2.user.agent.client.id':
+	'liferay.dlfoldertemplate.etc.node.oauth.application.user.agent.oauth2.user.agent.client.id':
 		'id-f39269e7-1412-ea1c-30b2-8da323478c',
 	'liferay.oauth.application.external.reference.codes':
-		'liferay-dlfoldertemplate-oauth-application-server,liferay-dlfoldertemplate-oauth-application-user-agent',
+		'liferay-dlfoldertemplate-etc-node-oauth-application-server,liferay-dlfoldertemplate-etc-node-oauth-application-user-agent',
 	'ready.path': '/ready',
 	'server.port': 8050,
 };
