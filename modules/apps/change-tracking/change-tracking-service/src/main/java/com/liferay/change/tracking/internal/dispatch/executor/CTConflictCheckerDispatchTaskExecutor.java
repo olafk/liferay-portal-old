f8be4ef02aa839ef839ecc874be3df9b54075fd5
pei-jung <pei-jung.lan@liferay.com>
2024-02-29 16:36:00 -0800

    LPD-17344 Send notifications when conflicts are found during scheduled conflict checks

--- modules/apps/change-tracking/change-tracking-service/src/main/java/com/liferay/change/tracking/internal/dispatch/executor/CTConflictCheckerDispatchTaskExecutor.java
+++ modules/apps/change-tracking/change-tracking-service/src/main/java/com/liferay/change/tracking/internal/dispatch/executor/CTConflictCheckerDispatchTaskExecutor.java
@@ -5,6 +5,9 @@
 
 package com.liferay.change.tracking.internal.dispatch.executor;
 
+import com.liferay.change.tracking.conflict.ConflictInfo;
+import com.liferay.change.tracking.constants.PublicationRoleConstants;
+import com.liferay.change.tracking.internal.helper.CTUserNotificationHelper;
 import com.liferay.change.tracking.model.CTCollection;
 import com.liferay.change.tracking.service.CTCollectionLocalService;
 import com.liferay.dispatch.executor.BaseDispatchTaskExecutor;
@@ -13,7 +16,13 @@ import com.liferay.dispatch.executor.DispatchTaskExecutorOutput;
 import com.liferay.dispatch.model.DispatchTrigger;
 import com.liferay.portal.kernel.dao.orm.ActionableDynamicQuery;
 import com.liferay.portal.kernel.dao.orm.RestrictionsFactoryUtil;
+import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.notifications.UserNotificationDefinition;
+import com.liferay.portal.kernel.util.HtmlUtil;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
+
+import java.util.List;
+import java.util.Map;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -48,8 +57,35 @@ public class CTConflictCheckerDispatchTaskExecutor
 					"status", WorkflowConstants.STATUS_SCHEDULED)));
 		actionableDynamicQuery.setCompanyId(dispatchTrigger.getCompanyId());
 		actionableDynamicQuery.setPerformActionMethod(
-			(CTCollection ctCollection) ->
-				_ctCollectionLocalService.checkConflicts(ctCollection));
+			(CTCollection ctCollection) -> {
+				Map<Long, List<ConflictInfo>> conflictMap =
+					_ctCollectionLocalService.checkConflicts(ctCollection);
+
+				if (conflictMap.isEmpty()) {
+					return;
+				}
+
+				_publicationRoleUserHelper.sendUserNotificationEvents(
+					ctCollection,
+					JSONUtil.put(
+						"ctCollectionId", ctCollection.getCtCollectionId()
+					).put(
+						"ctCollectionName",
+						HtmlUtil.escape(ctCollection.getName())
+					).put(
+						"notificationType",
+						UserNotificationDefinition.
+							NOTIFICATION_TYPE_REVIEW_ENTRY
+					).put(
+						"scheduled", true
+					).put(
+						"showConflicts", true
+					),
+					_publicationRoleUserHelper.getPublicationRoleUserIds(
+						ctCollection, true, PublicationRoleConstants.NAME_ADMIN,
+						PublicationRoleConstants.NAME_EDITOR,
+						PublicationRoleConstants.NAME_PUBLISHER));
+			});
 
 		actionableDynamicQuery.performActions();
 	}
@@ -61,5 +97,8 @@ public class CTConflictCheckerDispatchTaskExecutor
 
 	@Reference
 	private CTCollectionLocalService _ctCollectionLocalService;
+
+	@Reference
+	private CTUserNotificationHelper _publicationRoleUserHelper;
 
 }
