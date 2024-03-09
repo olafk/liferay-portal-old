435aefa0a0094f478c9f492a296c5d8a465b2b28
Marcellus Tavares <marcellus.tavares@liferay.com>
2024-03-08 16:31:00 -0800

    LPD-19885 Remove trial license

--- modules/dxp/apps/osb/osb-faro/osb-faro-docker/cloud/100_liferay_image_setup.sh
+++ modules/dxp/apps/osb/osb-faro/osb-faro-docker/cloud/100_liferay_image_setup.sh
@@ -7,3 +7,5 @@ cp -R /home/liferay/resources/log4j ${LIFERAY_HOME}/osgi/log4j
 cp -R /home/liferay/resources/portal-log4j-ext.xml ${LIFERAY_HOME}/tomcat/webapps/ROOT/WEB-INF/classes/META-INF
 cp -R /home/liferay/resources/rewrite.config ${LIFERAY_HOME}/tomcat/webapps/ROOT/WEB-INF
 cp -R /home/liferay/resources/system-ext.properties ${LIFERAY_HOME}/tomcat/webapps/ROOT/WEB-INF/classes
+
+rm ${LIFERAY_HOME}/deploy/trial-dxp-license-*.xml
