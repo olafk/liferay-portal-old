a45fe61fee1663c683ea2c04e1ed9340ffa661ca
Brian Chan <brian.chan@liferay.com>
2024-03-12 18:13:09 +0530

    LPD-19282 the javadocs say the list returned may be null, but that's not true according to the code. Javadocs have to be accurate, or it's better to not have them.

--- portal-impl/src/com/liferay/portal/service/impl/LayoutLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/LayoutLocalServiceImpl.java
@@ -2434,20 +2434,6 @@ public class LayoutLocalServiceImpl extends LayoutLocalServiceBaseImpl {
 			layoutSetPrototype, layoutUuid);
 	}
 
-	/**
-	 * Returns a range of all the layouts belonging to the group.
-	 *
-	 * @param  groupId the primary key of the group
-	 * @param  privateLayout whether the layout is private to the group
-	 * @param  keywords keywords
-	 * @param  searchOnlyByTitle searchOnlyByTitle
-	 * @param  types layout types
-	 * @param  start the lower bound of the range of layouts
-	 * @param  end the upper bound of the range of layouts (not inclusive)
-	 * @param  orderByComparator the comparator to order the layouts
-	 * @return the matching layouts, or <code>null</code> if no matches were
-	 *         found
-	 */
 	@Override
 	public List<Layout> search(
 			long groupId, boolean privateLayout, String keywords,
@@ -2460,21 +2446,6 @@ public class LayoutLocalServiceImpl extends LayoutLocalServiceBaseImpl {
 			start, end, orderByComparator);
 	}
 
-	/**
-	 * Returns a range of all the layouts belonging to the group.
-	 *
-	 * @param  groupId the primary key of the group
-	 * @param  userId the primary key of the user
-	 * @param  privateLayout whether the layout is private to the group
-	 * @param  keywords keywords
-	 * @param  searchOnlyByTitle searchOnlyByTitle
-	 * @param  types layout types
-	 * @param  start the lower bound of the range of layouts
-	 * @param  end the upper bound of the range of layouts (not inclusive)
-	 * @param  orderByComparator the comparator to order the layouts
-	 * @return the matching layouts, or <code>null</code> if no matches were
-	 *         found
-	 */
 	@Override
 	public List<Layout> search(
 			long groupId, long userId, boolean privateLayout, String keywords,
@@ -2487,21 +2458,6 @@ public class LayoutLocalServiceImpl extends LayoutLocalServiceBaseImpl {
 			new int[0], start, end, orderByComparator);
 	}
 
-	/**
-	 * Returns a range of all the layouts belonging to the group.
-	 *
-	 * @param  groupId the primary key of the group
-	 * @param  userId the primary key of the user
-	 * @param  privateLayout whether the layout is private to the group
-	 * @param  keywords keywords
-	 * @param  searchOnlyByTitle searchOnlyByTitle
-	 * @param  types layout types
-	 * @param  start the lower bound of the range of layouts
-	 * @param  end the upper bound of the range of layouts (not inclusive)
-	 * @param  orderByComparator the comparator to order the layouts
-	 * @return the matching layouts, or <code>null</code> if no matches were
-	 *         found
-	 */
 	@Override
 	public List<Layout> search(
 			long groupId, long userId, boolean privateLayout, String keywords,
