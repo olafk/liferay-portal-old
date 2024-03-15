304b64cdf15728556c6b29616ae4c03f9d566819
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2024-03-15 20:56:52 +0100

    LPD-19162 Implements methods to get layout page template entries by status

--- modules/apps/layout/layout-page-template-service/src/main/java/com/liferay/layout/page/template/service/impl/LayoutPageTemplateCollectionServiceImpl.java
+++ modules/apps/layout/layout-page-template-service/src/main/java/com/liferay/layout/page/template/service/impl/LayoutPageTemplateCollectionServiceImpl.java
@@ -113,14 +113,6 @@ public class LayoutPageTemplateCollectionServiceImpl
 
 	@Override
 	public List<LayoutPageTemplateCollection> getLayoutPageTemplateCollections(
-		long groupId, long layoutPageTemplateCollectionId) {
-
-		return layoutPageTemplateCollectionPersistence.filterFindByG_P(
-			groupId, layoutPageTemplateCollectionId);
-	}
-
-	@Override
-	public List<LayoutPageTemplateCollection> getLayoutPageTemplateCollections(
 		long groupId, int type, int start, int end) {
 
 		return layoutPageTemplateCollectionPersistence.filterFindByG_T(
@@ -134,6 +126,14 @@ public class LayoutPageTemplateCollectionServiceImpl
 
 		return layoutPageTemplateCollectionPersistence.filterFindByG_T(
 			groupId, type, start, end, orderByComparator);
+	}
+
+	@Override
+	public List<LayoutPageTemplateCollection> getLayoutPageTemplateCollections(
+		long groupId, long layoutPageTemplateCollectionId) {
+
+		return layoutPageTemplateCollectionPersistence.filterFindByG_P(
+			groupId, layoutPageTemplateCollectionId);
 	}
 
 	@Override
