df201f4ed2ec6763c6ac6493297178f20929afdd
Alan Huang <alan.huang@liferay.com>
2024-03-18 20:58:47 +0800

    LPD-20678 Move package, remove portal.settings

--- modules/apps/portal-settings/portal-settings-authentication-ldap-web/src/main/java/com/liferay/portal/settings/authentication/ldap/web/internal/configuration/admin/display/ExportLDAPPortalSettingsConfigurationScreenWrapper.java
+++ modules/apps/portal-settings/portal-settings-authentication-ldap-web/src/main/java/com/liferay/portal/settings/authentication/ldap/web/internal/configuration/admin/display/ExportLDAPPortalSettingsConfigurationScreenWrapper.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portal.settings.authentication.ldap.web.internal.portal.settings.configuration.admin.display;
+package com.liferay.portal.settings.authentication.ldap.web.internal.configuration.admin.display;
 
 import com.liferay.configuration.admin.display.ConfigurationScreen;
 import com.liferay.configuration.admin.display.ConfigurationScreenWrapper;
