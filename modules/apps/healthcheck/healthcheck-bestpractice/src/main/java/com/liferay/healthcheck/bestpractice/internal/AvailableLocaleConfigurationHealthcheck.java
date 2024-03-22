d03f5e12164feccd7a701b3ffc0db235e8a38b13
Olaf Kock <olaf.kock+github@liferay.com>
2024-03-22 17:17:22 +0100

    LPD-253 trivial initial healthchecks

---
+++ modules/apps/healthcheck/healthcheck-bestpractice/src/main/java/com/liferay/healthcheck/bestpractice/internal/AvailableLocaleConfigurationHealthcheck.java
@@ -0,0 +1,109 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.healthcheck.bestpractice.internal;
+
+import com.liferay.healthcheck.Healthcheck;
+import com.liferay.healthcheck.HealthcheckItem;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.util.PropsValues;
+
+import java.text.DateFormat;
+
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.Locale;
+import java.util.Set;
+import java.util.TreeSet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * Some installations simplified the locales to just two letters for the
+ * language. In some cases this lead to follow-up problems, as none of the
+ * OOTB languages omits a country/locale.
+ *
+ * This check makes sure that available languages have at least 5 characters
+ * (e.g. "en_GB"), and are supported by the underlying JVM
+ *
+ * @author Olaf Kock
+ */
+@Component(service = Healthcheck.class)
+public class AvailableLocaleConfigurationHealthcheck implements Healthcheck {
+
+	@Override
+	public Collection<HealthcheckItem> check(long companyId) {
+		LinkedList<HealthcheckItem> result = new LinkedList<>();
+
+		_validateProperties(result, PropsValues.LOCALES, "locales");
+		_validateProperties(
+			result, PropsValues.LOCALES_ENABLED, "locales.enabled");
+		_validateProperties(result, PropsValues.LOCALES_BETA, "locales.beta");
+
+		if (result.isEmpty()) {
+			Object[] info = {};
+
+			result.add(
+				new HealthcheckItem(
+					this, true, getClass().getName(), _LINK, _MSG, info));
+		}
+
+		return result;
+	}
+
+	@Override
+	public String getCategory() {
+		return "healthcheck-category-bestpractice";
+	}
+
+	private void _validateProperties(
+		LinkedList<HealthcheckItem> result, String[] locales,
+		String collectionName) {
+
+		for (String loc : locales) {
+
+			// some JVM naming seems to be different from ours...
+
+			String alternativeLoc = StringUtil.replace(loc, "latin", "#Latn");
+
+			if (loc.length() < 5) {
+				result.add(
+					new HealthcheckItem(
+						this, false, getClass().getName(), _LINK,
+						_MSG_SHORT_LOCALE, loc, collectionName));
+			}
+
+			if (!(_availableLocales.contains(loc) ||
+				  _availableLocales.contains(alternativeLoc))) {
+
+				result.add(
+					new HealthcheckItem(
+						this, false, getClass().getName(), _LINK,
+						_MSG_MISSING_LOCALE, loc, collectionName));
+			}
+		}
+	}
+
+	private static final String _LINK =
+		"https://docs.liferay.com/portal/7.4-latest/propertiesdoc/portal." +
+			"properties.html#Languages%20and%20Time%20Zones";
+
+	private static final String _MSG = "configured-locales-are-provided-by-jvm";
+
+	private static final String _MSG_MISSING_LOCALE =
+		"configured-locale-x-from-section-x-not-provided-by-jvm";
+
+	private static final String _MSG_SHORT_LOCALE =
+		"configured-locale-x-from-section-x-has-less-than-5-characters-length";
+
+	private static final Set<String> _availableLocales = new TreeSet<>();
+
+	{
+		for (Locale locale : DateFormat.getAvailableLocales()) {
+			_availableLocales.add(locale.toString());
+		}
+	}
+
+}
---
+++ modules/apps/healthcheck/healthcheck-bestpractice/src/main/java/com/liferay/healthcheck/bestpractice/internal/BlindSelfSignedCertificateTrustHealthcheck.java
@@ -0,0 +1,98 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.healthcheck.bestpractice.internal;
+
+import com.liferay.configuration.admin.constants.ConfigurationAdminPortletKeys;
+import com.liferay.dynamic.data.mapping.data.provider.configuration.DDMDataProviderConfiguration;
+import com.liferay.healthcheck.Healthcheck;
+import com.liferay.healthcheck.HealthcheckItem;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.configuration.module.configuration.ConfigurationProvider;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.module.configuration.ConfigurationException;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Olaf Kock
+ */
+@Component(
+
+// 	configurationPid = "com.liferay.healthcheck.bestpractice.internal.configuration.HealthcheckBestPracticeConfiguration",
+
+	service = Healthcheck.class
+)
+public class BlindSelfSignedCertificateTrustHealthcheck implements Healthcheck {
+
+	@Override
+	public Collection<HealthcheckItem> check(long companyId)
+		throws PortalException {
+
+		return Arrays.asList(
+			new HealthcheckItem(
+				this, !getTrustSetting(companyId), getClass().getName(), _LINK,
+				_MSG));
+	}
+
+	@Override
+	public String getCategory() {
+		return "healthcheck-category-bestpractice";
+	}
+
+	protected boolean getTrustSetting(long companyId)
+		throws ConfigurationException {
+
+		DDMDataProviderConfiguration ddmDataProviderConfiguration =
+			_configurationProvider.getCompanyConfiguration(
+				DDMDataProviderConfiguration.class, companyId);
+
+		return ddmDataProviderConfiguration.trustSelfSignedCertificates();
+	}
+
+	private static final String _DDM =
+		"com.liferay.dynamic.data.mapping.data.provider.configuration." +
+			"DDMDataProviderConfiguration";
+
+	// TODO: Replace with _portal.getControlPanelFullURL() implementation
+
+	private static final String _LINK = new StringBundler(
+	).append(
+		"/group/control_panel/manage?p_p_id="
+	).append(
+		ConfigurationAdminPortletKeys.INSTANCE_SETTINGS
+	).append(
+		"&_"
+	).append(
+		ConfigurationAdminPortletKeys.INSTANCE_SETTINGS
+	).append(
+		"_mvcRenderCommandName=%2Fconfiguration_admin%2Fedit_configuration&_"
+	).append(
+		ConfigurationAdminPortletKeys.INSTANCE_SETTINGS
+	).append(
+		"_factoryPid="
+	).append(
+		_DDM
+	).append(
+		"&_"
+	).append(
+		ConfigurationAdminPortletKeys.INSTANCE_SETTINGS
+	).append(
+		"_pid="
+	).append(
+		_DDM
+	).toString();
+
+	private static final String _MSG =
+		"do-not-trust-self-signed-dataprovider-certificates";
+
+	@Reference
+	private ConfigurationProvider _configurationProvider;
+
+}
