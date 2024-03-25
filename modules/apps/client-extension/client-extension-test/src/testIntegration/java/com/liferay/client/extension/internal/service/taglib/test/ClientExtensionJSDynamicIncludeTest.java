f08ff12d95a1115626e68e4aabe3c46168450dad
John Co <john.co@liferay.com>
2024-03-25 11:36:14 -0700

    LPD-21277 Quarantine failing frontend infra tests

--- modules/apps/client-extension/client-extension-test/src/testIntegration/java/com/liferay/client/extension/internal/service/taglib/test/ClientExtensionJSDynamicIncludeTest.java
+++ modules/apps/client-extension/client-extension-test/src/testIntegration/java/com/liferay/client/extension/internal/service/taglib/test/ClientExtensionJSDynamicIncludeTest.java
@@ -45,6 +45,7 @@ import java.util.List;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.ClassRule;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -55,6 +56,7 @@ import org.springframework.mock.web.MockHttpServletResponse;
 /**
  * @author Víctor Galán
  */
+@Ignore
 @RunWith(Arquillian.class)
 public class ClientExtensionJSDynamicIncludeTest {
 
