liferay-workflow/liferay-portal

LPD-18756 Create Groovy script use and a factory interface to retrieve a list of it

modules/apps/portal-security/portal-security-script-management-api/src/main/java/com/liferay/portal/security/script/management/groovy/script/use/provider/GroovyScriptUsesFactory.java
@@ -1,0 +1,21 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.security.script.management.groovy.script.use.provider;
+
+import com.liferay.portal.security.script.management.groovy.script.use.GroovyScriptUse;
+
+import java.util.List;
+
+import javax.portlet.ResourceRequest;
+
+/**
+ * @author Feliphe Marinho
+ */
+public interface GroovyScriptUsesFactory {
+
+	public List<GroovyScriptUse> create(ResourceRequest resourceRequest);
+
+}

olafk/liferay-portal-old

modules/apps/portal-security/portal-security-script-management-api/src/main/java/com/liferay/portal/security/script/management/groovy/script/uses/factory/GroovyScriptUsesFactory.java
@@ -1,0 +1,21 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.security.script.management.groovy.script.uses.factory;
+
+import com.liferay.portal.security.script.management.groovy.script.use.GroovyScriptUse;
+
+import java.util.List;
+
+import javax.portlet.ResourceRequest;
+
+/**
+ * @author Feliphe Marinho
+ */
+public interface GroovyScriptUsesFactory {
+
+	public List<GroovyScriptUse> create(ResourceRequest resourceRequest);
+
+}