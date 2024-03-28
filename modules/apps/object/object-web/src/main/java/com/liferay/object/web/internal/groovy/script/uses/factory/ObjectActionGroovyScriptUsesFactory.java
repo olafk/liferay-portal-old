liferay-workflow/liferay-portal

LPD-18756 Create ObjectActionGroovyScriptUsesFactory and ObjectValidationRuleGroovyScriptUsesFactory

modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/groovy/script/use/provider/object/action/ObjectActionGroovyScriptUsesFactory.java
@@ -1,0 +1,63 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.object.web.internal.groovy.script.use.provider.object.action;
+
+import com.liferay.object.constants.ObjectActionExecutorConstants;
+import com.liferay.object.service.ObjectActionLocalService;
+import com.liferay.object.web.internal.groovy.script.use.provider.object.GroovyScriptUseSourceURLFactory;
+import com.liferay.object.web.internal.object.definitions.constants.ObjectDefinitionsScreenNavigationEntryConstants;
+import com.liferay.petra.function.transform.TransformUtil;
+import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.service.CompanyLocalService;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.security.script.management.groovy.script.use.GroovyScriptUse;
+import com.liferay.portal.security.script.management.groovy.script.use.provider.GroovyScriptUsesFactory;
+
+import java.util.List;
+
+import javax.portlet.ResourceRequest;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Feliphe Marinho
+ */
+@Component(service = GroovyScriptUsesFactory.class)
+public class ObjectActionGroovyScriptUsesFactory
+	implements GroovyScriptUsesFactory {
+
+	@Override
+	public List<GroovyScriptUse> create(ResourceRequest resourceRequest) {
+		return TransformUtil.transform(
+			_objectActionLocalService.getObjectActions(
+				true, ObjectActionExecutorConstants.KEY_GROOVY),
+			objectAction -> {
+				Company company = _companyLocalService.getCompany(
+					objectAction.getCompanyId());
+
+				return new GroovyScriptUse(
+					company.getWebId(),
+					objectAction.getLabel(resourceRequest.getLocale()),
+					new GroovyScriptUseSourceURLFactory(
+					).create(
+						company, objectAction.getObjectDefinitionId(), _portal,
+						ObjectDefinitionsScreenNavigationEntryConstants.
+							CATEGORY_KEY_ACTIONS
+					));
+			});
+	}
+
+	@Reference
+	private CompanyLocalService _companyLocalService;
+
+	@Reference
+	private ObjectActionLocalService _objectActionLocalService;
+
+	@Reference
+	private Portal _portal;
+
+}

modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/groovy/script/use/provider/object/validation/ObjectValidationRuleGroovyScriptUsesFactory.java
@@ -1,0 +1,64 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.object.web.internal.groovy.script.use.provider.object.validation;
+
+import com.liferay.object.constants.ObjectValidationRuleConstants;
+import com.liferay.object.service.ObjectValidationRuleLocalService;
+import com.liferay.object.web.internal.groovy.script.use.provider.object.GroovyScriptUseSourceURLFactory;
+import com.liferay.object.web.internal.object.definitions.constants.ObjectDefinitionsScreenNavigationEntryConstants;
+import com.liferay.petra.function.transform.TransformUtil;
+import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.service.CompanyLocalService;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.security.script.management.groovy.script.use.GroovyScriptUse;
+import com.liferay.portal.security.script.management.groovy.script.use.provider.GroovyScriptUsesFactory;
+
+import java.util.List;
+
+import javax.portlet.ResourceRequest;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Feliphe Marinho
+ */
+@Component(service = GroovyScriptUsesFactory.class)
+public class ObjectValidationRuleGroovyScriptUsesFactory
+	implements GroovyScriptUsesFactory {
+
+	@Override
+	public List<GroovyScriptUse> create(ResourceRequest resourceRequest) {
+		return TransformUtil.transform(
+			_objectValidationRuleLocalService.getObjectValidationRules(
+				true, ObjectValidationRuleConstants.ENGINE_TYPE_GROOVY),
+			objectValidationRule -> {
+				Company company = _companyLocalService.getCompany(
+					objectValidationRule.getCompanyId());
+
+				return new GroovyScriptUse(
+					company.getWebId(),
+					objectValidationRule.getName(resourceRequest.getLocale()),
+					new GroovyScriptUseSourceURLFactory(
+					).create(
+						company, objectValidationRule.getObjectDefinitionId(),
+						_portal,
+						ObjectDefinitionsScreenNavigationEntryConstants.
+							CATEGORY_KEY_VALIDATIONS
+					));
+			});
+	}
+
+	@Reference
+	private CompanyLocalService _companyLocalService;
+
+	@Reference
+	private ObjectValidationRuleLocalService _objectValidationRuleLocalService;
+
+	@Reference
+	private Portal _portal;
+
+}

olafk/liferay-portal-old

modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/groovy/script/uses/factory/ObjectActionGroovyScriptUsesFactory.java
@@ -1,0 +1,62 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.object.web.internal.groovy.script.uses.factory;
+
+import com.liferay.object.constants.ObjectActionExecutorConstants;
+import com.liferay.object.service.ObjectActionLocalService;
+import com.liferay.object.web.internal.object.definitions.constants.ObjectDefinitionsScreenNavigationEntryConstants;
+import com.liferay.petra.function.transform.TransformUtil;
+import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.service.CompanyLocalService;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.security.script.management.groovy.script.use.GroovyScriptUse;
+import com.liferay.portal.security.script.management.groovy.script.uses.factory.GroovyScriptUsesFactory;
+
+import java.util.List;
+
+import javax.portlet.ResourceRequest;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Feliphe Marinho
+ */
+@Component(service = GroovyScriptUsesFactory.class)
+public class ObjectActionGroovyScriptUsesFactory
+	implements GroovyScriptUsesFactory {
+
+	@Override
+	public List<GroovyScriptUse> create(ResourceRequest resourceRequest) {
+		return TransformUtil.transform(
+			_objectActionLocalService.getObjectActions(
+				true, ObjectActionExecutorConstants.KEY_GROOVY),
+			objectAction -> {
+				Company company = _companyLocalService.getCompany(
+					objectAction.getCompanyId());
+
+				return new GroovyScriptUse(
+					company.getWebId(),
+					objectAction.getLabel(resourceRequest.getLocale()),
+					new GroovyScriptUseSourceURLFactory(
+					).create(
+						company, objectAction.getObjectDefinitionId(), _portal,
+						ObjectDefinitionsScreenNavigationEntryConstants.
+							CATEGORY_KEY_ACTIONS
+					));
+			});
+	}
+
+	@Reference
+	private CompanyLocalService _companyLocalService;
+
+	@Reference
+	private ObjectActionLocalService _objectActionLocalService;
+
+	@Reference
+	private Portal _portal;
+
+}

modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/groovy/script/uses/factory/ObjectValidationRuleGroovyScriptUsesFactory.java
@@ -1,0 +1,63 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.object.web.internal.groovy.script.uses.factory;
+
+import com.liferay.object.constants.ObjectValidationRuleConstants;
+import com.liferay.object.service.ObjectValidationRuleLocalService;
+import com.liferay.object.web.internal.object.definitions.constants.ObjectDefinitionsScreenNavigationEntryConstants;
+import com.liferay.petra.function.transform.TransformUtil;
+import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.service.CompanyLocalService;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.security.script.management.groovy.script.use.GroovyScriptUse;
+import com.liferay.portal.security.script.management.groovy.script.uses.factory.GroovyScriptUsesFactory;
+
+import java.util.List;
+
+import javax.portlet.ResourceRequest;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Feliphe Marinho
+ */
+@Component(service = GroovyScriptUsesFactory.class)
+public class ObjectValidationRuleGroovyScriptUsesFactory
+	implements GroovyScriptUsesFactory {
+
+	@Override
+	public List<GroovyScriptUse> create(ResourceRequest resourceRequest) {
+		return TransformUtil.transform(
+			_objectValidationRuleLocalService.getObjectValidationRules(
+				true, ObjectValidationRuleConstants.ENGINE_TYPE_GROOVY),
+			objectValidationRule -> {
+				Company company = _companyLocalService.getCompany(
+					objectValidationRule.getCompanyId());
+
+				return new GroovyScriptUse(
+					company.getWebId(),
+					objectValidationRule.getName(resourceRequest.getLocale()),
+					new GroovyScriptUseSourceURLFactory(
+					).create(
+						company, objectValidationRule.getObjectDefinitionId(),
+						_portal,
+						ObjectDefinitionsScreenNavigationEntryConstants.
+							CATEGORY_KEY_VALIDATIONS
+					));
+			});
+	}
+
+	@Reference
+	private CompanyLocalService _companyLocalService;
+
+	@Reference
+	private ObjectValidationRuleLocalService _objectValidationRuleLocalService;
+
+	@Reference
+	private Portal _portal;
+
+}