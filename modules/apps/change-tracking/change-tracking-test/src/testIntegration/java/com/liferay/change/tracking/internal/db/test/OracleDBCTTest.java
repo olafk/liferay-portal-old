fbf95c2b38129e633344725969d2a969f565dd51
David Truong <david.truong@liferay.com>
2024-04-03 21:43:19 -0500

    LPD-20798 rename test to OracleDBCTTest

--- modules/apps/change-tracking/change-tracking-test/src/testIntegration/java/com/liferay/change/tracking/internal/db/test/OracleDBCTTest.java
+++ modules/apps/change-tracking/change-tracking-test/src/testIntegration/java/com/liferay/change/tracking/internal/db/test/OracleDBCTTest.java
@@ -1,9 +1,9 @@
 /**
- * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.change.tracking.internal.test;
+package com.liferay.change.tracking.internal.db.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.change.tracking.model.CTCollection;
@@ -38,7 +38,7 @@ import org.junit.runner.RunWith;
  * @author Preston Crary
  */
 @RunWith(Arquillian.class)
-public class LargeDatasetCTTest {
+public class OracleDBCTTest {
 
 	@ClassRule
 	@Rule
