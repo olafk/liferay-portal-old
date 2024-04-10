97a6dd44ce4eea8660d30e4ce4fa71f3d036ed5a
Pedro Oliveira <pedro.oliveira@liferay.com>
2024-04-10 16:58:52 -0300

    ISSD-3026 Update account roles on Customer Portal

--- modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/common/utils/getProjectRoles.js
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/common/utils/getProjectRoles.js
@@ -31,9 +31,19 @@ export function getRolesFiltered(items, project) {
 				}
 
 				if (!isProjectPartner) {
-					isValidRole =
-						role.name !== ROLE_TYPES.partnerManager.key &&
-						role.name !== ROLE_TYPES.partnerMember.key;
+					const partnerRoles = [
+						ROLE_TYPES.partnerManager.key,
+						ROLE_TYPES.partnerMarketingUser.key,
+						ROLE_TYPES.partnerMember.key,
+						ROLE_TYPES.partnerSalesUser.key,
+						ROLE_TYPES.partnerTechnicalUser.key,
+					];
+
+					isValidRole = !partnerRoles.includes(role.name);
+				}
+
+				if (role.name === ROLE_TYPES.partnerMember.key) {
+					isValidRole = false;
 				}
 
 				if (isValidRole) {
