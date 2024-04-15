liferay-bpm/liferay-portal

LPD-19635 Add listTypeEntry if not exist in defaultValue

modules/apps/object/object-admin-rest-impl/src/main/java/com/liferay/object/admin/rest/internal/dto/v1_0/util/ObjectFieldUtil.java
@@ -79,22 +79,11 @@ public class ObjectFieldUtil {
 					userId, objectField.getSystem());
 		}
 
-		ObjectFieldSetting[] objectFieldSettings = ArrayUtil.filter(
+		ObjectFieldSetting[] stateFlowObjectFieldSettings = ArrayUtil.filter(
 			objectField.getObjectFieldSettings(),
 			objectFieldSetting -> StringUtil.equals(
 				objectFieldSetting.getName(),
 				ObjectFieldSettingConstants.NAME_STATE_FLOW));
-
-		if (ArrayUtil.isEmpty(objectFieldSettings)) {
-			return listTypeDefinition.getListTypeDefinitionId();
-		}
-
-		JSONObject jsonObject = JSONFactoryUtil.createJSONObject(
-			JSONFactoryUtil.looseSerializeDeep(
-				objectFieldSettings[0].getValue()));
-
-		JSONArray objectStatesJSONArray = jsonObject.getJSONArray(
-			"objectStates");
 
 		Map<String, ListTypeEntry> listTypeEntries = new HashMap<>();
 
@@ -104,22 +93,59 @@ public class ObjectFieldUtil {
 			listTypeEntry -> listTypeEntries.put(
 				listTypeEntry.getKey(), listTypeEntry));
 
-		for (int i = 0; i < objectStatesJSONArray.length(); i++) {
-			JSONObject objectStateJSONObject =
-				objectStatesJSONArray.getJSONObject(i);
+		if (!ArrayUtil.isEmpty(stateFlowObjectFieldSettings)) {
+			JSONObject jsonObject = JSONFactoryUtil.createJSONObject(
+				JSONFactoryUtil.looseSerializeDeep(
+					stateFlowObjectFieldSettings[0].getValue()));
 
-			String key = objectStateJSONObject.getString("key");
+			JSONArray objectStatesJSONArray = jsonObject.getJSONArray(
+				"objectStates");
 
-			if (listTypeEntries.containsKey(key)) {
-				listTypeEntries.remove(key);
+			for (int i = 0; i < objectStatesJSONArray.length(); i++) {
+				JSONObject objectStateJSONObject =
+					objectStatesJSONArray.getJSONObject(i);
 
-				continue;
+				String key = objectStateJSONObject.getString("key");
+
+				if (listTypeEntries.containsKey(key)) {
+					listTypeEntries.remove(key);
+
+					continue;
+				}
+
+				listTypeEntryLocalService.addListTypeEntry(
+					null, userId, listTypeDefinition.getListTypeDefinitionId(),
+					key,
+					Collections.singletonMap(LocaleUtil.getDefault(), key));
 			}
-
-			listTypeEntryLocalService.addListTypeEntry(
-				null, userId, listTypeDefinition.getListTypeDefinitionId(), key,
-				Collections.singletonMap(LocaleUtil.getDefault(), key));
 		}
+
+		ObjectFieldSetting[] defaultValueObjectFieldSettings = null;
+
+		if (!objectField.getState()) {
+			defaultValueObjectFieldSettings = ArrayUtil.filter(
+				objectField.getObjectFieldSettings(),
+				objectFieldSetting -> StringUtil.equals(
+					objectFieldSetting.getName(),
+					ObjectFieldSettingConstants.NAME_DEFAULT_VALUE));
+		}
+
+		if (ArrayUtil.isEmpty(defaultValueObjectFieldSettings)) {
+			return listTypeDefinition.getListTypeDefinitionId();
+		}
+
+		String defaultValueObjectFieldSetting = GetterUtil.getString(
+			defaultValueObjectFieldSettings[0].getValue());
+
+		if (listTypeEntries.containsKey(defaultValueObjectFieldSetting)) {
+			listTypeEntries.remove(defaultValueObjectFieldSetting);
+		}
+
+		listTypeEntryLocalService.addListTypeEntry(
+			null, userId, listTypeDefinition.getListTypeDefinitionId(),
+			defaultValueObjectFieldSetting,
+			Collections.singletonMap(
+				LocaleUtil.getDefault(), defaultValueObjectFieldSetting));
 
 		for (ListTypeEntry listTypeEntry : listTypeEntries.values()) {
 			listTypeEntryLocalService.deleteListTypeEntry(listTypeEntry);

olafk/liferay-portal-old

modules/apps/object/object-admin-rest-impl/src/main/java/com/liferay/object/admin/rest/internal/dto/v1_0/util/ObjectFieldUtil.java
@@ -81,22 +81,11 @@ public class ObjectFieldUtil {
 					userId, objectField.getSystem());
 		}
 
-		ObjectFieldSetting[] objectFieldSettings = ArrayUtil.filter(
+		ObjectFieldSetting[] stateFlowObjectFieldSettings = ArrayUtil.filter(
 			objectField.getObjectFieldSettings(),
 			objectFieldSetting -> StringUtil.equals(
 				objectFieldSetting.getName(),
 				ObjectFieldSettingConstants.NAME_STATE_FLOW));
-
-		if (ArrayUtil.isEmpty(objectFieldSettings)) {
-			return listTypeDefinition.getListTypeDefinitionId();
-		}
-
-		JSONObject jsonObject = JSONFactoryUtil.createJSONObject(
-			JSONFactoryUtil.looseSerializeDeep(
-				objectFieldSettings[0].getValue()));
-
-		JSONArray objectStatesJSONArray = jsonObject.getJSONArray(
-			"objectStates");
 
 		Map<String, ListTypeEntry> listTypeEntries = new HashMap<>();
 
@@ -106,22 +95,59 @@ public class ObjectFieldUtil {
 			listTypeEntry -> listTypeEntries.put(
 				listTypeEntry.getKey(), listTypeEntry));
 
-		for (int i = 0; i < objectStatesJSONArray.length(); i++) {
-			JSONObject objectStateJSONObject =
-				objectStatesJSONArray.getJSONObject(i);
+		if (!ArrayUtil.isEmpty(stateFlowObjectFieldSettings)) {
+			JSONObject jsonObject = JSONFactoryUtil.createJSONObject(
+				JSONFactoryUtil.looseSerializeDeep(
+					stateFlowObjectFieldSettings[0].getValue()));
 
-			String key = objectStateJSONObject.getString("key");
+			JSONArray objectStatesJSONArray = jsonObject.getJSONArray(
+				"objectStates");
 
-			if (listTypeEntries.containsKey(key)) {
-				listTypeEntries.remove(key);
+			for (int i = 0; i < objectStatesJSONArray.length(); i++) {
+				JSONObject objectStateJSONObject =
+					objectStatesJSONArray.getJSONObject(i);
 
-				continue;
+				String key = objectStateJSONObject.getString("key");
+
+				if (listTypeEntries.containsKey(key)) {
+					listTypeEntries.remove(key);
+
+					continue;
+				}
+
+				listTypeEntryLocalService.addListTypeEntry(
+					null, userId, listTypeDefinition.getListTypeDefinitionId(),
+					key,
+					Collections.singletonMap(LocaleUtil.getDefault(), key));
 			}
-
-			listTypeEntryLocalService.addListTypeEntry(
-				null, userId, listTypeDefinition.getListTypeDefinitionId(), key,
-				Collections.singletonMap(LocaleUtil.getDefault(), key));
 		}
+
+		ObjectFieldSetting[] defaultObjectFieldSettings = null;
+
+		if (!objectField.getState()) {
+			defaultObjectFieldSettings = ArrayUtil.filter(
+				objectField.getObjectFieldSettings(),
+				objectFieldSetting -> StringUtil.equals(
+					objectFieldSetting.getName(),
+					ObjectFieldSettingConstants.NAME_DEFAULT_VALUE));
+		}
+
+		if (ArrayUtil.isEmpty(defaultObjectFieldSettings)) {
+			return listTypeDefinition.getListTypeDefinitionId();
+		}
+
+		String defaultObjectFieldSettingValue = GetterUtil.getString(
+			defaultObjectFieldSettings[0].getValue());
+
+		if (listTypeEntries.containsKey(defaultObjectFieldSettingValue)) {
+			listTypeEntries.remove(defaultObjectFieldSettingValue);
+		}
+
+		listTypeEntryLocalService.addListTypeEntry(
+			null, userId, listTypeDefinition.getListTypeDefinitionId(),
+			defaultObjectFieldSettingValue,
+			Collections.singletonMap(
+				LocaleUtil.getDefault(), defaultObjectFieldSettingValue));
 
 		for (ListTypeEntry listTypeEntry : listTypeEntries.values()) {
 			listTypeEntryLocalService.deleteListTypeEntry(listTypeEntry);