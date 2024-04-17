liferay-customer-solutions/liferay-portal

ISSD-3168 Refactor usePagination function

modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-partner-portal/src/main/resources/site-initializer/fragments/group/src/partner-portal/fragments/back-button/index.js
@@ -9,16 +9,14 @@ if (backButton) {
 	const siteURL = Liferay.ThemeDisplay.getPortalURL().split('/l/')[0];
 
 	backButton.onclick = () => {
-		const urlParams = new URLSearchParams(window.location.href);
+		const urlParams = new URLSearchParams(window.location.href.split('?')[1]);
 
 		if (urlParams.has('returnurl')) {
 			const returnUrl = urlParams.get('returnurl');
 
-			if (urlParams.has('activepage')) {
-				location.assign(`${siteURL}${returnUrl}?&activepage=${urlParams.get('activepage')}`);
-			} else {
-				location.assign(`${siteURL}${returnUrl}`);
-			}
+			urlParams.delete('returnurl');
+
+			location.assign(`${siteURL}${returnUrl}?${urlParams.toString()}`);
 		}
 		else {
 			history.back();

olafk/liferay-portal-old

modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-partner-portal/src/main/resources/site-initializer/fragments/group/src/partner-portal/fragments/back-button/index.js
@@ -10,12 +10,14 @@ if (backButton) {
 	const siteURL = Liferay.ThemeDisplay.getPortalURL().split('/l/')[0];
 
 	backButton.onclick = () => {
-		const urlParams = new URLSearchParams(window.location.href);
+		const urlParams = new URLSearchParams(window.location.href.split('?')[1]);
 
 		if (urlParams.has('p_l_back_url')) {
 			const backURL = urlParams.get('p_l_back_url');
 
-			location.assign(`${siteURL}${decodeURIComponent(backURL)}`);
+			urlParams.delete('p_l_back_url');
+
+			location.assign(`${siteURL}${decodeURIComponent(`${backURL}?${urlParams.toString()}`)}`);
 		}
 		else {
 			location.assign(decodeURIComponent(configuration.backURL));