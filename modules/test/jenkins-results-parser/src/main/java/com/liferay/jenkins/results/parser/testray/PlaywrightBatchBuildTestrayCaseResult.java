534881a3fdcdd93e98c42ca005535962da05850c
CalumR23 <searagan@gmail.com>
2024-04-26 08:43:44 -0700

    LRCI-4200 Extend junit class

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/testray/PlaywrightBatchBuildTestrayCaseResult.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/testray/PlaywrightBatchBuildTestrayCaseResult.java
@@ -5,9 +5,7 @@
 
 package com.liferay.jenkins.results.parser.testray;
 
-import com.liferay.jenkins.results.parser.Build;
 import com.liferay.jenkins.results.parser.JenkinsResultsParserUtil;
-import com.liferay.jenkins.results.parser.TestResult;
 import com.liferay.jenkins.results.parser.TopLevelBuild;
 import com.liferay.jenkins.results.parser.test.clazz.PlaywrightTestClass;
 import com.liferay.jenkins.results.parser.test.clazz.TestClass;
@@ -15,33 +13,30 @@ import com.liferay.jenkins.results.parser.test.clazz.group.AxisTestClassGroup;
 
 import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
 
 /**
  * @author Kenji Heigel
  */
 public class PlaywrightBatchBuildTestrayCaseResult
-	extends BatchBuildTestrayCaseResult {
+	extends JUnitBatchBuildTestrayCaseResult {
 
 	public PlaywrightBatchBuildTestrayCaseResult(
 		TestrayBuild testrayBuild, TopLevelBuild topLevelBuild,
 		AxisTestClassGroup axisTestClassGroup, TestClass testClass) {
 
-		super(testrayBuild, topLevelBuild, axisTestClassGroup);
+		super(testrayBuild, topLevelBuild, axisTestClassGroup, testClass);
 
-		_testClass = testClass;
+		_playwrightTestClass = (PlaywrightTestClass)testClass;
 	}
 
 	@Override
 	public String getComponentName() {
-		PlaywrightTestClass playwrightTestClass = _getPlaywrightTestClass();
-
-		if (playwrightTestClass == null) {
+		if (_playwrightTestClass == null) {
 			return super.getComponentName();
 		}
 
 		String testrayMainComponentName =
-			playwrightTestClass.getTestrayMainComponentName();
+			_playwrightTestClass.getTestrayMainComponentName();
 
 		if (JenkinsResultsParserUtil.isNullOrEmpty(testrayMainComponentName)) {
 			return super.getComponentName();
@@ -51,44 +46,12 @@ public class PlaywrightBatchBuildTestrayCaseResult
 	}
 
 	@Override
-	public String getErrors() {
-		TestResult testResult = _getTestResult();
-
-		if (testResult == null) {
-			return super.getErrors();
-		}
-
-		if (!testResult.isFailing()) {
-			return null;
-		}
-
-		return testResult.getErrorStackTrace();
-	}
-
-	@Override
 	public String getName() {
-		PlaywrightTestClass playwrightTestClass = _getPlaywrightTestClass();
-
-		if (playwrightTestClass == null) {
+		if (_playwrightTestClass == null) {
 			return super.getName();
 		}
 
-		return playwrightTestClass.getSpecFilePath();
-	}
-
-	@Override
-	public Status getStatus() {
-		TestResult testResult = _getTestResult();
-
-		if (testResult == null) {
-			return Status.UNTESTED;
-		}
-
-		if (testResult.isFailing()) {
-			return Status.FAILED;
-		}
-
-		return Status.PASSED;
+		return _playwrightTestClass.getSpecFilePath();
 	}
 
 	@Override
@@ -112,41 +75,6 @@ public class PlaywrightBatchBuildTestrayCaseResult
 			getAxisBuildURLPath() + "/playwright-report/index.html");
 	}
 
-	private PlaywrightTestClass _getPlaywrightTestClass() {
-		if (!(_testClass instanceof PlaywrightTestClass)) {
-			return null;
-		}
-
-		return (PlaywrightTestClass)_testClass;
-	}
-
-	private TestResult _getTestResult() {
-		Build build = getBuild();
-
-		if (build == null) {
-			return null;
-		}
-
-		PlaywrightTestClass playwrightTestClass = _getPlaywrightTestClass();
-
-		if (playwrightTestClass == null) {
-			return null;
-		}
-
-		String specFilePath = playwrightTestClass.getSpecFilePath();
-		String specTitle = playwrightTestClass.getSpecTitle();
-
-		for (TestResult testResult : build.getTestResults()) {
-			if (Objects.equals(specFilePath, testResult.getClassName()) &&
-				Objects.equals(specTitle, testResult.getTestName())) {
-
-				return testResult;
-			}
-		}
-
-		return null;
-	}
-
-	private final TestClass _testClass;
+	private final PlaywrightTestClass _playwrightTestClass;
 
 }
