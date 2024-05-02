96e0d237135c4eb6508070ddeef753ef9fcefcb7
Julius Lee <meng-huan.lee@liferay.com>
2024-05-02 13:52:37 -0700

    LPD-24836 Move JSONWebService interfaces and exception into new module portal-remote-json-web-service-api
    # breaking
    
    ## What portal-kernel/src/com/liferay/portal/kernel/jsonwebservice/JSONWebServiceAction.java
    
    JSONWebServiceAction is moved to portal-remote-json-web-service-api
    
    ## Why
    
    portal has no more usage of JSONWebServiceAction. Moving to modules simplifies portal structure.
    
    ## Alternatives
    
    All public methods remain available. Add new dependency to portal-remote-json-web-service-api and change import statements accordingly.
    
    ----
    
    # breaking
    
    ## What portal-kernel/src/com/liferay/portal/kernel/jsonwebservice/JSONWebServiceActionMapping.java
    
    JSONWebServiceActionMapping is moved to portal-remote-json-web-service-api
    
    ## Why
    
    portal has no more usage of JSONWebServiceActionMapping. Moving to modules simplifies portal structure.
    
    ## Alternatives
    
    All public methods remain available. Add new dependency to portal-remote-json-web-service-api and change import statements accordingly.
    
    ----
    
    # breaking
    
    ## What portal-kernel/src/com/liferay/portal/kernel/jsonwebservice/JSONWebServiceActionsManager.java
    
    JSONWebServiceActionsManager is moved to portal-remote-json-web-service-api
    
    ## Why
    
    portal has no more usage of JSONWebServiceActionsManager. Moving to modules simplifies portal structure.
    
    ## Alternatives
    
    All public methods remain available. Add new dependency to portal-remote-json-web-service-api and change import statements accordingly.
    
    ----
    
    # breaking
    
    ## What portal-kernel/src/com/liferay/portal/kernel/jsonwebservice/NoSuchJSONWebServiceException.java
    
    NoSuchJSONWebServiceException is moved to portal-remote-json-web-service-api
    
    ## Why
    
    portal has no more usage of NoSuchJSONWebServiceException. Moving to modules simplifies portal structure.
    
    ## Alternatives
    
    All public methods remain available. Add new dependency to portal-remote-json-web-service-api and change import statements accordingly.
    
    ----

--- modules/apps/portal-remote/portal-remote-json-web-service-api/src/main/java/com/liferay/portal/remote/json/web/service/JSONWebServiceActionsManager.java
+++ modules/apps/portal-remote/portal-remote-json-web-service-api/src/main/java/com/liferay/portal/remote/json/web/service/JSONWebServiceActionsManager.java
@@ -3,7 +3,9 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portal.kernel.jsonwebservice;
+package com.liferay.portal.remote.json.web.service;
+
+import com.liferay.portal.remote.json.web.service.exception.NoSuchJSONWebServiceException;
 
 import java.lang.reflect.Method;
 
