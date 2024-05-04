brianchandotcom/liferay-portal

LPD-21645 Default permissions web module

modules/apps/portal/portal-default-permissions-web/src/main/resources/META-INF/resources/init.jsp
@@ -1,0 +1,53 @@
+<%--
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+--%>
+
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+
+<%@ taglib uri="http://java.sun.com/portlet_2_0" prefix="portlet" %>
+
+<%@ taglib uri="http://liferay.com/tld/aui" prefix="aui" %><%@
+taglib uri="http://liferay.com/tld/clay" prefix="clay" %><%@
+taglib uri="http://liferay.com/tld/frontend" prefix="liferay-frontend" %><%@
+taglib uri="http://liferay.com/tld/react" prefix="react" %><%@
+taglib uri="http://liferay.com/tld/theme" prefix="liferay-theme" %><%@
+taglib uri="http://liferay.com/tld/ui" prefix="liferay-ui" %><%@
+taglib uri="http://liferay.com/tld/util" prefix="liferay-util" %>
+
+<%@ page import="com.liferay.petra.string.StringBundler" %><%@
+page import="com.liferay.petra.string.StringPool" %><%@
+page import="com.liferay.portal.defaultpermissions.configuration.PortalDefaultPermissionsCompanyConfiguration" %><%@
+page import="com.liferay.portal.defaultpermissions.web.internal.constants.PortalDefaultPermissionsWebKeys" %><%@
+page import="com.liferay.portal.defaultpermissions.web.internal.display.context.EditPortalConfigurationPermissionsDisplayContext" %><%@
+page import="com.liferay.portal.defaultpermissions.web.internal.display.context.PortalDefaultPermissionsCompanyConfigurationDisplayContext" %><%@
+page import="com.liferay.portal.defaultpermissions.web.internal.display.context.PortalDefaultPermissionsManagementToolbarDisplayContext" %><%@
+page import="com.liferay.portal.defaultpermissions.web.internal.portlet.action.EditPortalConfigurationPermissionsMVCActionCommand" %><%@
+page import="com.liferay.portal.defaultpermissions.web.internal.search.PortalDefaultPermissionsSearchEntry" %><%@
+page import="com.liferay.portal.kernel.dao.search.SearchContainer" %><%@
+page import="com.liferay.portal.kernel.language.LanguageUtil" %><%@
+page import="com.liferay.portal.kernel.model.Role" %><%@
+page import="com.liferay.portal.kernel.model.role.RoleConstants" %><%@
+page import="com.liferay.portal.kernel.security.permission.ActionKeys" %><%@
+page import="com.liferay.portal.kernel.util.FriendlyURLNormalizerUtil" %><%@
+page import="com.liferay.portal.kernel.util.HashMapBuilder" %><%@
+page import="com.liferay.portal.kernel.util.PortalUtil" %><%@
+page import="com.liferay.portal.kernel.util.WebKeys" %><%@
+page import="com.liferay.roles.admin.constants.RolesAdminWebKeys" %><%@
+page import="com.liferay.roles.admin.role.type.contributor.RoleTypeContributor" %><%@
+page import="com.liferay.roles.admin.role.type.contributor.provider.RoleTypeContributorProvider" %><%@
+page import="com.liferay.taglib.search.ResultRow" %>
+
+<%@ page import="java.util.List" %>
+
+<liferay-frontend:defineObjects />
+
+<liferay-theme:defineObjects />
+
+<portlet:defineObjects />
+
+<%
+PortalDefaultPermissionsCompanyConfigurationDisplayContext portalDefaultPermissionsCompanyConfigurationDisplayContext = (PortalDefaultPermissionsCompanyConfigurationDisplayContext)request.getAttribute(PortalDefaultPermissionsCompanyConfigurationDisplayContext.class.getName());
+%>

modules/apps/portal/portal-default-permissions-web/src/main/resources/META-INF/resources/js/index.js
@@ -1,0 +1,7 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+export {default as editPortalConfigurationPermissions} from './editPortalConfigurationPermissions';
+export {default as portalDefaultPermissionsCompanyConfiguration} from './portalDefaultPermissionsCompanyConfiguration';

olafk/liferay-portal-old

modules/apps/portal/portal-default-permissions-web/src/main/resources/META-INF/resources/init.jsp
@@ -1,0 +1,53 @@
+<%--
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+--%>
+
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+
+<%@ taglib uri="http://java.sun.com/portlet_2_0" prefix="portlet" %>
+
+<%@ taglib uri="http://liferay.com/tld/aui" prefix="aui" %><%@
+taglib uri="http://liferay.com/tld/clay" prefix="clay" %><%@
+taglib uri="http://liferay.com/tld/frontend" prefix="liferay-frontend" %><%@
+taglib uri="http://liferay.com/tld/react" prefix="react" %><%@
+taglib uri="http://liferay.com/tld/theme" prefix="liferay-theme" %><%@
+taglib uri="http://liferay.com/tld/ui" prefix="liferay-ui" %><%@
+taglib uri="http://liferay.com/tld/util" prefix="liferay-util" %>
+
+<%@ page import="com.liferay.petra.string.StringBundler" %><%@
+page import="com.liferay.petra.string.StringPool" %><%@
+page import="com.liferay.portal.defaultpermissions.configuration.manager.PortalDefaultPermissionsConfigurationManager" %><%@
+page import="com.liferay.portal.defaultpermissions.web.internal.constants.PortalDefaultPermissionsWebKeys" %><%@
+page import="com.liferay.portal.defaultpermissions.web.internal.display.context.EditPortalDefaultPermissionsConfigurationDisplayContext" %><%@
+page import="com.liferay.portal.defaultpermissions.web.internal.display.context.PortalDefaultPermissionsManagementToolbarDisplayContext" %><%@
+page import="com.liferay.portal.defaultpermissions.web.internal.display.context.ViewCompanyPortalDefaultPermissionsConfigurationDisplayContext" %><%@
+page import="com.liferay.portal.defaultpermissions.web.internal.portlet.action.EditPortalDefaultPermissionsConfigurationMVCActionCommand" %><%@
+page import="com.liferay.portal.defaultpermissions.web.internal.search.PortalDefaultPermissionsSearchEntry" %><%@
+page import="com.liferay.portal.kernel.dao.search.SearchContainer" %><%@
+page import="com.liferay.portal.kernel.language.LanguageUtil" %><%@
+page import="com.liferay.portal.kernel.model.Role" %><%@
+page import="com.liferay.portal.kernel.model.role.RoleConstants" %><%@
+page import="com.liferay.portal.kernel.security.permission.ActionKeys" %><%@
+page import="com.liferay.portal.kernel.util.FriendlyURLNormalizerUtil" %><%@
+page import="com.liferay.portal.kernel.util.HashMapBuilder" %><%@
+page import="com.liferay.portal.kernel.util.PortalUtil" %><%@
+page import="com.liferay.portal.kernel.util.WebKeys" %><%@
+page import="com.liferay.roles.admin.constants.RolesAdminWebKeys" %><%@
+page import="com.liferay.roles.admin.role.type.contributor.RoleTypeContributor" %><%@
+page import="com.liferay.roles.admin.role.type.contributor.provider.RoleTypeContributorProvider" %><%@
+page import="com.liferay.taglib.search.ResultRow" %>
+
+<%@ page import="java.util.List" %>
+
+<liferay-frontend:defineObjects />
+
+<liferay-theme:defineObjects />
+
+<portlet:defineObjects />
+
+<%
+ViewCompanyPortalDefaultPermissionsConfigurationDisplayContext viewCompanyPortalDefaultPermissionsConfigurationDisplayContext = (ViewCompanyPortalDefaultPermissionsConfigurationDisplayContext)request.getAttribute(WebKeys.PORTLET_DISPLAY_CONTEXT);
+%>

modules/apps/portal/portal-default-permissions-web/src/main/resources/META-INF/resources/js/index.js
@@ -1,0 +1,7 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+export {default as editPortalDefaultPermissionsConfiguration} from './editPortalDefaultPermissionsConfiguration';
+export {default as viewCompanyPortalDefaultPermissionsConfiguration} from './viewCompanyPortalDefaultPermissionsConfiguration';