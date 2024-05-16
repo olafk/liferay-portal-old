fab141830d6b765d80ed3f4eca36e0fbc11f23ea
gustavolimav <gustavo.lima@liferay.com>
2024-05-16 14:43:37 -0300

    LPD-7822 LPD-8351 portal-search-spi: Move contributors to new package
    https://liferay.atlassian.net/browse/LPD-8351
    
    # breaking
    
    ## What modules/apps/portal-search/portal-search-spi/src/main/java/com/liferay/portal/search/spi/model/permission/SearchPermissionFilterContributor.java
    
    modules/apps/portal-search/portal-search-spi/src/main/java/com/liferay/portal/search/spi/model/permission/SearchPermissionFilterContributor.java was moved to
    modules/apps/portal-search/portal-search-spi/src/main/java/com/liferay/portal/search/spi/model/permission/contributor/SearchPermissionFilterContributor.java
    
    ## Why
    
    Most contributors are located in contributor packages
    
    ----

--- modules/apps/portal-search/portal-search-spi/src/main/java/com/liferay/portal/search/spi/model/permission/contributor/SearchPermissionFilterContributor.java
+++ modules/apps/portal-search/portal-search-spi/src/main/java/com/liferay/portal/search/spi/model/permission/contributor/SearchPermissionFilterContributor.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portal.search.spi.model.permission;
+package com.liferay.portal.search.spi.model.permission.contributor;
 
 import com.liferay.portal.kernel.search.filter.BooleanFilter;
 import com.liferay.portal.kernel.security.permission.PermissionChecker;
