9e4bc45308ffeaea9dc6cba8f16ee3e4da9eab91
Michael Hashimoto <michael.hashimoto@liferay.com>
2024-06-03 13:08:49 -0700

    LRCI-4256 Rely on the FailureType stored in TestrayCaseResults

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/GenerateTestrayCSVUtil.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/GenerateTestrayCSVUtil.java
@@ -16,13 +16,8 @@ import java.io.IOException;
 import java.net.URL;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import org.apache.commons.lang3.StringUtils;
 
 /**
  * @author Charlotte Wong
@@ -87,11 +82,15 @@ public class GenerateTestrayCSVUtil {
 			return;
 		}
 
-		sb.append(_generate(testrayCaseResults, Type.UNIQUE));
+		sb.append(
+			_generate(testrayCaseResults, TestrayCaseResult.ErrorType.UNIQUE));
 		sb.append("\n");
-		sb.append(_generate(testrayCaseResults, Type.DID_NOT_RUN));
+		sb.append(
+			_generate(
+				testrayCaseResults, TestrayCaseResult.ErrorType.DID_NOT_RUN));
 		sb.append("\n");
-		sb.append(_generate(testrayCaseResults, Type.COMMON));
+		sb.append(
+			_generate(testrayCaseResults, TestrayCaseResult.ErrorType.COMMON));
 
 		try {
 			System.out.println("Setting testray results to: " + sb.toString());
@@ -115,43 +114,29 @@ public class GenerateTestrayCSVUtil {
 		generate(projectBuildDir, Long.valueOf(projectTestrayBuildId), null);
 	}
 
-	public enum Type {
-
-		COMMON("Common"), DID_NOT_RUN("Did not run"), UNIQUE("Unique");
-
-		@Override
-		public String toString() {
-			return _description;
-		}
-
-		private Type(String description) {
-			_description = description;
-		}
-
-		private final String _description;
-
-	}
-
 	private static String _cleanCSVData(String string) {
 		return string.replace(_CSV_DELIMITER, ".");
 	}
 
 	private static String _generate(
 		List<TestrayCaseResult> testrayCaseResults,
-		Type testrayCaseResultType) {
+		TestrayCaseResult.ErrorType testrayCaseResultErrorType) {
 
 		System.out.println(
 			"Parsing Testray case results for " +
-				testrayCaseResultType.toString() + ".");
+				testrayCaseResultErrorType.toString() + ".");
 
 		StringBuilder sb = new StringBuilder();
 
 		for (TestrayCaseResult testrayCaseResult : testrayCaseResults) {
-			if (_getType(testrayCaseResult) != testrayCaseResultType) {
+			if (testrayCaseResult.getErrorType() !=
+					testrayCaseResultErrorType) {
+
 				continue;
 			}
 
-			sb.append(_generateCSV(testrayCaseResult, testrayCaseResultType));
+			sb.append(
+				_generateCSV(testrayCaseResult, testrayCaseResultErrorType));
 			sb.append("\n");
 		}
 
@@ -159,16 +144,18 @@ public class GenerateTestrayCSVUtil {
 			sb.append(
 				JenkinsResultsParserUtil.join(
 					_CSV_DELIMITER, "NONE", "N/A",
-					testrayCaseResultType.toString(), "N/A"));
+					testrayCaseResultErrorType.toString(), "N/A"));
 			sb.append("\n");
 		}
 
 		return JenkinsResultsParserUtil.combine(
-			testrayCaseResultType.toString(), " Failures\n", sb.toString());
+			testrayCaseResultErrorType.toString(), " Failures\n",
+			sb.toString());
 	}
 
 	private static String _generateCSV(
-		TestrayCaseResult testrayCaseResult, Type testrayCaseResultType) {
+		TestrayCaseResult testrayCaseResult,
+		TestrayCaseResult.ErrorType testrayCaseResultType) {
 
 		return JenkinsResultsParserUtil.join(
 			_CSV_DELIMITER, _cleanCSVData(testrayCaseResult.getName()),
@@ -177,71 +164,6 @@ public class GenerateTestrayCSVUtil {
 			_cleanCSVData(testrayCaseResult.getErrors()));
 	}
 
-	private static Type _getType(TestrayCaseResult testrayCaseResult) {
-		for (String didNotRunErrorMessage : _didNotRunErrorMessages) {
-			Pattern pattern = Pattern.compile(didNotRunErrorMessage);
-
-			Matcher matcher = pattern.matcher(testrayCaseResult.getErrors());
-
-			if (matcher.find()) {
-				return Type.DID_NOT_RUN;
-			}
-		}
-
-		for (TestrayCaseResult previousTestrayCaseResult :
-				testrayCaseResult.getTestrayCaseResultHistory(25)) {
-
-			if (Objects.equals(
-					testrayCaseResult.getID(),
-					previousTestrayCaseResult.getID())) {
-
-				continue;
-			}
-
-			if (_isSimilarError(testrayCaseResult, previousTestrayCaseResult) &&
-				!Objects.equals(
-					testrayCaseResult.getPullRequestSenderUsername(),
-					previousTestrayCaseResult.getPullRequestSenderUsername())) {
-
-				return Type.COMMON;
-			}
-		}
-
-		return Type.UNIQUE;
-	}
-
-	private static boolean _isSimilarError(
-		TestrayCaseResult testrayCaseResult1,
-		TestrayCaseResult testrayCaseResult2) {
-
-		String testrayCaseResultErrors1 = testrayCaseResult1.getErrors();
-		String testrayCaseResultErrors2 = testrayCaseResult2.getErrors();
-
-		try {
-			Double distance = StringUtils.getJaroWinklerDistance(
-				testrayCaseResultErrors1, testrayCaseResultErrors2);
-
-			if (distance > _MAX_DISTANCE) {
-				return true;
-			}
-
-			return false;
-		}
-		catch (IllegalArgumentException illegalArgumentException) {
-			if (testrayCaseResultErrors1 == testrayCaseResultErrors2) {
-				return true;
-			}
-
-			return false;
-		}
-	}
-
 	private static final String _CSV_DELIMITER = ",";
-
-	private static final double _MAX_DISTANCE = 0.8;
-
-	private static final List<String> _didNotRunErrorMessages = Arrays.asList(
-		"Aborted prior to running test", "Failed prior to running test",
-		"Failed for unknown reason", "timed out after 2 hours");
 
 }
