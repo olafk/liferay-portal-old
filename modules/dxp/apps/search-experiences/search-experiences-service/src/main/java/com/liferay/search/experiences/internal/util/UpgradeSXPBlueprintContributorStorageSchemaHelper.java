d8e746b83a5ba890a327a01eea2be527565c03df
Joshua Cords <joshua.cords@liferay.com>
2024-06-06 09:08:13 -0700

    LPD-22974 LPD-26515 search-experiences-service: create helper for upgrading SXPBlueprints to optimize how contributors are stored
    https://liferay.atlassian.net/browse/LPD-26515

---
+++ modules/dxp/apps/search-experiences/search-experiences-service/src/main/java/com/liferay/search/experiences/internal/util/UpgradeSXPBlueprintContributorStorageSchemaHelper.java
@@ -0,0 +1,65 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.search.experiences.internal.util;
+
+import com.liferay.petra.string.StringPool;
+import com.liferay.search.experiences.model.SXPBlueprint;
+import com.liferay.search.experiences.rest.dto.v1_0.Configuration;
+import com.liferay.search.experiences.rest.dto.v1_0.GeneralConfiguration;
+import com.liferay.search.experiences.rest.dto.v1_0.util.ConfigurationUtil;
+
+import java.util.Objects;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Joshua Cords
+ */
+@Component(
+	enabled = false,
+	service = UpgradeSXPBlueprintContributorStorageSchemaHelper.class
+)
+public class UpgradeSXPBlueprintContributorStorageSchemaHelper {
+
+	public SXPBlueprint upgradeContributorStorageSchema(
+		SXPBlueprint sxpBlueprint) {
+
+		if (!Objects.equals(sxpBlueprint.getSchemaVersion(), "1.0")) {
+			return sxpBlueprint;
+		}
+
+		sxpBlueprint.setSchemaVersion("1.1");
+
+		Configuration configuration = ConfigurationUtil.toConfiguration(
+			sxpBlueprint.getConfigurationJSON());
+
+		GeneralConfiguration generalConfiguration =
+			configuration.getGeneralConfiguration();
+
+		String[] clauseContributorsExcludes =
+			generalConfiguration.getClauseContributorsExcludes();
+		String[] clauseContributorsIncludes =
+			generalConfiguration.getClauseContributorsIncludes();
+
+		if (clauseContributorsExcludes.length == 0) {
+			generalConfiguration.setClauseContributorsIncludes(() -> _WILDCARD);
+		}
+		else if (clauseContributorsIncludes.length == 0) {
+			generalConfiguration.setClauseContributorsExcludes(() -> _WILDCARD);
+		}
+		else {
+			generalConfiguration.setClauseContributorsExcludes(
+				() -> new String[0]);
+		}
+
+		sxpBlueprint.setConfigurationJSON(configuration.toString());
+
+		return sxpBlueprint;
+	}
+
+	private static final String[] _WILDCARD = {StringPool.STAR};
+
+}
