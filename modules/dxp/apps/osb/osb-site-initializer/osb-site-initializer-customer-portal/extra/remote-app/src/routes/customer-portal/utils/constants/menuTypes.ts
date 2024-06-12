825ec3216e9510e0f3091e2bed9a5f9dbe1e1819
Pedro Oliveira <pedro.oliveira@liferay.com>
2024-06-12 11:28:49 -0300

    LRSD-4165 Move Liferay PaaS out of Product Activation

--- modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/routes/customer-portal/utils/constants/menuTypes.ts
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/routes/customer-portal/utils/constants/menuTypes.ts
@@ -5,6 +5,7 @@
 
 export const MENU_TYPES = {
 	attachments: 'Attachments',
+	liferayPaaS: 'Liferay PaaS',
 	liferaySaaS: 'Liferay SaaS',
 	overview: 'Overview',
 	productActivation: 'Product Activation',
