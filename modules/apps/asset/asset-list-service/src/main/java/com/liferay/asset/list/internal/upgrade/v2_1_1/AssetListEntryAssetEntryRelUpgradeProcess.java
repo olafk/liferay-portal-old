liferay-page-management/liferay-portal

LPD-28124 Compacts the positions of the AssetListEntries where at least a relationship has been deleted to prevent them from crashing when updated due to the unique index IX_FAAE938C. Because of this index, the compaction cannot be processed concurrently.

modules/apps/asset/asset-list-service/src/main/java/com/liferay/asset/list/internal/upgrade/v2_1_1/AssetListEntryAssetEntryRelUpgradeProcess.java
@@ -7,8 +7,20 @@ package com.liferay.asset.list.internal.upgrade.v2_1_1;
 
 import com.liferay.petra.string.StringBundler;
 import com.liferay.portal.dao.orm.common.SQLTransformer;
+import com.liferay.portal.kernel.dao.jdbc.AutoBatchPreparedStatementUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
 import com.liferay.portal.kernel.util.LoggingTimer;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * @author Lourdes Fernández Besada
@@ -18,9 +30,13 @@ public class AssetListEntryAssetEntryRelUpgradeProcess extends UpgradeProcess {
 	@Override
 	protected void doUpgrade() throws Exception {
 		String sql = StringBundler.concat(
-			"select distinct ctCollectionId, assetListEntryAssetEntryRelId ",
-			"from AssetListEntryAssetEntryRel where not exists (select 1 from ",
+			"select distinct ctCollectionId, assetListEntryAssetEntryRelId, ",
+			"assetListEntryId, segmentsEntryId, position from ",
+			"AssetListEntryAssetEntryRel where not exists (select 1 from ",
 			"AssetEntry where entryId = assetEntryId)");
+
+		Map<Long, Map<Long, Map<Long, List<Integer>>>> map =
+			new ConcurrentHashMap<>();
 
 		try (LoggingTimer loggingTimer = new LoggingTimer()) {
 			processConcurrently(
@@ -29,7 +45,10 @@ public class AssetListEntryAssetEntryRelUpgradeProcess extends UpgradeProcess {
 					"ctCollectionId = ? and assetListEntryAssetEntryRelId = ?",
 				resultSet -> new Object[] {
 					resultSet.getLong("ctCollectionId"),
-					resultSet.getLong("assetListEntryAssetEntryRelId")
+					resultSet.getLong("assetListEntryAssetEntryRelId"),
+					resultSet.getLong("assetListEntryId"),
+					resultSet.getLong("segmentsEntryId"),
+					resultSet.getInt("position")
 				},
 				(values, preparedStatement) -> {
 					long ctCollectionId = (Long)values[0];
@@ -39,9 +58,123 @@ public class AssetListEntryAssetEntryRelUpgradeProcess extends UpgradeProcess {
 					preparedStatement.setLong(2, assetListEntryAssetEntryRelId);
 
 					preparedStatement.addBatch();
+
+					Map<Long, Map<Long, List<Integer>>> assetListEntryIdsMap =
+						map.computeIfAbsent(
+							ctCollectionId,
+							curCTCollectionId -> new ConcurrentHashMap<>());
+
+					Map<Long, List<Integer>> segmentsEntryIdsMap =
+						assetListEntryIdsMap.computeIfAbsent(
+							(Long)values[2],
+							curAssetListEntryId -> new ConcurrentHashMap<>());
+
+					List<Integer> positions =
+						segmentsEntryIdsMap.computeIfAbsent(
+							(Long)values[3],
+							curSegmentsEntryId -> new ArrayList<>());
+
+					positions.add((Integer)values[4]);
 				},
 				"Unable to update AssetListEntryAssetEntryRels");
+
+			try (PreparedStatement preparedStatement =
+					AutoBatchPreparedStatementUtil.concurrentAutoBatch(
+						connection,
+						"update AssetListEntryAssetEntryRel set position = ? " +
+							"where ctCollectionId = ? and " +
+								"assetListEntryAssetEntryRelId = ?")) {
+
+				for (Map.Entry<Long, Map<Long, Map<Long, List<Integer>>>>
+						ctCollectionIdEntry : map.entrySet()) {
+
+					Map<Long, Map<Long, List<Integer>>> assetListEntryIdsMap =
+						ctCollectionIdEntry.getValue();
+
+					for (Map.Entry<Long, Map<Long, List<Integer>>>
+							assetListEntryIdEntry :
+								assetListEntryIdsMap.entrySet()) {
+
+						Map<Long, List<Integer>> segmentsEntryIdsMap =
+							assetListEntryIdEntry.getValue();
+
+						for (Map.Entry<Long, List<Integer>>
+								segmentsEntryIdEntry :
+									segmentsEntryIdsMap.entrySet()) {
+
+							List<Integer> positions =
+								segmentsEntryIdEntry.getValue();
+
+							Collections.sort(positions);
+
+							_updateAssetListEntryAssetEntryRelPositions(
+								assetListEntryIdEntry.getKey(),
+								ctCollectionIdEntry.getKey(), positions,
+								preparedStatement,
+								segmentsEntryIdEntry.getKey());
+						}
+					}
+				}
+			}
 		}
 	}
+
+	private void _updateAssetListEntryAssetEntryRelPositions(
+		long assetListEntryId, long ctCollectionId, List<Integer> positions,
+		PreparedStatement preparedStatement, long segmentsEntryId) {
+
+		try (PreparedStatement selectPreparedStatement =
+				connection.prepareStatement(
+					SQLTransformer.transform(
+						StringBundler.concat(
+							"select distinct assetListEntryAssetEntryRelId, ",
+							"position from AssetListEntryAssetEntryRel where ",
+							"ctCollectionId = ? and assetListEntryId = ? and ",
+							"segmentsEntryId = ? and position > ? order by ",
+							"position asc")))) {
+
+			selectPreparedStatement.setLong(1, ctCollectionId);
+			selectPreparedStatement.setLong(2, assetListEntryId);
+			selectPreparedStatement.setLong(3, segmentsEntryId);
+			selectPreparedStatement.setLong(4, positions.get(0));
+
+			try (ResultSet resultSet = selectPreparedStatement.executeQuery()) {
+				while (resultSet.next()) {
+					int curPosition = resultSet.getInt("position");
+
+					for (Integer position : positions) {
+						if (curPosition < position) {
+							break;
+						}
+
+						curPosition--;
+					}
+
+					preparedStatement.setLong(1, curPosition);
+
+					preparedStatement.setLong(2, ctCollectionId);
+
+					preparedStatement.setLong(
+						3, resultSet.getLong("assetListEntryAssetEntryRelId"));
+
+					preparedStatement.executeUpdate();
+				}
+			}
+		}
+		catch (Exception exception) {
+			if (_log.isWarnEnabled()) {
+				_log.warn(
+					StringBundler.concat(
+						"Unable to update AssetListEntryAssetEntryRels  ",
+						"ctCollectionId: ", ctCollectionId,
+						" assetListEntryId: ", assetListEntryId,
+						" segmentsEntryId: ", segmentsEntryId),
+					exception);
+			}
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		AssetListEntryAssetEntryRelUpgradeProcess.class);
 
 }

olafk/liferay-portal-old

modules/apps/asset/asset-list-service/src/main/java/com/liferay/asset/list/internal/upgrade/v2_1_1/AssetListEntryAssetEntryRelUpgradeProcess.java
@@ -7,8 +7,20 @@ package com.liferay.asset.list.internal.upgrade.v2_1_1;
 
 import com.liferay.petra.string.StringBundler;
 import com.liferay.portal.dao.orm.common.SQLTransformer;
+import com.liferay.portal.kernel.dao.jdbc.AutoBatchPreparedStatementUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
 import com.liferay.portal.kernel.util.LoggingTimer;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * @author Lourdes Fernández Besada
@@ -18,9 +30,13 @@ public class AssetListEntryAssetEntryRelUpgradeProcess extends UpgradeProcess {
 	@Override
 	protected void doUpgrade() throws Exception {
 		String sql = StringBundler.concat(
-			"select distinct ctCollectionId, assetListEntryAssetEntryRelId ",
-			"from AssetListEntryAssetEntryRel where not exists (select 1 from ",
+			"select distinct ctCollectionId, assetListEntryAssetEntryRelId, ",
+			"assetListEntryId, segmentsEntryId, position from ",
+			"AssetListEntryAssetEntryRel where not exists (select 1 from ",
 			"AssetEntry where entryId = assetEntryId)");
+
+		Map<Long, Map<Long, Map<Long, List<Integer>>>> map =
+			new ConcurrentHashMap<>();
 
 		try (LoggingTimer loggingTimer = new LoggingTimer()) {
 			processConcurrently(
@@ -29,7 +45,10 @@ public class AssetListEntryAssetEntryRelUpgradeProcess extends UpgradeProcess {
 					"ctCollectionId = ? and assetListEntryAssetEntryRelId = ?",
 				resultSet -> new Object[] {
 					resultSet.getLong("ctCollectionId"),
-					resultSet.getLong("assetListEntryAssetEntryRelId")
+					resultSet.getLong("assetListEntryAssetEntryRelId"),
+					resultSet.getLong("assetListEntryId"),
+					resultSet.getLong("segmentsEntryId"),
+					resultSet.getInt("position")
 				},
 				(values, preparedStatement) -> {
 					long ctCollectionId = (Long)values[0];
@@ -39,9 +58,121 @@ public class AssetListEntryAssetEntryRelUpgradeProcess extends UpgradeProcess {
 					preparedStatement.setLong(2, assetListEntryAssetEntryRelId);
 
 					preparedStatement.addBatch();
+
+					Map<Long, Map<Long, List<Integer>>> assetListEntryIdsMap =
+						map.computeIfAbsent(
+							ctCollectionId,
+							curCTCollectionId -> new ConcurrentHashMap<>());
+
+					Map<Long, List<Integer>> segmentsEntryIdsMap =
+						assetListEntryIdsMap.computeIfAbsent(
+							(Long)values[2],
+							curAssetListEntryId -> new ConcurrentHashMap<>());
+
+					List<Integer> positions =
+						segmentsEntryIdsMap.computeIfAbsent(
+							(Long)values[3],
+							curSegmentsEntryId -> new ArrayList<>());
+
+					positions.add((Integer)values[4]);
 				},
 				"Unable to update AssetListEntryAssetEntryRels");
+
+			try (PreparedStatement preparedStatement =
+					AutoBatchPreparedStatementUtil.concurrentAutoBatch(
+						connection,
+						"update AssetListEntryAssetEntryRel set position = ? " +
+							"where ctCollectionId = ? and " +
+								"assetListEntryAssetEntryRelId = ?")) {
+
+				for (Map.Entry<Long, Map<Long, Map<Long, List<Integer>>>>
+						ctCollectionIdEntry : map.entrySet()) {
+
+					Map<Long, Map<Long, List<Integer>>> assetListEntryIdsMap =
+						ctCollectionIdEntry.getValue();
+
+					for (Map.Entry<Long, Map<Long, List<Integer>>>
+							assetListEntryIdEntry :
+								assetListEntryIdsMap.entrySet()) {
+
+						Map<Long, List<Integer>> segmentsEntryIdsMap =
+							assetListEntryIdEntry.getValue();
+
+						for (Map.Entry<Long, List<Integer>>
+								segmentsEntryIdEntry :
+									segmentsEntryIdsMap.entrySet()) {
+
+							List<Integer> positions =
+								segmentsEntryIdEntry.getValue();
+
+							Collections.sort(positions);
+
+							_updateAssetListEntryAssetEntryRelPositions(
+								assetListEntryIdEntry.getKey(),
+								ctCollectionIdEntry.getKey(), positions,
+								preparedStatement,
+								segmentsEntryIdEntry.getKey());
+						}
+					}
+				}
+			}
 		}
 	}
+
+	private void _updateAssetListEntryAssetEntryRelPositions(
+		long assetListEntryId, long ctCollectionId, List<Integer> positions,
+		PreparedStatement preparedStatement1, long segmentsEntryId) {
+
+		try (PreparedStatement preparedStatement2 = connection.prepareStatement(
+				SQLTransformer.transform(
+					StringBundler.concat(
+						"select distinct assetListEntryAssetEntryRelId, ",
+						"position from AssetListEntryAssetEntryRel where ",
+						"ctCollectionId = ? and assetListEntryId = ? and ",
+						"segmentsEntryId = ? and position > ? order by ",
+						"position asc")))) {
+
+			preparedStatement2.setLong(1, ctCollectionId);
+			preparedStatement2.setLong(2, assetListEntryId);
+			preparedStatement2.setLong(3, segmentsEntryId);
+			preparedStatement2.setLong(4, positions.get(0));
+
+			try (ResultSet resultSet = preparedStatement2.executeQuery()) {
+				while (resultSet.next()) {
+					int curPosition = resultSet.getInt("position");
+
+					for (Integer position : positions) {
+						if (curPosition < position) {
+							break;
+						}
+
+						curPosition--;
+					}
+
+					preparedStatement1.setLong(1, curPosition);
+
+					preparedStatement1.setLong(2, ctCollectionId);
+
+					preparedStatement1.setLong(
+						3, resultSet.getLong("assetListEntryAssetEntryRelId"));
+
+					preparedStatement1.executeUpdate();
+				}
+			}
+		}
+		catch (Exception exception) {
+			if (_log.isWarnEnabled()) {
+				_log.warn(
+					StringBundler.concat(
+						"Unable to update AssetListEntryAssetEntryRels for ",
+						"asset list entry ID ", assetListEntryId,
+						" and segments entry ID ", segmentsEntryId),
+					exception);
+			}
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		AssetListEntryAssetEntryRelUpgradeProcess.class);
 
 }