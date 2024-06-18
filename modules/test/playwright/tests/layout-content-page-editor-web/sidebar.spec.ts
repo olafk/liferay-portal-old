dd04929f4002f12c51a13d40eade8e3d64ad3a23
Veronica Gonzalez <veronica.gonzalez@liferay.com>
2024-06-18 16:48:12 +0200

    LPD-28304 Migrate test to check that the panel buttons have the correct aria-selected

--- modules/test/playwright/tests/layout-content-page-editor-web/sidebar.spec.ts
+++ modules/test/playwright/tests/layout-content-page-editor-web/sidebar.spec.ts
@@ -51,7 +51,13 @@ test('renders all panel buttons in the vertical bar', async ({
 	await pageEditorPage.goto(layout, site.friendlyUrlPath);
 
 	for (const panel of PANELS) {
-		await expect(page.getByLabel(panel, {exact: true})).toBeVisible();
+		const panelButton = await page.getByLabel(panel, {exact: true});
+
+		await expect(panelButton).toBeVisible();
+		await expect(panelButton).toHaveAttribute(
+			'aria-selected',
+			panel === PANELS[0] ? 'true' : 'false'
+		);
 	}
 });
 
