5d80188b2a3885a5d0e8ba3d824658c27705181d
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2024-06-21 11:01:39 +0200

    LPD-29375 Convert into unit tests

--- modules/apps/site-navigation/site-navigation-language-web/src/test/java/com/liferay/site/navigation/language/web/internal/display/context/SiteNavigationLanguageDisplayContextTest.java
+++ modules/apps/site-navigation/site-navigation-language-web/src/test/java/com/liferay/site/navigation/language/web/internal/display/context/SiteNavigationLanguageDisplayContextTest.java
@@ -5,15 +5,25 @@
 
 package com.liferay.site.navigation.language.web.internal.display.context;
 
+import com.liferay.petra.string.StringPool;
+import com.liferay.petra.string.StringUtil;
 import com.liferay.portal.configuration.module.configuration.ConfigurationProviderUtil;
+import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.GroupConstants;
 import com.liferay.portal.kernel.service.GroupLocalServiceUtil;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.model.impl.GroupImpl;
 import com.liferay.portal.test.rule.LiferayUnitTestRule;
 import com.liferay.site.navigation.language.web.internal.configuration.SiteNavigationLanguagePortletInstanceConfiguration;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -63,8 +73,12 @@ public class SiteNavigationLanguageDisplayContextTest {
 	public void testGetDisplayStyleGroupKeyWithConfiguration()
 		throws Exception {
 
-		_setUpSiteNavigationLanguagePortletInstanceConfiguration(
-			GroupConstants.CONTROL_PANEL);
+		Mockito.when(
+			_siteNavigationLanguagePortletInstanceConfiguration.
+				displayStyleGroupKey()
+		).thenReturn(
+			GroupConstants.CONTROL_PANEL
+		);
 
 		SiteNavigationLanguageDisplayContext
 			siteNavigationLanguageDisplayContext =
@@ -74,6 +88,47 @@ public class SiteNavigationLanguageDisplayContextTest {
 		Assert.assertEquals(
 			GroupConstants.CONTROL_PANEL,
 			siteNavigationLanguageDisplayContext.getDisplayStyleGroupKey());
+	}
+
+	@Test
+	public void testGetLanguageIdsFromInstanceSettings() throws Exception {
+		SiteNavigationLanguageDisplayContext
+			siteNavigationLanguageDisplayContext =
+				new SiteNavigationLanguageDisplayContext(
+					_mockHttpServletRequest());
+
+		Mockito.when(
+			_siteNavigationLanguagePortletInstanceConfiguration.languageIds()
+		).thenReturn(
+			StringUtil.merge(_availableLocales, StringPool.COMMA)
+		);
+
+		for (String languageId :
+				siteNavigationLanguageDisplayContext.getLanguageIds()) {
+
+			Assert.assertTrue(
+				_availableLocales.contains(
+					LocaleUtil.fromLanguageId(languageId)));
+		}
+	}
+
+	@Test
+	public void testGetLanguageIdsFromSiteSettings() throws Exception {
+		_setUpLanguage();
+		_setUpLanguageUtil();
+
+		SiteNavigationLanguageDisplayContext
+			siteNavigationLanguageDisplayContext =
+				new SiteNavigationLanguageDisplayContext(
+					_mockHttpServletRequest());
+
+		for (String languageId :
+				siteNavigationLanguageDisplayContext.getLanguageIds()) {
+
+			Assert.assertTrue(
+				_availableLocales.contains(
+					LocaleUtil.fromLanguageId(languageId)));
+		}
 	}
 
 	private HttpServletRequest _mockHttpServletRequest() {
@@ -112,23 +167,35 @@ public class SiteNavigationLanguageDisplayContextTest {
 		);
 	}
 
-	private void _setUpSiteNavigationLanguagePortletInstanceConfiguration(
-		String groupKey) {
-
+	private void _setUpLanguage() {
 		Mockito.when(
-			_siteNavigationLanguagePortletInstanceConfiguration.
-				displayStyleGroupKey()
+			_language.getAvailableLocales()
 		).thenReturn(
-			groupKey
+			_availableLocales
 		);
 	}
 
+	private void _setUpLanguageUtil() {
+		LanguageUtil languageUtil = new LanguageUtil();
+
+		Mockito.when(
+			_language.getAvailableLocales(Mockito.anyLong())
+		).thenReturn(
+			_availableLocales
+		);
+
+		languageUtil.setLanguage(_language);
+	}
+
+	private final Set<Locale> _availableLocales = new HashSet<>(
+		Arrays.asList(LocaleUtil.SPAIN, LocaleUtil.US));
 	private final MockedStatic<ConfigurationProviderUtil>
 		_configurationProviderUtilMockedStatic = Mockito.mockStatic(
 			ConfigurationProviderUtil.class);
 	private final MockedStatic<GroupLocalServiceUtil>
 		_groupLocalServiceUtilMockedStatic = Mockito.mockStatic(
 			GroupLocalServiceUtil.class);
+	private final Language _language = Mockito.mock(Language.class);
 	private final SiteNavigationLanguagePortletInstanceConfiguration
 		_siteNavigationLanguagePortletInstanceConfiguration = Mockito.mock(
 			SiteNavigationLanguagePortletInstanceConfiguration.class);
