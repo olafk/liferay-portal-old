53cc89d13210d22939c98437efeef7921b6db958
Adolfo Pérez Álvarez <adolfo.perez@liferay.com>
2024-07-04 14:00:38 +0200

    LPD-30519 Rename test class to match class under test

--- modules/apps/journal/journal-test/src/testIntegration/java/com/liferay/journal/internal/util/test/JournalConverterImplTest.java
+++ modules/apps/journal/journal-test/src/testIntegration/java/com/liferay/journal/internal/util/test/JournalConverterImplTest.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.journal.util.test;
+package com.liferay.journal.internal.util.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.dynamic.data.mapping.constants.DDMStructureConstants;
@@ -65,7 +65,7 @@ import org.junit.runner.RunWith;
  * @author Marcellus Tavares
  */
 @RunWith(Arquillian.class)
-public class JournalConverterUtilTest {
+public class JournalConverterImplTest {
 
 	@ClassRule
 	@Rule
