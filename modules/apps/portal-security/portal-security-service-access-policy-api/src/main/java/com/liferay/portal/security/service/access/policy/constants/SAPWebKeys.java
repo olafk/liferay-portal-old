daiazius/liferay-portal

LPD-26931 Add new constant, move to api and apply

modules/apps/portal-security/portal-security-service-access-policy-api/src/main/java/com/liferay/portal/security/service/access/policy/constants/SAPWebKeys.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portal.security.service.access.policy.web.internal.constants;
+package com.liferay.portal.security.service.access.policy.constants;
 
 /**
  * @author Tomas Polesovsky
@@ -12,5 +12,7 @@ public class SAPWebKeys {
 
 	public static final String SERVICE_CLASS_NAMES_TO_CONTEXT_NAMES =
 		"SERVICE_CLASS_NAMES_TO_CONTEXT_NAMES";
+
+	public static final String TEMPLATE_DIPSATCH = "TEMPLATE_DISPATCH";
 
 }

modules/apps/portal-security/portal-security-service-access-policy-service/src/main/java/com/liferay/portal/security/service/access/policy/internal/SAPAccessControlPolicy.java
@@ -25,6 +25,7 @@ import com.liferay.portal.kernel.settings.CompanyServiceSettingsLocator;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.security.service.access.policy.configuration.SAPConfiguration;
 import com.liferay.portal.security.service.access.policy.constants.SAPConstants;
+import com.liferay.portal.security.service.access.policy.constants.SAPWebKeys;
 import com.liferay.portal.security.service.access.policy.model.SAPEntry;
 import com.liferay.portal.security.service.access.policy.service.SAPEntryLocalService;
 
@@ -289,7 +290,9 @@ public class SAPAccessControlPolicy extends BaseAccessControlPolicy {
 			HttpServletRequest httpServletRequest =
 				accessControlContext.getRequest();
 
-			if (httpServletRequest.getAttribute("REST_CLIENT") != null) {
+			if (httpServletRequest.getAttribute(SAPWebKeys.TEMPLATE_DIPSATCH) !=
+					null) {
+
 				systemServiceAccessPolicyNames.add(
 					sapConfiguration.systemTemplateDefaultSAPEntryName());
 			}

modules/apps/portal-vulcan/portal-vulcan-impl/src/main/java/com/liferay/portal/vulcan/internal/template/servlet/RESTClientHttpRequest.java
@@ -14,6 +14,7 @@ import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.security.service.access.policy.constants.SAPWebKeys;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -52,6 +53,8 @@ public class RESTClientHttpRequest implements HttpServletRequest {
 		HttpServletRequest httpServletRequest) {
 
 		_attributes = HashMapBuilder.<String, Object>put(
+			SAPWebKeys.TEMPLATE_DIPSATCH, true
+		).put(
 			WebKeys.USER,
 			() -> {
 				if (contextObjects.containsKey("user")) {
@@ -60,8 +63,6 @@ public class RESTClientHttpRequest implements HttpServletRequest {
 
 				return null;
 			}
-		).put(
-			"REST_CLIENT", true
 		).build();
 		_headers = HashMapBuilder.put(
 			HttpHeaders.ACCEPT, ContentTypes.APPLICATION_JSON

olafk/liferay-portal-old

modules/apps/portal-security/portal-security-service-access-policy-api/src/main/java/com/liferay/portal/security/service/access/policy/constants/SAPWebKeys.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portal.security.service.access.policy.web.internal.constants;
+package com.liferay.portal.security.service.access.policy.constants;
 
 /**
  * @author Tomas Polesovsky
@@ -12,5 +12,7 @@ public class SAPWebKeys {
 
 	public static final String SERVICE_CLASS_NAMES_TO_CONTEXT_NAMES =
 		"SERVICE_CLASS_NAMES_TO_CONTEXT_NAMES";
+
+	public static final String TEMPLATE_DISPATCH = "TEMPLATE_DISPATCH";
 
 }

modules/apps/portal-security/portal-security-service-access-policy-service/src/main/java/com/liferay/portal/security/service/access/policy/internal/SAPAccessControlPolicy.java
@@ -25,6 +25,7 @@ import com.liferay.portal.kernel.settings.CompanyServiceSettingsLocator;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.security.service.access.policy.configuration.SAPConfiguration;
 import com.liferay.portal.security.service.access.policy.constants.SAPConstants;
+import com.liferay.portal.security.service.access.policy.constants.SAPWebKeys;
 import com.liferay.portal.security.service.access.policy.model.SAPEntry;
 import com.liferay.portal.security.service.access.policy.service.SAPEntryLocalService;
 
@@ -289,7 +290,9 @@ public class SAPAccessControlPolicy extends BaseAccessControlPolicy {
 			HttpServletRequest httpServletRequest =
 				accessControlContext.getRequest();
 
-			if (httpServletRequest.getAttribute("REST_CLIENT") != null) {
+			if (httpServletRequest.getAttribute(SAPWebKeys.TEMPLATE_DISPATCH) !=
+					null) {
+
 				systemServiceAccessPolicyNames.add(
 					sapConfiguration.systemTemplateDefaultSAPEntryName());
 			}

modules/apps/portal-vulcan/portal-vulcan-impl/src/main/java/com/liferay/portal/vulcan/internal/template/servlet/RESTClientHttpRequest.java
@@ -14,6 +14,7 @@ import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.security.service.access.policy.constants.SAPWebKeys;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -52,6 +53,8 @@ public class RESTClientHttpRequest implements HttpServletRequest {
 		HttpServletRequest httpServletRequest) {
 
 		_attributes = HashMapBuilder.<String, Object>put(
+			SAPWebKeys.TEMPLATE_DISPATCH, true
+		).put(
 			WebKeys.USER,
 			() -> {
 				if (contextObjects.containsKey("user")) {
@@ -60,8 +63,6 @@ public class RESTClientHttpRequest implements HttpServletRequest {
 
 				return null;
 			}
-		).put(
-			"REST_CLIENT", true
 		).build();
 		_headers = HashMapBuilder.put(
 			HttpHeaders.ACCEPT, ContentTypes.APPLICATION_JSON