29f4ef2e9594d5d0d5fa17219e6d54483177bf8a
Gislayne Vitorino <gislayne.vitorino@liferay.com>
2024-07-23 11:50:12 -0300

    LPD-30098 Add playwright test for PublicationsUserCollaboration#InviteUserAsAdmin

---
+++ modules/test/playwright/tests/change-tracking-web/publicationsUserCollaboration.spec.ts
@@ -0,0 +1,109 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import {expect, mergeTests} from '@playwright/test';
+
+import {apiHelpersTest} from '../../fixtures/apiHelpersTest';
+import {changeTrackingPagesTest} from '../../fixtures/changeTrackingPagesTest';
+import {isolatedSiteTest} from '../../fixtures/isolatedSiteTest';
+import {productMenuPageTest} from '../../fixtures/productMenuPageTest';
+import getRandomString from '../../utils/getRandomString';
+import performLogin, {performLogout} from '../../utils/performLogin';
+import {waitForSuccessAlert} from '../../utils/waitForSuccessAlert';
+import {journalPagesTest} from '../journal-web/fixtures/journalPagesTest';
+
+export const test = mergeTests(
+	apiHelpersTest,
+	changeTrackingPagesTest,
+	isolatedSiteTest,
+	journalPagesTest,
+	productMenuPageTest
+);
+
+test('LPD-30098 Invite user as admin', async ({
+	apiHelpers,
+	changeTrackingPage,
+	ctCollection,
+	journalEditArticlePage,
+	page,
+	site,
+}) => {
+	const user1 = await changeTrackingPage.addUserWithPublicationsUserRole();
+
+	const user2 = await changeTrackingPage.addUserWithPublicationsUserRole();
+
+	await changeTrackingPage.addUserToPublication(ctCollection.name, 'Admin', user1);
+
+	await journalEditArticlePage.goto({siteUrl: site.friendlyUrlPath});
+
+	const title = getRandomString();
+
+	await journalEditArticlePage.fillTitle(title);
+
+	await page.getByRole('button', {name: 'Publish'}).click();
+
+	await waitForSuccessAlert(
+		page,
+		`Success:${title} was created successfully.`
+	);
+
+	await performLogout(page);
+
+	await performLogin(page, user1.alternateName);
+
+	await page.getByTestId('userPersonalMenu').click();
+
+	await page.getByRole('menuitem', {name: 'Notifications'}).click();
+
+	await expect(
+		page.getByText(
+			`has invited you to work on ${ctCollection.name} as a Admin.`
+		)
+	).toBeVisible();
+
+	await changeTrackingPage.goto();
+
+	await page.waitForLoadState('load');
+
+	await page.getByRole('button', {name: 'Actions'}).click();
+
+	await page.getByRole('menuitem', {name: 'Edit'}).click();
+
+	await page.getByPlaceholder('Enter the name of the').fill(title);
+
+	await page.getByRole('button', {name: 'Save'}).click();
+
+	await waitForSuccessAlert(page, 'Success:Successfully updated');
+
+	await changeTrackingPage.addUserToPublication(title, 'Admin', user2);
+
+	await changeTrackingPage.assertPublicationCommentsCRUDPermissions();
+
+	await page.reload();
+
+	await page.getByRole('link', {name: 'Publish'}).click();
+
+	await page.getByRole('button', {name: 'Publish'}).click();
+
+	await expect(page.getByRole('link', {name: 'History'})).toBeVisible();
+
+	await expect(
+		page.locator('div').filter({hasText: title}).first()
+	).toBeVisible();
+
+	await waitForSuccessAlert(
+		page,
+		'Success:Your request completed successfully.'
+	);
+
+	await performLogout(page);
+
+	await performLogin(page, 'test');
+
+	await apiHelpers.headlessAdminUser.deleteUserAccount(Number(user1.id));
+	await apiHelpers.headlessAdminUser.deleteUserAccount(Number(user2.id));
+
+	await apiHelpers.headlessChangeTracking.deleteCTCollection(ctCollection.id);
+});
