d7514df97af8c1080d1eb90711f96d5f96c3fb29
Joshua Cords <joshua.cords@liferay.com>
2024-07-31 14:17:32 -0700

    LPD-28413 LPD-31076 portal-search: index userExternalReferenceCode in AuditedModelDocumentContributor
    https://liferay.atlassian.net/browse/LPD-28413

--- modules/apps/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/spi/model/index/contributor/AuditedModelDocumentContributor.java
+++ modules/apps/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/spi/model/index/contributor/AuditedModelDocumentContributor.java
@@ -5,11 +5,17 @@
 
 package com.liferay.portal.search.internal.spi.model.index.contributor;
 
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.AuditedModel;
 import com.liferay.portal.kernel.model.BaseModel;
+import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.search.Document;
 import com.liferay.portal.kernel.search.DocumentContributor;
 import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.util.Portal;
 
 import org.osgi.service.component.annotations.Component;
@@ -35,6 +41,8 @@ public class AuditedModelDocumentContributor
 		document.addKeyword(Field.COMPANY_ID, auditedModel.getCompanyId());
 		document.addDate(Field.CREATE_DATE, auditedModel.getCreateDate());
 		document.addDate(Field.MODIFIED_DATE, auditedModel.getModifiedDate());
+		document.addKeyword(
+			"userExternalReferenceCode", _getUserERC(auditedModel));
 		document.addKeyword(Field.USER_ID, auditedModel.getUserId());
 		document.addKeyword(
 			Field.USER_NAME,
@@ -45,5 +53,31 @@ public class AuditedModelDocumentContributor
 
 	@Reference
 	protected Portal portal;
+
+	@Reference
+	protected UserLocalService userLocalService;
+
+	private String _getUserERC(AuditedModel auditedModel) {
+		String userExternalReferenceCode = StringPool.BLANK;
+
+		try {
+			User user = userLocalService.getUser(auditedModel.getUserId());
+
+			userExternalReferenceCode = user.getExternalReferenceCode();
+		}
+		catch (PortalException portalException) {
+			if (_log.isWarnEnabled()) {
+				_log.warn(
+					"Unable to retrieve user " + auditedModel.getUserId() +
+						" while indexing document.",
+					portalException);
+			}
+		}
+
+		return userExternalReferenceCode;
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		AuditedModelDocumentContributor.class);
 
 }
