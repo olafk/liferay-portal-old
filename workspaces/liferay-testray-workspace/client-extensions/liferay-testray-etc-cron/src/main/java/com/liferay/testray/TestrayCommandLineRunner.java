bf2d464a9dd9d685c7af29949f97ebbe245031e1
Nilton Vieira <nilton.vieira@liferay.com>
2024-08-01 12:32:09 -0300

    LPD-31240 update workspaces/liferay-testray-workspace usages

--- workspaces/liferay-testray-workspace/client-extensions/liferay-testray-etc-cron/src/main/java/com/liferay/testray/TestrayCommandLineRunner.java
+++ workspaces/liferay-testray-workspace/client-extensions/liferay-testray-etc-cron/src/main/java/com/liferay/testray/TestrayCommandLineRunner.java
@@ -5,15 +5,12 @@
 
 package com.liferay.testray;
 
+import com.liferay.client.extension.util.spring.boot.BaseRestController;
 import com.liferay.client.extension.util.spring.boot.LiferayOAuth2AccessTokenManager;
-
-import java.net.URI;
 
 import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
 import java.time.temporal.ChronoUnit;
-
-import java.util.function.Function;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -24,23 +21,21 @@ import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
-import org.springframework.web.reactive.function.client.ExchangeStrategies;
-import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.util.UriBuilder;
+import org.springframework.web.util.DefaultUriBuilderFactory;
 
 /**
  * @author Nilton Vieira
  */
 @Component
-public class TestrayCommandLineRunner implements CommandLineRunner {
+public class TestrayCommandLineRunner
+	extends BaseRestController implements CommandLineRunner {
 
 	public void archiveTestrayBuilds() throws Exception {
-		JSONArray jsonArray = _get(
-			uriBuilder -> uriBuilder.path(
+		String response = get(
+			_getAuthorization(),
+			_defaultUriBuilderFactory.builder(
+			).path(
 				"/o/c/builds"
 			).queryParam(
 				"filter",
@@ -49,7 +44,11 @@ public class TestrayCommandLineRunner implements CommandLineRunner {
 						_currentDateTime.minusDays(_maxDaysOpened)
 			).queryParam(
 				"pageSize", "-1"
-			).build()
+			).build(
+			).toString());
+
+		JSONArray jsonArray = new JSONObject(
+			response
 		).getJSONArray(
 			"items"
 		);
@@ -68,12 +67,14 @@ public class TestrayCommandLineRunner implements CommandLineRunner {
 			_log.info("Archiving " + jsonArray.length() + " Testray builds");
 		}
 
-		_put(jsonArray.toString(), "/o/c/builds/batch");
+		put(_getAuthorization(), jsonArray.toString(), "/o/c/builds/batch");
 	}
 
 	public void deleteTestrayBuilds() throws Exception {
-		JSONArray jsonArray = _get(
-			uriBuilder -> uriBuilder.path(
+		String response = get(
+			_getAuthorization(),
+			_defaultUriBuilderFactory.builder(
+			).path(
 				"/o/c/builds"
 			).queryParam(
 				"fields", "id"
@@ -83,7 +84,11 @@ public class TestrayCommandLineRunner implements CommandLineRunner {
 					_currentDateTime.minusDays(_maxDaysArchived)
 			).queryParam(
 				"pageSize", "-1"
-			).build()
+			).build(
+			).toString());
+
+		JSONArray jsonArray = new JSONObject(
+			response
 		).getJSONArray(
 			"items"
 		);
@@ -92,7 +97,7 @@ public class TestrayCommandLineRunner implements CommandLineRunner {
 			_log.info("Deleting " + jsonArray.length() + " Testray builds");
 		}
 
-		_delete(jsonArray.toString(), "/o/c/builds/batch");
+		delete(_getAuthorization(), jsonArray.toString(), "/o/c/builds/batch");
 	}
 
 	@Override
@@ -102,83 +107,9 @@ public class TestrayCommandLineRunner implements CommandLineRunner {
 		archiveTestrayBuilds();
 	}
 
-	private void _delete(String bodyValue, String path) {
-		_getWebClient(
-		).method(
-			HttpMethod.DELETE
-		).uri(
-			uriBuilder -> uriBuilder.path(
-				path
-			).build()
-		).contentType(
-			MediaType.APPLICATION_JSON
-		).accept(
-			MediaType.APPLICATION_JSON
-		).header(
-			HttpHeaders.AUTHORIZATION, _getAuthorization()
-		).bodyValue(
-			bodyValue
-		).retrieve(
-		).bodyToMono(
-			String.class
-		).block();
-	}
-
-	private JSONObject _get(Function<UriBuilder, URI> uriFunction) {
-		return new JSONObject(
-			_getWebClient(
-			).get(
-			).uri(
-				uriBuilder -> uriFunction.apply(uriBuilder)
-			).accept(
-				MediaType.APPLICATION_JSON
-			).header(
-				HttpHeaders.AUTHORIZATION, _getAuthorization()
-			).retrieve(
-			).bodyToMono(
-				String.class
-			).block());
-	}
-
 	private String _getAuthorization() {
 		return _liferayOAuth2AccessTokenManager.getAuthorization(
 			_liferayOAuthApplicationExternalReferenceCodes);
-	}
-
-	private WebClient _getWebClient() {
-		return WebClient.builder(
-		).baseUrl(
-			_lxcDXPServerProtocol + "://" + _lxcDXPMainDomain
-		).exchangeStrategies(
-			ExchangeStrategies.builder(
-			).codecs(
-				clientCodecConfigurer -> clientCodecConfigurer.defaultCodecs(
-				).maxInMemorySize(
-					5 * 1024 * 1024
-				)
-			).build()
-		).build();
-	}
-
-	private void _put(String bodyValue, String path) {
-		_getWebClient(
-		).put(
-		).uri(
-			uriBuilder -> uriBuilder.path(
-				path
-			).build()
-		).accept(
-			MediaType.APPLICATION_JSON
-		).contentType(
-			MediaType.APPLICATION_JSON
-		).header(
-			HttpHeaders.AUTHORIZATION, _getAuthorization()
-		).bodyValue(
-			bodyValue
-		).retrieve(
-		).bodyToMono(
-			Void.class
-		).block();
 	}
 
 	private static final Log _log = LogFactory.getLog(
@@ -189,18 +120,14 @@ public class TestrayCommandLineRunner implements CommandLineRunner {
 	).truncatedTo(
 		ChronoUnit.SECONDS
 	);
+	private final DefaultUriBuilderFactory _defaultUriBuilderFactory =
+		new DefaultUriBuilderFactory();
 
 	@Autowired
 	private LiferayOAuth2AccessTokenManager _liferayOAuth2AccessTokenManager;
 
 	@Value("${liferay.oauth.application.external.reference.codes}")
 	private String _liferayOAuthApplicationExternalReferenceCodes;
-
-	@Value("${com.liferay.lxc.dxp.mainDomain}")
-	private String _lxcDXPMainDomain;
-
-	@Value("${com.liferay.lxc.dxp.server.protocol}")
-	private String _lxcDXPServerProtocol;
 
 	@Value("${liferay.testray.etc.cron.max.days.archived}")
 	private Long _maxDaysArchived;
