b3550fb8efc52bfd52054219fe565be461d20505
Felipe Lorenz <felipe.lorenz@hotmail.com>
2024-08-02 14:19:35 -0300

    LPD-29572 LPD-32708 portal-search-tuning-synonyms-web: Add synonym filter on default configuration for fi_FI
    https://liferay.atlassian.net/browse/LPD-32708

--- modules/dxp/apps/portal-search-tuning/portal-search-tuning-synonyms-web/src/main/java/com/liferay/portal/search/tuning/synonyms/web/internal/configuration/SynonymsConfiguration.java
+++ modules/dxp/apps/portal-search-tuning/portal-search-tuning-synonyms-web/src/main/java/com/liferay/portal/search/tuning/synonyms/web/internal/configuration/SynonymsConfiguration.java
@@ -20,7 +20,7 @@ import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClass
 public interface SynonymsConfiguration {
 
 	@Meta.AD(
-		deflt = "liferay_filter_synonym_de|liferay_filter_synonym_en|liferay_filter_synonym_es|liferay_filter_synonym_fr|liferay_filter_synonym_hu|liferay_filter_synonym_nl|liferay_filter_synonym_pt_BR|liferay_filter_synonym_sv",
+		deflt = "liferay_filter_synonym_de|liferay_filter_synonym_en|liferay_filter_synonym_es|liferay_filter_synonym_fi|liferay_filter_synonym_fr|liferay_filter_synonym_hu|liferay_filter_synonym_nl|liferay_filter_synonym_pt_BR|liferay_filter_synonym_sv",
 		description = "synonym-filter-names-help",
 		name = "synonym-filter-names", required = false
 	)
