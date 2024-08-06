adca2206d35129d8f1ca5f3abee775fdba2b20c3
Gianmarco Brunialti <gianmarco.brunialti@liferay.com>
2024-08-06 10:27:06 +0200

    LPD-33868 Commerce FDS REST Application constants

---
+++ modules/apps/commerce/commerce-frontend-api/src/main/java/com/liferay/commerce/frontend/constants/CommerceFDSRestApplicationConstants.java
@@ -0,0 +1,57 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.commerce.frontend.constants;
+
+/**
+ * @author Gianmarco Brunialti Masera
+ */
+public class CommerceFDSRestApplicationConstants {
+
+	public static final String ADMIN_ACCOUNT_V1 =
+		"/headless-commerce-admin-account/v1.0";
+
+	public static final String ADMIN_CATALOG_V1 =
+		"/headless-commerce-admin-catalog/v1.0";
+
+	public static final String ADMIN_CHANNEL_V1 =
+		"/headless-commerce-admin-channel/v1.0";
+
+	public static final String ADMIN_INVENTORY_V1 =
+		"/headless-commerce-admin-inventory/v1.0";
+
+	public static final String ADMIN_ORDER_V1 =
+		"/headless-commerce-admin-order/v1.0";
+
+	public static final String ADMIN_PAYMENT_V1 =
+		"/headless-commerce-admin-payment/v1.0";
+
+	public static final String ADMIN_PRICING_V1 =
+		"/headless-commerce-admin-pricing/v1.0";
+
+	public static final String ADMIN_PRICING_V2 =
+		"/headless-commerce-admin-pricing/v2.0";
+
+	public static final String ADMIN_SHIPMENT_V1 =
+		"/headless-commerce-admin-shipment/v1.0";
+
+	public static final String ADMIN_SITE_SETTING_V1 =
+		"/headless-commerce-admin-site-setting/v1.0";
+
+	public static final String DELIVERY_CART_V1 =
+		"/headless-commerce-delivery-cart/v1.0";
+
+	public static final String DELIVERY_CATALOG_V1 =
+		"/headless-commerce-delivery-catalog/v1.0";
+
+	public static final String DELIVERY_ORDER_V1 =
+		"/headless-commerce-delivery-order/v1.0";
+
+	public static final String MACHINE_LEARNING_V1 =
+		"/headless-commerce-machine-learning/v1.0";
+
+	public static final String PUNCHOUT_V1 = "/headless-commerce-punchout/v1.0";
+
+}
