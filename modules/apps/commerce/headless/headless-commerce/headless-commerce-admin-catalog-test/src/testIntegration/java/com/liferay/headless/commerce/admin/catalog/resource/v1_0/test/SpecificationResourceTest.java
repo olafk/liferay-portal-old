af665fe3d9698be9c97c85d9e4d6a319e7190244
Brian Chan <brian.chan@liferay.com>
2024-08-11 17:36:23 -0300

    LPD-31974 Please resend the test because deleting all specifications before a test is an antipattern. What if we need it add it one day or another test requires it.

--- modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-catalog-test/src/testIntegration/java/com/liferay/headless/commerce/admin/catalog/resource/v1_0/test/SpecificationResourceTest.java
+++ modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-catalog-test/src/testIntegration/java/com/liferay/headless/commerce/admin/catalog/resource/v1_0/test/SpecificationResourceTest.java
@@ -6,155 +6,15 @@
 package com.liferay.headless.commerce.admin.catalog.resource.v1_0.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
-import com.liferay.commerce.product.service.CPSpecificationOptionLocalService;
-import com.liferay.headless.commerce.admin.catalog.client.dto.v1_0.Specification;
-import com.liferay.headless.commerce.core.util.LanguageUtils;
-import com.liferay.portal.kernel.test.util.RandomTestUtil;
-import com.liferay.portal.kernel.util.StringUtil;
-import com.liferay.portal.test.rule.Inject;
 
-import org.junit.Before;
 import org.junit.Ignore;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 
 /**
  * @author Zoltán Takács
  */
+@Ignore
 @RunWith(Arquillian.class)
 public class SpecificationResourceTest
 	extends BaseSpecificationResourceTestCase {
-
-	@Before
-	@Override
-	public void setUp() throws Exception {
-		super.setUp();
-
-		_cpSpecificationOptionLocalService.deleteCPSpecificationOptions(
-			testCompany.getCompanyId());
-	}
-
-	@Ignore
-	@Override
-	@Test
-	public void testGraphQLGetSpecification() throws Exception {
-	}
-
-	@Ignore
-	@Override
-	@Test
-	public void testGraphQLGetSpecificationByExternalReferenceCode()
-		throws Exception {
-	}
-
-	@Ignore
-	@Override
-	@Test
-	public void testGraphQLGetSpecificationByExternalReferenceCodeNotFound()
-		throws Exception {
-	}
-
-	@Ignore
-	@Override
-	@Test
-	public void testGraphQLGetSpecificationNotFound() throws Exception {
-	}
-
-	@Ignore
-	@Override
-	@Test
-	public void testGraphQLGetSpecificationsPage() throws Exception {
-	}
-
-	@Override
-	protected String[] getIgnoredEntityFieldNames() {
-		return new String[] {"title"};
-	}
-
-	@Override
-	protected Specification randomSpecification() throws Exception {
-		return new Specification() {
-			{
-				externalReferenceCode = RandomTestUtil.randomString();
-				facetable = true;
-				id = RandomTestUtil.randomLong();
-				key = StringUtil.toLowerCase(RandomTestUtil.randomString());
-				title = LanguageUtils.getLanguageIdMap(
-					RandomTestUtil.randomLocaleStringMap());
-			}
-		};
-	}
-
-	@Override
-	protected Specification testDeleteSpecification_addSpecification()
-		throws Exception {
-
-		return specificationResource.postSpecification(randomSpecification());
-	}
-
-	@Override
-	protected Specification
-			testDeleteSpecificationByExternalReferenceCode_addSpecification()
-		throws Exception {
-
-		return specificationResource.postSpecification(randomSpecification());
-	}
-
-	@Override
-	protected Specification testGetSpecification_addSpecification()
-		throws Exception {
-
-		return specificationResource.postSpecification(randomSpecification());
-	}
-
-	@Override
-	protected Specification
-			testGetSpecificationByExternalReferenceCode_addSpecification()
-		throws Exception {
-
-		return specificationResource.postSpecification(randomSpecification());
-	}
-
-	@Override
-	protected Specification testGetSpecificationsPage_addSpecification(
-			Specification specification)
-		throws Exception {
-
-		return specificationResource.postSpecification(randomSpecification());
-	}
-
-	@Override
-	protected Specification testGraphQLSpecification_addSpecification()
-		throws Exception {
-
-		return specificationResource.postSpecification(randomSpecification());
-	}
-
-	@Override
-	protected Specification testPatchSpecification_addSpecification()
-		throws Exception {
-
-		return specificationResource.postSpecification(randomSpecification());
-	}
-
-	@Override
-	protected Specification
-			testPatchSpecificationByExternalReferenceCode_addSpecification()
-		throws Exception {
-
-		return specificationResource.postSpecification(randomSpecification());
-	}
-
-	@Override
-	protected Specification testPostSpecification_addSpecification(
-			Specification specification)
-		throws Exception {
-
-		return specificationResource.postSpecification(randomSpecification());
-	}
-
-	@Inject
-	private static CPSpecificationOptionLocalService
-		_cpSpecificationOptionLocalService;
-
 }
