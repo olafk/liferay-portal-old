a17cfc8d89d6abfb16643f6a8902a546e32264c8
Brian Chan <brian.chan@liferay.com>
2024-08-13 15:09:03 -0300

    LPD-28759 auto SF

--- portal-impl/src/com/liferay/portal/internal/servlet/MainServlet.java
+++ portal-impl/src/com/liferay/portal/internal/servlet/MainServlet.java
@@ -769,8 +769,8 @@ public class MainServlet extends HttpServlet {
 				GetterUtil.getString(
 					PropsValues.COMPANY_DEFAULT_VIRTUAL_HOST_MAIL_DOMAIN,
 					PropsValues.COMPANY_DEFAULT_WEB_ID),
-				0, true, PropsValues.COMPANY_DEFAULT_ADD_DEFAULT_ADMIN_USER, null,
-				null, null, null, null, null);
+				0, true, PropsValues.COMPANY_DEFAULT_ADD_DEFAULT_ADMIN_USER,
+				null, null, null, null, null, null);
 		}
 
 		if (Validator.isNull(PropsValues.COMPANY_DEFAULT_WEB_ID)) {
