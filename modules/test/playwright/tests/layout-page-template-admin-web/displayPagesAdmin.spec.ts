9120ed2b9e8849e8a2b395dd234bf2b8455a93f7
Sandro Chinea <sandrochinea@gmail.com>
2024-08-21 13:42:10 +0100

    LPD-34205 Group tests

--- modules/test/playwright/tests/layout-page-template-admin-web/displayPagesAdmin.spec.ts
+++ modules/test/playwright/tests/layout-page-template-admin-web/displayPagesAdmin.spec.ts
@@ -71,6 +71,343 @@ async function addBasicJournalArticleWithSpecificDisplayPageTemplate(
 	);
 }
 
+test.describe('Usages', () => {
+	test(
+		'Can assign usage to default even if the default display page template does not exist',
+		{
+			tag: '@LPS-121199',
+		},
+		async ({
+			apiHelpers,
+			displayPageTemplatesPage,
+			journalEditArticlePage,
+			journalPage,
+			page,
+			site,
+		}) => {
+
+			// Create a display page template for Basic Web Content
+
+			await displayPageTemplatesPage.goto(site.friendlyUrlPath);
+
+			const displayPageTemplateName =
+				'basicWebContentDpt' + getRandomInt();
+
+			await displayPageTemplatesPage.createTemplate({
+				contentSubtype: 'Basic Web Content',
+				contentType: 'Web Content Article',
+				name: displayPageTemplateName,
+			});
+
+			// Create a Basic Web Content and set the template as its specific display page template
+
+			const journalArticleTitle = getRandomString();
+
+			await addBasicJournalArticleWithSpecificDisplayPageTemplate(
+				apiHelpers,
+				displayPageTemplateName,
+				journalArticleTitle,
+				journalEditArticlePage,
+				journalPage,
+				page,
+				site
+			);
+
+			// View display page template usages and check that option 'Assign to Default' is displayed
+
+			await displayPageTemplatesPage.goto(site.friendlyUrlPath);
+
+			await displayPageTemplatesPage.viewUsages(displayPageTemplateName);
+
+			await expect(page.getByText(journalArticleTitle)).toBeVisible();
+
+			const firstRowCheckbox = page
+				.getByRole('row')
+				.getByRole('checkbox')
+				.first();
+
+			await firstRowCheckbox.click();
+
+			await page.getByRole('button', {name: 'Actions'}).click();
+
+			await expect(
+				page.getByRole('menuitem', {
+					exact: true,
+					name: 'Assign to Default',
+				})
+			).toBeVisible();
+		}
+	);
+
+	test(
+		'Can assign usage to default even if the default display page template exists',
+		{
+			tag: '@LPS-121199',
+		},
+		async ({
+			apiHelpers,
+			displayPageTemplatesPage,
+			journalEditArticlePage,
+			journalPage,
+			page,
+			site,
+		}) => {
+
+			// Create a display page template for Basic Web Content and mark as default
+
+			await displayPageTemplatesPage.goto(site.friendlyUrlPath);
+
+			const defaultDisplayPageTemplateName = getRandomString();
+
+			await displayPageTemplatesPage.createTemplate({
+				contentSubtype: 'Basic Web Content',
+				contentType: 'Web Content Article',
+				name: defaultDisplayPageTemplateName,
+			});
+
+			await displayPageTemplatesPage.markAsDefault(
+				defaultDisplayPageTemplateName
+			);
+
+			// Create a display page template for Basic Web Content
+
+			const displayPageTemplateName =
+				'basicWebContentDpt' + getRandomInt();
+
+			await displayPageTemplatesPage.createTemplate({
+				contentSubtype: 'Basic Web Content',
+				contentType: 'Web Content Article',
+				name: displayPageTemplateName,
+			});
+
+			// Create a Basic Web Content and set the template as its specific display page template
+
+			const journalArticleTitle = getRandomString();
+
+			await addBasicJournalArticleWithSpecificDisplayPageTemplate(
+				apiHelpers,
+				displayPageTemplateName,
+				journalArticleTitle,
+				journalEditArticlePage,
+				journalPage,
+				page,
+				site
+			);
+
+			// View display page template usages and check that option 'Assign to Default (defaultTemplate)' is displayed
+
+			await displayPageTemplatesPage.goto(site.friendlyUrlPath);
+
+			await displayPageTemplatesPage.viewUsages(displayPageTemplateName);
+
+			await expect(page.getByText(journalArticleTitle)).toBeVisible();
+
+			const firstRowCheckbox = page
+				.getByRole('row')
+				.getByRole('checkbox')
+				.first();
+
+			await firstRowCheckbox.click();
+
+			await page.getByRole('button', {name: 'Actions'}).click();
+
+			await expect(
+				page.getByRole('menuitem', {
+					exact: true,
+					name: `Assign to Default (${defaultDisplayPageTemplateName})`,
+				})
+			).toBeVisible();
+		}
+	);
+
+	test(
+		'Can assign multiple usages to default',
+		{
+			tag: '@LPS-121199',
+		},
+		async ({
+			apiHelpers,
+			displayPageTemplatesPage,
+			journalEditArticlePage,
+			journalPage,
+			page,
+			site,
+		}) => {
+
+			// Create a display page template for Basic Web Content
+
+			await displayPageTemplatesPage.goto(site.friendlyUrlPath);
+
+			const displayPageTemplateName = getRandomString();
+
+			await displayPageTemplatesPage.createTemplate({
+				contentSubtype: 'Basic Web Content',
+				contentType: 'Web Content Article',
+				name: displayPageTemplateName,
+			});
+
+			// Create three basic web contents and set its specific display page templates
+
+			for (let i = 1; i < 4; i++) {
+				const journalArticleTitle = getRandomString();
+
+				await addBasicJournalArticleWithSpecificDisplayPageTemplate(
+					apiHelpers,
+					displayPageTemplateName,
+					journalArticleTitle,
+					journalEditArticlePage,
+					journalPage,
+					page,
+					site
+				);
+			}
+
+			// View usages of display page template
+
+			await displayPageTemplatesPage.goto(site.friendlyUrlPath);
+
+			await displayPageTemplatesPage.viewUsages(displayPageTemplateName);
+
+			// Assign to default all the usages leaving the usages empty
+
+			for (const rowCheckbox of await page
+				.getByRole('row')
+				.getByRole('checkbox')
+				.all()) {
+				await expect(rowCheckbox).toBeVisible();
+				await rowCheckbox.click();
+			}
+
+			await page.getByRole('button', {name: 'Actions'}).click();
+
+			const assignToDefaultMenuItem = page.getByRole('menuitem', {
+				exact: true,
+				name: `Assign to Default`,
+			});
+
+			await expect(assignToDefaultMenuItem).toBeVisible();
+
+			page.on('dialog', async (dialog) => {
+				dialog.accept();
+			});
+
+			await assignToDefaultMenuItem.click();
+
+			await expect(
+				page.getByText('There are no display page template usages.')
+			).toBeVisible();
+		}
+	);
+
+	test(
+		'View usages for blogs entry',
+		{
+			tag: '@LPS-123480',
+		},
+		async ({
+			blogsEditBlogEntryPage,
+			displayPageTemplatesPage,
+			page,
+			site,
+		}) => {
+
+			// Create a display page template for Blogs Entry
+
+			await displayPageTemplatesPage.goto(site.friendlyUrlPath);
+
+			const displayPageTemplateName = getRandomString();
+
+			await displayPageTemplatesPage.createTemplate({
+				contentType: 'Blogs Entry',
+				name: displayPageTemplateName,
+			});
+
+			// Create a Blogs Entry and set the template as its specific display page template
+
+			await blogsEditBlogEntryPage.goto(site.friendlyUrlPath);
+
+			const title = getRandomString();
+			const content = getRandomString();
+
+			await blogsEditBlogEntryPage.editBlogEntry({
+				content,
+				publish: false,
+				title,
+			});
+
+			await blogsEditBlogEntryPage.selectSpecificDisplayPage(
+				displayPageTemplateName
+			);
+
+			await blogsEditBlogEntryPage.publishBlogEntry();
+
+			// View usages of display page template
+
+			await displayPageTemplatesPage.goto(site.friendlyUrlPath);
+
+			await displayPageTemplatesPage.viewUsages(displayPageTemplateName);
+
+			const firstRowCheckbox = page
+				.getByRole('row')
+				.getByRole('checkbox')
+				.first();
+			await expect(firstRowCheckbox).toBeVisible();
+		}
+	);
+
+	test(
+		'View usages for basic document',
+		{
+			tag: '@LPS-123480',
+		},
+		async ({
+			displayPageTemplatesPage,
+			documentLibraryEditFilePage,
+			page,
+			site,
+		}) => {
+
+			// Create a display page template for Basic Document
+
+			await displayPageTemplatesPage.goto(site.friendlyUrlPath);
+
+			const displayPageTemplateName = getRandomString();
+
+			await displayPageTemplatesPage.createTemplate({
+				contentSubtype: 'Basic Document',
+				contentType: 'Document',
+				name: displayPageTemplateName,
+			});
+
+			// Create a Basic Document and set the template as its specific display page template
+
+			await documentLibraryEditFilePage.goto(site.friendlyUrlPath);
+
+			const title = getRandomString();
+
+			await page.getByLabel('Title').fill(title);
+
+			await documentLibraryEditFilePage.selectSpecificDisplayPage(
+				displayPageTemplateName
+			);
+
+			await documentLibraryEditFilePage.publishFileEntry();
+
+			// View usages of display page template
+
+			await displayPageTemplatesPage.goto(site.friendlyUrlPath);
+
+			await displayPageTemplatesPage.viewUsages(displayPageTemplateName);
+
+			const firstRowCheckbox = page
+				.getByRole('row')
+				.getByRole('checkbox')
+				.first();
+			await expect(firstRowCheckbox).toBeVisible();
+		}
+	);
+});
+
 test('Checks that the card checkbox has the correct aria label', async ({
 	displayPageTemplatesPage,
 	page,
@@ -95,231 +432,6 @@ test('Checks that the card checkbox has the correct aria label', async ({
 		page.getByLabel(`Select ${displayPageTemplateName}`)
 	).toBeVisible();
 });
-
-test(
-	'Can assign usage to default even if the default display page template does not exist',
-	{
-		tag: '@LPS-121199',
-	},
-	async ({
-		apiHelpers,
-		displayPageTemplatesPage,
-		journalEditArticlePage,
-		journalPage,
-		page,
-		site,
-	}) => {
-
-		// Create a display page template for Basic Web Content
-
-		await displayPageTemplatesPage.goto(site.friendlyUrlPath);
-
-		const displayPageTemplateName = 'basicWebContentDpt' + getRandomInt();
-
-		await displayPageTemplatesPage.createTemplate({
-			contentSubtype: 'Basic Web Content',
-			contentType: 'Web Content Article',
-			name: displayPageTemplateName,
-		});
-
-		// Create a Basic Web Content and set the template as its specific display page template
-
-		const journalArticleTitle = getRandomString();
-
-		await addBasicJournalArticleWithSpecificDisplayPageTemplate(
-			apiHelpers,
-			displayPageTemplateName,
-			journalArticleTitle,
-			journalEditArticlePage,
-			journalPage,
-			page,
-			site
-		);
-
-		// View display page template usages and check that option 'Assign to Default' is displayed
-
-		await displayPageTemplatesPage.goto(site.friendlyUrlPath);
-
-		await displayPageTemplatesPage.viewUsages(displayPageTemplateName);
-
-		await expect(page.getByText(journalArticleTitle)).toBeVisible();
-
-		const firstRowCheckbox = page
-			.getByRole('row')
-			.getByRole('checkbox')
-			.first();
-
-		await firstRowCheckbox.click();
-
-		await page.getByRole('button', {name: 'Actions'}).click();
-
-		await expect(
-			page.getByRole('menuitem', {
-				exact: true,
-				name: 'Assign to Default',
-			})
-		).toBeVisible();
-	}
-);
-
-test(
-	'Can assign usage to default even if the default display page template exists',
-	{
-		tag: '@LPS-121199',
-	},
-	async ({
-		apiHelpers,
-		displayPageTemplatesPage,
-		journalEditArticlePage,
-		journalPage,
-		page,
-		site,
-	}) => {
-
-		// Create a display page template for Basic Web Content and mark as default
-
-		await displayPageTemplatesPage.goto(site.friendlyUrlPath);
-
-		const defaultDisplayPageTemplateName = getRandomString();
-
-		await displayPageTemplatesPage.createTemplate({
-			contentSubtype: 'Basic Web Content',
-			contentType: 'Web Content Article',
-			name: defaultDisplayPageTemplateName,
-		});
-
-		await displayPageTemplatesPage.markAsDefault(
-			defaultDisplayPageTemplateName
-		);
-
-		// Create a display page template for Basic Web Content
-
-		const displayPageTemplateName = 'basicWebContentDpt' + getRandomInt();
-
-		await displayPageTemplatesPage.createTemplate({
-			contentSubtype: 'Basic Web Content',
-			contentType: 'Web Content Article',
-			name: displayPageTemplateName,
-		});
-
-		// Create a Basic Web Content and set the template as its specific display page template
-
-		const journalArticleTitle = getRandomString();
-
-		await addBasicJournalArticleWithSpecificDisplayPageTemplate(
-			apiHelpers,
-			displayPageTemplateName,
-			journalArticleTitle,
-			journalEditArticlePage,
-			journalPage,
-			page,
-			site
-		);
-
-		// View display page template usages and check that option 'Assign to Default (defaultTemplate)' is displayed
-
-		await displayPageTemplatesPage.goto(site.friendlyUrlPath);
-
-		await displayPageTemplatesPage.viewUsages(displayPageTemplateName);
-
-		await expect(page.getByText(journalArticleTitle)).toBeVisible();
-
-		const firstRowCheckbox = page
-			.getByRole('row')
-			.getByRole('checkbox')
-			.first();
-
-		await firstRowCheckbox.click();
-
-		await page.getByRole('button', {name: 'Actions'}).click();
-
-		await expect(
-			page.getByRole('menuitem', {
-				exact: true,
-				name: `Assign to Default (${defaultDisplayPageTemplateName})`,
-			})
-		).toBeVisible();
-	}
-);
-
-test(
-	'Can assign multiple usages to default',
-	{
-		tag: '@LPS-121199',
-	},
-	async ({
-		apiHelpers,
-		displayPageTemplatesPage,
-		journalEditArticlePage,
-		journalPage,
-		page,
-		site,
-	}) => {
-
-		// Create a display page template for Basic Web Content
-
-		await displayPageTemplatesPage.goto(site.friendlyUrlPath);
-
-		const displayPageTemplateName = getRandomString();
-
-		await displayPageTemplatesPage.createTemplate({
-			contentSubtype: 'Basic Web Content',
-			contentType: 'Web Content Article',
-			name: displayPageTemplateName,
-		});
-
-		// Create three basic web contents and set its specific display page templates
-
-		for (let i = 1; i < 4; i++) {
-			const journalArticleTitle = getRandomString();
-
-			await addBasicJournalArticleWithSpecificDisplayPageTemplate(
-				apiHelpers,
-				displayPageTemplateName,
-				journalArticleTitle,
-				journalEditArticlePage,
-				journalPage,
-				page,
-				site
-			);
-		}
-
-		// View usages of display page template
-
-		await displayPageTemplatesPage.goto(site.friendlyUrlPath);
-
-		await displayPageTemplatesPage.viewUsages(displayPageTemplateName);
-
-		// Assign to default all the usages leaving the usages empty
-
-		for (const rowCheckbox of await page
-			.getByRole('row')
-			.getByRole('checkbox')
-			.all()) {
-			await expect(rowCheckbox).toBeVisible();
-			await rowCheckbox.click();
-		}
-
-		await page.getByRole('button', {name: 'Actions'}).click();
-
-		const assignToDefaultMenuItem = page.getByRole('menuitem', {
-			exact: true,
-			name: `Assign to Default`,
-		});
-
-		await expect(assignToDefaultMenuItem).toBeVisible();
-
-		page.on('dialog', async (dialog) => {
-			dialog.accept();
-		});
-
-		await assignToDefaultMenuItem.click();
-
-		await expect(
-			page.getByText('There are no display page template usages.')
-		).toBeVisible();
-	}
-);
 
 test('User can delete default display page template', async ({
 	displayPageTemplatesPage,
@@ -381,109 +493,6 @@ test('User can rename a display page', async ({
 	).toBeVisible();
 });
 
-test(
-	'View usages for blogs entry',
-	{
-		tag: '@LPS-123480',
-	},
-	async ({blogsEditBlogEntryPage, displayPageTemplatesPage, page, site}) => {
-
-		// Create a display page template for Blogs Entry
-
-		await displayPageTemplatesPage.goto(site.friendlyUrlPath);
-
-		const displayPageTemplateName = getRandomString();
-
-		await displayPageTemplatesPage.createTemplate({
-			contentType: 'Blogs Entry',
-			name: displayPageTemplateName,
-		});
-
-		// Create a Blogs Entry and set the template as its specific display page template
-
-		await blogsEditBlogEntryPage.goto(site.friendlyUrlPath);
-
-		const title = getRandomString();
-		const content = getRandomString();
-
-		await blogsEditBlogEntryPage.editBlogEntry({
-			content,
-			publish: false,
-			title,
-		});
-
-		await blogsEditBlogEntryPage.selectSpecificDisplayPage(
-			displayPageTemplateName
-		);
-
-		await blogsEditBlogEntryPage.publishBlogEntry();
-
-		// View usages of display page template
-
-		await displayPageTemplatesPage.goto(site.friendlyUrlPath);
-
-		await displayPageTemplatesPage.viewUsages(displayPageTemplateName);
-
-		const firstRowCheckbox = page
-			.getByRole('row')
-			.getByRole('checkbox')
-			.first();
-		await expect(firstRowCheckbox).toBeVisible();
-	}
-);
-
-test(
-	'View usages for basic document',
-	{
-		tag: '@LPS-123480',
-	},
-	async ({
-		displayPageTemplatesPage,
-		documentLibraryEditFilePage,
-		page,
-		site,
-	}) => {
-
-		// Create a display page template for Basic Document
-
-		await displayPageTemplatesPage.goto(site.friendlyUrlPath);
-
-		const displayPageTemplateName = getRandomString();
-
-		await displayPageTemplatesPage.createTemplate({
-			contentSubtype: 'Basic Document',
-			contentType: 'Document',
-			name: displayPageTemplateName,
-		});
-
-		// Create a Basic Document and set the template as its specific display page template
-
-		await documentLibraryEditFilePage.goto(site.friendlyUrlPath);
-
-		const title = getRandomString();
-
-		await page.getByLabel('Title').fill(title);
-
-		await documentLibraryEditFilePage.selectSpecificDisplayPage(
-			displayPageTemplateName
-		);
-
-		await documentLibraryEditFilePage.publishFileEntry();
-
-		// View usages of display page template
-
-		await displayPageTemplatesPage.goto(site.friendlyUrlPath);
-
-		await displayPageTemplatesPage.viewUsages(displayPageTemplateName);
-
-		const firstRowCheckbox = page
-			.getByRole('row')
-			.getByRole('checkbox')
-			.first();
-		await expect(firstRowCheckbox).toBeVisible();
-	}
-);
-
 testInfoPanel(
 	'View the info panel for a display page and for a folder',
 	{
@@ -520,6 +529,7 @@ testInfoPanel(
 		await expect(
 			infoPanel.locator('.sidebar-header .component-title')
 		).toContainText(displayPageTemplateFolderName);
+
 		await expect(
 			infoPanel.locator('.sidebar-header .component-subtitle')
 		).toContainText('Folder');
@@ -527,6 +537,7 @@ testInfoPanel(
 		await expect(
 			infoPanel.locator('.sidebar-body .mb-4').nth(0)
 		).toContainText('Location');
+
 		await expect(
 			infoPanel.locator('.sidebar-body .mb-4').nth(0)
 		).toContainText(`Home > ${displayPageTemplateFolderName}`);
@@ -534,6 +545,7 @@ testInfoPanel(
 		await expect(
 			infoPanel.locator('.sidebar-body .mb-4').nth(1)
 		).toContainText('Number of Items');
+
 		await expect(
 			infoPanel.locator('.sidebar-body .mb-4').nth(1)
 		).toContainText('1');
@@ -555,9 +567,11 @@ testInfoPanel(
 		await expect(
 			infoPanel.locator('.sidebar-header .component-title')
 		).toContainText(displayPageTemplateName);
+
 		await expect(
 			infoPanel.locator('.sidebar-header .component-subtitle')
 		).toContainText('Display Page Template');
+
 		await expect(
 			infoPanel.locator('.sidebar-header .label-item')
 		).toContainText('Approved');
@@ -565,6 +579,7 @@ testInfoPanel(
 		await expect(
 			infoPanel.locator('.sidebar-body .mb-4').nth(0)
 		).toContainText('Location');
+
 		await expect(
 			infoPanel.locator('.sidebar-body .mb-4').nth(0)
 		).toContainText(`Home > ${displayPageTemplateFolderName}`);
@@ -572,6 +587,7 @@ testInfoPanel(
 		await expect(
 			infoPanel.locator('.sidebar-body .mb-4').nth(1)
 		).toContainText('Content Type');
+
 		await expect(
 			infoPanel.locator('.sidebar-body .mb-4').nth(1)
 		).toContainText('Blogs Entry');
