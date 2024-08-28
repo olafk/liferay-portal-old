d898bff849920e8aaa6909032c1fe6ad131d203c
Attila Bakay <attila.bakay@liferay.com>
2024-08-28 14:34:33 +0200

    LPD-34159 Use GetterUtil.getString to avoid 'null'

--- modules/apps/headless/headless-delivery/headless-delivery-api/src/main/java/com/liferay/headless/delivery/dto/v1_0/util/ContentFieldUtil.java
+++ modules/apps/headless/headless-delivery/headless-delivery-api/src/main/java/com/liferay/headless/delivery/dto/v1_0/util/ContentFieldUtil.java
@@ -34,6 +34,7 @@ import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.repository.model.FileEntry;
 import com.liferay.portal.kernel.service.LayoutLocalService;
 import com.liferay.portal.kernel.util.DateUtil;
+import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.vulcan.dto.converter.DTOConverter;
@@ -550,7 +551,7 @@ public class ContentFieldUtil {
 			return new ContentFieldValue();
 		}
 
-		String valueString = String.valueOf(value.getString(locale));
+		String valueString = GetterUtil.getString(value.getString(locale));
 
 		return _getContentFieldValue(
 			ddmFormField, dlAppService, dlURLHelper, dtoConverterContext,
