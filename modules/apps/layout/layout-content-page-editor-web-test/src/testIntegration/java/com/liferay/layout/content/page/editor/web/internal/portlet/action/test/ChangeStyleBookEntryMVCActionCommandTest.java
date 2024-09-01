5f7fd9ffe7e844fe24e045a324035a100daaccce
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2024-09-01 18:28:33 +0100

    LPD-35310 Migrates CannotLockContentPageWhenChangeMasterPageOrStyleBook to integration test

---
+++ modules/apps/layout/layout-content-page-editor-web-test/src/testIntegration/java/com/liferay/layout/content/page/editor/web/internal/portlet/action/test/ChangeStyleBookEntryMVCActionCommandTest.java
@@ -0,0 +1,45 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.layout.content.page.editor.web.internal.portlet.action.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.test.ReflectionTestUtil;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@RunWith(Arquillian.class)
+public class ChangeStyleBookEntryMVCActionCommandTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			PermissionCheckerMethodTestRule.INSTANCE);
+
+	@Test
+	public void testIsLayoutLockRequired() {
+		Assert.assertFalse(
+			ReflectionTestUtil.invoke(
+				_mvcActionCommand, "isLayoutLockRequired()", new Class<?>[0]));
+	}
+
+	@Inject(
+		filter = "mvc.command.name=/layout_content_page_editor/change_style_book_entry"
+	)
+	private MVCActionCommand _mvcActionCommand;
+}
