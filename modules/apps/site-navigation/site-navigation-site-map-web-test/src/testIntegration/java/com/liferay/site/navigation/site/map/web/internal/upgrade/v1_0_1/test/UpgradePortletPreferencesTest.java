eee2754f6056b1c357c589911a972baefa088b61
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2024-09-04 08:03:50 +0200

    LPD-33733 Add integration tests to assert the upgrade process to update the Site Navigation Site Map Portlet preferences

---
+++ modules/apps/site-navigation/site-navigation-site-map-web-test/src/testIntegration/java/com/liferay/site/navigation/site/map/web/internal/upgrade/v1_0_1/test/UpgradePortletPreferencesTest.java
@@ -0,0 +1,46 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.site.navigation.site.map.web.internal.upgrade.v1_0_1.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.portal.kernel.version.Version;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
+import com.liferay.portlet.display.template.test.util.BaseUpgradePortletPreferencesTestCase;
+
+import org.junit.runner.RunWith;
+
+/**
+ * @author Lourdes Fernández Besada
+ */
+@RunWith(Arquillian.class)
+public class UpgradePortletPreferencesTest
+	extends BaseUpgradePortletPreferencesTestCase {
+
+	@Override
+	protected String getPortletId() {
+		return "com_liferay_site_navigation_site_map_web_portlet_" +
+			"SiteNavigationSiteMapPortlet";
+	}
+
+	@Override
+	protected UpgradeStepRegistrator getUpgradeStepRegistrator() {
+		return _upgradeStepRegistrator;
+	}
+
+	@Override
+	protected Version getVersion() {
+		return _VERSION;
+	}
+
+	private static final Version _VERSION = new Version(1, 0, 1);
+
+	@Inject(
+		filter = "(&(component.name=com.liferay.site.navigation.site.map.web.internal.upgrade.registry.SiteNavigationSiteMapWebUpgradeStepRegistrator))"
+	)
+	private static UpgradeStepRegistrator _upgradeStepRegistrator;
+
+}
