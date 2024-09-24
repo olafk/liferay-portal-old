070328bc1e8246971b0fe9e9807402c277ad1ec3
Pedro Leite <pedro.leite@liferay.com>
2024-09-24 09:10:28 -0300

    LPD-35168 Remove deletion from the test method

--- modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/service/test/ObjectRelationshipLocalServiceTest.java
+++ modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/service/test/ObjectRelationshipLocalServiceTest.java
@@ -487,6 +487,10 @@ public class ObjectRelationshipLocalServiceTest {
 						_objectDefinitionLocalService::getObjectDefinition),
 					_objectDefinitionLocalService);
 			});
+
+		TreeTestUtil.deleteObjectDefinitionHierarchy(
+			_objectDefinitionLocalService, new String[] {"C_AA", "C_A"},
+			_objectEntryLocalService);
 	}
 
 	@Test
@@ -505,6 +509,10 @@ public class ObjectRelationshipLocalServiceTest {
 						objectDefinition1.getObjectDefinitionId(),
 						_objectDefinitionLocalService::getObjectDefinition),
 					_objectDefinitionLocalService));
+
+		TreeTestUtil.deleteObjectDefinitionHierarchy(
+			_objectDefinitionLocalService, new String[] {"C_AA", "C_A"},
+			_objectEntryLocalService);
 	}
 
 	@Test
@@ -532,6 +540,10 @@ public class ObjectRelationshipLocalServiceTest {
 						objectDefinition1.getObjectDefinitionId(),
 						_objectDefinitionLocalService::getObjectDefinition),
 					_objectDefinitionLocalService));
+
+		TreeTestUtil.deleteObjectDefinitionHierarchy(
+			_objectDefinitionLocalService, new String[] {"C_AA", "C_A"},
+			_objectEntryLocalService);
 	}
 
 	@Test
@@ -1228,10 +1240,6 @@ public class ObjectRelationshipLocalServiceTest {
 				objectDefinition1.getObjectDefinitionId()),
 			_objectDefinitionLocalService.getObjectDefinition(
 				objectDefinition2.getObjectDefinitionId()));
-
-		TreeTestUtil.deleteObjectDefinitionHierarchy(
-			_objectDefinitionLocalService, new String[] {"C_AA", "C_A"},
-			_objectEntryLocalService);
 	}
 
 	private void _testCreateManyToManyObjectRelationshipTable(
