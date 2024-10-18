a32d7e6d5310a015e742e536c963120146737653
rafaellajordao <rafaella.jordao@liferay.com>
2024-10-18 15:18:41 -0300

    LPD-39610 creating the empty state condition in the mockAssetAppearsOnReq

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/test/graphql-data.js
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/test/graphql-data.js
@@ -67,7 +67,79 @@ const METRIC_TYPENAME_MAP = {
 	trend: 'Trend'
 };
 
-export function mockAssetAppearsOnReq(variables) {
+export function mockAssetAppearsOnReq(variables, empty) {
+	let assetMetrics = [];
+
+	if (!empty) {
+		assetMetrics = [
+			{
+				__typename: 'BlogMetric',
+				assetId:
+					'http://liferay.com/web/test/abc/123/a42d8ae1-d145-40da-8150-3fe28deb04ad',
+				assetTitle: 'a42d8ae1-d145-40da-8150-3fe28deb04ad',
+				selectedMetrics: [
+					{
+						__typename: 'AssetMetric',
+						name: 'viewsMetric',
+						value: 113948
+					}
+				]
+			},
+			{
+				__typename: 'BlogMetric',
+				assetId:
+					'http://liferay.com/web/test/abc/123/a9bd5ff0-d623-4743-a2b5-3dbafd8d2a86',
+				assetTitle: 'a9bd5ff0-d623-4743-a2b5-3dbafd8d2a86',
+				selectedMetrics: [
+					{
+						__typename: 'AssetMetric',
+						name: 'viewsMetric',
+						value: 912285
+					}
+				]
+			},
+			{
+				__typename: 'BlogMetric',
+				assetId:
+					'http://liferay.com/web/test/abc/123/b8cbb4b5-5a1f-425d-a06b-c0544e2991ce',
+				assetTitle: 'b8cbb4b5-5a1f-425d-a06b-c0544e2991ce',
+				selectedMetrics: [
+					{
+						__typename: 'AssetMetric',
+						name: 'viewsMetric',
+						value: 431627
+					}
+				]
+			},
+			{
+				__typename: 'BlogMetric',
+				assetId:
+					'http://liferay.com/web/test/abc/123/280b69a6-b2dc-4d8f-a0d4-421b450c257b',
+				assetTitle: '280b69a6-b2dc-4d8f-a0d4-421b450c257b',
+				selectedMetrics: [
+					{
+						__typename: 'AssetMetric',
+						name: 'viewsMetric',
+						value: 273970
+					}
+				]
+			},
+			{
+				__typename: 'BlogMetric',
+				assetId:
+					'http://liferay.com/web/test/abc/123/a9ca649d-fc1f-4499-ab8a-c39f2ca1b024',
+				assetTitle: 'a9ca649d-fc1f-4499-ab8a-c39f2ca1b024',
+				selectedMetrics: [
+					{
+						__typename: 'AssetMetric',
+						name: 'viewsMetric',
+						value: 95519
+					}
+				]
+			}
+		];
+	}
+
 	return {
 		request: {
 			query: AssetAppearsOnQuery,
@@ -88,74 +160,8 @@ export function mockAssetAppearsOnReq(variables) {
 			data: {
 				assetPages: {
 					__typename: 'AssetPages',
-					assetMetrics: [
-						{
-							__typename: 'BlogMetric',
-							assetId:
-								'http://liferay.com/web/test/abc/123/a42d8ae1-d145-40da-8150-3fe28deb04ad',
-							assetTitle: 'a42d8ae1-d145-40da-8150-3fe28deb04ad',
-							selectedMetrics: [
-								{
-									__typename: 'AssetMetric',
-									name: 'viewsMetric',
-									value: 113948
-								}
-							]
-						},
-						{
-							__typename: 'BlogMetric',
-							assetId:
-								'http://liferay.com/web/test/abc/123/a9bd5ff0-d623-4743-a2b5-3dbafd8d2a86',
-							assetTitle: 'a9bd5ff0-d623-4743-a2b5-3dbafd8d2a86',
-							selectedMetrics: [
-								{
-									__typename: 'AssetMetric',
-									name: 'viewsMetric',
-									value: 912285
-								}
-							]
-						},
-						{
-							__typename: 'BlogMetric',
-							assetId:
-								'http://liferay.com/web/test/abc/123/b8cbb4b5-5a1f-425d-a06b-c0544e2991ce',
-							assetTitle: 'b8cbb4b5-5a1f-425d-a06b-c0544e2991ce',
-							selectedMetrics: [
-								{
-									__typename: 'AssetMetric',
-									name: 'viewsMetric',
-									value: 431627
-								}
-							]
-						},
-						{
-							__typename: 'BlogMetric',
-							assetId:
-								'http://liferay.com/web/test/abc/123/280b69a6-b2dc-4d8f-a0d4-421b450c257b',
-							assetTitle: '280b69a6-b2dc-4d8f-a0d4-421b450c257b',
-							selectedMetrics: [
-								{
-									__typename: 'AssetMetric',
-									name: 'viewsMetric',
-									value: 273970
-								}
-							]
-						},
-						{
-							__typename: 'BlogMetric',
-							assetId:
-								'http://liferay.com/web/test/abc/123/a9ca649d-fc1f-4499-ab8a-c39f2ca1b024',
-							assetTitle: 'a9ca649d-fc1f-4499-ab8a-c39f2ca1b024',
-							selectedMetrics: [
-								{
-									__typename: 'AssetMetric',
-									name: 'viewsMetric',
-									value: 95519
-								}
-							]
-						}
-					],
-					total: 1000
+					assetMetrics,
+					total: empty ? 0 : 1000
 				}
 			}
 		}
