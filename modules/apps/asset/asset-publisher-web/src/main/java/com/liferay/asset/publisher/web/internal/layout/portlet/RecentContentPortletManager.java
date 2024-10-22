7c5d1af2fe7ca7bb18fd36eb3dec12e90b8c28b4
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2024-10-22 11:15:03 +0200

    LPD-39785 Don't show highest rated assets, most viewed assets, recent content and related assets in add panel

---
+++ modules/apps/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/internal/layout/portlet/RecentContentPortletManager.java
@@ -0,0 +1,35 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.asset.publisher.web.internal.layout.portlet;
+
+import com.liferay.asset.publisher.constants.AssetPublisherPortletKeys;
+import com.liferay.layout.portlet.PortletManager;
+import com.liferay.portal.kernel.feature.flag.FeatureFlagManagerUtil;
+import com.liferay.portal.kernel.model.Layout;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@Component(
+	property = "javax.portlet.name=" + AssetPublisherPortletKeys.RECENT_CONTENT,
+	service = PortletManager.class
+)
+public class RecentContentPortletManager implements PortletManager {
+
+	@Override
+	public boolean isVisible(Layout layout) {
+		if (!FeatureFlagManagerUtil.isEnabled(
+				layout.getCompanyId(), "LPD-39304")) {
+
+			return false;
+		}
+
+		return true;
+	}
+
+}
