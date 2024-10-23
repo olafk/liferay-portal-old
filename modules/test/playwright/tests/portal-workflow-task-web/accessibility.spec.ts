323872dc42e69079cabd6c6f1866ac8d8626dc8b
Carlos Montenegro <carlos.montenegro@liferay.com>
2024-10-23 12:26:13 -0300

    LPD-39861 Add Playwright test to check if workflow tasks view page is accessible

---
+++ modules/test/playwright/tests/portal-workflow-task-web/accessibility.spec.ts
@@ -0,0 +1,103 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import {expect, mergeTests} from '@playwright/test';
+
+import {apiHelpersTest} from '../../fixtures/apiHelpersTest';
+import {loginTest} from '../../fixtures/loginTest';
+import {workflowPagesTest} from '../../fixtures/workflowPagesTest';
+import {checkAccessibility} from '../../utils/checkAccessibility';
+import clearInvalidUserNotifications from '../../utils/clearInvalidUserNotifications';
+import retryGetWorkflowTasksBySubmittingUser from '../../utils/retryGetWorkflowTasksBySubmittingUser';
+
+export const test = mergeTests(apiHelpersTest, loginTest(), workflowPagesTest);
+
+interface CreatedEntities {
+	blogPosts?: TBlogPost[];
+}
+
+const createdEntities: CreatedEntities = {};
+
+test.afterEach(async ({apiHelpers, configurationTabPage, page}) => {
+	await configurationTabPage.goTo();
+
+	await configurationTabPage.unassignWorkflowFromAssetType('Blogs Entry');
+
+	if (createdEntities.blogPosts?.length) {
+		for (const blog of createdEntities.blogPosts) {
+			await apiHelpers.headlessDelivery.deleteBlog(blog.id);
+		}
+	}
+
+	delete createdEntities.blogPosts;
+
+	await clearInvalidUserNotifications(page);
+});
+
+test('Workflow task view page is accessible', async ({
+	apiHelpers,
+	configurationTabPage,
+	page,
+	workflowTaskDetailsPage,
+}) => {
+	await configurationTabPage.goTo();
+
+	await configurationTabPage.assignWorkflowToAssetType(
+		'Single Approver',
+		'Blogs Entry'
+	);
+
+	const site = await apiHelpers.headlessSite.getSiteByERC('L_GUEST');
+
+	const blogPost = await apiHelpers.headlessDelivery.postBlog(site.id);
+
+	createdEntities.blogPosts = [blogPost];
+
+	const submittingUserId = blogPost.creator.id;
+
+	const workflowTaskDefinitions = await retryGetWorkflowTasksBySubmittingUser(
+		{
+			expectedNumberOfTasks: 1,
+			page,
+			submittingUser: submittingUserId,
+		}
+	);
+
+	const workflowTaskDefinition = workflowTaskDefinitions.items[0];
+
+	await apiHelpers.headlessAdminWorkflow.postAssignTaskToUser(
+		workflowTaskDefinition.id,
+		submittingUserId
+	);
+
+	await page.getByTitle('User Profile Menu').click();
+
+	await page.getByRole('menuitem', {name: 'My Workflow Tasks'}).click();
+
+	await page
+		.getByRole('link', {
+			name: workflowTaskDefinition.objectReviewed.assetTitle,
+		})
+		.click();
+
+	await page.waitForLoadState();
+
+	const taskContentActionsSelectors = ['edit', 'view', 'viewUsages'].map(
+		(sufix) => `#${workflowTaskDetailsPage.portletNameSpace + sufix}`
+	);
+
+	const openActionsMenuLinkSelector =
+		'a[title="Open Actions Menu"][role="button"]';
+
+	await checkAccessibility({
+		page,
+		selectors: [
+			...taskContentActionsSelectors,
+			openActionsMenuLinkSelector,
+		],
+	});
+
+	expect(test.info().errors).toHaveLength(0);
+});
