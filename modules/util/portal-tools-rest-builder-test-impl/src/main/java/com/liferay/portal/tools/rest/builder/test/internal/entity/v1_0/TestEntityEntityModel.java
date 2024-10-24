d00c7fa6a4b4414cc757d92ed458ec3af73586c1
Magdalena Jedraszak <magdalena.jedraszak@liferay.com>
2024-10-24 12:24:59 +0200

    LPD-35336 Create EntityModel for TestEntity

---
+++ modules/util/portal-tools-rest-builder-test-impl/src/main/java/com/liferay/portal/tools/rest/builder/test/internal/entity/v1_0/TestEntityEntityModel.java
@@ -0,0 +1,72 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.tools.rest.builder.test.internal.entity.v1_0;
+
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.odata.entity.BooleanEntityField;
+import com.liferay.portal.odata.entity.CollectionEntityField;
+import com.liferay.portal.odata.entity.ComplexEntityField;
+import com.liferay.portal.odata.entity.DateEntityField;
+import com.liferay.portal.odata.entity.DateTimeEntityField;
+import com.liferay.portal.odata.entity.DoubleEntityField;
+import com.liferay.portal.odata.entity.EntityField;
+import com.liferay.portal.odata.entity.EntityModel;
+import com.liferay.portal.odata.entity.IdEntityField;
+import com.liferay.portal.odata.entity.IntegerEntityField;
+import com.liferay.portal.odata.entity.StringEntityField;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Magdalena Jedraszak
+ */
+public class TestEntityEntityModel implements EntityModel {
+
+	public TestEntityEntityModel(List<EntityField> entityFields) {
+		_entityFieldsMap = EntityFieldsMapFactory.create(
+			new BooleanEntityField("published", locale -> Field.STATUS),
+			new CollectionEntityField(
+				new IntegerEntityField("statusCode", locale -> Field.STATUS)),
+			new CollectionEntityField(
+				new StringEntityField(
+					"keywords", locale -> "assetTagNames.lowercase")),
+			new ComplexEntityField("customFields", entityFields),
+			new DateEntityField(
+				"expirationDate", locale -> Field.EXPIRATION_DATE,
+				locale -> Field.EXPIRATION_DATE),
+			new DateTimeEntityField(
+				"dateModified",
+				locale -> Field.getSortableFieldName(Field.MODIFIED_DATE),
+				locale -> Field.MODIFIED_DATE),
+			new DoubleEntityField(
+				Field.PRIORITY,
+				locale -> Field.getSortableFieldName(
+					Field.PRIORITY + "_Number")),
+			new IdEntityField(
+				"companyId", locale -> Field.COMPANY_ID,
+				locale -> Field.getSortableFieldName(Field.COMPANY_ID)),
+			new IdEntityField(
+				"folderId", locale -> Field.FOLDER_ID,
+				locale -> Field.getSortableFieldName(Field.FOLDER_ID)),
+			new IntegerEntityField("creatorId", locale -> Field.USER_ID),
+			new IntegerEntityField("viewCount", locale -> "viewCount"),
+			new StringEntityField("description", locale -> Field.DESCRIPTION),
+			new StringEntityField(
+				"friendlyUrl",
+				locale -> Field.getSortableFieldName("urlTitle_String")),
+			new StringEntityField(
+				"title", locale -> Field.getSortableFieldName(Field.TITLE)));
+	}
+
+	@Override
+	public Map<String, EntityField> getEntityFieldsMap() {
+		return _entityFieldsMap;
+	}
+
+	private final Map<String, EntityField> _entityFieldsMap;
+
+}
--- modules/util/portal-tools-rest-builder-test-impl/src/main/java/com/liferay/portal/tools/rest/builder/test/internal/resource/v1_0/TestEntityResourceImpl.java
+++ modules/util/portal-tools-rest-builder-test-impl/src/main/java/com/liferay/portal/tools/rest/builder/test/internal/resource/v1_0/TestEntityResourceImpl.java
@@ -5,13 +5,22 @@
 
 package com.liferay.portal.tools.rest.builder.test.internal.resource.v1_0;
 
+import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.odata.entity.BooleanEntityField;
+import com.liferay.portal.odata.entity.EntityField;
+import com.liferay.portal.odata.entity.EntityModel;
+import com.liferay.portal.odata.entity.IntegerEntityField;
+import com.liferay.portal.odata.entity.StringEntityField;
 import com.liferay.portal.tools.rest.builder.test.dto.v1_0.TestEntity;
+import com.liferay.portal.tools.rest.builder.test.internal.entity.v1_0.TestEntityEntityModel;
 import com.liferay.portal.tools.rest.builder.test.resource.v1_0.TestEntityResource;
 import com.liferay.portal.vulcan.pagination.Page;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+
+import javax.ws.rs.core.MultivaluedMap;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.ServiceScope;
@@ -24,6 +33,15 @@ import org.osgi.service.component.annotations.ServiceScope;
 	scope = ServiceScope.PROTOTYPE, service = TestEntityResource.class
 )
 public class TestEntityResourceImpl extends BaseTestEntityResourceImpl {
+
+	@Override
+	public EntityModel getEntityModel(MultivaluedMap multivaluedMap)
+		throws Exception {
+
+		List<EntityField> entityFields = _getEntityFields();
+
+		return new TestEntityEntityModel(entityFields);
+	}
 
 	@Override
 	public Page<TestEntity> getTestEntitiesPage() {
@@ -70,6 +88,15 @@ public class TestEntityResourceImpl extends BaseTestEntityResourceImpl {
 		testEntity.setId(oldTestEntity.getId());
 
 		return testEntity;
+	}
+
+	private List<EntityField> _getEntityFields() {
+		return ListUtil.fromArray(
+			new StringEntityField(
+				"customAttribute1", locale -> "customAttribute1"),
+			new IntegerEntityField(
+				"customAttribute2", locale -> "customAttribute2"),
+			new BooleanEntityField("customFlag", locale -> "customFlag"));
 	}
 
 	private static final List<TestEntity> _testEntities = new ArrayList<>();
