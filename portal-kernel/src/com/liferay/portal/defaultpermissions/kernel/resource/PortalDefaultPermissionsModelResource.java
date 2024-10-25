1fbacce2b45a2886d4cee08800cea22745ad91b7
Crescenzo Rega <crescenzo.rega@liferay.com>
2024-10-25 16:08:44 +0200

    LPD-35542 Move classes into portal-kernel

--- portal-kernel/src/com/liferay/portal/defaultpermissions/kernel/resource/PortalDefaultPermissionsModelResource.java
+++ portal-kernel/src/com/liferay/portal/defaultpermissions/kernel/resource/PortalDefaultPermissionsModelResource.java
@@ -3,9 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portal.defaultpermissions.resource;
-
-import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
+package com.liferay.portal.defaultpermissions.kernel.resource;
 
 /**
  * @author Stefano Motta
@@ -16,6 +14,10 @@ public interface PortalDefaultPermissionsModelResource {
 
 	public String getLabel();
 
-	public ExtendedObjectClassDefinition.Scope getScope();
+	public String getScope();
+
+	public default boolean isAllowOverridePermissions() {
+		return false;
+	}
 
 }
--- portal-kernel/src/com/liferay/portal/defaultpermissions/kernel/resource/PortalDefaultPermissionsModelResourceRegistry.java
+++ portal-kernel/src/com/liferay/portal/defaultpermissions/kernel/resource/PortalDefaultPermissionsModelResourceRegistry.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portal.defaultpermissions.resource;
+package com.liferay.portal.defaultpermissions.kernel.resource;
 
 import java.util.List;
 
