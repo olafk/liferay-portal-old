4e0c07edbb18d8620d1a5dd2b80abd898fd86921
Kenji Heigel <kenji.heigel@liferay.com>
2024-10-29 14:45:14 -0700

    LRCI-4871 SF

--- modules/test/playwright/tests/client-extension-web/env/set_up.sh
+++ modules/test/playwright/tests/client-extension-web/env/set_up.sh
@@ -7,10 +7,10 @@ echo CURRENT_DIR_NAME=${CURRENT_DIR_NAME}
 source ${CURRENT_DIR_NAME}/../../../env/common.sh
 
 function main {
-  combine_properties_files \
-    $(get_tomcat_portal_ext_properties_file) \
-    \
-    $(get_portal_project_dir)/workspaces/liferay-sample-workspace/configs/local/portal-ext.properties
+	combine_properties_files \
+		$(get_tomcat_portal_ext_properties_file) \
+		\
+		$(get_portal_project_dir)/workspaces/liferay-sample-workspace/configs/local/portal-ext.properties
 
 	default_set_up
 }
