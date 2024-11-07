liferay-commerce/liferay-portal

LPD-35329 add delivery group to address checkout step

modules/apps/commerce/commerce-checkout-web/src/main/java/com/liferay/commerce/checkout/web/internal/constants/CommerceCheckoutFDSNames.java
@@ -1,0 +1,18 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.commerce.checkout.web.internal.constants;
+
+import com.liferay.commerce.constants.CommercePortletKeys;
+
+/**
+ * @author Luca Pellizzon
+ */
+public class CommerceCheckoutFDSNames {
+
+	public static final String DELIVERY_GROUP =
+		CommercePortletKeys.COMMERCE_CHECKOUT + "-deliveryGroup";
+
+}

modules/apps/commerce/commerce-checkout-web/src/main/java/com/liferay/commerce/checkout/web/internal/frontend/data/set/provider/CommerceCheckoutDeliveryGroupFDSDataProvider.java
@@ -1,0 +1,111 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.commerce.checkout.web.internal.frontend.data.set.provider;
+
+import com.liferay.commerce.checkout.web.internal.constants.CommerceCheckoutFDSNames;
+import com.liferay.commerce.checkout.web.internal.model.DeliveryGroup;
+import com.liferay.commerce.checkout.web.internal.util.CommerceOrderUtil;
+import com.liferay.commerce.model.CommerceAddress;
+import com.liferay.commerce.model.CommerceOrder;
+import com.liferay.commerce.model.CommerceOrderItem;
+import com.liferay.commerce.service.CommerceAddressLocalService;
+import com.liferay.commerce.service.CommerceOrderLocalService;
+import com.liferay.frontend.data.set.provider.FDSDataProvider;
+import com.liferay.frontend.data.set.provider.search.FDSKeywords;
+import com.liferay.frontend.data.set.provider.search.FDSPagination;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.Country;
+import com.liferay.portal.kernel.search.Sort;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Portal;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Luca Pellizzon
+ */
+@Component(
+	property = "fds.data.provider.key=" + CommerceCheckoutFDSNames.DELIVERY_GROUP,
+	service = FDSDataProvider.class
+)
+public class CommerceCheckoutDeliveryGroupFDSDataProvider
+	implements FDSDataProvider<DeliveryGroup> {
+
+	@Override
+	public List<DeliveryGroup> getItems(
+			FDSKeywords fdsKeywords, FDSPagination fdsPagination,
+			HttpServletRequest httpServletRequest, Sort sort)
+		throws PortalException {
+
+		Map<String, DeliveryGroup> deliveryGroupMap = new HashMap<>();
+
+		long commerceOrderId = ParamUtil.getLong(
+			httpServletRequest, "commerceOrderId");
+
+		CommerceOrder commerceOrder =
+			_commerceOrderLocalService.getCommerceOrder(commerceOrderId);
+
+		for (CommerceOrderItem commerceOrderItem :
+				commerceOrder.getCommerceOrderItems()) {
+
+			if (!deliveryGroupMap.containsKey(
+					commerceOrderItem.getDeliveryGroup())) {
+
+				CommerceAddress commerceAddress =
+					_commerceAddressLocalService.getCommerceAddress(
+						commerceOrderItem.getShippingAddressId());
+
+				Country country = commerceAddress.getCountry();
+
+				DeliveryGroup deliveryGroup = new DeliveryGroup(
+					StringBundler.concat(
+						commerceAddress.getStreet1(),
+						StringPool.COMMA_AND_SPACE, commerceAddress.getCity(),
+						StringPool.COMMA_AND_SPACE,
+						country.getName(_portal.getLocale(httpServletRequest))),
+					commerceAddress.getCommerceAddressId(),
+					commerceOrderItem.getRequestedDeliveryDate(),
+					commerceOrderItem.getDeliveryGroup());
+
+				deliveryGroupMap.put(deliveryGroup.getName(), deliveryGroup);
+			}
+		}
+
+		return new ArrayList<>(deliveryGroupMap.values());
+	}
+
+	@Override
+	public int getItemsCount(
+			FDSKeywords fdsKeywords, HttpServletRequest httpServletRequest)
+		throws PortalException {
+
+		long commerceOrderId = ParamUtil.getLong(
+			httpServletRequest, "commerceOrderId");
+
+		return CommerceOrderUtil.commerceOrderDeliveryGroupQuantity(
+			_commerceOrderLocalService.getCommerceOrder(commerceOrderId));
+	}
+
+	@Reference
+	private CommerceAddressLocalService _commerceAddressLocalService;
+
+	@Reference
+	private CommerceOrderLocalService _commerceOrderLocalService;
+
+	@Reference
+	private Portal _portal;
+
+}

olafk/liferay-portal-old

modules/apps/commerce/commerce-checkout-web/src/main/java/com/liferay/commerce/checkout/web/internal/constants/CommerceCheckoutFDSNames.java
@@ -1,0 +1,18 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.commerce.checkout.web.internal.constants;
+
+import com.liferay.commerce.constants.CommercePortletKeys;
+
+/**
+ * @author Luca Pellizzon
+ */
+public class CommerceCheckoutFDSNames {
+
+	public static final String DELIVERY_GROUPS =
+		CommercePortletKeys.COMMERCE_CHECKOUT + "-deliveryGroups";
+
+}

modules/apps/commerce/commerce-checkout-web/src/main/java/com/liferay/commerce/checkout/web/internal/frontend/data/set/provider/CommerceCheckoutDeliveryGroupFDSDataProvider.java
@@ -1,0 +1,110 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.commerce.checkout.web.internal.frontend.data.set.provider;
+
+import com.liferay.commerce.checkout.web.internal.constants.CommerceCheckoutFDSNames;
+import com.liferay.commerce.checkout.web.internal.model.DeliveryGroupInfo;
+import com.liferay.commerce.checkout.web.internal.util.CommerceOrderUtil;
+import com.liferay.commerce.model.CommerceAddress;
+import com.liferay.commerce.model.CommerceOrder;
+import com.liferay.commerce.model.CommerceOrderItem;
+import com.liferay.commerce.service.CommerceAddressLocalService;
+import com.liferay.commerce.service.CommerceOrderLocalService;
+import com.liferay.frontend.data.set.provider.FDSDataProvider;
+import com.liferay.frontend.data.set.provider.search.FDSKeywords;
+import com.liferay.frontend.data.set.provider.search.FDSPagination;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.Country;
+import com.liferay.portal.kernel.search.Sort;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Portal;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Luca Pellizzon
+ */
+@Component(
+	property = "fds.data.provider.key=" + CommerceCheckoutFDSNames.DELIVERY_GROUP,
+	service = FDSDataProvider.class
+)
+public class CommerceCheckoutDeliveryGroupFDSDataProvider
+	implements FDSDataProvider<DeliveryGroupInfo> {
+
+	@Override
+	public List<DeliveryGroupInfo> getItems(
+			FDSKeywords fdsKeywords, FDSPagination fdsPagination,
+			HttpServletRequest httpServletRequest, Sort sort)
+		throws PortalException {
+
+		Map<String, DeliveryGroupInfo> deliveryGroupMap = new HashMap<>();
+
+		long commerceOrderId = ParamUtil.getLong(
+			httpServletRequest, "commerceOrderId");
+
+		CommerceOrder commerceOrder =
+			_commerceOrderLocalService.getCommerceOrder(commerceOrderId);
+
+		for (CommerceOrderItem commerceOrderItem :
+				commerceOrder.getCommerceOrderItems()) {
+
+			if (!deliveryGroupMap.containsKey(
+					commerceOrderItem.getDeliveryGroup())) {
+
+				CommerceAddress commerceAddress =
+				_commerceAddressLocalService.getCommerceAddress(
+					commerceOrderItem.getShippingAddressId());
+	
+				Country country = commerceAddress.getCountry();
+	
+				DeliveryGroupInfo deliveryGroupInfo = new DeliveryGroupInfo(
+					commerceAddress.getCommerceAddressId(),
+					StringBundler.concat(
+						commerceAddress.getStreet1(), StringPool.COMMA_AND_SPACE,
+						commerceAddress.getCity(), StringPool.COMMA_AND_SPACE,
+						country.getName(_portal.getLocale(httpServletRequest))),
+					commerceOrderItem.getRequestedDeliveryDate(),
+					commerceOrderItem.getDeliveryGroup());
+	
+				deliveryGroupMap.put(deliveryGroupInfo.getName(), deliveryGroupInfo);
+			}
+		}
+
+		return new ArrayList<>(deliveryGroupMap.values());
+	}
+
+	@Override
+	public int getItemsCount(
+			FDSKeywords fdsKeywords, HttpServletRequest httpServletRequest)
+		throws PortalException {
+
+		long commerceOrderId = ParamUtil.getLong(
+			httpServletRequest, "commerceOrderId");
+
+		return CommerceOrderUtil.getCommerceOrderDeliveryGroupQuantity(
+			_commerceOrderLocalService.getCommerceOrder(commerceOrderId));
+	}
+
+	@Reference
+	private CommerceAddressLocalService _commerceAddressLocalService;
+
+	@Reference
+	private CommerceOrderLocalService _commerceOrderLocalService;
+
+	@Reference
+	private Portal _portal;
+
+}