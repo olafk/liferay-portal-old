bcd8f2b33d0fb58284be1b407b4782ad758d3533
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2024-11-08 14:47:51 +0100

    LPD-41530 Add test for DELETE endpoint

--- modules/apps/headless/headless-admin-site/headless-admin-site-test/src/testIntegration/java/com/liferay/headless/admin/site/resource/v1_0/test/MasterPageResourceTest.java
+++ modules/apps/headless/headless-admin-site/headless-admin-site-test/src/testIntegration/java/com/liferay/headless/admin/site/resource/v1_0/test/MasterPageResourceTest.java
@@ -8,8 +8,10 @@ package com.liferay.headless.admin.site.resource.v1_0.test;
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.headless.admin.site.client.dto.v1_0.MasterPage;
 import com.liferay.headless.admin.site.client.problem.Problem;
+import com.liferay.layout.page.template.service.LayoutPageTemplateEntryLocalService;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.test.rule.FeatureFlags;
+import com.liferay.portal.test.rule.Inject;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -21,6 +23,46 @@ import org.junit.runner.RunWith;
 @FeatureFlags("LPD-35443")
 @RunWith(Arquillian.class)
 public class MasterPageResourceTest extends BaseMasterPageResourceTestCase {
+
+	@Override
+	@Test
+	public void testDeleteSiteSiteByExternalReferenceCodeMasterPage()
+		throws Exception {
+
+		MasterPage postMasterPage =
+			testPostSiteSiteByExternalReferenceCodeMasterPage_addMasterPage(
+				randomMasterPage());
+
+		Assert.assertNotNull(
+			_layoutPageTemplateEntryLocalService.
+				fetchLayoutPageTemplateEntryByExternalReferenceCode(
+					postMasterPage.getExternalReferenceCode(),
+					testGroup.getGroupId()));
+
+		masterPageResource.deleteSiteSiteByExternalReferenceCodeMasterPage(
+			testGroup.getExternalReferenceCode(),
+			postMasterPage.getExternalReferenceCode());
+
+		Assert.assertNull(
+			_layoutPageTemplateEntryLocalService.
+				fetchLayoutPageTemplateEntryByExternalReferenceCode(
+					postMasterPage.getExternalReferenceCode(),
+					testGroup.getGroupId()));
+
+		try {
+			masterPageResource.deleteSiteSiteByExternalReferenceCodeMasterPage(
+				testGroup.getExternalReferenceCode(),
+				postMasterPage.getExternalReferenceCode());
+
+			Assert.fail();
+		}
+		catch (Problem.ProblemException problemException) {
+			Problem problem = problemException.getProblem();
+
+			Assert.assertEquals("NOT_FOUND", problem.getStatus());
+			Assert.assertNull(problem.getTitle());
+		}
+	}
 
 	@Override
 	@Test
@@ -94,5 +136,9 @@ public class MasterPageResourceTest extends BaseMasterPageResourceTestCase {
 		return testGetSiteSiteByExternalReferenceCodeMasterPagesPage_addMasterPage(
 			testGroup.getExternalReferenceCode(), masterPage);
 	}
+
+	@Inject
+	private LayoutPageTemplateEntryLocalService
+		_layoutPageTemplateEntryLocalService;
 
 }
