e095a9cd8f09487838f366be642315f7f1e99a7c
Veronica Gonzalez <veronica.gonzalez@liferay.com>
2024-11-18 19:56:43 +0100

    LPD-42402 Add badge for deprecated functionalities in NavigationBar component

--- modules/apps/frontend-taglib/frontend-taglib-clay/src/main/resources/META-INF/resources/NavigationBar.js
+++ modules/apps/frontend-taglib/frontend-taglib-clay/src/main/resources/META-INF/resources/NavigationBar.js
@@ -3,9 +3,9 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import ClayButton from '@clayui/button';
-import ClayLink from '@clayui/link';
+import ClayBadge from '@clayui/badge';
 import ClayNavigationBar from '@clayui/navigation-bar';
+import {LinkOrButton} from '@clayui/shared';
 import React from 'react';
 
 export default function NavigationBar({
@@ -22,21 +22,32 @@ export default function NavigationBar({
 			inverted={inverted}
 			triggerLabel={navigationItems.find(({active}) => active)?.label}
 		>
-			{navigationItems.map(({active, href, label}, index) => {
-				return (
-					<ClayNavigationBar.Item
-						active={active}
-						data-nav-item-index={index}
-						key={label}
-					>
-						{href ? (
-							<ClayLink href={href}>{label}</ClayLink>
-						) : (
-							<ClayButton>{label}</ClayButton>
-						)}
-					</ClayNavigationBar.Item>
-				);
-			})}
+			{navigationItems.map(
+				({active, deprecated = false, href, label}, index) => {
+					return (
+						<ClayNavigationBar.Item
+							active={active}
+							data-nav-item-index={index}
+							key={label}
+						>
+							<LinkOrButton className="nav-link" href={href}>
+								<span>{label}</span>
+
+								{deprecated ? (
+									<ClayBadge
+										className="ml-2 text-uppercase"
+										displayType="warning"
+										label={Liferay.Language.get(
+											'deprecated'
+										)}
+										translucent
+									/>
+								) : null}
+							</LinkOrButton>
+						</ClayNavigationBar.Item>
+					);
+				}
+			)}
 		</ClayNavigationBar>
 	);
 }
