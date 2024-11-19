a303db8a614b26a4346661a34e41346c6fd055f0
Marko Čikoš <marko.cikos@liferay.com>
2024-11-19 15:16:11 +0100

    LPD-40743 Add two sample system data set implementations

---
+++ modules/apps/frontend-data-set/frontend-data-set-sample-web/src/main/java/com/liferay/frontend/data/set/sample/web/internal/frontend/data/set/ClassicSystemFDSEntry.java
@@ -0,0 +1,62 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.frontend.data.set.sample.web.internal.frontend.data.set;
+
+import com.liferay.frontend.data.set.SystemFDSEntry;
+import com.liferay.frontend.data.set.sample.web.internal.constants.FDSSampleFDSNames;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Marko Cikos
+ */
+@Component(
+	property = "frontend.data.set.name=" + FDSSampleFDSNames.CLASSIC,
+	service = SystemFDSEntry.class
+)
+public class ClassicSystemFDSEntry implements SystemFDSEntry {
+
+	@Override
+	public String getAdditionalAPIURLParameters() {
+		return "";
+	}
+
+	@Override
+	public String getDescription() {
+		return "The \"Classic\" sample of Frontend Data Set";
+	}
+
+	@Override
+	public String getName() {
+		return FDSSampleFDSNames.CLASSIC;
+	}
+
+	@Override
+	public String getRESTApplication() {
+		return "c/fdssamples";
+	}
+
+	@Override
+	public String getRESTEndpoint() {
+		return "/";
+	}
+
+	@Override
+	public String getRESTSchema() {
+		return "FDSSample";
+	}
+
+	@Override
+	public String getSymbol() {
+		return "braces";
+	}
+
+	@Override
+	public String getTitle() {
+		return "Classic Sample";
+	}
+
+}
