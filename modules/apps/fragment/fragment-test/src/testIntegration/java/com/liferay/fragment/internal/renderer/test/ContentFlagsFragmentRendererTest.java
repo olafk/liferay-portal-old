59dc107e7f8ff9fd5287e4fe86856e430c79157a
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2024-11-20 08:52:44 +0000

    LPD-42527 Adds test to assert correct behavior

---
+++ modules/apps/fragment/fragment-test/src/testIntegration/java/com/liferay/fragment/internal/renderer/test/ContentFlagsFragmentRendererTest.java
@@ -0,0 +1,151 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.fragment.internal.renderer.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.asset.kernel.service.AssetEntryLocalService;
+import com.liferay.fragment.constants.FragmentConstants;
+import com.liferay.fragment.model.FragmentEntryLink;
+import com.liferay.fragment.renderer.DefaultFragmentRendererContext;
+import com.liferay.fragment.renderer.FragmentRenderer;
+import com.liferay.fragment.renderer.FragmentRendererContext;
+import com.liferay.fragment.service.FragmentEntryLinkLocalService;
+import com.liferay.info.item.InfoItemDetails;
+import com.liferay.info.item.InfoItemServiceRegistry;
+import com.liferay.info.item.provider.InfoItemDetailsProvider;
+import com.liferay.journal.constants.JournalArticleConstants;
+import com.liferay.journal.model.JournalArticle;
+import com.liferay.journal.test.util.JournalTestUtil;
+import com.liferay.layout.test.util.LayoutTestUtil;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.test.ReflectionTestUtil;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.util.Tuple;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
+import com.liferay.segments.service.SegmentsExperienceLocalService;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.mock.web.MockHttpServletRequest;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@RunWith(Arquillian.class)
+public class ContentFlagsFragmentRendererTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			PermissionCheckerMethodTestRule.INSTANCE);
+
+	@Before
+	public void setUp() throws Exception {
+		_group = GroupTestUtil.addGroup();
+
+		_journalArticle = JournalTestUtil.addArticle(
+			_group.getGroupId(), JournalArticleConstants.CLASS_NAME_ID_DEFAULT);
+
+		_layout = LayoutTestUtil.addTypeContentLayout(_group);
+	}
+
+	@Test
+	public void testGetDisplayObjectTupleForAssetEntry() throws Exception {
+		FragmentEntryLink fragmentEntryLink = _addFragmentEntryLink();
+
+		DefaultFragmentRendererContext defaultFragmentRendererContext =
+			new DefaultFragmentRendererContext(fragmentEntryLink);
+
+		InfoItemDetailsProvider<JournalArticle> infoItemDetailsProvider =
+			_infoItemServiceRegistry.getFirstInfoItemService(
+				InfoItemDetailsProvider.class, JournalArticle.class.getName());
+
+		InfoItemDetails infoItemDetails =
+			infoItemDetailsProvider.getInfoItemDetails(_journalArticle);
+
+		defaultFragmentRendererContext.setContextInfoItemReference(
+			infoItemDetails.getInfoItemReference());
+
+		Tuple tuple = ReflectionTestUtil.invoke(
+			_fragmentRenderer, "getDisplayObjectTuple",
+			new Class<?>[] {
+				FragmentRendererContext.class, HttpServletRequest.class
+			},
+			defaultFragmentRendererContext, _getMockHttpServletRequest());
+
+		Assert.assertEquals(JournalArticle.class.getName(), tuple.getObject(0));
+		Assert.assertEquals(
+			_journalArticle.getResourcePrimKey(), tuple.getObject(1));
+	}
+
+	private FragmentEntryLink _addFragmentEntryLink() throws Exception {
+		return _fragmentEntryLinkLocalService.addFragmentEntryLink(
+			null, TestPropsValues.getUserId(), _group.getGroupId(), 0, 0,
+			_segmentsExperienceLocalService.fetchDefaultSegmentsExperienceId(
+				_layout.getPlid()),
+			_layout.getPlid(), StringPool.BLANK, StringPool.BLANK,
+			StringPool.BLANK, StringPool.BLANK, StringPool.BLANK,
+			StringPool.BLANK, 0,
+			"com.liferay.fragment.internal.renderer." +
+				"ContentFlagsFragmentRenderer",
+			FragmentConstants.TYPE_COMPONENT,
+			ServiceContextTestUtil.getServiceContext(_group.getGroupId()));
+	}
+
+	private HttpServletRequest _getMockHttpServletRequest() throws Exception {
+		HttpServletRequest httpServletRequest = new MockHttpServletRequest();
+
+		httpServletRequest.setAttribute(
+			WebKeys.LAYOUT_ASSET_ENTRY,
+			_assetEntryLocalService.getEntry(
+				JournalArticle.class.getName(),
+				_journalArticle.getResourcePrimKey()));
+
+		return httpServletRequest;
+	}
+
+	@Inject
+	private AssetEntryLocalService _assetEntryLocalService;
+
+	@Inject
+	private FragmentEntryLinkLocalService _fragmentEntryLinkLocalService;
+
+	@Inject(
+		filter = "component.name=com.liferay.fragment.internal.renderer.ContentFlagsFragmentRenderer"
+	)
+	private FragmentRenderer _fragmentRenderer;
+
+	@DeleteAfterTestRun
+	private Group _group;
+
+	@Inject
+	private InfoItemServiceRegistry _infoItemServiceRegistry;
+
+	private JournalArticle _journalArticle;
+	private Layout _layout;
+
+	@Inject
+	private SegmentsExperienceLocalService _segmentsExperienceLocalService;
+
+}
