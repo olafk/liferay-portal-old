b3c2ea8aa6924d53fb5eaa9a2138ad33e8ed41f7
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2024-11-22 17:36:03 +0100

    LPD-41552 Implements POST endpoint for WidgetPageWidgetInstance

--- modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/resource/v1_0/WidgetPageWidgetInstanceResourceImpl.java
+++ modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/resource/v1_0/WidgetPageWidgetInstanceResourceImpl.java
@@ -8,8 +8,10 @@ package com.liferay.headless.admin.site.internal.resource.v1_0;
 import com.liferay.headless.admin.site.dto.v1_0.WidgetPageWidgetInstance;
 import com.liferay.headless.admin.site.internal.resource.util.GroupUtil;
 import com.liferay.headless.admin.site.resource.v1_0.WidgetPageWidgetInstanceResource;
+import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.exception.NoSuchPortletException;
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.feature.flag.FeatureFlagManagerUtil;
 import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.model.LayoutType;
@@ -157,6 +159,56 @@ public class WidgetPageWidgetInstanceResourceImpl
 
 		return _toWidgetPageWidgetInstance(
 			layout, widgetInstanceExternalReferenceCode);
+	}
+
+	@Override
+	public WidgetPageWidgetInstance
+			postSiteSiteByExternalReferenceCodeSitePageWidgetInstance(
+				String siteExternalReferenceCode,
+				String sitePageExternalReferenceCode,
+				WidgetPageWidgetInstance widgetPageWidgetInstance)
+		throws Exception {
+
+		if (!FeatureFlagManagerUtil.isEnabled("LPD-35443")) {
+			throw new UnsupportedOperationException();
+		}
+
+		Layout layout = _layoutLocalService.fetchLayoutByExternalReferenceCode(
+			sitePageExternalReferenceCode,
+			GroupUtil.getGroupId(
+				false, contextCompany.getCompanyId(),
+				siteExternalReferenceCode));
+
+		if (layout == null) {
+			throw new UnsupportedOperationException();
+		}
+
+		LayoutType layoutType = layout.getLayoutType();
+
+		if (!(layoutType instanceof LayoutTypePortlet)) {
+			throw new UnsupportedOperationException();
+		}
+
+		LayoutTypePortlet layoutTypePortlet =
+			(LayoutTypePortlet)layout.getLayoutType();
+
+		String portletId = PortletIdCodec.encode(
+			widgetPageWidgetInstance.getWidgetName(),
+			widgetPageWidgetInstance.getWidgetInstanceId());
+
+		String addedPortletId = layoutTypePortlet.addPortletId(
+			contextUser.getUserId(), portletId,
+			widgetPageWidgetInstance.getParentSectionId(),
+			widgetPageWidgetInstance.getPosition());
+
+		if (addedPortletId == null) {
+			throw new PortalException(
+				StringBundler.concat(
+					"Portlet ", portletId, " cannot be added to layout ",
+					layout.getPlid(), " by user ", contextUser.getUserId()));
+		}
+
+		return _toWidgetPageWidgetInstance(layout, addedPortletId);
 	}
 
 	private String _getParentSectionId(Layout layout, String portletId) {
