a15c27ba4a54a07db7b0412ab28df9c02916af5c
Stian Sigvartsen <stian.sigvartsen@liferay.com>
2024-11-29 17:40:22 +0000

    LPD-36717 Integrate into existing test class

--- modules/dxp/apps/saml/saml-test/src/testIntegration/java/com/liferay/saml/opensaml/integration/processor/factory/test/UserProcessorFactoryTest.java
+++ modules/dxp/apps/saml/saml-test/src/testIntegration/java/com/liferay/saml/opensaml/integration/processor/factory/test/UserProcessorFactoryTest.java
@@ -10,10 +10,13 @@ import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.model.UserGroup;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.UserGroupLocalService;
+import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.test.util.UserGroupTestUtil;
 import com.liferay.portal.kernel.test.util.UserTestUtil;
+import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.saml.opensaml.integration.field.expression.handler.registry.UserFieldExpressionHandlerRegistry;
@@ -28,6 +31,8 @@ import org.junit.runner.RunWith;
 
 /**
  * @author Jorge García Jiménez
+ * @author Stian Sigvartsen
+ * @author Tamas Biro
  */
 @RunWith(Arquillian.class)
 public class UserProcessorFactoryTest {
@@ -36,6 +41,31 @@ public class UserProcessorFactoryTest {
 	@Rule
 	public static final LiferayIntegrationTestRule liferayIntegrationTestRule =
 		new LiferayIntegrationTestRule();
+
+	@Test
+	public void testAllUserFieldsAreUpdatedWhenEmailIsChanged()
+		throws Exception {
+
+		_user = _userLocalService.createUser(0);
+
+		_user.setCompanyId(TestPropsValues.getCompanyId());
+
+		_assertProcess("John", "Doe", "JOHN.DOE@example.com", "john.doe");
+
+		Assert.assertNotEquals(0, _user.getUserId());
+
+		// Assert handling case-insensitive email addresses during update
+
+		_assertProcess(
+			"John-changed", "Doe-changed", "JOHN.DOE@example.com",
+			"john.doe.changed");
+
+		// Updated email entirely changed
+
+		_assertProcess("Jane", "Doena", "jane.doena@example.com", "jane.doena");
+
+		_assertProcess(null, null, "jane.doena@example.com", null);
+	}
 
 	@Test
 	public void testMembershipUserGroups() throws Exception {
@@ -57,7 +87,18 @@ public class UserProcessorFactoryTest {
 		UserProcessor userProcessor = _userProcessorFactory.create(
 			_user, _userFieldExpressionHandlerRegistry);
 
-		if (userGroupName != null) {
+		if (userGroupName == null) {
+			int expected = _userGroupLocalService.getUserUserGroupsCount(
+				_user.getUserId());
+
+			userProcessor.process(serviceContext);
+
+			Assert.assertEquals(
+				expected,
+				_userGroupLocalService.getUserUserGroupsCount(
+					_user.getUserId()));
+		}
+		else {
 			userProcessor.setValueArray(
 				String.class, "membership:userGroups", userGroupName);
 
@@ -68,16 +109,41 @@ public class UserProcessorFactoryTest {
 				_userGroupLocalService.getUserUserGroupsCount(
 					_user.getUserId()));
 		}
-		else {
-			int expected = _userGroupLocalService.getUserUserGroupsCount(
-				_user.getUserId());
+	}
 
-			userProcessor.process(serviceContext);
+	private void _assertProcess(
+			String firstName, String lastName, String emailAddress,
+			String screenName)
+		throws Exception {
 
-			Assert.assertEquals(
-				expected,
-				_userGroupLocalService.getUserUserGroupsCount(
-					_user.getUserId()));
+		UserProcessor userProcessor = _userProcessorFactory.create(
+			_user, _userFieldExpressionHandlerRegistry);
+
+		_set(userProcessor, "emailAddress", emailAddress);
+		_set(userProcessor, "firstName", firstName);
+		_set(userProcessor, "lastName", lastName);
+		_set(userProcessor, "screenName", screenName);
+
+		User user2 = userProcessor.process(
+			ServiceContextTestUtil.getServiceContext());
+
+		user2 = _userLocalService.getUser(user2.getUserId());
+
+		Assert.assertEquals(
+			StringUtil.toLowerCase(_user.getEmailAddress()),
+			user2.getEmailAddress());
+		Assert.assertEquals(_user.getFirstName(), user2.getFirstName());
+		Assert.assertEquals(_user.getLastName(), user2.getLastName());
+		Assert.assertEquals(_user.getScreenName(), user2.getScreenName());
+
+		_user = user2;
+	}
+
+	private void _set(
+		UserProcessor userProcessor, String fieldExpression, String value) {
+
+		if (value != null) {
+			userProcessor.setValueArray(fieldExpression, new String[] {value});
 		}
 	}
 
@@ -93,6 +159,9 @@ public class UserProcessorFactoryTest {
 
 	@Inject
 	private UserGroupLocalService _userGroupLocalService;
+
+	@Inject
+	private UserLocalService _userLocalService;
 
 	@Inject
 	private UserProcessorFactory _userProcessorFactory;
