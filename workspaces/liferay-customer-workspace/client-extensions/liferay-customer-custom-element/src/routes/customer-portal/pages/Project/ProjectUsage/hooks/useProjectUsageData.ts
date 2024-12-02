cef6e2738c74a53aba00eb2c3d1b131c74c14452
Ryan Schuhler <ryan.schuhler@liferay.com>
2024-12-02 10:07:30 -0800

    LRSD-7233 SF

--- workspaces/liferay-customer-workspace/client-extensions/liferay-customer-custom-element/src/routes/customer-portal/pages/Project/ProjectUsage/hooks/useProjectUsageData.ts
+++ workspaces/liferay-customer-workspace/client-extensions/liferay-customer-custom-element/src/routes/customer-portal/pages/Project/ProjectUsage/hooks/useProjectUsageData.ts
@@ -86,89 +86,78 @@ const useProjectUsageData = () => {
 
 	const getSiteAndUsers = useCallback(async () => {
 		if (project?.id) {
-			if (displayUsage) {
-				const response =
-					await Liferay.OAuth2Client.FromUserAgentApplication(
-						'liferay-customer-etc-spring-boot-oaua'
-					)
-						.fetch(`/accounts/${project?.id}/usage`)
-						.then((response) => response.json())
-						.catch(console.error);
+			if (!displayUsage) {
+				setIsLoading(false);
 
-				if (response) {
-					const formatedData = {
-						resourceUsage: [
-							{
-								...response[
-									SiteAndUserDataEnum
-										.CLIENT_EXTENSIONS_CAPACITY_RAM
-								],
-								dataSizeUnits: 'GB',
-								infoText: i18n.translate(
-									'extension-capacity-ram'
-								),
-								maxCountText: 'RAM',
-								title: i18n.translate('extension-capacity-ram'),
-							},
-							{
-								...response[
-									SiteAndUserDataEnum
-										.CLIENT_EXTENSIONS_CAPACITY_CPU
-								],
-								infoText: i18n.translate(
-									'extension-capacity-vcpu'
-								),
-								maxCountText: 'vCPU',
-								title: i18n.translate(
-									'extension-capacity-vcpu'
-								),
-							},
-							{
-								...response[
-									SiteAndUserDataEnum
-										.STORAGE_CAPACITY_DOCUMENT_LIBRARY
-								],
-								dataSizeUnits: 'GB',
-								infoText: i18n.translate('storage-capacity'),
-								maxCountText: 'Storage',
-								title: i18n.translate('storage-capacity'),
-							},
-						],
-						siteAndUsers: [
-							{
-								...response[SiteAndUserDataEnum.SITES],
-								infoText: i18n.translate('number-of-sites'),
-								title: i18n.translate('number-of-sites'),
-							},
-							{
-								...response[
-									SiteAndUserDataEnum
-										.MONTHLY_ACTIVE_LOGGED_IN_USERS
-								],
-								infoText: i18n.translate(
-									'authenticated-logins-malus'
-								),
-								title: i18n.translate(
-									'authenticated-logins-malus'
-								),
-							},
-							{
-								...response[
-									SiteAndUserDataEnum.ANONYMOUS_PAGE_VIEWS
-								],
-								infoText: i18n.translate(
-									'anonymous-page-views-apv'
-								),
-								title: i18n.translate(
-									'anonymous-page-views-apv'
-								),
-							},
-						],
-					};
-
-					setUsageData(formatedData);
-				}
+				return;
 			}
+
+			const response =
+				await Liferay.OAuth2Client.FromUserAgentApplication(
+					'liferay-customer-etc-spring-boot-oaua'
+				)
+					.fetch(`/accounts/${project?.id}/usage`)
+					.then((response) => response.json())
+					.catch(console.error);
+
+			if (!response) {
+				setIsLoading(false);
+
+				return;
+			}
+
+			const formatedData = {
+				resourceUsage: [
+					{
+						...response[
+							SiteAndUserDataEnum.CLIENT_EXTENSIONS_CAPACITY_RAM
+						],
+						dataSizeUnits: 'GB',
+						infoText: i18n.translate('extension-capacity-ram'),
+						maxCountText: 'RAM',
+						title: i18n.translate('extension-capacity-ram'),
+					},
+					{
+						...response[
+							SiteAndUserDataEnum.CLIENT_EXTENSIONS_CAPACITY_CPU
+						],
+						infoText: i18n.translate('extension-capacity-vcpu'),
+						maxCountText: 'vCPU',
+						title: i18n.translate('extension-capacity-vcpu'),
+					},
+					{
+						...response[
+							SiteAndUserDataEnum
+								.STORAGE_CAPACITY_DOCUMENT_LIBRARY
+						],
+						dataSizeUnits: 'GB',
+						infoText: i18n.translate('storage-capacity'),
+						maxCountText: 'Storage',
+						title: i18n.translate('storage-capacity'),
+					},
+				],
+				siteAndUsers: [
+					{
+						...response[SiteAndUserDataEnum.SITES],
+						infoText: i18n.translate('number-of-sites'),
+						title: i18n.translate('number-of-sites'),
+					},
+					{
+						...response[
+							SiteAndUserDataEnum.MONTHLY_ACTIVE_LOGGED_IN_USERS
+						],
+						infoText: i18n.translate('authenticated-logins-malus'),
+						title: i18n.translate('authenticated-logins-malus'),
+					},
+					{
+						...response[SiteAndUserDataEnum.ANONYMOUS_PAGE_VIEWS],
+						infoText: i18n.translate('anonymous-page-views-apv'),
+						title: i18n.translate('anonymous-page-views-apv'),
+					},
+				],
+			};
+
+			setUsageData(formatedData);
 
 			setIsLoading(false);
 		}
