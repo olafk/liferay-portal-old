43c660f126c2ecff0c0b52fabfddc5846ad5fa2a
Charlotte Wong <chiharutaru@gmail.com>
2024-12-02 09:56:09 -0800

    LRCI-4913 SF

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/failure/message/generator/UpgradeFailureMessageGenerator.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/failure/message/generator/UpgradeFailureMessageGenerator.java
@@ -1,28 +1,30 @@
 /**
- * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
- package com.liferay.jenkins.results.parser.failure.message.generator;
+package com.liferay.jenkins.results.parser.failure.message.generator;
 
- /**
-  * @author Charlotte Wong
-  */
- public class UpgradeFailureMessageGenerator
-     extends BaseFailureMessageGenerator {
- 
-     @Override
-     public String getMessage(String consoleText) {
-        if (!consoleText.contains(_LIFERAY_UPGRADE_ERROR_TEXT)) {
-            return null;
-        }
-            
-        int start = consoleText.indexOf(_LIFERAY_UPGRADE_ERROR_TEXT);
+/**
+ * @author Charlotte Wong
+ */
+public class UpgradeFailureMessageGenerator
+	extends BaseFailureMessageGenerator {
+
+	@Override
+	public String getMessage(String consoleText) {
+		if (!consoleText.contains(_LIFERAY_UPGRADE_ERROR_TEXT)) {
+			return null;
+		}
+
+		int start = consoleText.indexOf(_LIFERAY_UPGRADE_ERROR_TEXT);
 
 		start = consoleText.lastIndexOf("\n", start);
 
 		return getConsoleTextSnippetByStart(consoleText, start);
-     }
- 
-     private static final String _LIFERAY_UPGRADE_ERROR_TEXT = "Failed Liferay upgrade";
- }
+	}
+
+	private static final String _LIFERAY_UPGRADE_ERROR_TEXT =
+		"Failed Liferay upgrade";
+
+}
