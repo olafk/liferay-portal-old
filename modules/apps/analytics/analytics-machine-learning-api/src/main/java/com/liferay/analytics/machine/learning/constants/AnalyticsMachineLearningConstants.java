fc7d2ec96a781b116bdcbdb9bd906453288af1c8
Rachael Koestartyo <rachael.koestartyo@liferay.com>
2024-12-10 00:18:59 -0800

    LPD-20646 | LPD-39719 add constants

---
+++ modules/apps/analytics/analytics-machine-learning-api/src/main/java/com/liferay/analytics/machine/learning/constants/AnalyticsMachineLearningConstants.java
@@ -0,0 +1,23 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.analytics.machine.learning.constants;
+
+/**
+ * @author Rachael Koestartyo
+ */
+public class AnalyticsMachineLearningConstants {
+
+	public static final String DISPATCH_TRIGGER_NAME_ASSET_ENTITIES =
+		"export-analytics-asset-entities";
+
+	public static final String DISPATCH_TRIGGER_NAME_MOST_VIEWED_RECOMMENDER =
+		"analytics-download-most-viewed-content-recommendation";
+
+	public static final String
+		DISPATCH_TRIGGER_NAME_USER_PERSONALIZATION_RECOMMENDER =
+			"analytics-download-user-content-recommendation";
+
+}
