22270c4dc2880e3b934da07a3dbddc4151c3bb06
Alberto Moreno <alberto.moreno@liferay.com>
2024-12-18 15:00:20 +0100

    LPD-44648 Add the remaining picklists

--- modules/apps/headless/headless-builder/headless-builder-impl/src/main/java/com/liferay/headless/builder/internal/upgrade/v0_2_0/DeletePiklistObjectStatesUpgradeProcess.java
+++ modules/apps/headless/headless-builder/headless-builder-impl/src/main/java/com/liferay/headless/builder/internal/upgrade/v0_2_0/DeletePiklistObjectStatesUpgradeProcess.java
@@ -14,6 +14,7 @@ import com.liferay.object.model.ObjectField;
 import com.liferay.object.service.ObjectDefinitionLocalService;
 import com.liferay.object.service.ObjectFieldLocalService;
 import com.liferay.object.service.ObjectStateFlowLocalService;
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.service.CompanyLocalService;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
 
@@ -42,48 +43,174 @@ public class DeletePiklistObjectStatesUpgradeProcess extends UpgradeProcess {
 	protected void doUpgrade() throws Exception {
 		_companyLocalService.forEachCompanyId(
 			companyId -> {
-				ObjectDefinition objectDefinition =
-					_objectDefinitionLocalService.
-						fetchObjectDefinitionByExternalReferenceCode(
-							"L_API_APPLICATION", companyId);
-
-				if (objectDefinition == null) {
-					return;
-				}
-
-				ObjectField applicationStatusObjectField =
-					_objectFieldLocalService.fetchObjectField(
-						"APPLICATION_STATUS",
-						objectDefinition.getObjectDefinitionId());
-
-				_objectStateFlowLocalService.deleteObjectFieldObjectStateFlow(
-					applicationStatusObjectField.getObjectFieldId());
-
-				ListTypeDefinition applicationStatusListTypeDefinition =
-					_listTypeDefinitionLocalService.
-						fetchListTypeDefinitionByExternalReferenceCode(
-							"APPLICATION_STATUS_PICKLIST", companyId);
-
-				applicationStatusListTypeDefinition.setName(
-					"Application Status");
-
-				ListTypeEntry publishedListTypeEntry =
-					_listTypeEntryLocalService.getListTypeEntry(
-						applicationStatusListTypeDefinition.
-							getListTypeDefinitionId(),
-						"published");
-
-				publishedListTypeEntry.setExternalReferenceCode("PUBLISHED");
-
-				ListTypeEntry unpublishedListTypeEntry =
-					_listTypeEntryLocalService.getListTypeEntry(
-						applicationStatusListTypeDefinition.
-							getListTypeDefinitionId(),
-						"unpublished");
-
-				unpublishedListTypeEntry.setExternalReferenceCode(
-					"UNPUBLISHED");
+				_modifyApplicationStatusPicklist(companyId);
+				_modifyHTTPMethodPicklist(companyId);
+				_modifyRetrieveTypePicklist(companyId);
+				_modifyScopePicklist(companyId);
 			});
+	}
+
+	private void _modifyApplicationStatusPicklist(Long companyId)
+		throws PortalException {
+
+		ObjectDefinition objectDefinition =
+			_objectDefinitionLocalService.
+				fetchObjectDefinitionByExternalReferenceCode(
+					"L_API_APPLICATION", companyId);
+
+		if (objectDefinition == null) {
+			return;
+		}
+
+		ObjectField applicationStatusObjectField =
+			_objectFieldLocalService.fetchObjectField(
+				"APPLICATION_STATUS", objectDefinition.getObjectDefinitionId());
+
+		_objectStateFlowLocalService.deleteObjectFieldObjectStateFlow(
+			applicationStatusObjectField.getObjectFieldId());
+
+		ListTypeDefinition applicationStatusListTypeDefinition =
+			_listTypeDefinitionLocalService.
+				fetchListTypeDefinitionByExternalReferenceCode(
+					"APPLICATION_STATUS_PICKLIST", companyId);
+
+		applicationStatusListTypeDefinition.setName("Application Status");
+
+		ListTypeEntry publishedListTypeEntry =
+			_listTypeEntryLocalService.getListTypeEntry(
+				applicationStatusListTypeDefinition.getListTypeDefinitionId(),
+				"published");
+
+		publishedListTypeEntry.setExternalReferenceCode("PUBLISHED");
+
+		ListTypeEntry unpublishedListTypeEntry =
+			_listTypeEntryLocalService.getListTypeEntry(
+				applicationStatusListTypeDefinition.getListTypeDefinitionId(),
+				"unpublished");
+
+		unpublishedListTypeEntry.setExternalReferenceCode("UNPUBLISHED");
+	}
+
+	private void _modifyHTTPMethodPicklist(Long companyId)
+		throws PortalException {
+
+		ObjectDefinition objectDefinition =
+			_objectDefinitionLocalService.
+				fetchObjectDefinitionByExternalReferenceCode(
+					"L_API_ENDPOINT", companyId);
+
+		if (objectDefinition == null) {
+			return;
+		}
+
+		ObjectField applicationStatusObjectField =
+			_objectFieldLocalService.fetchObjectField(
+				"HTTP_METHOD",
+				objectDefinition.getObjectDefinitionId());
+
+		_objectStateFlowLocalService.deleteObjectFieldObjectStateFlow(
+			applicationStatusObjectField.getObjectFieldId());
+
+		ListTypeDefinition applicationStatusListTypeDefinition =
+			_listTypeDefinitionLocalService.
+				fetchListTypeDefinitionByExternalReferenceCode(
+					"HTTP_METHOD_PICKLIST", companyId);
+
+		applicationStatusListTypeDefinition.setName("HTTP Method");
+
+		ListTypeEntry publishedListTypeEntry =
+			_listTypeEntryLocalService.getListTypeEntry(
+				applicationStatusListTypeDefinition.getListTypeDefinitionId(),
+				"get");
+
+		publishedListTypeEntry.setExternalReferenceCode("GET");
+
+		ListTypeEntry unpublishedListTypeEntry =
+			_listTypeEntryLocalService.getListTypeEntry(
+				applicationStatusListTypeDefinition.getListTypeDefinitionId(),
+				"post");
+
+		unpublishedListTypeEntry.setExternalReferenceCode("POST");
+	}
+
+	private void _modifyRetrieveTypePicklist(Long companyId)
+		throws PortalException {
+
+		ObjectDefinition objectDefinition =
+			_objectDefinitionLocalService.
+				fetchObjectDefinitionByExternalReferenceCode(
+					"L_API_ENDPOINT", companyId);
+
+		if (objectDefinition == null) {
+			return;
+		}
+
+		ObjectField applicationStatusObjectField =
+			_objectFieldLocalService.fetchObjectField(
+				"RETRIEVE_TYPE", objectDefinition.getObjectDefinitionId());
+
+		_objectStateFlowLocalService.deleteObjectFieldObjectStateFlow(
+			applicationStatusObjectField.getObjectFieldId());
+
+		ListTypeDefinition applicationStatusListTypeDefinition =
+			_listTypeDefinitionLocalService.
+				fetchListTypeDefinitionByExternalReferenceCode(
+					"RETRIEVE_TYPE_PICKLIST", companyId);
+
+		applicationStatusListTypeDefinition.setName("Retrieve Type");
+
+		ListTypeEntry publishedListTypeEntry =
+			_listTypeEntryLocalService.getListTypeEntry(
+				applicationStatusListTypeDefinition.getListTypeDefinitionId(),
+				"collection");
+
+		publishedListTypeEntry.setExternalReferenceCode("COLLECTION");
+
+		ListTypeEntry unpublishedListTypeEntry =
+			_listTypeEntryLocalService.getListTypeEntry(
+				applicationStatusListTypeDefinition.getListTypeDefinitionId(),
+				"singleElement");
+
+		unpublishedListTypeEntry.setExternalReferenceCode("SINGLE_ELEMENT");
+	}
+
+	private void _modifyScopePicklist(Long companyId) throws PortalException {
+		ObjectDefinition objectDefinition =
+			_objectDefinitionLocalService.
+				fetchObjectDefinitionByExternalReferenceCode(
+					"L_API_ENDPOINT", companyId);
+
+		if (objectDefinition == null) {
+			return;
+		}
+
+		ObjectField applicationStatusObjectField =
+			_objectFieldLocalService.fetchObjectField(
+				"SCOPE", objectDefinition.getObjectDefinitionId());
+
+		_objectStateFlowLocalService.deleteObjectFieldObjectStateFlow(
+			applicationStatusObjectField.getObjectFieldId());
+
+		ListTypeDefinition applicationStatusListTypeDefinition =
+			_listTypeDefinitionLocalService.
+				fetchListTypeDefinitionByExternalReferenceCode(
+					"SCOPE_PICKLIST", companyId);
+
+		applicationStatusListTypeDefinition.setName("Scope");
+
+		ListTypeEntry publishedListTypeEntry =
+			_listTypeEntryLocalService.getListTypeEntry(
+				applicationStatusListTypeDefinition.getListTypeDefinitionId(),
+				"company");
+
+		publishedListTypeEntry.setExternalReferenceCode("COMPANY");
+
+		ListTypeEntry unpublishedListTypeEntry =
+			_listTypeEntryLocalService.getListTypeEntry(
+				applicationStatusListTypeDefinition.getListTypeDefinitionId(),
+				"site");
+
+		unpublishedListTypeEntry.setExternalReferenceCode("SITE");
 	}
 
 	private final CompanyLocalService _companyLocalService;
