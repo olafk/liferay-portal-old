162fe0b9ff03c53b72773f61cf6bb61f668e1fdc
Alejandro Tardín <alejandro.tardin@liferay.com>
2024-12-19 16:21:43 +0100

    LPD-45048 Registering portlet data handlers for batch entities
    Co-authored-by: Carlos Correa <carlos.correa@liferay.com>
    Co-authored-by: Vendel Toreki <vendel.toreki@liferay.com>

---
+++ modules/apps/portal-vulcan/portal-vulcan-impl/src/main/java/com/liferay/portal/vulcan/internal/exportimport/data/handler/VulcanBatchEnginePortletDataHandler.java
@@ -0,0 +1,206 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.vulcan.internal.exportimport.data.handler;
+
+import com.liferay.batch.engine.BatchEngineExportTaskExecutor;
+import com.liferay.batch.engine.BatchEngineImportTaskExecutor;
+import com.liferay.batch.engine.BatchEngineTaskExecuteStatus;
+import com.liferay.batch.engine.BatchEngineTaskOperation;
+import com.liferay.batch.engine.constants.BatchEngineImportTaskConstants;
+import com.liferay.batch.engine.constants.CreateStrategy;
+import com.liferay.batch.engine.model.BatchEngineExportTask;
+import com.liferay.batch.engine.service.BatchEngineExportTaskService;
+import com.liferay.batch.engine.service.BatchEngineImportTaskService;
+import com.liferay.exportimport.kernel.lar.BasePortletDataHandler;
+import com.liferay.exportimport.kernel.lar.ManifestSummary;
+import com.liferay.exportimport.kernel.lar.PortletDataContext;
+import com.liferay.exportimport.kernel.lar.PortletDataHandlerBoolean;
+import com.liferay.exportimport.kernel.lar.PortletDataHandlerControl;
+import com.liferay.petra.io.StreamUtil;
+import com.liferay.petra.io.unsync.UnsyncByteArrayOutputStream;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
+import com.liferay.portal.kernel.util.FileUtil;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+
+import java.io.File;
+import java.io.InputStream;
+import java.io.Serializable;
+
+import java.util.Collections;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+import javax.portlet.PortletPreferences;
+
+/**
+ * @author Vendel Toreki
+ * @author Alejandro Tardín
+ */
+public class VulcanBatchEnginePortletDataHandler
+	extends BasePortletDataHandler {
+
+	public static final String SCHEMA_VERSION = "4.0.0";
+
+	public VulcanBatchEnginePortletDataHandler(
+		BatchEngineExportTaskExecutor batchEngineExportTaskExecutor,
+		BatchEngineExportTaskService batchEngineExportTaskService,
+		BatchEngineImportTaskExecutor batchEngineImportTaskExecutor,
+		BatchEngineImportTaskService batchEngineImportTaskService,
+		String className, String taskItemDelegateName) {
+
+		_batchEngineExportTaskExecutor = batchEngineExportTaskExecutor;
+		_batchEngineExportTaskService = batchEngineExportTaskService;
+		_batchEngineImportTaskExecutor = batchEngineImportTaskExecutor;
+		_batchEngineImportTaskService = batchEngineImportTaskService;
+		_className = className;
+		_taskItemDelegateName = taskItemDelegateName;
+
+		_fileName = taskItemDelegateName + ".json";
+
+		setExportControls(
+			new PortletDataHandlerBoolean(
+				taskItemDelegateName, taskItemDelegateName, true, true, null,
+				className));
+	}
+
+	@Override
+	public String[] getClassNames() {
+		return new String[] {_className};
+	}
+
+	@Override
+	public String getName() {
+		return _className + StringPool.POUND + _taskItemDelegateName;
+	}
+
+	@Override
+	public String getSchemaVersion() {
+		return SCHEMA_VERSION;
+	}
+
+	@Override
+	public boolean isCompany() {
+		return true;
+	}
+
+	@Override
+	protected String doExportData(
+			PortletDataContext portletDataContext, String portletId,
+			PortletPreferences portletPreferences)
+		throws Exception {
+
+		BatchEngineExportTask batchEngineExportTask =
+			_batchEngineExportTaskService.addBatchEngineExportTask(
+				null, portletDataContext.getCompanyId(), _getUserId(), null,
+				_className, "JSON", BatchEngineTaskExecuteStatus.INITIAL.name(),
+				Collections.emptyList(), Collections.emptyMap(),
+				_taskItemDelegateName);
+
+		_batchEngineExportTaskExecutor.execute(batchEngineExportTask);
+
+		portletDataContext.addZipEntry(
+			_fileName, _getBytes(batchEngineExportTask));
+
+		return getExportDataRootElementString(
+			addExportDataRootElement(portletDataContext));
+	}
+
+	@Override
+	protected PortletPreferences doImportData(
+			PortletDataContext portletDataContext, String portletId,
+			PortletPreferences portletPreferences, String data)
+		throws Exception {
+
+		InputStream inputStream = portletDataContext.getZipEntryAsInputStream(
+			_fileName);
+
+		if (inputStream == null) {
+			return portletPreferences;
+		}
+
+		_batchEngineImportTaskExecutor.execute(
+			_batchEngineImportTaskService.addBatchEngineImportTask(
+				null, portletDataContext.getCompanyId(), _getUserId(), 100,
+				null, _className, _getBytes(_fileName, inputStream), "JSON",
+				BatchEngineTaskExecuteStatus.INITIAL.name(),
+				Collections.emptyMap(),
+				BatchEngineImportTaskConstants.
+					IMPORT_STRATEGY_ON_ERROR_CONTINUE,
+				BatchEngineTaskOperation.CREATE.name(),
+				HashMapBuilder.<String, Serializable>put(
+					"createStrategy", CreateStrategy.UPSERT.getDBOperation()
+				).build(),
+				_taskItemDelegateName));
+
+		return portletPreferences;
+	}
+
+	@Override
+	protected long getExportModelCount(
+		ManifestSummary manifestSummary,
+		PortletDataHandlerControl[] portletDataHandlerControls) {
+
+		// TODO The object will not be displayed without count
+
+		return 1;
+	}
+
+	private byte[] _getBytes(BatchEngineExportTask batchEngineExportTask)
+		throws Exception {
+
+		try (InputStream inputStream =
+				_batchEngineExportTaskService.openContentInputStream(
+					batchEngineExportTask.getBatchEngineExportTaskId())) {
+
+			// TODO We should export the unzipped JSON file
+
+			File batchZipFile = FileUtil.createTempFile(inputStream);
+
+			File tempFolder = FileUtil.createTempFolder();
+
+			FileUtil.unzip(batchZipFile, tempFolder);
+
+			return FileUtil.getBytes(new File(tempFolder, "export.json"));
+		}
+	}
+
+	private byte[] _getBytes(String fileName, InputStream inputStream)
+		throws Exception {
+
+		UnsyncByteArrayOutputStream unsyncByteArrayOutputStream =
+			new UnsyncByteArrayOutputStream();
+
+		try (ZipOutputStream zipOutputStream = new ZipOutputStream(
+				unsyncByteArrayOutputStream)) {
+
+			ZipEntry zipEntry = new ZipEntry(fileName);
+
+			zipOutputStream.putNextEntry(zipEntry);
+
+			StreamUtil.transfer(inputStream, zipOutputStream, false);
+		}
+
+		return unsyncByteArrayOutputStream.toByteArray();
+	}
+
+	private long _getUserId() {
+		PermissionChecker permissionChecker =
+			PermissionThreadLocal.getPermissionChecker();
+
+		return permissionChecker.getUserId();
+	}
+
+	private final BatchEngineExportTaskExecutor _batchEngineExportTaskExecutor;
+	private final BatchEngineExportTaskService _batchEngineExportTaskService;
+	private final BatchEngineImportTaskExecutor _batchEngineImportTaskExecutor;
+	private final BatchEngineImportTaskService _batchEngineImportTaskService;
+	private final String _className;
+	private final String _fileName;
+	private final String _taskItemDelegateName;
+
+}
---
+++ modules/apps/portal-vulcan/portal-vulcan-impl/src/main/java/com/liferay/portal/vulcan/internal/exportimport/data/handler/VulcanBatchEnginePortletDataHandlerRegistry.java
@@ -0,0 +1,160 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.vulcan.internal.exportimport.data.handler;
+
+import com.liferay.batch.engine.BatchEngineExportTaskExecutor;
+import com.liferay.batch.engine.BatchEngineImportTaskExecutor;
+import com.liferay.batch.engine.service.BatchEngineExportTaskService;
+import com.liferay.batch.engine.service.BatchEngineImportTaskService;
+import com.liferay.exportimport.kernel.lar.PortletDataHandler;
+import com.liferay.osgi.util.ServiceTrackerFactory;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.HashMapDictionaryBuilder;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.vulcan.batch.engine.VulcanBatchEngineTaskItemDelegate;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.util.tracker.ServiceTracker;
+import org.osgi.util.tracker.ServiceTrackerCustomizer;
+
+/**
+ * @author Alejandro Tardín
+ */
+@Component(service = {})
+public class VulcanBatchEnginePortletDataHandlerRegistry {
+
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		_serviceTracker = ServiceTrackerFactory.open(
+			bundleContext, "(batch.engine.task.item.delegate=true)",
+			new VulcanBatchEngineTaskItemDelegateServiceTrackerCustomizer(
+				bundleContext));
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		_serviceTracker.close();
+
+		for (ServiceRegistration<PortletDataHandler> serviceRegistration :
+				_serviceRegistrations.values()) {
+
+			try {
+				serviceRegistration.unregister();
+			}
+			catch (IllegalStateException illegalStateException) {
+				_log.error(illegalStateException);
+			}
+		}
+
+		_serviceRegistrations.clear();
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		VulcanBatchEnginePortletDataHandlerRegistry.class);
+
+	@Reference
+	private BatchEngineExportTaskExecutor _batchEngineExportTaskExecutor;
+
+	@Reference
+	private BatchEngineExportTaskService _batchEngineExportTaskService;
+
+	@Reference
+	private BatchEngineImportTaskExecutor _batchEngineImportTaskExecutor;
+
+	@Reference
+	private BatchEngineImportTaskService _batchEngineImportTaskService;
+
+	private final Map
+		<ServiceReference<VulcanBatchEngineTaskItemDelegate>,
+		 ServiceRegistration<PortletDataHandler>> _serviceRegistrations =
+			new ConcurrentHashMap<>();
+	private ServiceTracker
+		<VulcanBatchEngineTaskItemDelegate, PortletDataHandler> _serviceTracker;
+
+	private class VulcanBatchEngineTaskItemDelegateServiceTrackerCustomizer
+		implements ServiceTrackerCustomizer
+			<VulcanBatchEngineTaskItemDelegate, PortletDataHandler> {
+
+		public VulcanBatchEngineTaskItemDelegateServiceTrackerCustomizer(
+			BundleContext bundleContext) {
+
+			_bundleContext = bundleContext;
+		}
+
+		@Override
+		public PortletDataHandler addingService(
+			ServiceReference<VulcanBatchEngineTaskItemDelegate>
+				serviceReference) {
+
+			String portletId = (String)serviceReference.getProperty(
+				"batch.engine.task.item.delegate.portlet.id");
+
+			if (Validator.isNull(portletId)) {
+				return null;
+			}
+
+			VulcanBatchEnginePortletDataHandler
+				vulcanBatchEnginePortletDataHandler =
+					new VulcanBatchEnginePortletDataHandler(
+						_batchEngineExportTaskExecutor,
+						_batchEngineExportTaskService,
+						_batchEngineImportTaskExecutor,
+						_batchEngineImportTaskService,
+						(String)serviceReference.getProperty(
+							"batch.engine.task.item.delegate.class.name"),
+						(String)serviceReference.getProperty(
+							"batch.engine.task.item.delegate.name"));
+
+			_serviceRegistrations.put(
+				serviceReference,
+				_bundleContext.registerService(
+					PortletDataHandler.class,
+					vulcanBatchEnginePortletDataHandler,
+					HashMapDictionaryBuilder.<String, Object>put(
+						"javax.portlet.name", portletId
+					).build()));
+
+			return vulcanBatchEnginePortletDataHandler;
+		}
+
+		@Override
+		public void modifiedService(
+			ServiceReference<VulcanBatchEngineTaskItemDelegate>
+				serviceReference,
+			PortletDataHandler portletDataHandler) {
+
+			removedService(serviceReference, portletDataHandler);
+
+			addingService(serviceReference);
+		}
+
+		@Override
+		public void removedService(
+			ServiceReference<VulcanBatchEngineTaskItemDelegate>
+				serviceReference,
+			PortletDataHandler portletDataHandler) {
+
+			ServiceRegistration<PortletDataHandler> serviceRegistration =
+				_serviceRegistrations.remove(serviceReference);
+
+			serviceRegistration.unregister();
+		}
+
+		private final BundleContext _bundleContext;
+
+	}
+
+}
