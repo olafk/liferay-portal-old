1eba404e58dd3b0013228e01aa1a0882ba7bd393
David Truong <david.truong@liferay.com>
2024-12-19 13:58:11 -0600

    LPD-39850 update the test to ensure score expectations

--- modules/apps/change-tracking/change-tracking-test/src/testIntegration/java/com/liferay/change/tracking/service/test/CTScoreLocalServiceTest.java
+++ modules/apps/change-tracking/change-tracking-test/src/testIntegration/java/com/liferay/change/tracking/service/test/CTScoreLocalServiceTest.java
@@ -6,17 +6,28 @@
 package com.liferay.change.tracking.service.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.blogs.service.BlogsEntryLocalService;
 import com.liferay.change.tracking.model.CTCollection;
 import com.liferay.change.tracking.service.CTCollectionLocalService;
+import com.liferay.document.library.kernel.model.DLFolder;
+import com.liferay.document.library.test.util.DLTestUtil;
+import com.liferay.dynamic.data.mapping.model.DDMStructure;
+import com.liferay.dynamic.data.mapping.service.DDMStructureLocalService;
+import com.liferay.dynamic.data.mapping.test.util.DDMStructureTestUtil;
 import com.liferay.journal.constants.JournalFolderConstants;
-import com.liferay.journal.test.util.JournalTestUtil;
+import com.liferay.journal.model.JournalArticle;
+import com.liferay.journal.service.JournalArticleLocalService;
 import com.liferay.petra.lang.SafeCloseable;
 import com.liferay.portal.kernel.change.tracking.CTCollectionThreadLocal;
 import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
@@ -43,7 +54,13 @@ public class CTScoreLocalServiceTest {
 
 	@Before
 	public void setUp() throws Exception {
-		_ctCollection = _ctCollectionLocalService.addCTCollection(
+		_ctCollection1 = _ctCollectionLocalService.addCTCollection(
+			null, TestPropsValues.getCompanyId(), TestPropsValues.getUserId(),
+			0, CTCollectionLocalServiceTest.class.getSimpleName(), null);
+		_ctCollection2 = _ctCollectionLocalService.addCTCollection(
+			null, TestPropsValues.getCompanyId(), TestPropsValues.getUserId(),
+			0, CTCollectionLocalServiceTest.class.getSimpleName(), null);
+		_ctCollection3 = _ctCollectionLocalService.addCTCollection(
 			null, TestPropsValues.getCompanyId(), TestPropsValues.getUserId(),
 			0, CTCollectionLocalServiceTest.class.getSimpleName(), null);
 		_group = GroupTestUtil.addGroup();
@@ -51,28 +68,92 @@ public class CTScoreLocalServiceTest {
 
 	@Test
 	public void testAddCTScore() throws Exception {
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(_group.getGroupId());
+
 		try (SafeCloseable safeCloseable =
 				CTCollectionThreadLocal.setCTCollectionIdWithSafeCloseable(
-					_ctCollection.getCtCollectionId())) {
+					_ctCollection1.getCtCollectionId())) {
 
-			JournalTestUtil.addArticle(
-				TestPropsValues.getUserId(), _group.getGroupId(),
-				JournalFolderConstants.DEFAULT_PARENT_FOLDER_ID);
+			_blogsEntryLocalService.addEntry(
+				TestPropsValues.getUserId(), RandomTestUtil.randomString(),
+				RandomTestUtil.randomString(), serviceContext);
 		}
 
-		_ctCollection = _ctCollectionLocalService.fetchCTCollection(
-			_ctCollection.getCtCollectionId());
+		try (SafeCloseable safeCloseable =
+				CTCollectionThreadLocal.setCTCollectionIdWithSafeCloseable(
+					_ctCollection2.getCtCollectionId())) {
 
-		Assert.assertNotEquals(0, _ctCollection.getScore());
+			DDMStructure ddmStructure =
+				_ddmStructureLocalService.fetchStructure(
+					_group.getGroupId(),
+					_portal.getClassNameId(JournalArticle.class.getName()),
+					"BASIC-WEB-CONTENT", true);
+
+			_journalArticleLocalService.addArticle(
+				null, TestPropsValues.getUserId(), _group.getGroupId(),
+				JournalFolderConstants.DEFAULT_PARENT_FOLDER_ID,
+				RandomTestUtil.randomLocaleStringMap(),
+				RandomTestUtil.randomLocaleStringMap(),
+				DDMStructureTestUtil.getSampleStructuredContent(),
+				ddmStructure.getStructureId(), "BASIC-WEB-CONTENT",
+				serviceContext);
+		}
+
+		try (SafeCloseable safeCloseable =
+				CTCollectionThreadLocal.setCTCollectionIdWithSafeCloseable(
+					_ctCollection3.getCtCollectionId())) {
+
+			DLFolder dlFolder = DLTestUtil.addDLFolder(_group.getGroupId());
+
+			for (int i = 0; i < 5; i++) {
+				DLTestUtil.addDLFileEntry(dlFolder.getFolderId());
+			}
+		}
+
+		_ctCollection1 = _ctCollectionLocalService.fetchCTCollection(
+			_ctCollection1.getCtCollectionId());
+
+		Assert.assertTrue(_ctCollection1.getScore() > 0);
+
+		_ctCollection2 = _ctCollectionLocalService.fetchCTCollection(
+			_ctCollection2.getCtCollectionId());
+
+		Assert.assertTrue(
+			_ctCollection2.getScore() > _ctCollection1.getScore());
+
+		_ctCollection3 = _ctCollectionLocalService.fetchCTCollection(
+			_ctCollection3.getCtCollectionId());
+
+		Assert.assertTrue(
+			_ctCollection3.getScore() > _ctCollection2.getScore());
 	}
 
+	@Inject
+	private BlogsEntryLocalService _blogsEntryLocalService;
+
 	@DeleteAfterTestRun
-	private CTCollection _ctCollection;
+	private CTCollection _ctCollection1;
+
+	@DeleteAfterTestRun
+	private CTCollection _ctCollection2;
+
+	@DeleteAfterTestRun
+	private CTCollection _ctCollection3;
 
 	@Inject
 	private CTCollectionLocalService _ctCollectionLocalService;
 
+	@Inject
+	private DDMStructureLocalService _ddmStructureLocalService;
+
 	@DeleteAfterTestRun
 	private Group _group;
+
+	@Inject
+	private JournalArticleLocalService _journalArticleLocalService;
+
+	@Inject
+	private Portal _portal;
 
 }
