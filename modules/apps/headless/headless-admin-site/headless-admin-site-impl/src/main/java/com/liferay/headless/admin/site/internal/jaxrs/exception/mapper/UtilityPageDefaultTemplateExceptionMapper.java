cd3fb85f3d6d5b594ada205ead84cb0dd8f572ce
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2024-12-26 12:10:18 +0100

    LPD-42587 Add exception mapper to manage LayoutUtilityPageEntryDefaultTemplateException

---
+++ modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/jaxrs/exception/mapper/UtilityPageDefaultTemplateExceptionMapper.java
@@ -0,0 +1,41 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.headless.admin.site.internal.jaxrs.exception.mapper;
+
+import com.liferay.layout.utility.page.exception.LayoutUtilityPageEntryDefaultTemplateException;
+import com.liferay.portal.vulcan.jaxrs.exception.mapper.BaseExceptionMapper;
+import com.liferay.portal.vulcan.jaxrs.exception.mapper.Problem;
+
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Lourdes Fernández Besada
+ */
+@Component(
+	property = {
+		"osgi.jaxrs.application.select=(osgi.jaxrs.name=Liferay.Headless.Admin.Site)",
+		"osgi.jaxrs.extension=true",
+		"osgi.jaxrs.name=Liferay.Headless.Admin.Site.UtilityPageDefaultTemplateExceptionMapper"
+	},
+	service = ExceptionMapper.class
+)
+@Provider
+public class UtilityPageDefaultTemplateExceptionMapper
+	extends BaseExceptionMapper
+		<LayoutUtilityPageEntryDefaultTemplateException> {
+
+	@Override
+	protected Problem getProblem(
+		LayoutUtilityPageEntryDefaultTemplateException
+			layoutUtilityPageEntryDefaultTemplateException) {
+
+		return new Problem(layoutUtilityPageEntryDefaultTemplateException);
+	}
+
+}
