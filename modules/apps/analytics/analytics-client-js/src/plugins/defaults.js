36acf463ea62f83a7a4723779ab54e926e2d8eed
Adriano Interaminense <adriano.interaminense@gmail.com>
2025-01-03 00:08:56 -0300

    LPD-43539 | LPD-44536 LPD-44537 LPD-45319 Create new documentsFragment plugin to support document mapped on a DXP fragment

--- modules/apps/analytics/analytics-client-js/src/plugins/defaults.js
+++ modules/apps/analytics/analytics-client-js/src/plugins/defaults.js
@@ -6,6 +6,7 @@
 import blogs from './blogs';
 import custom from './custom';
 import documents from './documents';
+import documentsFragment from './documentsFragment';
 import dxp from './dxp';
 import forms from './forms';
 import read from './read';
@@ -14,7 +15,17 @@ import timing from './timing';
 import visibility from './visibility';
 import webContents from './web-contents';
 
-export {blogs, documents, dxp, forms, read, scrolling, timing, webContents};
+export {
+	blogs,
+	documents,
+	documentsFragment,
+	dxp,
+	forms,
+	read,
+	scrolling,
+	timing,
+	webContents,
+};
 export default [
 
 	// Dxp should be before other events plugins, because it can dispose analytics
@@ -24,6 +35,7 @@ export default [
 	blogs,
 	custom,
 	documents,
+	documentsFragment,
 	forms,
 	read,
 	scrolling,
