7c7fe17a61dfe64c535b99d32c6c82cc6971d9c0
Gislayne Vitorino <gislayne.vitorino@liferay.com>
2025-01-06 08:15:15 -0300

    LPD-44274 Add test

--- modules/test/playwright/tests/change-tracking-web/changeTrackingIndicator.spec.ts
+++ modules/test/playwright/tests/change-tracking-web/changeTrackingIndicator.spec.ts
@@ -6,6 +6,7 @@
 import {expect, mergeTests} from '@playwright/test';
 
 import {apiHelpersTest} from '../../fixtures/apiHelpersTest';
+import {changeTrackingPagesTest} from '../../fixtures/changeTrackingPagesTest';
 import {loginTest} from '../../fixtures/loginTest';
 import getRandomString from '../../utils/getRandomString';
 import performLogin, {performLogout, userData} from '../../utils/performLogin';
@@ -13,6 +14,7 @@ import {featureFlagPagesTest} from '../feature-flag-web/fixtures/featureFlagPage
 
 export const test = mergeTests(
 	apiHelpersTest,
+	changeTrackingPagesTest,
 	featureFlagPagesTest,
 	loginTest()
 );
@@ -124,5 +126,47 @@ test('LPD-36221 Publications bar breaks when enabling the FF for LPD-20131', asy
 
 	await apiHelpers.headlessChangeTracking.deleteCTCollection(
 		ctCollection.body.id
+	);
+});
+
+test('LPD-44274 Assert cursor type is pointer when hover over a not selected publication', async ({
+	apiHelpers,
+	ctCollection,
+	page,
+}) => {
+	const ctCollection2 =
+		await apiHelpers.headlessChangeTracking.createCTCollection(
+			getRandomString()
+		);
+
+	await performLogout(page);
+	await performLogin(page, 'test');
+
+	await apiHelpers.headlessChangeTracking.checkoutCTCollection(
+		ctCollection2.body.id
+	);
+
+	await page.reload();
+
+	await page.locator('.change-tracking-indicator-button').click();
+
+	const selectPublicationMenuItem = page.getByRole('menuitem', {
+		name: 'Select a Publication',
+	});
+
+	await expect(selectPublicationMenuItem).toBeVisible();
+
+	await selectPublicationMenuItem.click();
+
+	const cursorType = await page
+		.getByText(ctCollection.body.name)
+		.evaluate((element) =>
+			window.getComputedStyle(element).getPropertyValue('cursor')
+		);
+
+	await expect(cursorType).toEqual('pointer');
+
+	await apiHelpers.headlessChangeTracking.deleteCTCollection(
+		ctCollection2.body.id
 	);
 });
