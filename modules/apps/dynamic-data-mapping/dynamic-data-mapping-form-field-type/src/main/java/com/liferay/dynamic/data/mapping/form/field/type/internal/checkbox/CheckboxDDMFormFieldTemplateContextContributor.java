cgmonte/liferay-portal

LPD-45015 Add localized propertys to the json

modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/java/com/liferay/dynamic/data/mapping/form/field/type/internal/checkbox/CheckboxDDMFormFieldTemplateContextContributor.java
@@ -8,7 +8,9 @@ package com.liferay.dynamic.data.mapping.form.field.type.internal.checkbox;
 import com.liferay.configuration.admin.constants.ConfigurationAdminPortletKeys;
 import com.liferay.dynamic.data.mapping.form.field.type.DDMFormFieldTemplateContextContributor;
 import com.liferay.dynamic.data.mapping.form.field.type.constants.DDMFormFieldTypeConstants;
+import com.liferay.dynamic.data.mapping.form.field.type.internal.util.DDMFormFieldTemplateContextContributorUtil;
 import com.liferay.dynamic.data.mapping.form.field.type.internal.util.DDMFormFieldTypeUtil;
+import com.liferay.dynamic.data.mapping.model.DDMForm;
 import com.liferay.dynamic.data.mapping.model.DDMFormField;
 import com.liferay.dynamic.data.mapping.render.DDMFormFieldRenderingContext;
 import com.liferay.petra.string.StringPool;
@@ -39,7 +41,14 @@ public class CheckboxDDMFormFieldTemplateContextContributor
 		DDMFormField ddmFormField,
 		DDMFormFieldRenderingContext ddmFormFieldRenderingContext) {
 
+		DDMForm ddmForm = ddmFormField.getDDMForm();
+
+		boolean localizedObjectField = GetterUtil.getBoolean(
+			ddmFormField.getProperty("localizedObjectField"));
+
 		return HashMapBuilder.<String, Object>put(
+			"localizedObjectField", localizedObjectField
+		).put(
 			"predefinedValue",
 			GetterUtil.getBoolean(
 				DDMFormFieldTypeUtil.getValue(
@@ -76,6 +85,9 @@ public class CheckboxDDMFormFieldTemplateContextContributor
 			GetterUtil.getBoolean(
 				DDMFormFieldTypeUtil.getValue(
 					ddmFormFieldRenderingContext.getValue()))
+		).putAll(
+			DDMFormFieldTemplateContextContributorUtil.getLocalesMap(
+				ddmForm.getDefaultLocale())
 		).build();
 	}
 

modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/java/com/liferay/dynamic/data/mapping/form/field/type/internal/localizable/text/LocalizableTextDDMFormFieldTemplateContextContributor.java
@@ -7,6 +7,7 @@ package com.liferay.dynamic.data.mapping.form.field.type.internal.localizable.te
 
 import com.liferay.dynamic.data.mapping.form.field.type.DDMFormFieldTemplateContextContributor;
 import com.liferay.dynamic.data.mapping.form.field.type.constants.DDMFormFieldTypeConstants;
+import com.liferay.dynamic.data.mapping.form.field.type.internal.util.DDMFormFieldTemplateContextContributorUtil;
 import com.liferay.dynamic.data.mapping.model.DDMForm;
 import com.liferay.dynamic.data.mapping.model.DDMFormField;
 import com.liferay.dynamic.data.mapping.model.LocalizedValue;
@@ -24,7 +25,6 @@ import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.ResourceBundleUtil;
-import com.liferay.portal.kernel.util.StringUtil;
 
 import java.util.HashMap;
 import java.util.Locale;
@@ -52,21 +52,7 @@ public class LocalizableTextDDMFormFieldTemplateContextContributor
 		Map<String, Object> parameters = new HashMap<>();
 
 		if (ddmFormFieldRenderingContext.isReturnFullContext()) {
-			parameters.put(
-				"availableLocales",
-				JSONUtil.toJSONArray(
-					_language.getAvailableLocales(), this::_getLocaleJSONObject,
-					_log));
-
-			DDMForm ddmForm = ddmFormField.getDDMForm();
-
-			JSONObject localeJSONObject = _getLocaleJSONObject(
-				ddmForm.getDefaultLocale());
-
-			parameters.put("defaultLocale", localeJSONObject);
-
 			parameters.put("displayStyle", _getDisplayStyle(ddmFormField));
-			parameters.put("editingLocale", localeJSONObject);
 			parameters.put(
 				"placeholder",
 				_getPlaceholder(ddmFormField, ddmFormFieldRenderingContext));
@@ -78,6 +64,12 @@ public class LocalizableTextDDMFormFieldTemplateContextContributor
 			parameters.put(
 				"tooltip",
 				_getTooltip(ddmFormField, ddmFormFieldRenderingContext));
+
+			DDMForm ddmForm = ddmFormField.getDDMForm();
+
+			parameters.putAll(
+				DDMFormFieldTemplateContextContributorUtil.getLocalesMap(
+					ddmForm.getDefaultLocale()));
 		}
 
 		String predefinedValue = _getPredefinedValue(
@@ -109,23 +101,6 @@ public class LocalizableTextDDMFormFieldTemplateContextContributor
 	private String _getDisplayStyle(DDMFormField ddmFormField) {
 		return GetterUtil.getString(
 			ddmFormField.getProperty("displayStyle"), "singleline");
-	}
-
-	private JSONObject _getLocaleJSONObject(Locale locale) {
-		JSONObject jsonObject = jsonFactory.createJSONObject();
-
-		String languageId = LocaleUtil.toLanguageId(locale);
-
-		jsonObject.put(
-			"displayName", locale.getDisplayName(locale)
-		).put(
-			"icon",
-			StringUtil.toLowerCase(StringUtil.replace(languageId, '_', "-"))
-		).put(
-			"localeId", languageId
-		);
-
-		return jsonObject;
 	}
 
 	private String _getPlaceholder(

modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/java/com/liferay/dynamic/data/mapping/form/field/type/internal/rich/text/RichTextDDMFormFieldTemplateContextContributor.java
@@ -8,6 +8,7 @@ package com.liferay.dynamic.data.mapping.form.field.type.internal.rich.text;
 import com.liferay.ai.creator.openai.manager.AICreatorOpenAIManager;
 import com.liferay.dynamic.data.mapping.form.field.type.DDMFormFieldTemplateContextContributor;
 import com.liferay.dynamic.data.mapping.form.field.type.constants.DDMFormFieldTypeConstants;
+import com.liferay.dynamic.data.mapping.form.field.type.internal.util.DDMFormFieldTemplateContextContributorUtil;
 import com.liferay.dynamic.data.mapping.form.field.type.internal.util.DDMFormFieldTypeUtil;
 import com.liferay.dynamic.data.mapping.model.DDMForm;
 import com.liferay.dynamic.data.mapping.model.DDMFormField;
@@ -18,19 +19,14 @@ import com.liferay.portal.kernel.editor.configuration.EditorConfigurationFactory
 import com.liferay.portal.kernel.json.JSONException;
 import com.liferay.portal.kernel.json.JSONFactory;
 import com.liferay.portal.kernel.json.JSONObject;
-import com.liferay.portal.kernel.json.JSONUtil;
-import com.liferay.portal.kernel.language.Language;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.portlet.RequestBackedPortletURLFactoryUtil;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.HashMapBuilder;
-import com.liferay.portal.kernel.util.LocaleUtil;
-import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.WebKeys;
 
-import java.util.Locale;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -56,22 +52,10 @@ public class RichTextDDMFormFieldTemplateContextContributor
 
 		DDMForm ddmForm = ddmFormField.getDDMForm();
 
-		JSONObject localeJSONObject = _getLocaleJSONObject(
-			ddmForm.getDefaultLocale());
-
 		boolean localizedObjectField = GetterUtil.getBoolean(
 			ddmFormField.getProperty("localizedObjectField"));
 
 		return HashMapBuilder.<String, Object>put(
-			"availableLocales",
-			JSONUtil.toJSONArray(
-				_language.getAvailableLocales(), this::_getLocaleJSONObject,
-				_log)
-		).put(
-			"defaultLocale", localeJSONObject
-		).put(
-			"editingLocale", localeJSONObject
-		).put(
 			"localizedObjectField", localizedObjectField
 		).put(
 			"predefinedValue",
@@ -95,6 +79,9 @@ public class RichTextDDMFormFieldTemplateContextContributor
 				return DDMFormFieldTypeUtil.getPropertyValue(
 					ddmFormFieldRenderingContext, "value");
 			}
+		).putAll(
+			DDMFormFieldTemplateContextContributorUtil.getLocalesMap(
+				ddmForm.getDefaultLocale())
 		).putAll(
 			_getData(ddmFormFieldRenderingContext, ddmFormField.getType())
 		).build();
@@ -130,23 +117,6 @@ public class RichTextDDMFormFieldTemplateContextContributor
 				RequestBackedPortletURLFactoryUtil.create(httpServletRequest));
 
 		return editorConfiguration.getData();
-	}
-
-	private JSONObject _getLocaleJSONObject(Locale locale) {
-		JSONObject jsonObject = _jsonFactory.createJSONObject();
-
-		String languageId = LocaleUtil.toLanguageId(locale);
-
-		jsonObject.put(
-			"displayName", locale.getDisplayName(locale)
-		).put(
-			"icon",
-			StringUtil.toLowerCase(StringUtil.replace(languageId, '_', "-"))
-		).put(
-			"localeId", languageId
-		);
-
-		return jsonObject;
 	}
 
 	private String _getPredefinedValue(
@@ -187,8 +157,5 @@ public class RichTextDDMFormFieldTemplateContextContributor
 
 	@Reference
 	private JSONFactory _jsonFactory;
-
-	@Reference
-	private Language _language;
 
 }

modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/java/com/liferay/dynamic/data/mapping/form/field/type/internal/util/DDMFormFieldTemplateContextContributorUtil.java
@@ -1,0 +1,56 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.dynamic.data.mapping.form.field.type.internal.util;
+
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+
+import java.util.Locale;
+import java.util.Map;
+
+/**
+ * @author Pedro Leite
+ */
+public class DDMFormFieldTemplateContextContributorUtil {
+
+	public static Map<String, Object> getLocalesMap(Locale defaultLocale) {
+		JSONObject localeJSONObject = _getLocaleJSONObject(defaultLocale);
+
+		return HashMapBuilder.<String, Object>put(
+			"availableLocales",
+			JSONUtil.toJSONArray(
+				LanguageUtil.getAvailableLocales(),
+				locale -> _getLocaleJSONObject(locale), _log)
+		).put(
+			"defaultLocale", localeJSONObject
+		).put(
+			"editingLocale", localeJSONObject
+		).build();
+	}
+
+	private static JSONObject _getLocaleJSONObject(Locale locale) {
+		String languageId = LocaleUtil.toLanguageId(locale);
+
+		return JSONUtil.put(
+			"displayName", locale.getDisplayName(locale)
+		).put(
+			"icon",
+			StringUtil.toLowerCase(StringUtil.replace(languageId, '_', "-"))
+		).put(
+			"localeId", languageId
+		);
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		DDMFormFieldTemplateContextContributorUtil.class);
+
+}

olafk/liferay-portal-old

modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/java/com/liferay/dynamic/data/mapping/form/field/type/internal/checkbox/CheckboxDDMFormFieldTemplateContextContributor.java
@@ -8,7 +8,9 @@ package com.liferay.dynamic.data.mapping.form.field.type.internal.checkbox;
 import com.liferay.configuration.admin.constants.ConfigurationAdminPortletKeys;
 import com.liferay.dynamic.data.mapping.form.field.type.DDMFormFieldTemplateContextContributor;
 import com.liferay.dynamic.data.mapping.form.field.type.constants.DDMFormFieldTypeConstants;
+import com.liferay.dynamic.data.mapping.form.field.type.internal.util.DDMFormFieldTemplateContextContributorUtil;
 import com.liferay.dynamic.data.mapping.form.field.type.internal.util.DDMFormFieldTypeUtil;
+import com.liferay.dynamic.data.mapping.model.DDMForm;
 import com.liferay.dynamic.data.mapping.model.DDMFormField;
 import com.liferay.dynamic.data.mapping.render.DDMFormFieldRenderingContext;
 import com.liferay.petra.string.StringPool;
@@ -39,7 +41,14 @@ public class CheckboxDDMFormFieldTemplateContextContributor
 		DDMFormField ddmFormField,
 		DDMFormFieldRenderingContext ddmFormFieldRenderingContext) {
 
+		DDMForm ddmForm = ddmFormField.getDDMForm();
+
+		boolean localizedObjectField = GetterUtil.getBoolean(
+			ddmFormField.getProperty("localizedObjectField"));
+
 		return HashMapBuilder.<String, Object>put(
+			"localizedObjectField", localizedObjectField
+		).put(
 			"predefinedValue",
 			GetterUtil.getBoolean(
 				DDMFormFieldTypeUtil.getValue(
@@ -76,6 +85,9 @@ public class CheckboxDDMFormFieldTemplateContextContributor
 			GetterUtil.getBoolean(
 				DDMFormFieldTypeUtil.getValue(
 					ddmFormFieldRenderingContext.getValue()))
+		).putAll(
+			DDMFormFieldTemplateContextContributorUtil.getLocaleMap(
+				ddmForm.getDefaultLocale())
 		).build();
 	}
 

modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/java/com/liferay/dynamic/data/mapping/form/field/type/internal/localizable/text/LocalizableTextDDMFormFieldTemplateContextContributor.java
@@ -7,6 +7,7 @@ package com.liferay.dynamic.data.mapping.form.field.type.internal.localizable.te
 
 import com.liferay.dynamic.data.mapping.form.field.type.DDMFormFieldTemplateContextContributor;
 import com.liferay.dynamic.data.mapping.form.field.type.constants.DDMFormFieldTypeConstants;
+import com.liferay.dynamic.data.mapping.form.field.type.internal.util.DDMFormFieldTemplateContextContributorUtil;
 import com.liferay.dynamic.data.mapping.model.DDMForm;
 import com.liferay.dynamic.data.mapping.model.DDMFormField;
 import com.liferay.dynamic.data.mapping.model.LocalizedValue;
@@ -24,7 +25,6 @@ import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.ResourceBundleUtil;
-import com.liferay.portal.kernel.util.StringUtil;
 
 import java.util.HashMap;
 import java.util.Locale;
@@ -52,21 +52,7 @@ public class LocalizableTextDDMFormFieldTemplateContextContributor
 		Map<String, Object> parameters = new HashMap<>();
 
 		if (ddmFormFieldRenderingContext.isReturnFullContext()) {
-			parameters.put(
-				"availableLocales",
-				JSONUtil.toJSONArray(
-					_language.getAvailableLocales(), this::_getLocaleJSONObject,
-					_log));
-
-			DDMForm ddmForm = ddmFormField.getDDMForm();
-
-			JSONObject localeJSONObject = _getLocaleJSONObject(
-				ddmForm.getDefaultLocale());
-
-			parameters.put("defaultLocale", localeJSONObject);
-
 			parameters.put("displayStyle", _getDisplayStyle(ddmFormField));
-			parameters.put("editingLocale", localeJSONObject);
 			parameters.put(
 				"placeholder",
 				_getPlaceholder(ddmFormField, ddmFormFieldRenderingContext));
@@ -78,6 +64,12 @@ public class LocalizableTextDDMFormFieldTemplateContextContributor
 			parameters.put(
 				"tooltip",
 				_getTooltip(ddmFormField, ddmFormFieldRenderingContext));
+
+			DDMForm ddmForm = ddmFormField.getDDMForm();
+
+			parameters.putAll(
+				DDMFormFieldTemplateContextContributorUtil.getLocaleMap(
+					ddmForm.getDefaultLocale()));
 		}
 
 		String predefinedValue = _getPredefinedValue(
@@ -109,23 +101,6 @@ public class LocalizableTextDDMFormFieldTemplateContextContributor
 	private String _getDisplayStyle(DDMFormField ddmFormField) {
 		return GetterUtil.getString(
 			ddmFormField.getProperty("displayStyle"), "singleline");
-	}
-
-	private JSONObject _getLocaleJSONObject(Locale locale) {
-		JSONObject jsonObject = jsonFactory.createJSONObject();
-
-		String languageId = LocaleUtil.toLanguageId(locale);
-
-		jsonObject.put(
-			"displayName", locale.getDisplayName(locale)
-		).put(
-			"icon",
-			StringUtil.toLowerCase(StringUtil.replace(languageId, '_', "-"))
-		).put(
-			"localeId", languageId
-		);
-
-		return jsonObject;
 	}
 
 	private String _getPlaceholder(

modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/java/com/liferay/dynamic/data/mapping/form/field/type/internal/rich/text/RichTextDDMFormFieldTemplateContextContributor.java
@@ -8,6 +8,7 @@ package com.liferay.dynamic.data.mapping.form.field.type.internal.rich.text;
 import com.liferay.ai.creator.openai.manager.AICreatorOpenAIManager;
 import com.liferay.dynamic.data.mapping.form.field.type.DDMFormFieldTemplateContextContributor;
 import com.liferay.dynamic.data.mapping.form.field.type.constants.DDMFormFieldTypeConstants;
+import com.liferay.dynamic.data.mapping.form.field.type.internal.util.DDMFormFieldTemplateContextContributorUtil;
 import com.liferay.dynamic.data.mapping.form.field.type.internal.util.DDMFormFieldTypeUtil;
 import com.liferay.dynamic.data.mapping.model.DDMForm;
 import com.liferay.dynamic.data.mapping.model.DDMFormField;
@@ -18,19 +19,14 @@ import com.liferay.portal.kernel.editor.configuration.EditorConfigurationFactory
 import com.liferay.portal.kernel.json.JSONException;
 import com.liferay.portal.kernel.json.JSONFactory;
 import com.liferay.portal.kernel.json.JSONObject;
-import com.liferay.portal.kernel.json.JSONUtil;
-import com.liferay.portal.kernel.language.Language;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.portlet.RequestBackedPortletURLFactoryUtil;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.HashMapBuilder;
-import com.liferay.portal.kernel.util.LocaleUtil;
-import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.WebKeys;
 
-import java.util.Locale;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -56,22 +52,10 @@ public class RichTextDDMFormFieldTemplateContextContributor
 
 		DDMForm ddmForm = ddmFormField.getDDMForm();
 
-		JSONObject localeJSONObject = _getLocaleJSONObject(
-			ddmForm.getDefaultLocale());
-
 		boolean localizedObjectField = GetterUtil.getBoolean(
 			ddmFormField.getProperty("localizedObjectField"));
 
 		return HashMapBuilder.<String, Object>put(
-			"availableLocales",
-			JSONUtil.toJSONArray(
-				_language.getAvailableLocales(), this::_getLocaleJSONObject,
-				_log)
-		).put(
-			"defaultLocale", localeJSONObject
-		).put(
-			"editingLocale", localeJSONObject
-		).put(
 			"localizedObjectField", localizedObjectField
 		).put(
 			"predefinedValue",
@@ -95,6 +79,9 @@ public class RichTextDDMFormFieldTemplateContextContributor
 				return DDMFormFieldTypeUtil.getPropertyValue(
 					ddmFormFieldRenderingContext, "value");
 			}
+		).putAll(
+			DDMFormFieldTemplateContextContributorUtil.getLocaleMap(
+				ddmForm.getDefaultLocale())
 		).putAll(
 			_getData(ddmFormFieldRenderingContext, ddmFormField.getType())
 		).build();
@@ -130,23 +117,6 @@ public class RichTextDDMFormFieldTemplateContextContributor
 				RequestBackedPortletURLFactoryUtil.create(httpServletRequest));
 
 		return editorConfiguration.getData();
-	}
-
-	private JSONObject _getLocaleJSONObject(Locale locale) {
-		JSONObject jsonObject = _jsonFactory.createJSONObject();
-
-		String languageId = LocaleUtil.toLanguageId(locale);
-
-		jsonObject.put(
-			"displayName", locale.getDisplayName(locale)
-		).put(
-			"icon",
-			StringUtil.toLowerCase(StringUtil.replace(languageId, '_', "-"))
-		).put(
-			"localeId", languageId
-		);
-
-		return jsonObject;
 	}
 
 	private String _getPredefinedValue(
@@ -187,8 +157,5 @@ public class RichTextDDMFormFieldTemplateContextContributor
 
 	@Reference
 	private JSONFactory _jsonFactory;
-
-	@Reference
-	private Language _language;
 
 }

modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/java/com/liferay/dynamic/data/mapping/form/field/type/internal/util/DDMFormFieldTemplateContextContributorUtil.java
@@ -1,0 +1,56 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.dynamic.data.mapping.form.field.type.internal.util;
+
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+
+import java.util.Locale;
+import java.util.Map;
+
+/**
+ * @author Pedro Leite
+ */
+public class DDMFormFieldTemplateContextContributorUtil {
+
+	public static Map<String, Object> getLocaleMap(Locale defaultLocale) {
+		JSONObject localeJSONObject = _getLocaleJSONObject(defaultLocale);
+
+		return HashMapBuilder.<String, Object>put(
+			"availableLocales",
+			JSONUtil.toJSONArray(
+				LanguageUtil.getAvailableLocales(),
+				locale -> _getLocaleJSONObject(locale), _log)
+		).put(
+			"defaultLocale", localeJSONObject
+		).put(
+			"editingLocale", localeJSONObject
+		).build();
+	}
+
+	private static JSONObject _getLocaleJSONObject(Locale locale) {
+		String languageId = LocaleUtil.toLanguageId(locale);
+
+		return JSONUtil.put(
+			"displayName", locale.getDisplayName(locale)
+		).put(
+			"icon",
+			StringUtil.toLowerCase(StringUtil.replace(languageId, '_', "-"))
+		).put(
+			"localeId", languageId
+		);
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		DDMFormFieldTemplateContextContributorUtil.class);
+
+}