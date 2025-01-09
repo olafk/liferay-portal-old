824e05af23873daee92ad4a261d6351cf25ed91c
Alberto Moreno <alberto.moreno@liferay.com>
2025-01-09 10:21:40 +0100

    LPD-44648 Move test to proper package

--- modules/apps/headless/headless-builder/headless-builder-test/src/testIntegration/java/com/liferay/headless/builder/internal/upgrade/v0_1_0/test/DeleteAPIPropertiesToAPIPropertiesUpgradeProcessTest.java
+++ modules/apps/headless/headless-builder/headless-builder-test/src/testIntegration/java/com/liferay/headless/builder/internal/upgrade/v0_1_0/test/DeleteAPIPropertiesToAPIPropertiesUpgradeProcessTest.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.headless.builder.upgrade.v0_1_0.test;
+package com.liferay.headless.builder.internal.upgrade.v0_1_0.test;
 
 import com.liferay.headless.builder.test.BaseTestCase;
 import com.liferay.object.constants.ObjectRelationshipConstants;
