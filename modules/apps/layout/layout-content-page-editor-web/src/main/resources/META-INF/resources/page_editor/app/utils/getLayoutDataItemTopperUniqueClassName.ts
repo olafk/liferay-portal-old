ae750e23d1606d0a47874345c96f9331a927ed0a
Sandro Chinea <sandrochinea@gmail.com>
2025-01-10 14:20:35 +0000

    LPD-37501 Migrate to typescript

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/utils/getLayoutDataItemTopperUniqueClassName.ts
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/utils/getLayoutDataItemTopperUniqueClassName.ts
@@ -3,8 +3,11 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
+import {LayoutDataItem} from '../../types/layout_data/LayoutData';
 import {LAYOUT_STRUCTURE_ITEM_CLASS_NAME_PREFIX} from '../config/constants/layoutStructureItemClassNamePrefix';
 
-export default function getLayoutDataItemTopperUniqueClassName(itemId) {
+export default function getLayoutDataItemTopperUniqueClassName(
+	itemId: LayoutDataItem['itemId']
+) {
 	return `${LAYOUT_STRUCTURE_ITEM_CLASS_NAME_PREFIX}topper-${itemId}`;
 }
