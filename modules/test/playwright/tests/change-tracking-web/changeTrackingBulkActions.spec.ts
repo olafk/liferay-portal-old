75b8c6aef244d1ba050adf2f255e98505f74459a
Noor Najjar <noor.najjar@liferay.com>
2025-01-17 13:39:37 -0800

    LPD-46060 Add playwright test

--- modules/test/playwright/tests/change-tracking-web/changeTrackingBulkActions.spec.ts
+++ modules/test/playwright/tests/change-tracking-web/changeTrackingBulkActions.spec.ts
@@ -11,6 +11,7 @@ import {apiHelpersTest} from '../../fixtures/apiHelpersTest';
 import {changeTrackingPagesTest} from '../../fixtures/changeTrackingPagesTest';
 import getRandomString from '../../utils/getRandomString';
 import getBasicWebContentStructureId from '../../utils/structured-content/getBasicWebContentStructureId';
+import {waitForAlert} from '../../utils/waitForAlert';
 import {featureFlagPagesTest} from '../feature-flag-web/fixtures/featureFlagPagesTest';
 
 export const test = mergeTests(
@@ -70,5 +71,94 @@ test('LPD-26363 Can delete ctEntries in bulk', async ({
 
 	await changeTrackingPage.goToReviewChanges(ctCollection.body.name);
 
+	const publicationActionMenu = page.locator('.publications-tbar .dropdown');
+
+	await publicationActionMenu.click();
+
+	const notificationsOption = page.getByRole('menuitem', {
+		name: 'Show System Changes',
+	});
+
+	await notificationsOption.click();
+
+	await publicationActionMenu.click();
+
+	await expect(
+		page.getByRole('menuitem', {
+			name: 'Hide System Changes',
+		})
+	).toBeVisible();
+
 	await expect(page.getByText('No Results Found')).toBeVisible();
+});
+
+test('LPD-46060 Can move ctEntries in bulk', async ({
+	apiHelpers,
+	changeTrackingPage,
+	ctCollection,
+	page,
+}) => {
+	const ctCollection2 =
+		await apiHelpers.headlessChangeTracking.createCTCollection(
+			getRandomString()
+		);
+
+	await changeTrackingPage.goToReviewChanges(ctCollection.body.name);
+
+	await expect(
+		changeTrackingPage.frontendDataSetEntries.getByText(file.title)
+	).toBeVisible();
+
+	await page.getByTitle('Select Items').check();
+
+	await expect(page.getByText('Items Selected')).toBeVisible();
+
+	const bulkMoveButton = page
+		.locator('.bulk-actions')
+		.getByRole('button')
+		.nth(0);
+
+	await expect(bulkMoveButton).toBeVisible();
+
+	await bulkMoveButton.click();
+
+	await expect(
+		page.getByRole('heading', {name: 'Moved Changes'})
+	).toBeVisible();
+
+	const publicationSelector = page.locator(
+		'#_com_liferay_change_tracking_web_portlet_PublicationsPortlet_toPublication'
+	);
+
+	await expect(publicationSelector).toBeVisible();
+
+	await publicationSelector.selectOption(ctCollection2.body.name);
+
+	await page.getByRole('button', {name: 'Move Changes'}).click();
+
+	await waitForAlert(page, 'Success:Your request completed successfully.');
+
+	const publicationActionMenu = page.locator('.publications-tbar .dropdown');
+
+	await publicationActionMenu.click();
+
+	const notificationsOption = page.getByRole('menuitem', {
+		name: 'Show System Changes',
+	});
+
+	await notificationsOption.click();
+
+	await publicationActionMenu.click();
+
+	await expect(
+		page.getByRole('menuitem', {
+			name: 'Hide System Changes',
+		})
+	).toBeVisible();
+
+	await expect(page.getByText('No Results Found')).toBeVisible();
+
+	await apiHelpers.headlessChangeTracking.deleteCTCollection(
+		ctCollection2.body.id
+	);
 });
