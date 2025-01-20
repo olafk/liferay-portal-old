f1de73261b06749ea5d6f9dc8bd6585cef3eaa0d
Daniel Sanz <daniel.sanz@liferay.com>
2025-01-20 20:49:54 +0100

    LPD-44142 Create FDSSerializer interface. Any class serializing FDS data will implement it

---
+++ modules/apps/frontend-data-set/frontend-data-set-api/src/main/java/com/liferay/frontend/data/set/serializer/FDSSerializer.java
@@ -0,0 +1,23 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.frontend.data.set.serializer;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Daniel Sanz
+ */
+public interface FDSSerializer<T> {
+
+	public static final String CUSTOM = "custom";
+
+	public static final String SYSTEM = "system";
+
+	public String getKey();
+
+	public T serialize(String fdsName, HttpServletRequest httpServletRequest);
+
+}
