liferay-content-management/liferay-portal

LPD-33262 Add externalReferenceCodes to PortletInstanceSettings

modules/apps/document-library/document-library-web/src/main/java/com/liferay/document/library/web/internal/settings/DLPortletInstanceSettings.java
@@ -83,9 +83,22 @@ public class DLPortletInstanceSettings {
 		return _typedSettings.getValues("mimeTypes", _MIME_TYPES_DEFAULT);
 	}
 
+	public String getRepositoryGroupExternalReferenceCode() {
+		return _typedSettings.getValue("repositoryGroupExternalReferenceCode");
+	}
+
+	public String getRootFolderExternalReferenceCode() {
+		return _typedSettings.getValue("rootFolderExternalReferenceCode");
+	}
+
 	public long getRootFolderId() {
 		return _typedSettings.getLongValue(
 			"rootFolderId", DLFolderConstants.DEFAULT_PARENT_FOLDER_ID);
+	}
+
+	public String getSelectedRepositoryExternalReferenceCode() {
+		return _typedSettings.getValue(
+			"selectedRepositoryExternalReferenceCode");
 	}
 
 	public long getSelectedRepositoryId() {

olafk/liferay-portal-old

modules/apps/document-library/document-library-web/src/main/java/com/liferay/document/library/web/internal/settings/DLPortletInstanceSettings.java
@@ -83,9 +83,22 @@ public class DLPortletInstanceSettings {
 		return _typedSettings.getValues("mimeTypes", _MIME_TYPES_DEFAULT);
 	}
 
+	public String getRootFolderExternalReferenceCode() {
+		return _typedSettings.getValue("rootFolderExternalReferenceCode");
+	}
+
 	public long getRootFolderId() {
 		return _typedSettings.getLongValue(
 			"rootFolderId", DLFolderConstants.DEFAULT_PARENT_FOLDER_ID);
+	}
+
+	public String getSelectedGroupExternalReferenceCode() {
+		return _typedSettings.getValue("selectedGroupExternalReferenceCode");
+	}
+
+	public String getSelectedRepositoryExternalReferenceCode() {
+		return _typedSettings.getValue(
+			"selectedRepositoryExternalReferenceCode");
 	}
 
 	public long getSelectedRepositoryId() {