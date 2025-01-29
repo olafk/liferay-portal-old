da58a7d1a3c169c2e4fdaf5692af256962bf712c
barbaracabrera <barbara.cabrera@liferay.com>
2025-01-29 14:00:33 +0100

    LPD-46101 Replace openMarketplaceModal with the new openModalComponent

--- modules/apps/fragment/fragment-web/src/main/resources/META-INF/resources/js/MarketplaceButtonPropsTransformer.js
+++ modules/apps/fragment/fragment-web/src/main/resources/META-INF/resources/js/MarketplaceButtonPropsTransformer.js
@@ -3,14 +3,16 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import {openMarketplaceModal} from '@liferay/layout-js-components-web';
+import {
+	MarketplaceModal,
+	openModalComponent,
+} from '@liferay/layout-js-components-web';
 
 export default function propsTransformer({
 	additionalProps,
 	portletNamespace,
 	...props
 }) {
-
 	const marketplaceBadge = document.getElementById(
 		`${portletNamespace}marketplaceBadge`
 	);
@@ -18,12 +20,18 @@ export default function propsTransformer({
 	return {
 		...props,
 		onClick() {
-			openMarketplaceModal({
-				component: additionalProps.component,
-				location: additionalProps.location,
+			openModalComponent({
+				ModalComponent: MarketplaceModal,
+				modalComponentProps: {
+					body: additionalProps.body,
+					heading: additionalProps.heading,
+				},
 			});
 
-			Liferay.Util.Session.set(portletNamespace + 'marketplaceButton', 'visited');
+			Liferay.Util.Session.set(
+				portletNamespace + 'marketplaceButton',
+				'visited'
+			);
 
 			if (marketplaceBadge) {
 				marketplaceBadge.remove();
