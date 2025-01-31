0d3df21c4834111db7cb99748c1910aec8121866
Feliphe Marinho <feliphemarinho18@gmail.com>
2025-01-31 10:41:58 -0300

    LPD-47730 Move RelatedModel class to object-api to use it in the test

--- modules/apps/object/object-api/src/main/java/com/liferay/object/entries/frontend/data/set/data/model/RelatedModel.java
+++ modules/apps/object/object-api/src/main/java/com/liferay/object/entries/frontend/data/set/data/model/RelatedModel.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.object.web.internal.object.entries.frontend.data.set.data.model;
+package com.liferay.object.entries.frontend.data.set.data.model;
 
 /**
  * @author Marco Leo
