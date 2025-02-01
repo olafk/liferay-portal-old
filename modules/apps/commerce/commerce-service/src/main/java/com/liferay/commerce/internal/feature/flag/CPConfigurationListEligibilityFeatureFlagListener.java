57bcfeca477a9aaef45f1b98492509eef9351d4b
Brian Chan <brian.chan@liferay.com>
2025-02-01 05:42:40 +0200

    LPD-47227 Our usual pattern. git grep for TEMP_TABLE

--- modules/apps/commerce/commerce-service/src/main/java/com/liferay/commerce/internal/feature/flag/CPConfigurationListEligibilityFeatureFlagListener.java
+++ modules/apps/commerce/commerce-service/src/main/java/com/liferay/commerce/internal/feature/flag/CPConfigurationListEligibilityFeatureFlagListener.java
@@ -377,15 +377,16 @@ public class CPConfigurationListEligibilityFeatureFlagListener
 			PreparedStatement selectPreparedStatement2 =
 				connection.prepareStatement(
 					StringBundler.concat(
-						"select CPDefinition.CPDefinitionId, Rel.type_, ",
-						"Rel.classPK, Rel.resourceId from CPDefinition join ",
-						"(select 'C' as type_, classPK, commerceChannelId as ",
-						"resourceId from CommerceChannelRel where classNameId ",
-						"= ? union select 'A' as type_, classPK, ",
-						"accountGroupId as resourceId from AccountGroupRel ",
-						"where classNameId = ?) Rel on (CPDefinition.",
-						"CPDefinitionId = Rel.classPK and CPDefinition.",
-						"groupId = ?) order by Rel.classPK"));
+						"select CPDefinition.CPDefinitionId, TEMP_TABLE.type_, ",
+						"TEMP_TABLE.classPK, TEMP_TABLE.resourceId from ",
+						"CPDefinition join (select 'C' as type_, classPK, ",
+						"commerceChannelId as resourceId from ",
+						"CommerceChannelRel where classNameId = ? union ",
+						"select 'A' as type_, classPK, accountGroupId as ",
+						"resourceId from AccountGroupRel where classNameId = ",
+						"?) TEMP_TABLE on (CPDefinition.CPDefinitionId = ",
+						"TEMP_TABLE.classPK and CPDefinition.groupId = ?) ",
+						"order by TEMP_TABLE.classPK"));
 
 			PreparedStatement selectPreparedStatement3 =
 				connection.prepareStatement(
