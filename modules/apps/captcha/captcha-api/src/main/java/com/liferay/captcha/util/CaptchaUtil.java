affc733a2ef130847a6268a959cb6820e4fdc94f
Rafael Praxedes <rafael.praxedes@liferay.com>
2025-02-03 19:08:02 -0300

    LPD-9391 Not needed

--- modules/apps/captcha/captcha-api/src/main/java/com/liferay/captcha/util/CaptchaUtil.java
+++ modules/apps/captcha/captcha-api/src/main/java/com/liferay/captcha/util/CaptchaUtil.java
@@ -7,10 +7,8 @@ package com.liferay.captcha.util;
 
 import com.liferay.captcha.provider.CaptchaProvider;
 import com.liferay.portal.kernel.captcha.Captcha;
-import com.liferay.portal.kernel.captcha.CaptchaConfigurationException;
 import com.liferay.portal.kernel.captcha.CaptchaException;
 import com.liferay.portal.kernel.module.service.Snapshot;
-import com.liferay.portal.kernel.util.PortalUtil;
 
 import java.io.IOException;
 
@@ -34,18 +32,15 @@ public class CaptchaUtil {
 	public static void check(PortletRequest portletRequest)
 		throws CaptchaException {
 
-		getCaptcha().check(PortalUtil.getHttpServletRequest(portletRequest));
+		getCaptcha().check(portletRequest);
 	}
 
 	public static void enforceCaptcha(HttpServletRequest httpServletRequest) {
 		getCaptcha().enforceCaptcha(httpServletRequest);
 	}
 
-	public static void enforceCaptcha(PortletRequest portletRequest)
-		throws CaptchaConfigurationException {
-
-		getCaptcha().enforceCaptcha(
-			PortalUtil.getHttpServletRequest(portletRequest));
+	public static void enforceCaptcha(PortletRequest portletRequest) {
+		getCaptcha().enforceCaptcha(portletRequest);
 	}
 
 	public static Captcha getCaptcha() {
@@ -63,8 +58,7 @@ public class CaptchaUtil {
 	}
 
 	public static boolean isEnabled(PortletRequest portletRequest) {
-		return getCaptcha().isEnabled(
-			PortalUtil.getHttpServletRequest(portletRequest));
+		return getCaptcha().isEnabled(portletRequest);
 	}
 
 	public static void serveImage(
