f3ef6c511c47ce40e3741cc83071b3d827e5eed1
Caleb Hall <caleb.hall@liferay.com>
2025-02-10 13:42:41 -0300

    LPD-48298 Fixed Format

--- workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/pages/CustomerDashboard/pages/Connections/index.tsx
+++ workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/pages/CustomerDashboard/pages/Connections/index.tsx
@@ -41,8 +41,11 @@ const ConnectionsTable = () => {
 				<div className="dxp-connections-table-no-connection">
 					<p>
 						No connection was found, to learn how to create the
-						connection please check this{' '}
-						<a href="https://learn.liferay.com/w/dxp/liferay-development/marketplace/connecting-liferay-dxp-to-marketplace">
+						connection please check this
+						<a
+							className="ml-1"
+							href="https://learn.liferay.com/w/dxp/liferay-development/marketplace/connecting-liferay-dxp-to-marketplace"
+						>
 							help page
 						</a>
 					</p>
