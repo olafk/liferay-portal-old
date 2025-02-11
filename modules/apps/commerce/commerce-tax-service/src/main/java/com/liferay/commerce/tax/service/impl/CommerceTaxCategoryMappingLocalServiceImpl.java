7dcec5bf88de3d66cbef615a5f576eef2b0a96b9
Ivica Cardic <ivica.cardic@liferay.com>
2025-02-11 14:54:31 +0100

    LPD-48166 - Add logic

--- modules/apps/commerce/commerce-tax-service/src/main/java/com/liferay/commerce/tax/service/impl/CommerceTaxCategoryMappingLocalServiceImpl.java
+++ modules/apps/commerce/commerce-tax-service/src/main/java/com/liferay/commerce/tax/service/impl/CommerceTaxCategoryMappingLocalServiceImpl.java
@@ -5,10 +5,19 @@
 
 package com.liferay.commerce.tax.service.impl;
 
+import com.liferay.commerce.tax.exception.DuplicateCommerceTaxCategoryMappingExternalReferenceCodeException;
+import com.liferay.commerce.tax.model.CommerceTaxCategoryMapping;
 import com.liferay.commerce.tax.service.base.CommerceTaxCategoryMappingLocalServiceBaseImpl;
 import com.liferay.portal.aop.AopService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.util.OrderByComparator;
+
+import java.util.List;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Marco Leo
@@ -20,4 +29,89 @@ import org.osgi.service.component.annotations.Component;
 )
 public class CommerceTaxCategoryMappingLocalServiceImpl
 	extends CommerceTaxCategoryMappingLocalServiceBaseImpl {
+
+	@Override
+	public CommerceTaxCategoryMapping addCommerceTaxCategoryMapping(
+			long userId, long groupId, long commerceTaxMethodId,
+			long cpTaxCategoryId, String externalReferenceCode)
+		throws PortalException {
+
+		User user = _userLocalService.getUser(userId);
+
+		_validate(cpTaxCategoryId, commerceTaxMethodId);
+
+		long commerceTaxFixedRateId = counterLocalService.increment();
+
+		CommerceTaxCategoryMapping commerceTaxCategoryMapping =
+			commerceTaxCategoryMappingPersistence.create(
+				commerceTaxFixedRateId);
+
+		commerceTaxCategoryMapping.setExternalReferenceCode(
+			externalReferenceCode);
+		commerceTaxCategoryMapping.setGroupId(groupId);
+		commerceTaxCategoryMapping.setCompanyId(user.getCompanyId());
+		commerceTaxCategoryMapping.setUserId(user.getUserId());
+		commerceTaxCategoryMapping.setUserName(user.getFullName());
+		commerceTaxCategoryMapping.setCommerceTaxMethodId(commerceTaxMethodId);
+		commerceTaxCategoryMapping.setCPTaxCategoryId(cpTaxCategoryId);
+
+		return commerceTaxCategoryMappingPersistence.update(
+			commerceTaxCategoryMapping);
+	}
+
+	@Override
+	public CommerceTaxCategoryMapping fetchCommerceTaxCategoryMapping(
+		long commerceTaxMethodId, long cpTaxCategoryId) {
+
+		return commerceTaxCategoryMappingPersistence.fetchByC_C(
+			commerceTaxMethodId, cpTaxCategoryId);
+	}
+
+	@Override
+	public int getCommerceTaxCategoryMappingCount(long commerceTaxMethodId)
+		throws PortalException {
+
+		return commerceTaxCategoryMappingPersistence.countByCommerceTaxMethodId(
+			commerceTaxMethodId);
+	}
+
+	@Override
+	public List<CommerceTaxCategoryMapping> getCommerceTaxCategoryMappings(
+		long commerceTaxMethodId, int start, int end,
+		OrderByComparator<CommerceTaxCategoryMapping> orderByComparator) {
+
+		return commerceTaxCategoryMappingPersistence.findByCommerceTaxMethodId(
+			commerceTaxMethodId, start, end, orderByComparator);
+	}
+
+	@Override
+	public CommerceTaxCategoryMapping updateCommerceTaxCategoryMapping(
+			long commerceTaxCategoryMappingId, String externalReferenceCode)
+		throws PortalException {
+
+		CommerceTaxCategoryMapping commerceTaxCategoryMapping =
+			commerceTaxCategoryMappingPersistence.findByPrimaryKey(
+				commerceTaxCategoryMappingId);
+
+		commerceTaxCategoryMapping.setExternalReferenceCode(
+			externalReferenceCode);
+
+		return commerceTaxCategoryMappingPersistence.update(
+			commerceTaxCategoryMapping);
+	}
+
+	private void _validate(long cpTaxCategoryId, long commerceTaxMethodId)
+		throws PortalException {
+
+		int count = commerceTaxCategoryMappingPersistence.countByC_C(
+			commerceTaxMethodId, cpTaxCategoryId);
+
+		if (count > 0) {
+			throw new DuplicateCommerceTaxCategoryMappingExternalReferenceCodeException();
+		}
+	}
+
+	@Reference
+	private UserLocalService _userLocalService;
+
 }
