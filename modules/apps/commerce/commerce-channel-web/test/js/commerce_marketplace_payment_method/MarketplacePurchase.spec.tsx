3dc50aefbff2262c29a0347ebf74c710ccf3acde
barbaracabrera <barbara.cabrera@liferay.com>
2025-02-18 15:26:27 +0100

    LPD-48223 Adapt import in jest tests

--- modules/apps/commerce/commerce-channel-web/test/js/commerce_marketplace_payment_method/MarketplacePurchase.spec.tsx
+++ modules/apps/commerce/commerce-channel-web/test/js/commerce_marketplace_payment_method/MarketplacePurchase.spec.tsx
@@ -3,14 +3,13 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import {MarketplaceContext} from '@liferay/marketplace-js-components-web';
-import {cleanup, fireEvent, render} from '@testing-library/react';
-import React from 'react';
-
 import {
+	MarketplaceContext,
 	MarketplacePurchase,
 	States,
-} from '../../../src/main/resources/META-INF/resources/js/commerce_marketplace_payment_method/MarketplacePurchase';
+} from '@liferay/marketplace-js-components-web';
+import {cleanup, fireEvent, render} from '@testing-library/react';
+import React from 'react';
 
 describe('CommerceMarketplacePaymentMethod', () => {
 	afterAll(() => {
