d3fd6f2a8316d8a66cbf558a83be5b597e0dd908
Thiago Buarque <thiago.buarque@liferay.com>
2025-02-18 15:53:37 -0300

    LPD-41336 SF

--- modules/apps/feature-flag/feature-flag-web/src/main/java/com/liferay/feature/flag/web/internal/upgrade/v1_0_1/FeatureFlagUpgradeProcess.java
+++ modules/apps/feature-flag/feature-flag-web/src/main/java/com/liferay/feature/flag/web/internal/upgrade/v1_0_1/FeatureFlagUpgradeProcess.java
@@ -41,15 +41,16 @@ public class FeatureFlagUpgradeProcess extends UpgradeProcess {
 			_OLD_NAMESPACE);
 
 		while (enumeration.hasMoreElements()) {
-			String featureFlag = enumeration.nextElement();
+			String portalPreferenceKey = enumeration.nextElement();
 
 			String value = portalPreferencesImpl.getValue(
-				_OLD_NAMESPACE, featureFlag);
+				_OLD_NAMESPACE, portalPreferenceKey);
 
-			portalPreferencesImpl.reset(_OLD_NAMESPACE, featureFlag);
+			portalPreferencesImpl.reset(_OLD_NAMESPACE, portalPreferenceKey);
 
 			portalPreferencesImpl.setValue(
-				FeatureFlagConstants.PREFERENCE_NAMESPACE, featureFlag, value);
+				FeatureFlagConstants.PREFERENCE_NAMESPACE, portalPreferenceKey,
+				value);
 		}
 
 		portalPreferencesImpl.setValue(
