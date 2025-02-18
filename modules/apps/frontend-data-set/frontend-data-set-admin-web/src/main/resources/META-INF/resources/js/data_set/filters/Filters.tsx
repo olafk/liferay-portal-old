liferay-frontend/liferay-portal

LPD-41485 - Feat. Add status column and the ToggleStatus component to filters page

modules/apps/frontend-data-set/frontend-data-set-admin-web/src/main/resources/META-INF/resources/js/data_set/filters/Filters.tsx
@@ -362,19 +362,6 @@ function Filters({
 		}
 	};
 
-	const onEdit = ({item}: {item: IFilter}) => {
-		if (
-			item.filterType === EFilterType.CLIENT_EXTENSION &&
-			!filterClientExtensionRenderers.length
-		) {
-			noFilterClientExtensionsAvailableModal();
-		}
-		else {
-			setActiveMode(FILTER_MODE.EDITION);
-			setActiveFilter(item);
-		}
-	};
-
 	const onDelete = async ({item}: {item: IFilter}) => {
 		openModal({
 			bodyHTML: Liferay.Language.get(
@@ -419,6 +406,60 @@ function Filters({
 			status: 'danger',
 			title: Liferay.Language.get('delete-filter'),
 		});
+	};
+
+	const onEdit = ({item}: {item: IFilter}) => {
+		if (
+			item.filterType === EFilterType.CLIENT_EXTENSION &&
+			!filterClientExtensionRenderers.length
+		) {
+			noFilterClientExtensionsAvailableModal();
+		}
+		else {
+			setActiveMode(FILTER_MODE.EDITION);
+			setActiveFilter(item);
+		}
+	};
+
+	const onUpdateStatus = async (item: IFilter) => {
+		const type: any =
+			item.filterType === 'DATE_RANGE'
+				? 'DATE_FILTERS'
+				: `${item.filterType}_FILTERS`;
+
+		const response = await fetch(
+			`${API_URL[type]}/by-external-reference-code/${item.externalReferenceCode}`,
+			{
+				body: JSON.stringify({inactive: !item.inactive}),
+				headers: DEFAULT_FETCH_HEADERS,
+				method: 'PATCH',
+			}
+		);
+
+		if (!response.ok) {
+			openDefaultFailureToast();
+
+			return;
+		}
+
+		const dataSetFilter: IFilter = await response.json();
+
+		if (dataSetFilter?.id) {
+			const updatedFilters = filters.map((filter) => {
+				if (filter.id === dataSetFilter.id) {
+					filter = {...filter, ...dataSetFilter};
+				}
+
+				return filter;
+			});
+
+			setFilters(updatedFilters);
+
+			openDefaultSuccessToast();
+		}
+		else {
+			openDefaultFailureToast();
+		}
 	};
 
 	const getBreadcrumbItems = () => {
@@ -550,6 +591,7 @@ function Filters({
 						editFilter={onEdit}
 						filterTypes={FILTER_TYPES}
 						filters={filters}
+						toggleChange={onUpdateStatus}
 						updateFiltersOrder={updateFiltersOrder}
 					/>
 				</ClayLayout.ContainerFluid>

olafk/liferay-portal-old

modules/apps/frontend-data-set/frontend-data-set-admin-web/src/main/resources/META-INF/resources/js/data_set/filters/Filters.tsx
@@ -362,19 +362,6 @@ function Filters({
 		}
 	};
 
-	const onEdit = ({item}: {item: IFilter}) => {
-		if (
-			item.filterType === EFilterType.CLIENT_EXTENSION &&
-			!filterClientExtensionRenderers.length
-		) {
-			noFilterClientExtensionsAvailableModal();
-		}
-		else {
-			setActiveMode(FILTER_MODE.EDITION);
-			setActiveFilter(item);
-		}
-	};
-
 	const onDelete = async ({item}: {item: IFilter}) => {
 		openModal({
 			bodyHTML: Liferay.Language.get(
@@ -419,6 +406,60 @@ function Filters({
 			status: 'danger',
 			title: Liferay.Language.get('delete-filter'),
 		});
+	};
+
+	const onEdit = ({item}: {item: IFilter}) => {
+		if (
+			item.filterType === EFilterType.CLIENT_EXTENSION &&
+			!filterClientExtensionRenderers.length
+		) {
+			noFilterClientExtensionsAvailableModal();
+		}
+		else {
+			setActiveMode(FILTER_MODE.EDITION);
+			setActiveFilter(item);
+		}
+	};
+
+	const onUpdateStatus = async (item: IFilter) => {
+		const type: any =
+			item.filterType === 'DATE_RANGE'
+				? 'DATE_FILTERS'
+				: `${item.filterType}_FILTERS`;
+
+		const response = await fetch(
+			`${API_URL[type]}/by-external-reference-code/${item.externalReferenceCode}`,
+			{
+				body: JSON.stringify({active: !item.active}),
+				headers: DEFAULT_FETCH_HEADERS,
+				method: 'PATCH',
+			}
+		);
+
+		if (!response.ok) {
+			openDefaultFailureToast();
+
+			return;
+		}
+
+		const dataSetFilter: IFilter = await response.json();
+
+		if (dataSetFilter?.id) {
+			const updatedFilters = filters.map((filter) => {
+				if (filter.id === dataSetFilter.id) {
+					filter = {...filter, ...dataSetFilter};
+				}
+
+				return filter;
+			});
+
+			setFilters(updatedFilters);
+
+			openDefaultSuccessToast();
+		}
+		else {
+			openDefaultFailureToast();
+		}
 	};
 
 	const getBreadcrumbItems = () => {
@@ -550,6 +591,7 @@ function Filters({
 						editFilter={onEdit}
 						filterTypes={FILTER_TYPES}
 						filters={filters}
+						toggleChange={onUpdateStatus}
 						updateFiltersOrder={updateFiltersOrder}
 					/>
 				</ClayLayout.ContainerFluid>