LuisOrtiz89/liferay-portal

LPD-46406 Missing updates in Migration Validator Tool

modules/util/portal-tools-db-partition-migration-validator-test/src/testIntegration/java/com/liferay/portal/tools/db/partition/migration/validator/test/DBPartitionMigrationValidatorTest.java
@@ -95,16 +95,16 @@ public class DBPartitionMigrationValidatorTest extends BaseDBPartitionTestCase {
 			_company.getName() +
 				" already exists in the target database. You must set a " +
 					"different value in " +
-						"DBPartitionInsertVirtualInstanceConfiguration.config.",
+						"InsertVirtualInstanceConfiguration.config.",
 			"[WARN] Virtual host " + _company.getVirtualHostname() +
 				" already exists in the target database. You must set a " +
 					"different value in " +
-						"DBPartitionInsertVirtualInstanceConfiguration.config.",
+						"InsertVirtualInstanceConfiguration.config.",
 			"[WARN] Web ID ",
 			_company.getWebId() +
 				" already exists in the target database. You must set a " +
 					"different value in " +
-						"DBPartitionInsertVirtualInstanceConfiguration.config."
+						"InsertVirtualInstanceConfiguration.config."
 		};
 
 		_testValidate(

modules/util/portal-tools-db-partition-migration-validator/src/main/java/com/liferay/portal/tools/db/partition/migration/validator/util/ValidatorUtil.java
@@ -85,7 +85,7 @@ public class ValidatorUtil {
 					"Company name ", sourceCompany.getCompanyName(),
 					" already exists in the target database. You must set a ",
 					"different value in ",
-					"DBPartitionInsertVirtualInstanceConfiguration.config."));
+					"InsertVirtualInstanceConfiguration.config."));
 		}
 
 		if (Objects.equals(
@@ -97,7 +97,7 @@ public class ValidatorUtil {
 					"Virtual host ", sourceCompany.getVirtualHostname(),
 					" already exists in the target database. You must set a ",
 					"different value in ",
-					"DBPartitionInsertVirtualInstanceConfiguration.config."));
+					"InsertVirtualInstanceConfiguration.config."));
 		}
 
 		if (Objects.equals(company.getWebId(), sourceCompany.getWebId())) {
@@ -106,7 +106,7 @@ public class ValidatorUtil {
 					"Web ID ", sourceCompany.getWebId(),
 					" already exists in the target database. You must set a ",
 					"different value in ",
-					"DBPartitionInsertVirtualInstanceConfiguration.config."));
+					"InsertVirtualInstanceConfiguration.config."));
 		}
 	}
 

modules/util/portal-tools-db-partition-migration-validator/src/test/java/com/liferay/portal/tools/db/partition/migration/validator/DBPartitionMigrationValidatorTest.java
@@ -117,7 +117,7 @@ public class DBPartitionMigrationValidatorTest extends BaseTestCase {
 				"source database before the migration",
 			"[WARN] Company name Liferay DXP already exists in the target " +
 				"database. You must set a different value in " +
-					"DBPartitionInsertVirtualInstanceConfiguration.config.",
+					"InsertVirtualInstanceConfiguration.config.",
 			"[WARN] Module com.liferay.asset.publisher.web is not present in " +
 				"the source database",
 			"[WARN] Module com.liferay.license.manager.web is not present in " +
@@ -127,10 +127,10 @@ public class DBPartitionMigrationValidatorTest extends BaseTestCase {
 			"[WARN] Table DDMTemplate is not present in the target database",
 			"[WARN] Virtual host localhost already exists in the target " +
 				"database. You must set a different value in " +
-					"DBPartitionInsertVirtualInstanceConfiguration.config.",
+					"InsertVirtualInstanceConfiguration.config.",
 			"[WARN] Web ID liferay.com already exists in the target " +
 				"database. You must set a different value in " +
-					"DBPartitionInsertVirtualInstanceConfiguration.config."
+					"InsertVirtualInstanceConfiguration.config."
 		};
 
 		_testValidate(

modules/util/portal-tools-db-partition-migration-validator/src/test/java/com/liferay/portal/tools/db/partition/migration/validator/util/ValidatorUtilTest.java
@@ -66,7 +66,7 @@ public class ValidatorUtilTest {
 					"[WARN] Company name ", company.getCompanyName(),
 					" already exists in the target database. You must set a ",
 					"different value in ",
-					"DBPartitionInsertVirtualInstanceConfiguration.config.")));
+					"InsertVirtualInstanceConfiguration.config.")));
 		_testValidateCompany(
 			true, true, false, true, company,
 			() -> _assertValidateDatabases(
@@ -75,7 +75,7 @@ public class ValidatorUtilTest {
 					"[WARN] Virtual host ", company.getVirtualHostname(),
 					" already exists in the target database. You must set a ",
 					"different value in ",
-					"DBPartitionInsertVirtualInstanceConfiguration.config.")));
+					"InsertVirtualInstanceConfiguration.config.")));
 		_testValidateCompany(
 			true, true, true, false, company,
 			() -> _assertValidateDatabases(
@@ -84,7 +84,7 @@ public class ValidatorUtilTest {
 					"[WARN] Web ID ", company.getWebId(),
 					" already exists in the target database. You must set a ",
 					"different value in ",
-					"DBPartitionInsertVirtualInstanceConfiguration.config.")));
+					"InsertVirtualInstanceConfiguration.config.")));
 	}
 
 	@Test

olafk/liferay-portal-old

modules/util/portal-tools-db-partition-migration-validator-test/src/testIntegration/java/com/liferay/portal/tools/db/partition/migration/validator/test/DBPartitionMigrationValidatorTest.java
@@ -95,16 +95,16 @@ public class DBPartitionMigrationValidatorTest extends BaseDBPartitionTestCase {
 			_company.getName() +
 				" already exists in the target database. You must set a " +
 					"different value in " +
-						"DBPartitionInsertVirtualInstanceConfiguration.config.",
+						"InsertPortalInstanceConfiguration.config.",
 			"[WARN] Virtual host " + _company.getVirtualHostname() +
 				" already exists in the target database. You must set a " +
 					"different value in " +
-						"DBPartitionInsertVirtualInstanceConfiguration.config.",
+						"InsertPortalInstanceConfiguration.config.",
 			"[WARN] Web ID ",
 			_company.getWebId() +
 				" already exists in the target database. You must set a " +
 					"different value in " +
-						"DBPartitionInsertVirtualInstanceConfiguration.config."
+						"InsertPortalInstanceConfiguration.config."
 		};
 
 		_testValidate(

modules/util/portal-tools-db-partition-migration-validator/src/main/java/com/liferay/portal/tools/db/partition/migration/validator/util/ValidatorUtil.java
@@ -85,7 +85,7 @@ public class ValidatorUtil {
 					"Company name ", sourceCompany.getCompanyName(),
 					" already exists in the target database. You must set a ",
 					"different value in ",
-					"DBPartitionInsertVirtualInstanceConfiguration.config."));
+					"InsertPortalInstanceConfiguration.config."));
 		}
 
 		if (Objects.equals(
@@ -97,7 +97,7 @@ public class ValidatorUtil {
 					"Virtual host ", sourceCompany.getVirtualHostname(),
 					" already exists in the target database. You must set a ",
 					"different value in ",
-					"DBPartitionInsertVirtualInstanceConfiguration.config."));
+					"InsertPortalInstanceConfiguration.config."));
 		}
 
 		if (Objects.equals(company.getWebId(), sourceCompany.getWebId())) {
@@ -106,7 +106,7 @@ public class ValidatorUtil {
 					"Web ID ", sourceCompany.getWebId(),
 					" already exists in the target database. You must set a ",
 					"different value in ",
-					"DBPartitionInsertVirtualInstanceConfiguration.config."));
+					"InsertPortalInstanceConfiguration.config."));
 		}
 	}
 

modules/util/portal-tools-db-partition-migration-validator/src/test/java/com/liferay/portal/tools/db/partition/migration/validator/DBPartitionMigrationValidatorTest.java
@@ -117,7 +117,7 @@ public class DBPartitionMigrationValidatorTest extends BaseTestCase {
 				"source database before the migration",
 			"[WARN] Company name Liferay DXP already exists in the target " +
 				"database. You must set a different value in " +
-					"DBPartitionInsertVirtualInstanceConfiguration.config.",
+					"InsertPortalInstanceConfiguration.config.",
 			"[WARN] Module com.liferay.asset.publisher.web is not present in " +
 				"the source database",
 			"[WARN] Module com.liferay.license.manager.web is not present in " +
@@ -127,10 +127,10 @@ public class DBPartitionMigrationValidatorTest extends BaseTestCase {
 			"[WARN] Table DDMTemplate is not present in the target database",
 			"[WARN] Virtual host localhost already exists in the target " +
 				"database. You must set a different value in " +
-					"DBPartitionInsertVirtualInstanceConfiguration.config.",
+					"InsertPortalInstanceConfiguration.config.",
 			"[WARN] Web ID liferay.com already exists in the target " +
 				"database. You must set a different value in " +
-					"DBPartitionInsertVirtualInstanceConfiguration.config."
+					"InsertPortalInstanceConfiguration.config."
 		};
 
 		_testValidate(

modules/util/portal-tools-db-partition-migration-validator/src/test/java/com/liferay/portal/tools/db/partition/migration/validator/util/ValidatorUtilTest.java
@@ -66,7 +66,7 @@ public class ValidatorUtilTest {
 					"[WARN] Company name ", company.getCompanyName(),
 					" already exists in the target database. You must set a ",
 					"different value in ",
-					"DBPartitionInsertVirtualInstanceConfiguration.config.")));
+					"InsertPortalInstanceConfiguration.config.")));
 		_testValidateCompany(
 			true, true, false, true, company,
 			() -> _assertValidateDatabases(
@@ -75,7 +75,7 @@ public class ValidatorUtilTest {
 					"[WARN] Virtual host ", company.getVirtualHostname(),
 					" already exists in the target database. You must set a ",
 					"different value in ",
-					"DBPartitionInsertVirtualInstanceConfiguration.config.")));
+					"InsertPortalInstanceConfiguration.config.")));
 		_testValidateCompany(
 			true, true, true, false, company,
 			() -> _assertValidateDatabases(
@@ -84,7 +84,7 @@ public class ValidatorUtilTest {
 					"[WARN] Web ID ", company.getWebId(),
 					" already exists in the target database. You must set a ",
 					"different value in ",
-					"DBPartitionInsertVirtualInstanceConfiguration.config.")));
+					"InsertPortalInstanceConfiguration.config.")));
 	}
 
 	@Test