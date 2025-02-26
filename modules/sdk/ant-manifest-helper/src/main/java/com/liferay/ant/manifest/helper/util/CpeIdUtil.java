d338a3548944ffede20e9385e85b2a5c179f8836
Drew Brokke <drew.brokke@liferay.com>
2025-02-26 17:19:42 -0600

    LPD-36587 ant-manifest-helper: copies method to dedicated class CpeIdUtil.java

---
+++ modules/sdk/ant-manifest-helper/src/main/java/com/liferay/ant/manifest/helper/util/CpeIdUtil.java
@@ -0,0 +1,58 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.ant.manifest.helper.util;
+
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.util.ReleaseInfo;
+
+import org.apache.tools.ant.Project;
+
+/**
+ * @author Istvan Sajtos
+ * @author Drew Brokke
+ */
+public class CpeIdUtil {
+
+	public static String getCpeId(Project project) {
+		try {
+			String patchVersion = "*";
+			String product = "portal";
+			String version =
+				ReleaseInfo.getVersion() +
+					project.getProperty("release.info.version.file.suffix");
+
+			if (ReleaseInfo.isDXP()) {
+				String versionDisplayName = ReleaseInfo.getVersionDisplayName();
+
+				int periodLastIndex = versionDisplayName.lastIndexOf(
+					StringPool.PERIOD);
+
+				patchVersion = versionDisplayName.substring(
+					periodLastIndex + 1);
+
+				if (patchVersion.endsWith(" LTS")) {
+					patchVersion = patchVersion.substring(
+						0, patchVersion.indexOf(" LTS"));
+				}
+
+				product = "dxp";
+
+				String qVersion = versionDisplayName.substring(
+					0, periodLastIndex);
+
+				version = qVersion.toLowerCase();
+			}
+
+			return String.format(
+				"cpe:2.3:a:liferay:%s:%s:%s:*:*:*:*:*:*", product, version,
+				patchVersion);
+		}
+		catch (Exception exception) {
+			throw new RuntimeException(exception);
+		}
+	}
+
+}
