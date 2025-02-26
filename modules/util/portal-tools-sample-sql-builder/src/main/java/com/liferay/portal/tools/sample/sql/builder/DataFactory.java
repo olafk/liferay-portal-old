liferay-core-infra/liferay-portal

LPD-44229 | LPD-48289 create layouts with many heading fragments

modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
@@ -249,6 +249,7 @@ import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.json.JSONArray;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.json.JSONUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.metadata.RawMetadataProcessor;
@@ -802,6 +803,10 @@ public class DataFactory {
 
 	public int getMaxDLFolderDepth() {
 		return BenchmarksPropsValues.MAX_DL_FOLDER_DEPTH;
+	}
+
+	public int getMaxFragmentsPerLayoutCount() {
+		return BenchmarksPropsValues.MAX_FRAGMENTS_PER_LAYOUT_COUNT;
 	}
 
 	public int getMaxGroupCount() {
@@ -4266,6 +4271,74 @@ public class DataFactory {
 	}
 
 	public FragmentEntryLinkModel newFragmentEntryLinkModel(
+			JournalArticleModel journalArticleModel, LayoutModel layoutModel)
+		throws Exception {
+
+		FragmentEntryLinkModel fragmentEntryLinkModel =
+			new FragmentEntryLinkModelImpl();
+
+		// PK fields
+
+		fragmentEntryLinkModel.setFragmentEntryLinkId(_counter.get());
+
+		// Group instance
+
+		fragmentEntryLinkModel.setGroupId(layoutModel.getGroupId());
+
+		// Audit fields
+
+		fragmentEntryLinkModel.setCompanyId(_companyId);
+		fragmentEntryLinkModel.setUserId(_sampleUserId);
+		fragmentEntryLinkModel.setUserName(_SAMPLE_USER_NAME);
+		fragmentEntryLinkModel.setCreateDate(new Date());
+		fragmentEntryLinkModel.setModifiedDate(new Date());
+
+		// Other fields
+
+		fragmentEntryLinkModel.setFragmentEntryId(0);
+		fragmentEntryLinkModel.setClassNameId(getClassNameId(Layout.class));
+		fragmentEntryLinkModel.setClassPK(layoutModel.getPlid());
+		fragmentEntryLinkModel.setCss(
+			_readFile(_getFragmentComponentInputStream("heading", "css")));
+		fragmentEntryLinkModel.setHtml(
+			_readFile(_getFragmentComponentInputStream("heading", "html")));
+		fragmentEntryLinkModel.setConfiguration(
+			_readFile(
+				"fragment_component/fragment_component_heading_configuration." +
+					"json"));
+		fragmentEntryLinkModel.setJs(StringPool.BLANK);
+
+		String editValue = _readFile(
+			"fragment_component/fragment_component_heading_editValue_" +
+				"attachment_journal_article_title.json");
+
+		fragmentEntryLinkModel.setEditableValues(
+			StringUtil.replace(
+				StringUtil.replace(
+					StringUtil.replace(
+						editValue, "${journalArticleClassNameId}",
+						String.valueOf(getClassNameId(JournalArticle.class))),
+					"${journalArticleClassPK}",
+					String.valueOf(journalArticleModel.getId())),
+				"${journalArticleTitle}", journalArticleModel.getUrlTitle()));
+
+		fragmentEntryLinkModel.setNamespace(StringUtil.randomId());
+		fragmentEntryLinkModel.setPlid(layoutModel.getPlid());
+		fragmentEntryLinkModel.setPosition(0);
+		fragmentEntryLinkModel.setRendererKey(
+			_FRAGMENT_COMPONENT_RENDER_KEY_HEADING);
+
+		// Autogenerated fields
+
+		String uuid = SequentialUUID.generate();
+
+		fragmentEntryLinkModel.setUuid(uuid);
+		fragmentEntryLinkModel.setExternalReferenceCode(uuid);
+
+		return fragmentEntryLinkModel;
+	}
+
+	public FragmentEntryLinkModel newFragmentEntryLinkModel(
 		LayoutModel layoutModel, FragmentEntryModel fragmentEntryModel) {
 
 		FragmentEntryLinkModel fragmentEntryLinkModel =
@@ -4308,6 +4381,23 @@ public class DataFactory {
 		fragmentEntryLinkModel.setExternalReferenceCode(uuid);
 
 		return fragmentEntryLinkModel;
+	}
+
+	public List<FragmentEntryLinkModel> newFragmentEntryLinkModels(
+			JournalArticleModel journalArticleModel, LayoutModel layoutModel)
+		throws Exception {
+
+		List<FragmentEntryLinkModel> fragmentEntryLinkModels =
+			new ArrayList<>();
+
+		for (int i = 0;
+			 i < BenchmarksPropsValues.MAX_FRAGMENTS_PER_LAYOUT_COUNT; i++) {
+
+			fragmentEntryLinkModels.add(
+				newFragmentEntryLinkModel(journalArticleModel, layoutModel));
+		}
+
+		return fragmentEntryLinkModels;
 	}
 
 	public List<FragmentEntryLinkModel> newFragmentEntryLinkModels(
@@ -4970,6 +5060,71 @@ public class DataFactory {
 				_layoutPageTemplateStructureRelData, "${fragmentEntryLinkId}",
 				String.valueOf(
 					fragmentEntryLinkModel.getFragmentEntryLinkId())));
+
+		// Autogenerated fields
+
+		layoutPageTemplateStructureRelModel.setUuid(SequentialUUID.generate());
+
+		return layoutPageTemplateStructureRelModel;
+	}
+
+	public LayoutPageTemplateStructureRelModel
+			newLayoutPageTemplateStructureRelModel(
+				LayoutModel layoutModel,
+				LayoutPageTemplateStructureModel
+					layoutPageTemplateStructureModel,
+				List<FragmentEntryLinkModel> fragmentEntryLinkModels)
+		throws Exception {
+
+		List<FragmentEntryLinkModel> targetFragmentEntryLinkModels =
+			new ArrayList<>();
+
+		for (FragmentEntryLinkModel model : fragmentEntryLinkModels) {
+			if (model.getPlid() == layoutModel.getPlid()) {
+				targetFragmentEntryLinkModels.add(model);
+			}
+		}
+
+		LayoutPageTemplateStructureRelModel
+			layoutPageTemplateStructureRelModel =
+				new LayoutPageTemplateStructureRelModelImpl();
+
+		// PK fields
+
+		layoutPageTemplateStructureRelModel.setLayoutPageTemplateStructureRelId(
+			_counter.get());
+
+		// Group instance
+
+		layoutPageTemplateStructureRelModel.setGroupId(
+			layoutPageTemplateStructureModel.getGroupId());
+
+		// Audit fields
+
+		layoutPageTemplateStructureRelModel.setCompanyId(_companyId);
+		layoutPageTemplateStructureRelModel.setUserId(_sampleUserId);
+		layoutPageTemplateStructureRelModel.setUserName(_SAMPLE_USER_NAME);
+		layoutPageTemplateStructureRelModel.setCreateDate(new Date());
+		layoutPageTemplateStructureRelModel.setModifiedDate(new Date());
+
+		// Other fields
+
+		layoutPageTemplateStructureRelModel.setLayoutPageTemplateStructureId(
+			layoutPageTemplateStructureModel.
+				getLayoutPageTemplateStructureId());
+
+		FragmentEntryLinkModel fragmentEntryLinkModel =
+			targetFragmentEntryLinkModels.get(0);
+
+		layoutPageTemplateStructureRelModel.setSegmentsExperienceId(
+			fragmentEntryLinkModel.getSegmentsExperienceId());
+
+		layoutPageTemplateStructureRelModel.setData(
+			_generateJsonData(targetFragmentEntryLinkModels));
+		layoutPageTemplateStructureRelModel.setStatusByUserId(_sampleUserId);
+		layoutPageTemplateStructureRelModel.setStatusByUserName(
+			_SAMPLE_USER_NAME);
+		layoutPageTemplateStructureRelModel.setStatusDate(new Date());
 
 		// Autogenerated fields
 
@@ -8637,6 +8792,77 @@ public class DataFactory {
 		catch (ReflectiveOperationException reflectiveOperationException) {
 			ReflectionUtil.throwException(reflectiveOperationException);
 		}
+	}
+
+	private String _generateJsonData(
+			List<FragmentEntryLinkModel> fragmentEntryLinkModels)
+		throws Exception {
+
+		Map<String, String> itemPageLayoutDefinitions = new LinkedHashMap<>();
+
+		String itemPageLayoutDefinitionTemplate = _readFile(
+			"content_page_template/item-page-layout-definition-template.json");
+
+		for (FragmentEntryLinkModel fragmentEntryLinkModel :
+				fragmentEntryLinkModels) {
+
+			String itemId = SequentialUUID.generate();
+
+			itemPageLayoutDefinitions.put(
+				itemId,
+				JSONFactoryUtil.createJSONObject(
+					StringUtil.replace(
+						StringUtil.replace(
+							itemPageLayoutDefinitionTemplate, "${itemId}",
+							itemId),
+						"${fragmentEntryLinkId}",
+						String.valueOf(
+							fragmentEntryLinkModel.getFragmentEntryLinkId()))
+				).toString());
+		}
+
+		String rootItemId = SequentialUUID.generate();
+
+		JSONObject itemsJSONObject = JSONUtil.put(
+			rootItemId,
+			JSONUtil.put(
+				"children",
+				JSONFactoryUtil.createJSONArray(
+					itemPageLayoutDefinitions.keySet())
+			).put(
+				"config", JSONFactoryUtil.createJSONObject()
+			).put(
+				"itemId", rootItemId
+			).put(
+				"parentId", StringPool.BLANK
+			).put(
+				"type", "root"
+			));
+
+		for (Map.Entry<String, String> entry :
+				itemPageLayoutDefinitions.entrySet()) {
+
+			itemsJSONObject.put(
+				entry.getKey(),
+				JSONFactoryUtil.createJSONObject(entry.getValue()));
+		}
+
+		return JSONUtil.put(
+			"deletedItems", JSONFactoryUtil.createJSONArray()
+		).put(
+			"items", itemsJSONObject
+		).put(
+			"pageRules", JSONFactoryUtil.createJSONArray()
+		).put(
+			"rootItems",
+			JSONUtil.put(
+				"dropZone", ""
+			).put(
+				"main", rootItemId
+			)
+		).put(
+			"version", 1.0
+		).toString();
 	}
 
 	private String _generateJsonData(

olafk/liferay-portal-old

modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
@@ -249,6 +249,7 @@ import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.json.JSONArray;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.json.JSONUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.metadata.RawMetadataProcessor;
@@ -802,6 +803,10 @@ public class DataFactory {
 
 	public int getMaxDLFolderDepth() {
 		return BenchmarksPropsValues.MAX_DL_FOLDER_DEPTH;
+	}
+
+	public int getMaxFragmentsPerLayoutCount() {
+		return BenchmarksPropsValues.MAX_FRAGMENTS_PER_LAYOUT_COUNT;
 	}
 
 	public int getMaxGroupCount() {
@@ -4266,6 +4271,76 @@ public class DataFactory {
 	}
 
 	public FragmentEntryLinkModel newFragmentEntryLinkModel(
+			JournalArticleModel journalArticleModel, LayoutModel layoutModel,
+			long segmentsExperienceId)
+		throws Exception {
+
+		FragmentEntryLinkModel fragmentEntryLinkModel =
+			new FragmentEntryLinkModelImpl();
+
+		// PK fields
+
+		fragmentEntryLinkModel.setFragmentEntryLinkId(_counter.get());
+		fragmentEntryLinkModel.setSegmentsExperienceId(segmentsExperienceId);
+
+		// Group instance
+
+		fragmentEntryLinkModel.setGroupId(layoutModel.getGroupId());
+
+		// Audit fields
+
+		fragmentEntryLinkModel.setCompanyId(_companyId);
+		fragmentEntryLinkModel.setUserId(_sampleUserId);
+		fragmentEntryLinkModel.setUserName(_SAMPLE_USER_NAME);
+		fragmentEntryLinkModel.setCreateDate(new Date());
+		fragmentEntryLinkModel.setModifiedDate(new Date());
+
+		// Other fields
+
+		fragmentEntryLinkModel.setFragmentEntryId(0);
+		fragmentEntryLinkModel.setClassNameId(getClassNameId(Layout.class));
+		fragmentEntryLinkModel.setClassPK(layoutModel.getPlid());
+		fragmentEntryLinkModel.setCss(
+			_readFile(_getFragmentComponentInputStream("heading", "css")));
+		fragmentEntryLinkModel.setHtml(
+			_readFile(_getFragmentComponentInputStream("heading", "html")));
+		fragmentEntryLinkModel.setConfiguration(
+			_readFile(
+				"fragment_component/fragment_component_heading_configuration." +
+					"json"));
+		fragmentEntryLinkModel.setJs(StringPool.BLANK);
+
+		String editValue = _readFile(
+			"fragment_component/fragment_component_heading_editValue_" +
+				"attachment_journal_article_title.json");
+
+		fragmentEntryLinkModel.setEditableValues(
+			StringUtil.replace(
+				StringUtil.replace(
+					StringUtil.replace(
+						editValue, "${journalArticleClassNameId}",
+						String.valueOf(getClassNameId(JournalArticle.class))),
+					"${journalArticleClassPK}",
+					String.valueOf(journalArticleModel.getResourcePrimKey())),
+				"${journalArticleTitle}", journalArticleModel.getUrlTitle()));
+
+		fragmentEntryLinkModel.setNamespace(StringUtil.randomId());
+		fragmentEntryLinkModel.setPlid(layoutModel.getPlid());
+		fragmentEntryLinkModel.setPosition(0);
+		fragmentEntryLinkModel.setRendererKey(
+			_FRAGMENT_COMPONENT_RENDER_KEY_HEADING);
+
+		// Autogenerated fields
+
+		String uuid = SequentialUUID.generate();
+
+		fragmentEntryLinkModel.setUuid(uuid);
+		fragmentEntryLinkModel.setExternalReferenceCode(uuid);
+
+		return fragmentEntryLinkModel;
+	}
+
+	public FragmentEntryLinkModel newFragmentEntryLinkModel(
 		LayoutModel layoutModel, FragmentEntryModel fragmentEntryModel) {
 
 		FragmentEntryLinkModel fragmentEntryLinkModel =
@@ -4308,6 +4383,25 @@ public class DataFactory {
 		fragmentEntryLinkModel.setExternalReferenceCode(uuid);
 
 		return fragmentEntryLinkModel;
+	}
+
+	public List<FragmentEntryLinkModel> newFragmentEntryLinkModels(
+			JournalArticleModel journalArticleModel, LayoutModel layoutModel,
+			long segmentsExperienceId)
+		throws Exception {
+
+		List<FragmentEntryLinkModel> fragmentEntryLinkModels =
+			new ArrayList<>();
+
+		for (int i = 0;
+			 i < BenchmarksPropsValues.MAX_FRAGMENTS_PER_LAYOUT_COUNT; i++) {
+
+			fragmentEntryLinkModels.add(
+				newFragmentEntryLinkModel(
+					journalArticleModel, layoutModel, segmentsExperienceId));
+		}
+
+		return fragmentEntryLinkModels;
 	}
 
 	public List<FragmentEntryLinkModel> newFragmentEntryLinkModels(
@@ -4970,6 +5064,71 @@ public class DataFactory {
 				_layoutPageTemplateStructureRelData, "${fragmentEntryLinkId}",
 				String.valueOf(
 					fragmentEntryLinkModel.getFragmentEntryLinkId())));
+
+		// Autogenerated fields
+
+		layoutPageTemplateStructureRelModel.setUuid(SequentialUUID.generate());
+
+		return layoutPageTemplateStructureRelModel;
+	}
+
+	public LayoutPageTemplateStructureRelModel
+			newLayoutPageTemplateStructureRelModel(
+				LayoutModel layoutModel,
+				LayoutPageTemplateStructureModel
+					layoutPageTemplateStructureModel,
+				List<FragmentEntryLinkModel> fragmentEntryLinkModels)
+		throws Exception {
+
+		List<FragmentEntryLinkModel> targetFragmentEntryLinkModels =
+			new ArrayList<>();
+
+		for (FragmentEntryLinkModel model : fragmentEntryLinkModels) {
+			if (model.getPlid() == layoutModel.getPlid()) {
+				targetFragmentEntryLinkModels.add(model);
+			}
+		}
+
+		LayoutPageTemplateStructureRelModel
+			layoutPageTemplateStructureRelModel =
+				new LayoutPageTemplateStructureRelModelImpl();
+
+		// PK fields
+
+		layoutPageTemplateStructureRelModel.setLayoutPageTemplateStructureRelId(
+			_counter.get());
+
+		// Group instance
+
+		layoutPageTemplateStructureRelModel.setGroupId(
+			layoutPageTemplateStructureModel.getGroupId());
+
+		// Audit fields
+
+		layoutPageTemplateStructureRelModel.setCompanyId(_companyId);
+		layoutPageTemplateStructureRelModel.setUserId(_sampleUserId);
+		layoutPageTemplateStructureRelModel.setUserName(_SAMPLE_USER_NAME);
+		layoutPageTemplateStructureRelModel.setCreateDate(new Date());
+		layoutPageTemplateStructureRelModel.setModifiedDate(new Date());
+
+		// Other fields
+
+		layoutPageTemplateStructureRelModel.setLayoutPageTemplateStructureId(
+			layoutPageTemplateStructureModel.
+				getLayoutPageTemplateStructureId());
+
+		FragmentEntryLinkModel fragmentEntryLinkModel =
+			targetFragmentEntryLinkModels.get(0);
+
+		layoutPageTemplateStructureRelModel.setSegmentsExperienceId(
+			fragmentEntryLinkModel.getSegmentsExperienceId());
+
+		layoutPageTemplateStructureRelModel.setData(
+			_generateJsonData(targetFragmentEntryLinkModels));
+		layoutPageTemplateStructureRelModel.setStatusByUserId(_sampleUserId);
+		layoutPageTemplateStructureRelModel.setStatusByUserName(
+			_SAMPLE_USER_NAME);
+		layoutPageTemplateStructureRelModel.setStatusDate(new Date());
 
 		// Autogenerated fields
 
@@ -8637,6 +8796,77 @@ public class DataFactory {
 		catch (ReflectiveOperationException reflectiveOperationException) {
 			ReflectionUtil.throwException(reflectiveOperationException);
 		}
+	}
+
+	private String _generateJsonData(
+			List<FragmentEntryLinkModel> fragmentEntryLinkModels)
+		throws Exception {
+
+		Map<String, String> itemPageLayoutDefinitions = new LinkedHashMap<>();
+
+		String itemPageLayoutDefinitionTemplate = _readFile(
+			"content_page_template/item-page-layout-definition-template.json");
+
+		for (FragmentEntryLinkModel fragmentEntryLinkModel :
+				fragmentEntryLinkModels) {
+
+			String itemId = SequentialUUID.generate();
+
+			itemPageLayoutDefinitions.put(
+				itemId,
+				JSONFactoryUtil.createJSONObject(
+					StringUtil.replace(
+						StringUtil.replace(
+							itemPageLayoutDefinitionTemplate, "${itemId}",
+							itemId),
+						"${fragmentEntryLinkId}",
+						String.valueOf(
+							fragmentEntryLinkModel.getFragmentEntryLinkId()))
+				).toString());
+		}
+
+		String rootItemId = SequentialUUID.generate();
+
+		JSONObject itemsJSONObject = JSONUtil.put(
+			rootItemId,
+			JSONUtil.put(
+				"children",
+				JSONFactoryUtil.createJSONArray(
+					itemPageLayoutDefinitions.keySet())
+			).put(
+				"config", JSONFactoryUtil.createJSONObject()
+			).put(
+				"itemId", rootItemId
+			).put(
+				"parentId", StringPool.BLANK
+			).put(
+				"type", "root"
+			));
+
+		for (Map.Entry<String, String> entry :
+				itemPageLayoutDefinitions.entrySet()) {
+
+			itemsJSONObject.put(
+				entry.getKey(),
+				JSONFactoryUtil.createJSONObject(entry.getValue()));
+		}
+
+		return JSONUtil.put(
+			"deletedItems", JSONFactoryUtil.createJSONArray()
+		).put(
+			"items", itemsJSONObject
+		).put(
+			"pageRules", JSONFactoryUtil.createJSONArray()
+		).put(
+			"rootItems",
+			JSONUtil.put(
+				"dropZone", ""
+			).put(
+				"main", rootItemId
+			)
+		).put(
+			"version", 1.0
+		).toString();
 	}
 
 	private String _generateJsonData(