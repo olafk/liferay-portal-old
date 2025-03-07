liferay-commerce/liferay-portal

LPD-48683 Commerce Channel Currency pages

modules/apps/commerce/commerce-channel-web/src/main/resources/META-INF/resources/commerce_channel/currencies.jsp
@@ -1,0 +1,39 @@
+<%--
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+--%>
+
+<%@ include file="/init.jsp" %>
+
+<%
+CommerceChannelCurrencyDisplayContext commerceChannelCurrenciesDisplayContext = (CommerceChannelCurrencyDisplayContext)request.getAttribute(WebKeys.PORTLET_DISPLAY_CONTEXT);
+%>
+
+<portlet:actionURL name="/commerce_channels/edit_commerce_channel_currency" var="editCommerceChannelCurrencyActionURL" />
+
+<aui:form action="<%= editCommerceChannelCurrencyActionURL %>" cssClass="hide" name="addCommerceChannelCurrencyFm">
+	<aui:input name="<%= Constants.CMD %>" type="hidden" value="<%= Constants.ADD_MULTIPLE %>" />
+	<aui:input name="redirect" type="hidden" value="<%= currentURL %>" />
+	<aui:input name="commerceChannelId" type="hidden" value="<%= commerceChannelCurrenciesDisplayContext.getCommerceChannelId() %>" />
+	<aui:input name="currencyIds" type="hidden" value="" />
+</aui:form>
+
+<frontend-data-set:classic-display
+	contextParams='<%=
+		HashMapBuilder.<String, String>put(
+			"commerceChannelId", String.valueOf(commerceChannelCurrenciesDisplayContext.getCommerceChannelId())
+		).build()
+	%>'
+	creationMenu="<%= commerceChannelCurrenciesDisplayContext.getCreationMenu() %>"
+	dataProviderKey="<%= CommerceChannelFDSNames.COMMERCE_CURRENCIES %>"
+	id="<%= CommerceChannelFDSNames.COMMERCE_CURRENCIES %>"
+	itemsPerPage="<%= 10 %>"
+	selectedItemsKey="currencyId"
+/>
+
+<liferay-frontend:component
+	context="<%= commerceChannelCurrenciesDisplayContext.getJSContext() %>"
+	module="{commerceChannelCurrency} from commerce-channel-web"
+/>

modules/apps/commerce/commerce-channel-web/src/main/resources/META-INF/resources/init.jsp
@@ -28,6 +28,7 @@ page import="com.liferay.commerce.channel.web.internal.constants.CommerceChannel
 page import="com.liferay.commerce.channel.web.internal.constants.CommerceChannelScreenNavigationConstants" %><%@
 page import="com.liferay.commerce.channel.web.internal.display.context.CommerceChannelAccountEntryQualifiersDisplayContext" %><%@
 page import="com.liferay.commerce.channel.web.internal.display.context.CommerceChannelCountryDisplayContext" %><%@
+page import="com.liferay.commerce.channel.web.internal.display.context.CommerceChannelCurrencyDisplayContext" %><%@
 page import="com.liferay.commerce.channel.web.internal.display.context.CommerceChannelDisplayContext" %><%@
 page import="com.liferay.commerce.channel.web.internal.display.context.SiteCommerceChannelTypeDisplayContext" %><%@
 page import="com.liferay.commerce.constants.CommerceOrderConstants" %><%@

modules/apps/commerce/commerce-channel-web/src/main/resources/META-INF/resources/js/index.js
@@ -8,5 +8,6 @@ export {default as CommerceChannelSite} from './CommerceChannelSite';
 export {default as accountEntries} from './accountEntries';
 export {default as addCommerceChannel} from './addCommerceChannel';
 export {default as commerceChannelCountry} from './commerce_channel/commerceChannelCountry';
+export {default as commerceChannelCurrency} from './commerce_channel/commerceChannelCurrency';
 export {default as CommerceChannelAddPaymentMethod} from './commerce_marketplace_payment_method';
 export {default as qualifiers} from './qualifiers';

olafk/liferay-portal-old

modules/apps/commerce/commerce-channel-web/src/main/resources/META-INF/resources/commerce_channel/currencies.jsp
@@ -1,0 +1,39 @@
+<%--
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+--%>
+
+<%@ include file="/init.jsp" %>
+
+<%
+CommerceChannelCommerceCurrencyDisplayContext commerceChannelCommerceCurrenciesDisplayContext = (CommerceChannelCommerceCurrencyDisplayContext)request.getAttribute(WebKeys.PORTLET_DISPLAY_CONTEXT);
+%>
+
+<portlet:actionURL name="/commerce_channels/edit_commerce_channel_commerce_currency" var="editCommerceChannelCommerceCurrencyActionURL" />
+
+<aui:form action="<%= editCommerceChannelCommerceCurrencyActionURL %>" cssClass="hide" name="addCommerceChannelCommerceCurrencyFm">
+	<aui:input name="<%= Constants.CMD %>" type="hidden" value="<%= Constants.ADD_MULTIPLE %>" />
+	<aui:input name="redirect" type="hidden" value="<%= currentURL %>" />
+	<aui:input name="commerceChannelId" type="hidden" value="<%= commerceChannelCommerceCurrenciesDisplayContext.getCommerceChannelId() %>" />
+	<aui:input name="currencyIds" type="hidden" value="" />
+</aui:form>
+
+<frontend-data-set:classic-display
+	contextParams='<%=
+		HashMapBuilder.<String, String>put(
+			"commerceChannelId", String.valueOf(commerceChannelCommerceCurrenciesDisplayContext.getCommerceChannelId())
+		).build()
+	%>'
+	creationMenu="<%= commerceChannelCommerceCurrenciesDisplayContext.getCreationMenu() %>"
+	dataProviderKey="<%= CommerceChannelFDSNames.COMMERCE_CURRENCIES %>"
+	id="<%= CommerceChannelFDSNames.COMMERCE_CURRENCIES %>"
+	itemsPerPage="<%= 10 %>"
+	selectedItemsKey="currencyId"
+/>
+
+<liferay-frontend:component
+	context="<%= commerceChannelCommerceCurrenciesDisplayContext.getJSContext() %>"
+	module="{commerceChannelCommerceCurrency} from commerce-channel-web"
+/>

modules/apps/commerce/commerce-channel-web/src/main/resources/META-INF/resources/init.jsp
@@ -27,6 +27,7 @@ page import="com.liferay.account.model.AccountEntry" %><%@
 page import="com.liferay.commerce.channel.web.internal.constants.CommerceChannelFDSNames" %><%@
 page import="com.liferay.commerce.channel.web.internal.constants.CommerceChannelScreenNavigationConstants" %><%@
 page import="com.liferay.commerce.channel.web.internal.display.context.CommerceChannelAccountEntryQualifiersDisplayContext" %><%@
+page import="com.liferay.commerce.channel.web.internal.display.context.CommerceChannelCommerceCurrencyDisplayContext" %><%@
 page import="com.liferay.commerce.channel.web.internal.display.context.CommerceChannelCountryDisplayContext" %><%@
 page import="com.liferay.commerce.channel.web.internal.display.context.CommerceChannelDisplayContext" %><%@
 page import="com.liferay.commerce.channel.web.internal.display.context.SiteCommerceChannelTypeDisplayContext" %><%@

modules/apps/commerce/commerce-channel-web/src/main/resources/META-INF/resources/js/index.js
@@ -7,6 +7,7 @@ export {default as CommerceChannelGeneral} from './CommerceChannelGeneral';
 export {default as CommerceChannelSite} from './CommerceChannelSite';
 export {default as accountEntries} from './accountEntries';
 export {default as addCommerceChannel} from './addCommerceChannel';
+export {default as commerceChannelCommerceCurrency} from './commerce_channel/commerceChannelCommerceCurrency';
 export {default as commerceChannelCountry} from './commerce_channel/commerceChannelCountry';
 export {default as CommerceChannelAddPaymentMethod} from './commerce_marketplace_payment_method';
 export {default as qualifiers} from './qualifiers';