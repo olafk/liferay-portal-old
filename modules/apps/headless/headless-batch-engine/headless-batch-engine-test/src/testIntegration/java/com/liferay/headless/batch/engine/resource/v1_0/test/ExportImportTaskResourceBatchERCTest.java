65596122a24a51c974e65e5ba06268f2ce13b4bc
Vendel Töreki <vendel.toreki@liferay.com>
2025-03-07 17:20:40 +0100

    LPD-47626 Add new integration test check the behavior of batchExternalReferenceCode paramater

---
+++ modules/apps/headless/headless-batch-engine/headless-batch-engine-test/src/testIntegration/java/com/liferay/headless/batch/engine/resource/v1_0/test/ExportImportTaskResourceBatchERCTest.java
@@ -0,0 +1,248 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.headless.batch.engine.resource.v1_0.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.batch.engine.BaseBatchEngineTaskItemDelegate;
+import com.liferay.batch.engine.BatchEngineTaskItemDelegate;
+import com.liferay.batch.engine.pagination.Page;
+import com.liferay.batch.engine.pagination.Pagination;
+import com.liferay.headless.batch.engine.entity.TestEntity;
+import com.liferay.headless.batch.engine.exception.TestEntityException;
+import com.liferay.headless.batch.engine.util.ExportImportTaskUtil;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.module.util.SystemBundleUtil;
+import com.liferay.portal.kernel.search.Sort;
+import com.liferay.portal.kernel.search.filter.Filter;
+import com.liferay.portal.kernel.test.randomizerbumpers.UniqueStringRandomizerBumper;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.HashMapDictionaryBuilder;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.test.log.LogCapture;
+import com.liferay.portal.test.log.LoggerTestUtil;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+
+import java.io.Serializable;
+
+import java.util.Map;
+import java.util.Objects;
+
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+
+/**
+ * @author Vendel Toreki
+ */
+@RunWith(Arquillian.class)
+public class ExportImportTaskResourceBatchERCTest {
+
+	@ClassRule
+	@Rule
+	public static final LiferayIntegrationTestRule liferayIntegrationTestRule =
+		new LiferayIntegrationTestRule();
+
+	@Before
+	public void setUp() throws Exception {
+		_batchExternalReferenceCode = RandomTestUtil.randomString();
+		_externalReferenceCode = RandomTestUtil.randomString();
+	}
+
+	@Test
+	public void testImportWithBatchERC() throws Exception {
+		try (TestERCValidatorDelegate testERCValidatorDelegate =
+				new TestERCValidatorDelegate(_batchExternalReferenceCode)) {
+
+			_testPostImportTask(
+				_batchExternalReferenceCode, null,
+				testERCValidatorDelegate.getDelegateName());
+		}
+	}
+
+	@Test
+	public void testImportWithBothERCs() throws Exception {
+		try (TestERCValidatorDelegate testERCValidatorDelegate =
+				new TestERCValidatorDelegate(_batchExternalReferenceCode)) {
+
+			_testPostImportTask(
+				_batchExternalReferenceCode, _externalReferenceCode,
+				testERCValidatorDelegate.getDelegateName());
+		}
+	}
+
+	@Test
+	public void testImportWithERC() throws Exception {
+		try (TestERCValidatorDelegate testERCValidatorDelegate =
+				new TestERCValidatorDelegate(_externalReferenceCode)) {
+
+			_testPostImportTask(
+				null, _externalReferenceCode,
+				testERCValidatorDelegate.getDelegateName());
+		}
+	}
+
+	@Test
+	public void testImportWithNoERCsFail() throws Exception {
+		try (TestERCValidatorDelegate testERCValidatorDelegate =
+				new TestERCValidatorDelegate(_externalReferenceCode);
+			LogCapture logCapture = LoggerTestUtil.configureLog4JLogger(
+				"com.liferay.batch.engine.internal." +
+					"BatchEngineImportTaskExecutorImpl",
+				LoggerTestUtil.ERROR)) {
+
+			_testPostImportTask(
+				null, null, testERCValidatorDelegate.getDelegateName(), false);
+		}
+	}
+
+	@Test
+	public void testImportWithNoERCsSuccess() throws Exception {
+		try (TestERCValidatorDelegate testERCValidatorDelegate =
+				new TestERCValidatorDelegate(null)) {
+
+			_testPostImportTask(
+				null, null, testERCValidatorDelegate.getDelegateName());
+		}
+	}
+
+	private void _testPostImportTask(
+			String batchExternalReferenceCode, String externalReferenceCode,
+			String testDelegateName)
+		throws Exception {
+
+		_testPostImportTask(
+			batchExternalReferenceCode, externalReferenceCode, testDelegateName,
+			true);
+	}
+
+	private void _testPostImportTask(
+			String batchExternalReferenceCode, String externalReferenceCode,
+			String testDelegateName, boolean expectSuccess)
+		throws Exception {
+
+		ExportImportTaskUtil.postImportTask(
+			JSONFactoryUtil.createJSONArray(
+			).put(
+				JSONUtil.put(
+					"intValue", String.valueOf(RandomTestUtil.nextInt())
+				).put(
+					"textValue",
+					StringUtil.getTitleCase(
+						RandomTestUtil.randomString(
+							8, UniqueStringRandomizerBumper.INSTANCE),
+						true, "")
+				)
+			).toString(),
+			"com.liferay.headless.batch.engine.entity.TestEntity",
+			expectSuccess ? "COMPLETED" : "FAILED",
+			HashMapBuilder.put(
+				"batchExternalReferenceCode",
+				() -> {
+					if (Validator.isNotNull(batchExternalReferenceCode)) {
+						return batchExternalReferenceCode;
+					}
+
+					return null;
+				}
+			).put(
+				"createStrategy", "INSERT"
+			).put(
+				"externalReferenceCode",
+				() -> {
+					if (Validator.isNotNull(externalReferenceCode)) {
+						return externalReferenceCode;
+					}
+
+					return null;
+				}
+			).put(
+				"taskItemDelegateName", testDelegateName
+			).build());
+	}
+
+	private String _batchExternalReferenceCode;
+	private String _externalReferenceCode;
+
+	private static class TestERCValidatorDelegate implements AutoCloseable {
+
+		public TestERCValidatorDelegate(String expectedExternalReferenceCode)
+			throws Exception {
+
+			BundleContext bundleContext = SystemBundleUtil.getBundleContext();
+
+			BatchEngineTaskItemDelegate<TestEntity> delegate =
+				new BaseBatchEngineTaskItemDelegate<TestEntity>() {
+
+					@Override
+					public TestEntity createItem(
+							TestEntity testEntity,
+							Map<String, Serializable> parameters)
+						throws Exception {
+
+						String externalReferenceCode = (String)parameters.get(
+							"externalReferenceCode");
+
+						if (!Objects.equals(
+								expectedExternalReferenceCode,
+								externalReferenceCode)) {
+
+							throw new TestEntityException(
+								StringBundler.concat(
+									"Received externalReferenceCode ",
+									externalReferenceCode,
+									" does not match expected ",
+									expectedExternalReferenceCode));
+						}
+
+						return new TestEntity();
+					}
+
+					@Override
+					public Page<TestEntity> read(
+							Filter filter, Pagination pagination, Sort[] sorts,
+							Map<String, Serializable> parameters, String search)
+						throws Exception {
+
+						return null;
+					}
+
+				};
+
+			_delegateName =
+				"test-delegate-erc-" +
+					StringUtil.lowerCase(RandomTestUtil.randomString(8));
+
+			_serviceRegistration = bundleContext.registerService(
+				BatchEngineTaskItemDelegate.class, delegate,
+				HashMapDictionaryBuilder.<String, Object>put(
+					"batch.engine.task.item.delegate.name", _delegateName
+				).build());
+		}
+
+		@Override
+		public void close() throws Exception {
+			_serviceRegistration.unregister();
+		}
+
+		public String getDelegateName() {
+			return _delegateName;
+		}
+
+		private final String _delegateName;
+		private final ServiceRegistration<?> _serviceRegistration;
+
+	}
+
+}
