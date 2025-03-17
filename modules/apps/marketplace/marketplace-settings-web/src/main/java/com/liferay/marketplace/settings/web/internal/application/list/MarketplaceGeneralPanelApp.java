9ff53c83653d78b6c11a6dc6c20fd78e2ee6360e
Ricardo Mariz <ricardo.mariz@liferay.com>
2025-03-17 15:03:12 -0300

    LPD-47048 Add marketplace general panel app

---
+++ modules/apps/marketplace/marketplace-settings-web/src/main/java/com/liferay/marketplace/settings/web/internal/application/list/MarketplaceGeneralPanelApp.java
@@ -0,0 +1,51 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.marketplace.settings.web.internal.application.list;
+
+import com.liferay.application.list.BasePanelApp;
+import com.liferay.application.list.PanelApp;
+import com.liferay.application.list.constants.PanelCategoryKeys;
+import com.liferay.marketplace.constants.MarketplacePortletKeys;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.Portlet;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Ricardo Mariz
+ */
+@Component(
+	property = {
+		"panel.app.order:Integer=100",
+		"panel.category.key=" + PanelCategoryKeys.MARKETPLACE
+	},
+	service = PanelApp.class
+)
+public class MarketplaceGeneralPanelApp extends BasePanelApp {
+
+	@Override
+	public Portlet getPortlet() {
+		return _portlet;
+	}
+
+	@Override
+	public String getPortletId() {
+		return MarketplacePortletKeys.GENERAL;
+	}
+
+	@Override
+	public boolean isShow(PermissionChecker permissionChecker, Group group) {
+		return false;
+	}
+
+	@Reference(
+		target = "(javax.portlet.name=" + MarketplacePortletKeys.GENERAL + ")"
+	)
+	private Portlet _portlet;
+
+}
