5de4718a991f3b61a32817814f5b0a213a829abb
Noor Najjar <noor.najjar@liferay.com>
2025-03-19 14:50:13 -0700

    LPD-51515 Remove unused file

--- modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/index.ts
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/index.ts
@@ -7,7 +7,6 @@ export {default as AllFDSPropsTransformer} from './main/FDSPropsTransformer/AllF
 export {default as ContentsFDSPropsTransformer} from './main/FDSPropsTransformer/ContentsFDSPropsTransformer';
 export {default as FilesFDSPropsTransformer} from './main/FDSPropsTransformer/FilesFDSPropsTransformer';
 export {default as StructuresFDSPropsTransformer} from './main/FDSPropsTransformer/StructuresFDSPropsTransformer';
-export {default as CategorizationMainView} from './main/categorization/CategorizationMainView';
 export {default as TagsView} from './main/categorization/tags/TagsView';
 export {default as VocabulariesView} from './main/categorization/vocabulary/VocabulariesView';
 export {default as SpacesSticker} from './main/components/SpaceSticker';
