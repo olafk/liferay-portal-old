df281fd90a02ecbb1673af5319b08a46050f54ac
Alan Huang <alan.huang@liferay.com>
2025-04-19 15:48:57 +0800

    LPD-52616 Format quotes

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/YMLStylingCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/YMLStylingCheck.java
@@ -5,6 +5,15 @@
 
 package com.liferay.source.formatter.check;
 
+import com.liferay.petra.string.CharPool;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.io.unsync.UnsyncBufferedReader;
+import com.liferay.portal.kernel.io.unsync.UnsyncStringReader;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
+
+import java.io.IOException;
+
 /**
  * @author Alan Huang
  */
@@ -12,7 +21,8 @@ public class YMLStylingCheck extends BaseFileCheck {
 
 	@Override
 	protected String doProcess(
-		String fileName, String absolutePath, String content) {
+			String fileName, String absolutePath, String content)
+		throws IOException {
 
 		if (content.startsWith("---\n")) {
 			content = content.substring(4);
@@ -27,16 +37,98 @@ public class YMLStylingCheck extends BaseFileCheck {
 		content = content.replaceAll(
 			"(\\A|\n)( *#)@? ?(review)(\\Z|\n)", "$1$2 @$3$4");
 
-		content = content.replaceAll(
-			"(\\A|\n)(( *)|(.+: ))'([^'\"]*)'(\\Z|\n)", "$1$2\"$5\"$6");
-
-		content = content.replaceAll(
-			"(\\A|\n)( *)'([^'\"]+)'(:.*)(\\Z|\n)", "$1$2\"$3\"$4$5");
-
 		if (fileName.endsWith("/rest-config.yaml")) {
 			content = content.replaceAll(
 				"(\\A|\n)( *baseURI: ((['\"](?!/))|(?!['\"/])))(.*)",
 				"$1$2/$5");
+		}
+
+		return _formatQuotes(content);
+	}
+
+	private String _fixQuotes(String s) {
+		if (Validator.isNull(s) || (s.length() == 1)) {
+			return s;
+		}
+
+		if ((s.charAt(0) == CharPool.APOSTROPHE) &&
+			(s.charAt(s.length() - 1) == CharPool.APOSTROPHE)) {
+
+			if (s.length() == 2) {
+				return StringPool.QUOTE + StringPool.QUOTE;
+			}
+
+			String unquotedValue = s.substring(1, s.length() - 1);
+
+			unquotedValue = StringUtil.replace(unquotedValue, "''", "'");
+
+			return CharPool.QUOTE + unquotedValue + CharPool.QUOTE;
+		}
+
+		if ((s.charAt(0) == CharPool.QUOTE) &&
+			(s.charAt(s.length() - 1) == CharPool.QUOTE)) {
+
+			if (s.length() == 2) {
+				return s;
+			}
+
+			String unquotedValue = s.substring(1, s.length() - 1);
+
+			if (unquotedValue.contains("\\") || unquotedValue.matches("\\d+") ||
+				unquotedValue.matches("[Ff]alse") ||
+				unquotedValue.matches("[Tt]rue") ||
+				unquotedValue.startsWith("#") ||
+				unquotedValue.startsWith("&") ||
+				unquotedValue.startsWith("*") ||
+				unquotedValue.startsWith("{")) {
+
+				return s;
+			}
+
+			return s.substring(1, s.length() - 1);
+		}
+
+		return s;
+	}
+
+	private String _formatQuotes(String content) throws IOException {
+		try (UnsyncBufferedReader unsyncBufferedReader =
+				new UnsyncBufferedReader(new UnsyncStringReader(content))) {
+
+			String line = null;
+			int lineNumber = 0;
+
+			while ((line = unsyncBufferedReader.readLine()) != null) {
+				lineNumber++;
+
+				String trimmedLine = StringUtil.trimLeading(line);
+
+				int x = trimmedLine.indexOf(": ");
+
+				if (x == -1) {
+					continue;
+				}
+
+				String key = trimmedLine.substring(0, x);
+
+				String newKey = _fixQuotes(key);
+
+				if (!key.equals(newKey)) {
+					return StringUtil.replaceFirst(
+						content, key, newKey,
+						getLineStartPos(content, lineNumber));
+				}
+
+				String value = trimmedLine.substring(x + 2);
+
+				String newValue = _fixQuotes(value);
+
+				if (!value.equals(newValue)) {
+					return StringUtil.replaceFirst(
+						content, value, newValue,
+						getLineStartPos(content, lineNumber));
+				}
+			}
 		}
 
 		return content;
