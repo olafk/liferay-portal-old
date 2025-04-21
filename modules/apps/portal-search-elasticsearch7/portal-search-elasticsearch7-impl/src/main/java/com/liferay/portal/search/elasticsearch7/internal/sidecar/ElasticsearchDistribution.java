9edf17ff7295e0a8de0676d400221ea5c020f5fe
Bryan Engler <bryan.engler@liferay.com>
2025-04-21 12:00:03 -0700

    LPD-53692 portal-search-elasticsearch7-impl: Bump sidecar version (when installing at runtime)
    https://liferay.atlassian.net/browse/LPD-53692

--- modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/sidecar/ElasticsearchDistribution.java
+++ modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/sidecar/ElasticsearchDistribution.java
@@ -15,7 +15,7 @@ import java.util.List;
  */
 public class ElasticsearchDistribution implements Distribution {
 
-	public static final String VERSION = "7.17.26";
+	public static final String VERSION = "7.17.28";
 
 	@Override
 	public Distributable getElasticsearchDistributable() {
