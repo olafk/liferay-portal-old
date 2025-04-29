4116e7d8d68780cd86a8e5a3cfca1571fc705bb0
Sandro Chinea <sandrochinea@gmail.com>
2025-04-29 15:49:30 +0100

    LPD-54411 Add main folder for analytics-web

--- modules/test/playwright/tests/analytics-web/main/support-liferay-cookie-manager.spec.ts
+++ modules/test/playwright/tests/analytics-web/main/support-liferay-cookie-manager.spec.ts
@@ -5,14 +5,14 @@
 
 import {Page, expect, mergeTests} from '@playwright/test';
 
-import {apiHelpersTest} from '../../fixtures/apiHelpersTest';
-import {dataApiHelpersTest} from '../../fixtures/dataApiHelpersTest';
-import {featureFlagsTest} from '../../fixtures/featureFlagsTest';
-import {loginAnalyticsCloudTest} from '../../fixtures/loginAnalyticsCloudTest';
-import {loginTest} from '../../fixtures/loginTest';
-import {ApiHelpers} from '../../helpers/ApiHelpers';
-import {liferayConfig} from '../../liferay.config';
-import getRandomString from '../../utils/getRandomString';
+import {apiHelpersTest} from '../../../fixtures/apiHelpersTest';
+import {dataApiHelpersTest} from '../../../fixtures/dataApiHelpersTest';
+import {featureFlagsTest} from '../../../fixtures/featureFlagsTest';
+import {loginAnalyticsCloudTest} from '../../../fixtures/loginAnalyticsCloudTest';
+import {loginTest} from '../../../fixtures/loginTest';
+import {ApiHelpers} from '../../../helpers/ApiHelpers';
+import {liferayConfig} from '../../../liferay.config';
+import getRandomString from '../../../utils/getRandomString';
 import {
 	connectToAnalyticsCloud,
 	disconnectFromAnalyticsCloud,
@@ -20,9 +20,9 @@ import {
 	goToAnalyticsCloudInstanceSettings,
 	syncAllContacts,
 	syncSite,
-} from '../analytics-settings-web/utils/analytics-settings';
-import {createChannel} from '../osb-faro-web/utils/channel';
-import {createDataSource} from '../osb-faro-web/utils/data-source';
+} from '../../analytics-settings-web/main/utils/analytics-settings';
+import {createChannel} from '../../osb-faro-web/main/utils/channel';
+import {createDataSource} from '../../osb-faro-web/main/utils/data-source';
 
 export const test = mergeTests(
 	apiHelpersTest,
