7a92a4e61460f8a01f1f7cf98cea26bac12ff277
Manuele Castro <manuele.castro@liferay.com>
2025-04-29 10:30:14 -0300

    LPD-44397 Enable CX side validation

--- modules/apps/captcha/captcha-impl/src/main/java/com/liferay/captcha/internal/function/captcha/FunctionCaptchaImpl.java
+++ modules/apps/captcha/captcha-impl/src/main/java/com/liferay/captcha/internal/function/captcha/FunctionCaptchaImpl.java
@@ -9,20 +9,33 @@ import com.liferay.captcha.internal.configuration.FunctionCaptchaImplConfigurati
 import com.liferay.captcha.simplecaptcha.SimpleCaptchaImpl;
 import com.liferay.client.extension.type.CustomElementCET;
 import com.liferay.client.extension.type.manager.CETManager;
+import com.liferay.osgi.util.configuration.ConfigurationFactoryUtil;
 import com.liferay.petra.string.CharPool;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
+import com.liferay.portal.catapult.PortalCatapult;
 import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
 import com.liferay.portal.kernel.captcha.Captcha;
+import com.liferay.portal.kernel.captcha.CaptchaConfigurationException;
+import com.liferay.portal.kernel.captcha.CaptchaException;
 import com.liferay.portal.kernel.content.security.policy.ContentSecurityPolicyNonceProviderUtil;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONFactory;
+import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.CompanyLocalService;
 import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.Http;
+import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
 
 import java.io.IOException;
@@ -30,7 +43,10 @@ import java.io.PrintWriter;
 
 import java.util.Map;
 
+import javax.portlet.PortletRequest;
+
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
 import javax.servlet.http.HttpServletResponse;
 
 import org.osgi.service.component.annotations.Activate;
@@ -39,6 +55,7 @@ import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Pedro Victor Silvestre
+ * @author Manuele Castro
  */
 @Component(
 	configurationPid = "com.liferay.captcha.internal.configuration.FunctionCaptchaImplConfiguration",
@@ -112,8 +129,120 @@ public class FunctionCaptchaImpl extends SimpleCaptchaImpl {
 
 	@Activate
 	protected void activate(Map<String, Object> properties) {
+		_companyId = ConfigurationFactoryUtil.getCompanyId(
+			_companyLocalService, properties);
+
 		_functionCaptchaImplConfiguration = ConfigurableUtil.createConfigurable(
 			FunctionCaptchaImplConfiguration.class, properties);
+	}
+
+	@Override
+	protected boolean validateChallenge(HttpServletRequest httpServletRequest)
+		throws CaptchaException {
+
+		String captchaResponse = ParamUtil.getString(
+			httpServletRequest,
+			_functionCaptchaImplConfiguration.responseParameterName());
+
+		while (Validator.isBlank(captchaResponse) &&
+			   (httpServletRequest instanceof
+				   HttpServletRequestWrapper httpServletRequestWrapper)) {
+
+			httpServletRequest =
+				(HttpServletRequest)httpServletRequestWrapper.getRequest();
+
+			captchaResponse = ParamUtil.getString(
+				httpServletRequest,
+				_functionCaptchaImplConfiguration.responseParameterName());
+		}
+
+		if (Validator.isBlank(captchaResponse)) {
+			_log.error(
+				"CAPTCHA challenge is null. User " +
+					httpServletRequest.getRemoteUser() +
+						" may be trying to circumvent the CAPTCHA.");
+
+			throw new CaptchaException();
+		}
+
+		JSONObject payloadJSONObject = _jsonFactory.createJSONObject(
+		).put(
+			"remoteip", httpServletRequest.getRemoteAddr()
+		).put(
+			"response", captchaResponse
+		);
+
+		try {
+			User user = _userLocalService.getUserByScreenName(
+				_companyId, "default-service-account");
+
+			JSONObject jsonObject = _jsonFactory.createJSONObject(
+				new String(
+					_portalCatapult.launch(
+						_companyId, Http.Method.POST,
+						_functionCaptchaImplConfiguration.
+							oAuth2ApplicationExternalReferenceCode(),
+						payloadJSONObject,
+						_functionCaptchaImplConfiguration.resourcePath(),
+						user.getUserId()
+					).get()));
+
+			if (jsonObject == null) {
+				_log.error(
+					_functionCaptchaImplConfiguration.captchaName() +
+						" did not return a result");
+
+				throw new CaptchaConfigurationException();
+			}
+
+			String success = jsonObject.getString("success");
+
+			if (StringUtil.equalsIgnoreCase(success, "true")) {
+				return true;
+			}
+
+			JSONArray jsonArray = jsonObject.getJSONArray("error-codes");
+
+			if ((jsonArray == null) || (jsonArray.length() == 0)) {
+				_log.error(
+					_functionCaptchaImplConfiguration.captchaName() +
+						" encountered an error");
+
+				throw new CaptchaConfigurationException();
+			}
+
+			StringBundler sb = new StringBundler((jsonArray.length() * 2) - 1);
+
+			for (int i = 0; i < jsonArray.length(); i++) {
+				sb.append(jsonArray.getString(i));
+
+				if (i < (jsonArray.length() - 1)) {
+					sb.append(StringPool.COMMA_AND_SPACE);
+				}
+			}
+
+			_log.error(
+				_functionCaptchaImplConfiguration.captchaName() +
+					" encountered an error: " + sb.toString());
+
+			throw new CaptchaConfigurationException();
+		}
+		catch (Exception exception) {
+			_log.error(
+				_functionCaptchaImplConfiguration.captchaName() +
+					" did not return a valid result.",
+				exception);
+
+			throw new CaptchaConfigurationException();
+		}
+	}
+
+	@Override
+	protected boolean validateChallenge(PortletRequest portletRequest)
+		throws CaptchaException {
+
+		return validateChallenge(
+			PortalUtil.getHttpServletRequest(portletRequest));
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(
@@ -122,10 +251,24 @@ public class FunctionCaptchaImpl extends SimpleCaptchaImpl {
 	@Reference
 	private CETManager _cetManager;
 
+	private long _companyId;
+
+	@Reference
+	private CompanyLocalService _companyLocalService;
+
 	private volatile FunctionCaptchaImplConfiguration
 		_functionCaptchaImplConfiguration;
 
 	@Reference
 	private GroupLocalService _groupLocalService;
+
+	@Reference
+	private JSONFactory _jsonFactory;
+
+	@Reference
+	private PortalCatapult _portalCatapult;
+
+	@Reference
+	private UserLocalService _userLocalService;
 
 }
