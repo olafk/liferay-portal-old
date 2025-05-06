49a256f1ef94913f1e6608e0b740aa60739eea1f
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2025-05-06 09:42:13 +0200

    LPD-54517 Move the code to create the display page template when getting the URL if it does not exist, so it applies when editing the entries too, no logic changes

--- modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/util/ActionUtil.java
+++ modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/util/ActionUtil.java
@@ -5,8 +5,24 @@
 
 package com.liferay.site.cms.site.initializer.internal.util;
 
+import com.liferay.fragment.listener.FragmentEntryLinkListener;
+import com.liferay.fragment.listener.FragmentEntryLinkListenerRegistry;
+import com.liferay.fragment.model.FragmentEntryLink;
+import com.liferay.layout.constants.LayoutTypeSettingsConstants;
+import com.liferay.layout.manager.FormManager;
+import com.liferay.layout.page.template.constants.LayoutPageTemplateEntryTypeConstants;
+import com.liferay.layout.page.template.model.LayoutPageTemplateEntry;
+import com.liferay.layout.page.template.model.LayoutPageTemplateStructure;
+import com.liferay.layout.page.template.service.LayoutPageTemplateEntryLocalServiceUtil;
+import com.liferay.layout.page.template.service.LayoutPageTemplateStructureLocalServiceUtil;
+import com.liferay.layout.util.structure.ContainerStyledLayoutStructureItem;
+import com.liferay.layout.util.structure.FormStyledLayoutStructureItem;
+import com.liferay.layout.util.structure.LayoutStructure;
+import com.liferay.object.model.ObjectDefinition;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Group;
@@ -16,8 +32,22 @@ import com.liferay.portal.kernel.portlet.FriendlyURLResolver;
 import com.liferay.portal.kernel.portlet.FriendlyURLResolverRegistryUtil;
 import com.liferay.portal.kernel.portlet.constants.FriendlyURLResolverConstants;
 import com.liferay.portal.kernel.service.GroupLocalServiceUtil;
+import com.liferay.portal.kernel.service.LayoutLocalServiceUtil;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextFactory;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.UnicodeProperties;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.segments.service.SegmentsExperienceLocalServiceUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author Eudaldo Alonso
@@ -25,12 +55,27 @@ import com.liferay.portal.kernel.util.PortalUtil;
 public class ActionUtil {
 
 	public static String getEditURL(
-		String classNameId, String id, Layout layout,
-		ThemeDisplay themeDisplay) {
+		FormManager formManager,
+		FragmentEntryLinkListenerRegistry fragmentEntryLinkListenerRegistry,
+		HttpServletRequest httpServletRequest, String id,
+		ObjectDefinition objectDefinition) {
 
 		try {
+			long classNameId = PortalUtil.getClassNameId(
+				objectDefinition.getClassName());
+
+			ThemeDisplay themeDisplay =
+				(ThemeDisplay)httpServletRequest.getAttribute(
+					WebKeys.THEME_DISPLAY);
+
 			Group group = GroupLocalServiceUtil.getGroup(
 				themeDisplay.getCompanyId(), GroupConstants.CMS);
+
+			Layout layout = _getLayout(
+				classNameId, formManager, fragmentEntryLinkListenerRegistry,
+				group, objectDefinition,
+				ParamUtil.getLong(httpServletRequest, "plid"),
+				ServiceContextFactory.getInstance(httpServletRequest));
 
 			return PortalUtil.addPreservedParameters(
 				themeDisplay,
@@ -48,6 +93,165 @@ public class ActionUtil {
 		}
 
 		return StringPool.BLANK;
+	}
+
+	private static LayoutPageTemplateEntry _addDefaultLayoutPageTemplateEntry(
+			long classNameId, FormManager formManager,
+			FragmentEntryLinkListenerRegistry fragmentEntryLinkListenerRegistry,
+			long groupId, String name, long plid, ServiceContext serviceContext)
+		throws Exception {
+
+		LayoutPageTemplateEntry layoutPageTemplateEntry =
+			LayoutPageTemplateEntryLocalServiceUtil.addLayoutPageTemplateEntry(
+				null, serviceContext.getUserId(), groupId, 0, null, classNameId,
+				0, name, LayoutPageTemplateEntryTypeConstants.DISPLAY_PAGE, 0,
+				true, 0, 0, 0, WorkflowConstants.STATUS_APPROVED,
+				serviceContext);
+
+		Layout layout = LayoutLocalServiceUtil.getLayout(
+			layoutPageTemplateEntry.getPlid());
+
+		Layout draftLayout = layout.fetchDraftLayout();
+
+		LayoutPageTemplateStructure layoutPageTemplateStructure =
+			LayoutPageTemplateStructureLocalServiceUtil.
+				fetchLayoutPageTemplateStructure(
+					draftLayout.getGroupId(), draftLayout.getPlid());
+
+		if (layoutPageTemplateStructure == null) {
+			return layoutPageTemplateEntry;
+		}
+
+		long segmentsExperienceId =
+			SegmentsExperienceLocalServiceUtil.fetchDefaultSegmentsExperienceId(
+				draftLayout.getPlid());
+
+		LayoutStructure layoutStructure = LayoutStructure.of(
+			layoutPageTemplateStructure.getData(segmentsExperienceId));
+
+		ContainerStyledLayoutStructureItem
+			parentContainerStyledLayoutStructureItem =
+				(ContainerStyledLayoutStructureItem)
+					layoutStructure.addContainerStyledLayoutStructureItem(
+						layoutStructure.getMainItemId(), 0);
+
+		parentContainerStyledLayoutStructureItem.updateItemConfig(
+			JSONUtil.put(
+				"styles",
+				JSONUtil.put(
+					"paddingBottom", "6"
+				).put(
+					"paddingTop", "6"
+				)));
+
+		ContainerStyledLayoutStructureItem
+			childContainerStyledLayoutStructureItem =
+				(ContainerStyledLayoutStructureItem)
+					layoutStructure.addContainerStyledLayoutStructureItem(
+						parentContainerStyledLayoutStructureItem.getItemId(),
+						0);
+
+		childContainerStyledLayoutStructureItem.setWidthType("fixed");
+
+		FormStyledLayoutStructureItem formStyledLayoutStructureItem =
+			(FormStyledLayoutStructureItem)
+				layoutStructure.addFormStyledLayoutStructureItem(
+					childContainerStyledLayoutStructureItem.getItemId(), 0);
+
+		formStyledLayoutStructureItem.setClassNameId(
+			layoutPageTemplateEntry.getClassNameId());
+
+		Layout backLayout = LayoutLocalServiceUtil.fetchLayout(plid);
+
+		if (backLayout != null) {
+			formStyledLayoutStructureItem.setSuccessMessageJSONObject(
+				JSONUtil.put(
+					"layout",
+					JSONUtil.put(
+						"groupId", backLayout.getGroupId()
+					).put(
+						"layoutId", backLayout.getLayoutId()
+					).put(
+						"layoutUuid", backLayout.getUuid()
+					).put(
+						"private", backLayout.isPrivateLayout()
+					).put(
+						"title", backLayout.getTitle()
+					)
+				).put(
+					"showNotification", true
+				).put(
+					"type", "page"
+				));
+		}
+
+		List<FragmentEntryLink> addedFragmentEntryLinks = new ArrayList<>();
+
+		formManager.addFragmentEntryLinksLayoutStructureItems(
+			addedFragmentEntryLinks, JSONFactoryUtil.createJSONObject(),
+			formStyledLayoutStructureItem, true, draftLayout, layoutStructure,
+			LocaleUtil.getMostRelevantLocale(), segmentsExperienceId,
+			serviceContext, null);
+
+		LayoutPageTemplateStructureLocalServiceUtil.
+			updateLayoutPageTemplateStructureData(
+				draftLayout.getGroupId(), draftLayout.getPlid(),
+				segmentsExperienceId, layoutStructure.toString());
+
+		for (FragmentEntryLink addedFragmentEntryLink :
+				addedFragmentEntryLinks) {
+
+			for (FragmentEntryLinkListener fragmentEntryLinkListener :
+					fragmentEntryLinkListenerRegistry.
+						getFragmentEntryLinkListeners()) {
+
+				fragmentEntryLinkListener.onAddFragmentEntryLink(
+					addedFragmentEntryLink);
+			}
+		}
+
+		LayoutLocalServiceUtil.copyLayoutContent(draftLayout, layout);
+
+		draftLayout = LayoutLocalServiceUtil.getLayout(draftLayout.getPlid());
+
+		draftLayout.setStatus(WorkflowConstants.STATUS_APPROVED);
+
+		draftLayout = LayoutLocalServiceUtil.updateLayout(draftLayout);
+
+		UnicodeProperties typeSettingsUnicodeProperties =
+			draftLayout.getTypeSettingsProperties();
+
+		typeSettingsUnicodeProperties.put(
+			LayoutTypeSettingsConstants.KEY_PUBLISHED, Boolean.TRUE.toString());
+
+		LayoutLocalServiceUtil.updateLayout(
+			layout.getGroupId(), layout.isPrivateLayout(), layout.getLayoutId(),
+			typeSettingsUnicodeProperties.toString());
+
+		return layoutPageTemplateEntry;
+	}
+
+	private static Layout _getLayout(
+			long classNameId, FormManager formManager,
+			FragmentEntryLinkListenerRegistry fragmentEntryLinkListenerRegistry,
+			Group group, ObjectDefinition objectDefinition, long plid,
+			ServiceContext serviceContext)
+		throws Exception {
+
+		LayoutPageTemplateEntry layoutPageTemplateEntry =
+			LayoutPageTemplateEntryLocalServiceUtil.
+				fetchDefaultLayoutPageTemplateEntry(
+					group.getGroupId(), classNameId, 0);
+
+		if (layoutPageTemplateEntry == null) {
+			layoutPageTemplateEntry = _addDefaultLayoutPageTemplateEntry(
+				classNameId, formManager, fragmentEntryLinkListenerRegistry,
+				group.getGroupId(), objectDefinition.getName(), plid,
+				serviceContext);
+		}
+
+		return LayoutLocalServiceUtil.fetchLayout(
+			layoutPageTemplateEntry.getPlid());
 	}
 
 	private static String _getURLSeparator() {
