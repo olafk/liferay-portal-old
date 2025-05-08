f9251cba724af9bb7571bc15fce369f3d5d3e8ed
Aildson Ferreira <aildson.ferreira@liferay.com>
2025-05-08 17:37:42 -0300

    LPD-53336 Implement languages options to dropdown

--- modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/main/dashboard/components/LanguagesDropdown.tsx
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/main/dashboard/components/LanguagesDropdown.tsx
@@ -8,15 +8,45 @@ import React, {useContext} from 'react';
 import {ViewDashboardContext} from '../ViewDashboardContext';
 import {FilterDropdown} from './FilterDropdown';
 
+type AvailableLocales = Exclude<
+	Liferay.Language.Locale,
+	'zh_Hans_CN' | 'zh_Hant_TW' | 'zh_TW'
+>;
+
+const localizations: Record<AvailableLocales, string> = {
+	ar_SA: Liferay.Language.get('language.ar'),
+	ca_ES: Liferay.Language.get('language.ca'),
+	de_DE: Liferay.Language.get('language.de'),
+	en_US: Liferay.Language.get('language.en'),
+	es_ES: Liferay.Language.get('language.es'),
+	fi_FI: Liferay.Language.get('language.fi'),
+	fr_FR: Liferay.Language.get('language.fr'),
+	hu_HU: Liferay.Language.get('language.hu'),
+	ja_JP: Liferay.Language.get('language.ja'),
+	nl_NL: Liferay.Language.get('language.nl'),
+	pt_BR: Liferay.Language.get('language.pt_BR'),
+	sv_SE: Liferay.Language.get('language.sv'),
+	zh_CN: Liferay.Language.get('language.zh_CN'),
+};
+
+/**
+ * Must update the code below to iterate through the collection
+ * of languages used by the assets. Expect to be represented as
+ * an array.
+ */
+const availableLanguages = Object.entries(localizations).map(
+	([locale, translation]) => ({
+		label: translation,
+		value: locale,
+	})
+);
+
 const languages = [
 	{
 		label: Liferay.Language.get('all-languages'),
 		value: 'all',
 	},
-	{
-		label: Liferay.Language.get('language-02'),
-		value: 'language02',
-	},
+	...availableLanguages,
 ];
 
 const LanguagesDropdown: React.FC<React.HTMLAttributes<HTMLElement>> = ({
