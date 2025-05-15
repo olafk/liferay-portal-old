fc6be847e31a0b82ff8f541e72a0077e02544f21
Keven Leone <keven.santos.sz@gmail.com>
2025-05-15 18:21:00 -0300

    LPD-54792 Minor fixes to Components and Imports

--- workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/components/AppPublish/Navbar/index.tsx
+++ workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/components/AppPublish/Navbar/index.tsx
@@ -3,18 +3,21 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
+import {ComponentProps} from 'react';
+import classNames from 'classnames';
 import ClayButton, {ClayButtonWithIcon} from '@clayui/button';
 import ClayIcon from '@clayui/icon';
 import ClayManagementToolbar from '@clayui/management-toolbar';
-import classNames from 'classnames';
-import {ComponentProps} from 'react';
+
+import i18n from '../../../i18n';
+import {getAccountImage} from '../../../utils/util';
+
+import './index.scss';
 
 import {
 	ProductWorkflowStatusCode,
 	ProductWorkflowStatusLabel,
 } from '../../../enums/Product';
-import i18n from '../../../i18n';
-import {getAccountImage} from '../../../utils/util';
 
 import './index.scss';
 
