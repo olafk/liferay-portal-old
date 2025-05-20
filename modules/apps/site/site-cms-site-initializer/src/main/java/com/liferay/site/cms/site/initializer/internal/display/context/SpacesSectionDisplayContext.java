liferay-content-management/liferay-portal

LPD-53429 Use the new methods and reformat to make it testable using integration tests

modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/display/context/SpacesSectionDisplayContext.java
@@ -1,0 +1,93 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.site.cms.site.initializer.internal.display.context;
+
+import com.liferay.headless.asset.library.dto.v1_0.AssetLibrary;
+import com.liferay.headless.asset.library.resource.v1_0.AssetLibraryResource;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.vulcan.pagination.Page;
+import com.liferay.portal.vulcan.pagination.Pagination;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Roberto Díaz
+ */
+public class SpacesSectionDisplayContext {
+
+	public SpacesSectionDisplayContext(
+		HttpServletRequest httpServletRequest,
+		AssetLibraryResource.Factory assetLibraryResourceFactory) {
+
+		_assetLibraryResourceFactory = assetLibraryResourceFactory;
+
+		_themeDisplay = (ThemeDisplay)httpServletRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+	}
+
+	public Page<AssetLibrary> getPage() throws Exception {
+		AssetLibraryResource.Builder builder =
+			_assetLibraryResourceFactory.create();
+
+		AssetLibraryResource assetLibraryResource = builder.user(
+			_themeDisplay.getUser()
+		).build();
+
+		Page<AssetLibrary> assetLibrariesPage =
+			assetLibraryResource.getAssetLibrariesPage(
+				null, null, null, Pagination.of(1, 5), null);
+
+		return Page.of(
+			assetLibrariesPage.getActions(),
+			_getAssetLibraries(
+				assetLibraryResource.getAssetLibrariesPinnedByMePage(
+					Pagination.of(1, 5)),
+				assetLibrariesPage),
+			Pagination.of(1, 5), assetLibrariesPage.getTotalCount());
+	}
+
+	private Collection<AssetLibrary> _getAssetLibraries(
+		Page<AssetLibrary> assetLibrariesPinnedByMePage,
+		Page<AssetLibrary> assetLibrariesPage) {
+
+		if (assetLibrariesPage.getTotalCount() == 0) {
+			Collections.emptyList();
+		}
+
+		if (assetLibrariesPinnedByMePage.getTotalCount() == 5) {
+			return assetLibrariesPinnedByMePage.getItems();
+		}
+
+		List<AssetLibrary> assetLibrariesPinnedByMe = new ArrayList<>(
+			assetLibrariesPinnedByMePage.getItems());
+
+		List<Long> assetLibraryPinnedByMeIds = ListUtil.toList(
+			assetLibrariesPinnedByMe, assetLibrary -> assetLibrary.getId());
+
+		for (AssetLibrary assetLibrary : assetLibrariesPage.getItems()) {
+			if (!assetLibraryPinnedByMeIds.contains(assetLibrary.getId())) {
+				assetLibrariesPinnedByMe.add(assetLibrary);
+			}
+
+			if (assetLibrariesPinnedByMe.size() == 5) {
+				return assetLibrariesPinnedByMe;
+			}
+		}
+
+		return assetLibrariesPinnedByMe;
+	}
+
+	private final AssetLibraryResource.Factory _assetLibraryResourceFactory;
+	private final ThemeDisplay _themeDisplay;
+
+}

olafk/liferay-portal-old

modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/display/context/SpacesSectionDisplayContext.java
@@ -1,0 +1,93 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.site.cms.site.initializer.internal.display.context;
+
+import com.liferay.headless.asset.library.dto.v1_0.AssetLibrary;
+import com.liferay.headless.asset.library.resource.v1_0.AssetLibraryResource;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.vulcan.pagination.Page;
+import com.liferay.portal.vulcan.pagination.Pagination;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Roberto Díaz
+ */
+public class SpacesSectionDisplayContext {
+
+	public SpacesSectionDisplayContext(
+		HttpServletRequest httpServletRequest,
+		AssetLibraryResource.Factory assetLibraryResourceFactory) {
+
+		_assetLibraryResourceFactory = assetLibraryResourceFactory;
+
+		_themeDisplay = (ThemeDisplay)httpServletRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+	}
+
+	public Page<AssetLibrary> getPage() throws Exception {
+		AssetLibraryResource.Builder builder =
+			_assetLibraryResourceFactory.create();
+
+		AssetLibraryResource assetLibraryResource = builder.user(
+			_themeDisplay.getUser()
+		).build();
+
+		Page<AssetLibrary> assetLibrariesPage =
+			assetLibraryResource.getAssetLibrariesPage(
+				null, null, null, Pagination.of(1, 5), null);
+
+		return Page.of(
+			assetLibrariesPage.getActions(),
+			_getAssetLibraries(
+				assetLibraryResource.getAssetLibrariesPinnedByMePage(
+					Pagination.of(1, 5)),
+				assetLibrariesPage),
+			Pagination.of(1, 5), assetLibrariesPage.getTotalCount());
+	}
+
+	private Collection<AssetLibrary> _getAssetLibraries(
+		Page<AssetLibrary> pinnedByMeAssetLibrariesPage,
+		Page<AssetLibrary> assetLibrariesPage) {
+
+		if (assetLibrariesPage.getTotalCount() == 0) {
+			Collections.emptyList();
+		}
+
+		if (pinnedByMeAssetLibrariesPage.getTotalCount() == 5) {
+			return pinnedByMeAssetLibrariesPage.getItems();
+		}
+
+		List<AssetLibrary> assetLibraries = new ArrayList<>(
+			pinnedByMeAssetLibrariesPage.getItems());
+
+		List<Long> assetLibraryIds = ListUtil.toList(
+			assetLibraries, assetLibrary -> assetLibrary.getId());
+
+		for (AssetLibrary assetLibrary : assetLibrariesPage.getItems()) {
+			if (!assetLibraryIds.contains(assetLibrary.getId())) {
+				assetLibraries.add(assetLibrary);
+			}
+
+			if (assetLibraries.size() == 5) {
+				return assetLibraries;
+			}
+		}
+
+		return assetLibraries;
+	}
+
+	private final AssetLibraryResource.Factory _assetLibraryResourceFactory;
+	private final ThemeDisplay _themeDisplay;
+
+}