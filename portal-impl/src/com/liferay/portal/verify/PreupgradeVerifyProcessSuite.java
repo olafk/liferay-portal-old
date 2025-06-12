javalosjr96/liferay-portal

LPD-53152 add PreupgradeVerifyDatabaseState

portal-impl/src/com/liferay/portal/verify/PreupgradeVerifyProcessSuite.java
@@ -14,6 +14,7 @@ public class PreupgradeVerifyProcessSuite extends PreupgradeVerifyProcess {
 	public void doVerify() throws Exception {
 		verify(new PreupgradeVerifyDefaultUsers());
 		verify(new PreupgradeVerifyProperties());
+		verify(new PreupgradeVerifyDatabaseState());
 	}
 
 	@Override

olafk/liferay-portal-old

portal-impl/src/com/liferay/portal/verify/PreupgradeVerifyProcessSuite.java
@@ -23,6 +23,7 @@ public class PreupgradeVerifyProcessSuite extends PreupgradeVerifyProcess {
 	public void doVerify() throws Exception {
 		_verify(new PreupgradeVerifyCompanyUsers());
 		_verify(new PreupgradeVerifyDatabaseCharacterSet());
+		_verify(new PreupgradeVerifyDatabaseState());
 		_verify(new PreupgradeVerifyProperties());
 
 		if (ListUtil.isNotEmpty(_exceptionMessages)) {