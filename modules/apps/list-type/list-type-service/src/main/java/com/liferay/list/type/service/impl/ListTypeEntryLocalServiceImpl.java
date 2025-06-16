e0234cc1282df248075a45a1752f6f195c9769b6
Alberto Sousa <alberto.sousa@liferay.com>
2025-06-16 15:58:57 -0300

    LPD-58185 Copy key to incomplete entry name

--- modules/apps/list-type/list-type-service/src/main/java/com/liferay/list/type/service/impl/ListTypeEntryLocalServiceImpl.java
+++ modules/apps/list-type/list-type-service/src/main/java/com/liferay/list/type/service/impl/ListTypeEntryLocalServiceImpl.java
@@ -26,6 +26,7 @@ import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.search.Indexable;
 import com.liferay.portal.kernel.search.IndexableType;
 import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.OrderByComparator;
 import com.liferay.portal.kernel.util.Validator;
@@ -235,7 +236,10 @@ public class ListTypeEntryLocalServiceImpl
 
 		return _addListTypeEntry(
 			null, _userLocalService.getUser(userId), listTypeDefinitionId, key,
-			null, WorkflowConstants.STATUS_INCOMPLETE, false);
+			HashMapBuilder.put(
+				LocaleUtil.getSiteDefault(), key
+			).build(),
+			WorkflowConstants.STATUS_INCOMPLETE, false);
 	}
 
 	@Indexable(type = IndexableType.REINDEX)
