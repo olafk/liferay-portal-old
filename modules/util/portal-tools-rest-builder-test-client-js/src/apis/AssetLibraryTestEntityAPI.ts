liferay-headless/liferay-portal

LPD-49422 buildREST

modules/util/portal-tools-rest-builder-test-client-js/src/apis/AssetLibraryTestEntityAPI.ts
@@ -1,0 +1,142 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import {ObjectSerializer} from '../utils/SerDes';
+
+		import {PageAssetLibraryTestEntity} from '../models/PageAssetLibraryTestEntity';
+
+/**
+ * @author Alejandro Tardín
+ * @generated
+ */
+
+export class AssetLibraryTestEntityAPI {
+	protected _basePath: string;
+	protected _defaultHeaders: any = {};
+
+	constructor(basePath?: string) {
+		if (basePath) {
+			this._basePath = basePath;
+		}
+	}
+
+	set defaultHeaders(defaultHeaders: any) {
+		this._defaultHeaders = defaultHeaders;
+	}
+
+		/**
+		 * 
+				 * @param assetLibraryId
+				 * @param assetLibraryTestEntityExternalReferenceCode
+		 * @param headers Optional custom request headers
+		 */
+		public async deleteAssetLibraryAssetLibraryTestEntityByExternalReferenceCodeAssetLibraryTestEntityExternalReferenceCode(
+						assetLibraryId: number,
+						assetLibraryTestEntityExternalReferenceCode: string,
+			headers?: {[name: string]: string},
+		): Promise<{
+				body?: any;
+			response: Response;
+		}> {
+
+			const path = this._basePath + "/test/v1.0/asset-libraries/{assetLibraryId}/asset-library-test-entities/by-external-reference-code/{assetLibraryTestEntityExternalReferenceCode}"
+						.replace("{assetLibraryId}",encodeURIComponent(assetLibraryId))
+										.replace("{assetLibraryTestEntityExternalReferenceCode}",encodeURIComponent(assetLibraryTestEntityExternalReferenceCode))
+				;
+
+			const queryParameters: any = {};
+
+						if (assetLibraryId === null || assetLibraryId === undefined) {
+							throw new Error("Required parameter assetLibraryId was null or undefined when calling deleteAssetLibraryAssetLibraryTestEntityByExternalReferenceCodeAssetLibraryTestEntityExternalReferenceCode.");
+						}
+
+						if (assetLibraryTestEntityExternalReferenceCode === null || assetLibraryTestEntityExternalReferenceCode === undefined) {
+							throw new Error("Required parameter assetLibraryTestEntityExternalReferenceCode was null or undefined when calling deleteAssetLibraryAssetLibraryTestEntityByExternalReferenceCodeAssetLibraryTestEntityExternalReferenceCode.");
+						}
+
+			const queryString = Object.keys(queryParameters).length ?
+				"?" + new URLSearchParams(queryParameters).toString() :
+					"";
+
+			const response = await fetch(path + queryString, {
+				headers:
+					Object.assign({}, this._defaultHeaders
+						,{
+								Accept: "application/json"
+						}
+					,headers || {}
+					),
+				method: "DELETE",
+			});
+
+			if (response.ok) {
+				const contentType = response.headers.get("content-type") || "";
+
+					if (contentType.includes("application/json")) {
+						return {body: await response.json(), response};
+					}
+					else {
+						return {body: await response.text(), response};
+					}
+			}
+			else {
+				throw new Error("HTTP Error " + response.status + ": " + response.statusText + ". " + await response.text());
+			}
+		}
+
+		/**
+		 * 
+				 * @param assetLibraryId
+		 * @param headers Optional custom request headers
+		 */
+		public async getAssetLibraryAssetLibraryTestEntitiesPage(
+						assetLibraryId: number,
+			headers?: {[name: string]: string},
+		): Promise<{
+				body: PageAssetLibraryTestEntity;
+			response: Response;
+		}> {
+
+			const path = this._basePath + "/test/v1.0/asset-libraries/{assetLibraryId}/asset-library-test-entities"
+						.replace("{assetLibraryId}",encodeURIComponent(assetLibraryId))
+				;
+
+			const queryParameters: any = {};
+
+						if (assetLibraryId === null || assetLibraryId === undefined) {
+							throw new Error("Required parameter assetLibraryId was null or undefined when calling getAssetLibraryAssetLibraryTestEntitiesPage.");
+						}
+
+			const queryString = Object.keys(queryParameters).length ?
+				"?" + new URLSearchParams(queryParameters).toString() :
+					"";
+
+			const response = await fetch(path + queryString, {
+				headers:
+					Object.assign({}, this._defaultHeaders
+						,{
+								Accept: "application/json"
+						}
+					,headers || {}
+					),
+				method: "GET",
+			});
+
+			if (response.ok) {
+				const contentType = response.headers.get("content-type") || "";
+
+					if (contentType.includes("application/json")) {
+						return {body: ObjectSerializer.deserialize(await response.json(), "PageAssetLibraryTestEntity"), response};
+					}
+					else {
+						return {body: await response.text() as any, response};
+					}
+			}
+			else {
+				throw new Error("HTTP Error " + response.status + ": " + response.statusText + ". " + await response.text());
+			}
+		}
+
+}

modules/util/portal-tools-rest-builder-test-client/src/main/java/com/liferay/portal/tools/rest/builder/test/client/dto/v1_0/AssetLibraryTestEntity.java
@@ -1,0 +1,198 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.tools.rest.builder.test.client.dto.v1_0;
+
+import com.liferay.portal.tools.rest.builder.test.client.function.UnsafeSupplier;
+import com.liferay.portal.tools.rest.builder.test.client.serdes.v1_0.AssetLibraryTestEntitySerDes;
+
+import java.io.Serializable;
+
+import java.util.Date;
+import java.util.Objects;
+
+import javax.annotation.Generated;
+
+/**
+ * @author Alejandro Tardín
+ * @generated
+ */
+@Generated("")
+public class AssetLibraryTestEntity implements Cloneable, Serializable {
+
+	public static AssetLibraryTestEntity toDTO(String json) {
+		return AssetLibraryTestEntitySerDes.toDTO(json);
+	}
+
+	public Long getAssetLibraryId() {
+		return assetLibraryId;
+	}
+
+	public void setAssetLibraryId(Long assetLibraryId) {
+		this.assetLibraryId = assetLibraryId;
+	}
+
+	public void setAssetLibraryId(
+		UnsafeSupplier<Long, Exception> assetLibraryIdUnsafeSupplier) {
+
+		try {
+			assetLibraryId = assetLibraryIdUnsafeSupplier.get();
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	protected Long assetLibraryId;
+
+	public Date getDateCreated() {
+		return dateCreated;
+	}
+
+	public void setDateCreated(Date dateCreated) {
+		this.dateCreated = dateCreated;
+	}
+
+	public void setDateCreated(
+		UnsafeSupplier<Date, Exception> dateCreatedUnsafeSupplier) {
+
+		try {
+			dateCreated = dateCreatedUnsafeSupplier.get();
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	protected Date dateCreated;
+
+	public Date getDateModified() {
+		return dateModified;
+	}
+
+	public void setDateModified(Date dateModified) {
+		this.dateModified = dateModified;
+	}
+
+	public void setDateModified(
+		UnsafeSupplier<Date, Exception> dateModifiedUnsafeSupplier) {
+
+		try {
+			dateModified = dateModifiedUnsafeSupplier.get();
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	protected Date dateModified;
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public void setDescription(
+		UnsafeSupplier<String, Exception> descriptionUnsafeSupplier) {
+
+		try {
+			description = descriptionUnsafeSupplier.get();
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	protected String description;
+
+	public String getExternalReferenceCode() {
+		return externalReferenceCode;
+	}
+
+	public void setExternalReferenceCode(String externalReferenceCode) {
+		this.externalReferenceCode = externalReferenceCode;
+	}
+
+	public void setExternalReferenceCode(
+		UnsafeSupplier<String, Exception> externalReferenceCodeUnsafeSupplier) {
+
+		try {
+			externalReferenceCode = externalReferenceCodeUnsafeSupplier.get();
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	protected String externalReferenceCode;
+
+	public
+		com.liferay.portal.tools.rest.builder.test.client.permission.
+			Permission[] getPermissions() {
+
+		return permissions;
+	}
+
+	public void setPermissions(
+		com.liferay.portal.tools.rest.builder.test.client.permission.
+			Permission[] permissions) {
+
+		this.permissions = permissions;
+	}
+
+	public void setPermissions(
+		UnsafeSupplier
+			<com.liferay.portal.tools.rest.builder.test.client.permission.
+				Permission[],
+			 Exception> permissionsUnsafeSupplier) {
+
+		try {
+			permissions = permissionsUnsafeSupplier.get();
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	protected
+		com.liferay.portal.tools.rest.builder.test.client.permission.
+			Permission[] permissions;
+
+	@Override
+	public AssetLibraryTestEntity clone() throws CloneNotSupportedException {
+		return (AssetLibraryTestEntity)super.clone();
+	}
+
+	@Override
+	public boolean equals(Object object) {
+		if (this == object) {
+			return true;
+		}
+
+		if (!(object instanceof AssetLibraryTestEntity)) {
+			return false;
+		}
+
+		AssetLibraryTestEntity assetLibraryTestEntity =
+			(AssetLibraryTestEntity)object;
+
+		return Objects.equals(toString(), assetLibraryTestEntity.toString());
+	}
+
+	@Override
+	public int hashCode() {
+		String string = toString();
+
+		return string.hashCode();
+	}
+
+	public String toString() {
+		return AssetLibraryTestEntitySerDes.toJSON(this);
+	}
+
+}

modules/util/portal-tools-rest-builder-test-impl/src/main/java/com/liferay/portal/tools/rest/builder/test/internal/resource/v1_0/BaseSiteTestEntityResourceImpl.java
@@ -89,6 +89,46 @@ public abstract class BaseSiteTestEntityResourceImpl
 			   VulcanBatchEngineTaskItemDelegate<SiteTestEntity>,
 			   VulcanCRUDItemDelegate<SiteTestEntity> {
 
+	/**
+	 * Invoke this method with the command line:
+	 *
+	 * curl -X 'DELETE' 'http://localhost:8080/o/test/v1.0/sites/{siteId}/site-test-entities/by-external-reference-code/{externalReferenceCode}'  -u 'test@liferay.com:test'
+	 */
+	@io.swagger.v3.oas.annotations.Parameters(
+		value = {
+			@io.swagger.v3.oas.annotations.Parameter(
+				in = io.swagger.v3.oas.annotations.enums.ParameterIn.PATH,
+				name = "externalReferenceCode"
+			),
+			@io.swagger.v3.oas.annotations.Parameter(
+				in = io.swagger.v3.oas.annotations.enums.ParameterIn.PATH,
+				name = "siteId"
+			)
+		}
+	)
+	@io.swagger.v3.oas.annotations.tags.Tags(
+		value = {
+			@io.swagger.v3.oas.annotations.tags.Tag(name = "SiteTestEntity")
+		}
+	)
+	@javax.ws.rs.DELETE
+	@javax.ws.rs.Path(
+		"/sites/{siteId}/site-test-entities/by-external-reference-code/{externalReferenceCode}"
+	)
+	@javax.ws.rs.Produces({"application/json", "application/xml"})
+	@Override
+	public void deleteSiteSiteTestEntityByExternalReferenceCode(
+			@io.swagger.v3.oas.annotations.Parameter(hidden = true)
+			@javax.validation.constraints.NotNull
+			@javax.ws.rs.PathParam("externalReferenceCode")
+			String externalReferenceCode,
+			@io.swagger.v3.oas.annotations.Parameter(hidden = true)
+			@javax.validation.constraints.NotNull
+			@javax.ws.rs.PathParam("siteId")
+			Long siteId)
+		throws Exception {
+	}
+
 	protected abstract Page<SiteTestEntity> doGetSiteSiteTestEntitiesPage(
 			Long siteId)
 		throws Exception;

olafk/liferay-portal-old

modules/util/portal-tools-rest-builder-test-client-js/src/apis/AssetLibraryTestEntityAPI.ts
@@ -1,0 +1,142 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import {ObjectSerializer} from '../utils/SerDes';
+
+		import {PageAssetLibraryTestEntity} from '../models/PageAssetLibraryTestEntity';
+
+/**
+ * @author Alejandro Tardín
+ * @generated
+ */
+
+export class AssetLibraryTestEntityAPI {
+	protected _basePath: string;
+	protected _defaultHeaders: any = {};
+
+	constructor(basePath?: string) {
+		if (basePath) {
+			this._basePath = basePath;
+		}
+	}
+
+	set defaultHeaders(defaultHeaders: any) {
+		this._defaultHeaders = defaultHeaders;
+	}
+
+		/**
+		 * 
+				 * @param assetLibraryId
+				 * @param externalReferenceCode
+		 * @param headers Optional custom request headers
+		 */
+		public async deleteAssetLibraryAssetLibraryTestEntityByExternalReferenceCode(
+						assetLibraryId: number,
+						externalReferenceCode: string,
+			headers?: {[name: string]: string},
+		): Promise<{
+				body?: any;
+			response: Response;
+		}> {
+
+			const path = this._basePath + "/test/v1.0/asset-libraries/{assetLibraryId}/asset-library-test-entities/by-external-reference-code/{externalReferenceCode}"
+						.replace("{assetLibraryId}",encodeURIComponent(assetLibraryId))
+										.replace("{externalReferenceCode}",encodeURIComponent(externalReferenceCode))
+				;
+
+			const queryParameters: any = {};
+
+						if (assetLibraryId === null || assetLibraryId === undefined) {
+							throw new Error("Required parameter assetLibraryId was null or undefined when calling deleteAssetLibraryAssetLibraryTestEntityByExternalReferenceCode.");
+						}
+
+						if (externalReferenceCode === null || externalReferenceCode === undefined) {
+							throw new Error("Required parameter externalReferenceCode was null or undefined when calling deleteAssetLibraryAssetLibraryTestEntityByExternalReferenceCode.");
+						}
+
+			const queryString = Object.keys(queryParameters).length ?
+				"?" + new URLSearchParams(queryParameters).toString() :
+					"";
+
+			const response = await fetch(path + queryString, {
+				headers:
+					Object.assign({}, this._defaultHeaders
+						,{
+								Accept: "application/json"
+						}
+					,headers || {}
+					),
+				method: "DELETE",
+			});
+
+			if (response.ok) {
+				const contentType = response.headers.get("content-type") || "";
+
+					if (contentType.includes("application/json")) {
+						return {body: await response.json(), response};
+					}
+					else {
+						return {body: await response.text(), response};
+					}
+			}
+			else {
+				throw new Error("HTTP Error " + response.status + ": " + response.statusText + ". " + await response.text());
+			}
+		}
+
+		/**
+		 * 
+				 * @param assetLibraryId
+		 * @param headers Optional custom request headers
+		 */
+		public async getAssetLibraryAssetLibraryTestEntitiesPage(
+						assetLibraryId: number,
+			headers?: {[name: string]: string},
+		): Promise<{
+				body: PageAssetLibraryTestEntity;
+			response: Response;
+		}> {
+
+			const path = this._basePath + "/test/v1.0/asset-libraries/{assetLibraryId}/asset-library-test-entities"
+						.replace("{assetLibraryId}",encodeURIComponent(assetLibraryId))
+				;
+
+			const queryParameters: any = {};
+
+						if (assetLibraryId === null || assetLibraryId === undefined) {
+							throw new Error("Required parameter assetLibraryId was null or undefined when calling getAssetLibraryAssetLibraryTestEntitiesPage.");
+						}
+
+			const queryString = Object.keys(queryParameters).length ?
+				"?" + new URLSearchParams(queryParameters).toString() :
+					"";
+
+			const response = await fetch(path + queryString, {
+				headers:
+					Object.assign({}, this._defaultHeaders
+						,{
+								Accept: "application/json"
+						}
+					,headers || {}
+					),
+				method: "GET",
+			});
+
+			if (response.ok) {
+				const contentType = response.headers.get("content-type") || "";
+
+					if (contentType.includes("application/json")) {
+						return {body: ObjectSerializer.deserialize(await response.json(), "PageAssetLibraryTestEntity"), response};
+					}
+					else {
+						return {body: await response.text() as any, response};
+					}
+			}
+			else {
+				throw new Error("HTTP Error " + response.status + ": " + response.statusText + ". " + await response.text());
+			}
+		}
+
+}

modules/util/portal-tools-rest-builder-test-client/src/main/java/com/liferay/portal/tools/rest/builder/test/client/dto/v1_0/AssetLibraryTestEntity.java
@@ -1,0 +1,198 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.tools.rest.builder.test.client.dto.v1_0;
+
+import com.liferay.portal.tools.rest.builder.test.client.function.UnsafeSupplier;
+import com.liferay.portal.tools.rest.builder.test.client.serdes.v1_0.AssetLibraryTestEntitySerDes;
+
+import jakarta.annotation.Generated;
+
+import java.io.Serializable;
+
+import java.util.Date;
+import java.util.Objects;
+
+/**
+ * @author Alejandro Tardín
+ * @generated
+ */
+@Generated("")
+public class AssetLibraryTestEntity implements Cloneable, Serializable {
+
+	public static AssetLibraryTestEntity toDTO(String json) {
+		return AssetLibraryTestEntitySerDes.toDTO(json);
+	}
+
+	public Long getAssetLibraryId() {
+		return assetLibraryId;
+	}
+
+	public void setAssetLibraryId(Long assetLibraryId) {
+		this.assetLibraryId = assetLibraryId;
+	}
+
+	public void setAssetLibraryId(
+		UnsafeSupplier<Long, Exception> assetLibraryIdUnsafeSupplier) {
+
+		try {
+			assetLibraryId = assetLibraryIdUnsafeSupplier.get();
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	protected Long assetLibraryId;
+
+	public Date getDateCreated() {
+		return dateCreated;
+	}
+
+	public void setDateCreated(Date dateCreated) {
+		this.dateCreated = dateCreated;
+	}
+
+	public void setDateCreated(
+		UnsafeSupplier<Date, Exception> dateCreatedUnsafeSupplier) {
+
+		try {
+			dateCreated = dateCreatedUnsafeSupplier.get();
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	protected Date dateCreated;
+
+	public Date getDateModified() {
+		return dateModified;
+	}
+
+	public void setDateModified(Date dateModified) {
+		this.dateModified = dateModified;
+	}
+
+	public void setDateModified(
+		UnsafeSupplier<Date, Exception> dateModifiedUnsafeSupplier) {
+
+		try {
+			dateModified = dateModifiedUnsafeSupplier.get();
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	protected Date dateModified;
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public void setDescription(
+		UnsafeSupplier<String, Exception> descriptionUnsafeSupplier) {
+
+		try {
+			description = descriptionUnsafeSupplier.get();
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	protected String description;
+
+	public String getExternalReferenceCode() {
+		return externalReferenceCode;
+	}
+
+	public void setExternalReferenceCode(String externalReferenceCode) {
+		this.externalReferenceCode = externalReferenceCode;
+	}
+
+	public void setExternalReferenceCode(
+		UnsafeSupplier<String, Exception> externalReferenceCodeUnsafeSupplier) {
+
+		try {
+			externalReferenceCode = externalReferenceCodeUnsafeSupplier.get();
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	protected String externalReferenceCode;
+
+	public
+		com.liferay.portal.tools.rest.builder.test.client.permission.
+			Permission[] getPermissions() {
+
+		return permissions;
+	}
+
+	public void setPermissions(
+		com.liferay.portal.tools.rest.builder.test.client.permission.
+			Permission[] permissions) {
+
+		this.permissions = permissions;
+	}
+
+	public void setPermissions(
+		UnsafeSupplier
+			<com.liferay.portal.tools.rest.builder.test.client.permission.
+				Permission[],
+			 Exception> permissionsUnsafeSupplier) {
+
+		try {
+			permissions = permissionsUnsafeSupplier.get();
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	protected
+		com.liferay.portal.tools.rest.builder.test.client.permission.
+			Permission[] permissions;
+
+	@Override
+	public AssetLibraryTestEntity clone() throws CloneNotSupportedException {
+		return (AssetLibraryTestEntity)super.clone();
+	}
+
+	@Override
+	public boolean equals(Object object) {
+		if (this == object) {
+			return true;
+		}
+
+		if (!(object instanceof AssetLibraryTestEntity)) {
+			return false;
+		}
+
+		AssetLibraryTestEntity assetLibraryTestEntity =
+			(AssetLibraryTestEntity)object;
+
+		return Objects.equals(toString(), assetLibraryTestEntity.toString());
+	}
+
+	@Override
+	public int hashCode() {
+		String string = toString();
+
+		return string.hashCode();
+	}
+
+	public String toString() {
+		return AssetLibraryTestEntitySerDes.toJSON(this);
+	}
+
+}

modules/util/portal-tools-rest-builder-test-impl/src/main/java/com/liferay/portal/tools/rest/builder/test/internal/resource/v1_0/BaseSiteTestEntityResourceImpl.java
@@ -89,6 +89,46 @@ public abstract class BaseSiteTestEntityResourceImpl
 			   VulcanBatchEngineTaskItemDelegate<SiteTestEntity>,
 			   VulcanCRUDItemDelegate<SiteTestEntity> {
 
+	/**
+	 * Invoke this method with the command line:
+	 *
+	 * curl -X 'DELETE' 'http://localhost:8080/o/test/v1.0/sites/{siteId}/site-test-entities/by-external-reference-code/{externalReferenceCode}'  -u 'test@liferay.com:test'
+	 */
+	@io.swagger.v3.oas.annotations.Parameters(
+		value = {
+			@io.swagger.v3.oas.annotations.Parameter(
+				in = io.swagger.v3.oas.annotations.enums.ParameterIn.PATH,
+				name = "externalReferenceCode"
+			),
+			@io.swagger.v3.oas.annotations.Parameter(
+				in = io.swagger.v3.oas.annotations.enums.ParameterIn.PATH,
+				name = "siteId"
+			)
+		}
+	)
+	@io.swagger.v3.oas.annotations.tags.Tags(
+		value = {
+			@io.swagger.v3.oas.annotations.tags.Tag(name = "SiteTestEntity")
+		}
+	)
+	@jakarta.ws.rs.DELETE
+	@jakarta.ws.rs.Path(
+		"/sites/{siteId}/site-test-entities/by-external-reference-code/{externalReferenceCode}"
+	)
+	@jakarta.ws.rs.Produces({"application/json", "application/xml"})
+	@Override
+	public void deleteSiteSiteTestEntityByExternalReferenceCode(
+			@io.swagger.v3.oas.annotations.Parameter(hidden = true)
+			@jakarta.validation.constraints.NotNull
+			@jakarta.ws.rs.PathParam("externalReferenceCode")
+			String externalReferenceCode,
+			@io.swagger.v3.oas.annotations.Parameter(hidden = true)
+			@jakarta.validation.constraints.NotNull
+			@jakarta.ws.rs.PathParam("siteId")
+			Long siteId)
+		throws Exception {
+	}
+
 	protected abstract Page<SiteTestEntity> doGetSiteSiteTestEntitiesPage(
 			Long siteId)
 		throws Exception;