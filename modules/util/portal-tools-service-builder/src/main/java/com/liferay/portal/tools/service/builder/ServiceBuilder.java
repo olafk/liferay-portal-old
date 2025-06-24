liferay-core-infra/liferay-portal

LPD-56081 SF, simplify

modules/util/portal-tools-service-builder/src/main/java/com/liferay/portal/tools/service/builder/ServiceBuilder.java
@@ -2251,6 +2251,10 @@ public class ServiceBuilder {
 		List<String> pkEntityColumnDBNames, IndexMetadata indexMetadata,
 		boolean optimizeDBIndexes) {
 
+		if (indexMetadata == null) {
+			return;
+		}
+
 		if ((pkEntityColumnDBNames != null) &&
 			!pkEntityColumnDBNames.isEmpty()) {
 
@@ -4221,15 +4225,12 @@ public class ServiceBuilder {
 			List<EntityFinder> entityFinders = entity.getEntityFinders();
 
 			for (EntityFinder entityFinder : entityFinders) {
-				IndexMetadata indexMetadata = _createIndexMetadata(
-					entity, entityFinder, _optimizeDBIndexes);
-
-				if (indexMetadata != null) {
-					_addIndexMetadata(
-						indexMetadatasMap, tableName,
-						entity.getPKEntityColumnDBNames(), indexMetadata,
-						_optimizeDBIndexes);
-				}
+				_addIndexMetadata(
+					indexMetadatasMap, tableName,
+					entity.getPKEntityColumnDBNames(),
+					_createIndexMetadata(
+						entity, entityFinder, _optimizeDBIndexes),
+					_optimizeDBIndexes);
 			}
 
 			indexMetadatas = indexMetadatasMap.get(tableName);
@@ -4242,15 +4243,11 @@ public class ServiceBuilder {
 			for (EntityFinder dynamicEntityFinder :
 					entity.getDynamicEntityFinders()) {
 
-				IndexMetadata dynamicIndexMetadata = _createIndexMetadata(
-					entity, dynamicEntityFinder, false);
-
-				if (dynamicIndexMetadata != null) {
-					_addIndexMetadata(
-						indexMetadatasMap, tableName,
-						entity.getPKEntityColumnDBNames(), dynamicIndexMetadata,
-						false);
-				}
+				_addIndexMetadata(
+					indexMetadatasMap, tableName,
+					entity.getPKEntityColumnDBNames(),
+					_createIndexMetadata(entity, dynamicEntityFinder, false),
+					false);
 			}
 		}
 

olafk/liferay-portal-old

modules/util/portal-tools-service-builder/src/main/java/com/liferay/portal/tools/service/builder/ServiceBuilder.java
@@ -2252,6 +2252,10 @@ public class ServiceBuilder {
 		List<String> pkEntityColumnDBNames, IndexMetadata indexMetadata,
 		boolean optimizeDBIndexes) {
 
+		if (indexMetadata == null) {
+			return;
+		}
+
 		if ((pkEntityColumnDBNames != null) &&
 			!pkEntityColumnDBNames.isEmpty()) {
 
@@ -4224,15 +4228,12 @@ public class ServiceBuilder {
 			List<EntityFinder> entityFinders = entity.getEntityFinders();
 
 			for (EntityFinder entityFinder : entityFinders) {
-				IndexMetadata indexMetadata = _createIndexMetadata(
-					entity, entityFinder, _optimizeDBIndexes);
-
-				if (indexMetadata != null) {
-					_addIndexMetadata(
-						indexMetadatasMap, tableName,
-						entity.getPKEntityColumnDBNames(), indexMetadata,
-						_optimizeDBIndexes);
-				}
+				_addIndexMetadata(
+					indexMetadatasMap, tableName,
+					entity.getPKEntityColumnDBNames(),
+					_createIndexMetadata(
+						entity, entityFinder, _optimizeDBIndexes),
+					_optimizeDBIndexes);
 			}
 
 			indexMetadatas = indexMetadatasMap.get(tableName);
@@ -4245,15 +4246,11 @@ public class ServiceBuilder {
 			for (EntityFinder indexOnlyEntityFinder :
 					entity.getIndexOnlyEntityFinders()) {
 
-				IndexMetadata indexOnlyIndexMetadata = _createIndexMetadata(
-					entity, indexOnlyEntityFinder, false);
-
-				if (indexOnlyIndexMetadata != null) {
-					_addIndexMetadata(
-						indexMetadatasMap, tableName,
-						entity.getPKEntityColumnDBNames(),
-						indexOnlyIndexMetadata, false);
-				}
+				_addIndexMetadata(
+					indexMetadatasMap, tableName,
+					entity.getPKEntityColumnDBNames(),
+					_createIndexMetadata(entity, indexOnlyEntityFinder, false),
+					false);
 			}
 		}
 