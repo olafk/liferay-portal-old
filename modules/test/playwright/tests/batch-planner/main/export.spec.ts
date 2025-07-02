33b210788aa400fbe0e382845a5442e73b5b9eec
Magdalena Jedraszak <magdalena.jedraszak@liferay.com>
2025-07-02 14:20:45 +0200

    LPD-59407 Move to playwright

--- modules/test/playwright/tests/batch-planner/main/export.spec.ts
+++ modules/test/playwright/tests/batch-planner/main/export.spec.ts
@@ -403,6 +403,57 @@ test('can export as JSONL with excluded fields', async ({
 	).toBe('{"name":"Stock Entry"}\n');
 });
 
+test('can see actions node in the downloaded file', async ({
+	apiHelpers,
+	dataMigrationCenterPage,
+}) => {
+	const objectDefinitionAPIClient =
+		await apiHelpers.buildRestClient(ObjectDefinitionAPI);
+
+	const {body: objectDefinition} =
+		await objectDefinitionAPIClient.postObjectDefinition(
+			stockObjectDefinition
+		);
+
+	apiHelpers.data.push({id: objectDefinition.id, type: 'objectDefinition'});
+
+	await apiHelpers.objectEntry.postObjectEntry(stockObjectEntry, 'c/stocks');
+
+	expect(
+		JSON.parse(
+			await dataMigrationCenterPage.exportFile(
+				'JSON',
+				'Stock (v1.0 - Liferay Object REST)'
+			)
+		).find((item: any) => item.name === 'Stock Entry').actions
+	).toEqual({
+		delete: {
+			href: expect.stringContaining('/o/c/stocks/'),
+			method: 'DELETE',
+		},
+		get: {
+			href: expect.stringContaining('/o/c/stocks/'),
+			method: 'GET',
+		},
+		permissions: {
+			href: expect.stringContaining('/o/c/stocks/'),
+			method: 'GET',
+		},
+		replace: {
+			href: expect.stringContaining('/o/c/stocks/'),
+			method: 'PUT',
+		},
+		update: {
+			href: expect.stringContaining('/o/c/stocks/'),
+			method: 'PATCH',
+		},
+		versions: {
+			href: expect.stringContaining('/o/c/stocks/'),
+			method: 'GET',
+		},
+	});
+});
+
 test('can see correct custom object name in dropdown', async ({
 	apiHelpers,
 	dataMigrationCenterPage,
