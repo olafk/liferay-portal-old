liferay-database-infra/liferay-portal

LPD-57714 wordmsith

modules/apps/portal-background-task/portal-background-task-service/src/main/java/com/liferay/portal/background/task/internal/messaging/BackgroundTaskMessageListener.java
@@ -65,7 +65,7 @@ public class BackgroundTaskMessageListener extends BaseMessageListener {
 	@Override
 	protected void doReceive(Message message) throws Exception {
 		long backgroundTaskId = (Long)message.get(
-			BackgroundTaskConstants.BACKGROUND_TASK_ID);
+			BackgroundTaskConstants.FIELD_BACKGROUND_TASK_ID);
 
 		try (SafeCloseable safeCloseable1 =
 				BackgroundTaskThreadLocal.setBackgroundTaskIdWithSafeCloseable(
@@ -179,7 +179,7 @@ public class BackgroundTaskMessageListener extends BaseMessageListener {
 				Message responseMessage = new Message();
 
 				responseMessage.put(
-					BackgroundTaskConstants.BACKGROUND_TASK_ID,
+					BackgroundTaskConstants.FIELD_BACKGROUND_TASK_ID,
 					backgroundTask.getBackgroundTaskId());
 				responseMessage.put("companyId", backgroundTask.getCompanyId());
 				responseMessage.put("name", backgroundTask.getName());

modules/apps/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/background/task/ReindexStatusMessageSenderImpl.java
@@ -29,7 +29,7 @@ public class ReindexStatusMessageSenderImpl
 		Message message = new Message();
 
 		message.put(
-			BackgroundTaskConstants.BACKGROUND_TASK_ID,
+			BackgroundTaskConstants.FIELD_BACKGROUND_TASK_ID,
 			BackgroundTaskThreadLocal.getBackgroundTaskId());
 		message.put(ReindexBackgroundTaskConstants.CLASS_NAME, className);
 		message.put(ReindexBackgroundTaskConstants.COUNT, count);
@@ -46,7 +46,7 @@ public class ReindexStatusMessageSenderImpl
 		Message message = new Message();
 
 		message.put(
-			BackgroundTaskConstants.BACKGROUND_TASK_ID,
+			BackgroundTaskConstants.FIELD_BACKGROUND_TASK_ID,
 			BackgroundTaskThreadLocal.getBackgroundTaskId());
 		message.put(ReindexBackgroundTaskConstants.COMPANY_ID, companyId);
 		message.put(ReindexBackgroundTaskConstants.COMPANY_IDS, companyIds);

modules/apps/staging/staging-configuration-web/src/main/java/com/liferay/staging/configuration/web/internal/portlet/StagingConfigurationPortlet.java
@@ -86,7 +86,8 @@ public class StagingConfigurationPortlet extends MVCPortlet {
 
 		try {
 			long backgroundTaskId = ParamUtil.getLong(
-				actionRequest, BackgroundTaskConstants.BACKGROUND_TASK_ID);
+				actionRequest,
+				BackgroundTaskConstants.FIELD_BACKGROUND_TASK_ID);
 
 			_backgroundTaskManager.deleteBackgroundTask(backgroundTaskId);
 

modules/dxp/apps/document-library-opener/document-library-opener-onedrive-web/src/main/java/com/liferay/document/library/opener/onedrive/web/internal/background/task/UploadOneDriveDocumentBackgroundTaskExecutor.java
@@ -185,7 +185,7 @@ public class UploadOneDriveDocumentBackgroundTaskExecutor
 		Message message = new Message();
 
 		message.put(
-			BackgroundTaskConstants.BACKGROUND_TASK_ID,
+			BackgroundTaskConstants.FIELD_BACKGROUND_TASK_ID,
 			BackgroundTaskThreadLocal.getBackgroundTaskId());
 		message.put(
 			OneDriveBackgroundTaskConstants.FILE_ENTRY_ID,

olafk/liferay-portal-old

modules/apps/portal-background-task/portal-background-task-service/src/main/java/com/liferay/portal/background/task/internal/messaging/BackgroundTaskMessageListener.java
@@ -65,7 +65,7 @@ public class BackgroundTaskMessageListener extends BaseMessageListener {
 	@Override
 	protected void doReceive(Message message) throws Exception {
 		long backgroundTaskId = (Long)message.get(
-			BackgroundTaskConstants.BACKGROUND_TASK_ID);
+			BackgroundTaskConstants.MESSAGE_KEY_BACKGROUND_TASK_ID);
 
 		try (SafeCloseable safeCloseable1 =
 				BackgroundTaskThreadLocal.setBackgroundTaskIdWithSafeCloseable(
@@ -179,7 +179,7 @@ public class BackgroundTaskMessageListener extends BaseMessageListener {
 				Message responseMessage = new Message();
 
 				responseMessage.put(
-					BackgroundTaskConstants.BACKGROUND_TASK_ID,
+					BackgroundTaskConstants.MESSAGE_KEY_BACKGROUND_TASK_ID,
 					backgroundTask.getBackgroundTaskId());
 				responseMessage.put("companyId", backgroundTask.getCompanyId());
 				responseMessage.put("name", backgroundTask.getName());

modules/apps/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/background/task/ReindexStatusMessageSenderImpl.java
@@ -29,7 +29,7 @@ public class ReindexStatusMessageSenderImpl
 		Message message = new Message();
 
 		message.put(
-			BackgroundTaskConstants.BACKGROUND_TASK_ID,
+			BackgroundTaskConstants.MESSAGE_KEY_BACKGROUND_TASK_ID,
 			BackgroundTaskThreadLocal.getBackgroundTaskId());
 		message.put(ReindexBackgroundTaskConstants.CLASS_NAME, className);
 		message.put(ReindexBackgroundTaskConstants.COUNT, count);
@@ -46,7 +46,7 @@ public class ReindexStatusMessageSenderImpl
 		Message message = new Message();
 
 		message.put(
-			BackgroundTaskConstants.BACKGROUND_TASK_ID,
+			BackgroundTaskConstants.MESSAGE_KEY_BACKGROUND_TASK_ID,
 			BackgroundTaskThreadLocal.getBackgroundTaskId());
 		message.put(ReindexBackgroundTaskConstants.COMPANY_ID, companyId);
 		message.put(ReindexBackgroundTaskConstants.COMPANY_IDS, companyIds);

modules/apps/staging/staging-configuration-web/src/main/java/com/liferay/staging/configuration/web/internal/portlet/StagingConfigurationPortlet.java
@@ -86,7 +86,8 @@ public class StagingConfigurationPortlet extends MVCPortlet {
 
 		try {
 			long backgroundTaskId = ParamUtil.getLong(
-				actionRequest, BackgroundTaskConstants.BACKGROUND_TASK_ID);
+				actionRequest,
+				BackgroundTaskConstants.MESSAGE_KEY_BACKGROUND_TASK_ID);
 
 			_backgroundTaskManager.deleteBackgroundTask(backgroundTaskId);
 

modules/dxp/apps/document-library-opener/document-library-opener-onedrive-web/src/main/java/com/liferay/document/library/opener/onedrive/web/internal/background/task/UploadOneDriveDocumentBackgroundTaskExecutor.java
@@ -185,7 +185,7 @@ public class UploadOneDriveDocumentBackgroundTaskExecutor
 		Message message = new Message();
 
 		message.put(
-			BackgroundTaskConstants.BACKGROUND_TASK_ID,
+			BackgroundTaskConstants.MESSAGE_KEY_BACKGROUND_TASK_ID,
 			BackgroundTaskThreadLocal.getBackgroundTaskId());
 		message.put(
 			OneDriveBackgroundTaskConstants.FILE_ENTRY_ID,