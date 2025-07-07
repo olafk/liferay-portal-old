e4db98d7b1416da3db3620208e1b4a3350a1c969
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2025-07-07 19:00:56 +0100

    LPD-60021 PatcherFixPack - Fix pagination

--- modules/dxp/apps/osb/osb-patcher/osb-patcher-web/src/main/java/com/liferay/osb/patcher/web/internal/display/context/PatcherFixPacksDisplayContext.java
+++ modules/dxp/apps/osb/osb-patcher/osb-patcher-web/src/main/java/com/liferay/osb/patcher/web/internal/display/context/PatcherFixPacksDisplayContext.java
@@ -5,10 +5,12 @@
 
 package com.liferay.osb.patcher.web.internal.display.context;
 
+import com.liferay.osb.patcher.constants.WorkflowConstants;
 import com.liferay.osb.patcher.model.PatcherFixPack;
 import com.liferay.osb.patcher.service.PatcherFixPackLocalServiceUtil;
 import com.liferay.petra.function.transform.TransformUtil;
 import com.liferay.portal.kernel.dao.search.SearchContainer;
+import com.liferay.portal.kernel.portlet.url.builder.PortletURLBuilder;
 import com.liferay.portal.kernel.search.Field;
 import com.liferay.portal.kernel.search.Hits;
 import com.liferay.portal.kernel.search.Indexer;
@@ -18,7 +20,9 @@ import com.liferay.portal.kernel.search.SearchContextFactory;
 import com.liferay.portal.kernel.search.SearchResultUtil;
 import com.liferay.portal.kernel.search.Sort;
 import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.ParamUtil;
 
+import jakarta.portlet.PortletURL;
 import jakarta.portlet.RenderRequest;
 import jakarta.portlet.RenderResponse;
 
@@ -47,7 +51,7 @@ public class PatcherFixPacksDisplayContext {
 
 		SearchContainer<PatcherFixPack> patcherPatcherFixPackSearchContainer =
 			new SearchContainer<>(
-				_renderRequest, _renderResponse.createRenderURL(), null,
+				_renderRequest, _getPortletURL(), null,
 				"there-are-no-fix-packs");
 
 		Indexer<PatcherFixPack> indexer = IndexerRegistryUtil.getIndexer(
@@ -79,10 +83,82 @@ public class PatcherFixPacksDisplayContext {
 		return _patcherPatcherFixPackSearchContainer;
 	}
 
+	private String _getKeywords() {
+		if (_keywords != null) {
+			return _keywords;
+		}
+
+		_keywords = ParamUtil.getString(_httpServletRequest, "keywords");
+
+		return _keywords;
+	}
+
+	private Long _getPatcherFixComponentId() {
+		if (_patcherFixComponentId != null) {
+			return _patcherFixComponentId;
+		}
+
+		_patcherFixComponentId = ParamUtil.getLong(
+			_httpServletRequest, "patcherFixComponentId");
+
+		return _patcherFixComponentId;
+	}
+
+	private Long _getPatcherProjectVersionId() {
+		if (_patcherProjectVersionId != null) {
+			return _patcherProjectVersionId;
+		}
+
+		_patcherProjectVersionId = ParamUtil.getLong(
+			_httpServletRequest, "patcherProjectVersionId");
+
+		return _patcherProjectVersionId;
+	}
+
+	private PortletURL _getPortletURL() {
+		if (_portletURL != null) {
+			return _portletURL;
+		}
+
+		_portletURL = PortletURLBuilder.createRenderURL(
+			_renderResponse
+		).setMVCRenderCommandName(
+			"/patcher/index_fix_packs"
+		).setKeywords(
+			_getKeywords()
+		).setTabs1(
+			"fix-packs"
+		).setParameter(
+			"patcherFixComponentId", _getPatcherFixComponentId()
+		).setParameter(
+			"patcherProjectVersionId", _getPatcherProjectVersionId()
+		).setParameter(
+			"status", _getStatus()
+		).buildPortletURL();
+
+		return _portletURL;
+	}
+
+	private int _getStatus() {
+		if (_status != null) {
+			return _status;
+		}
+
+		_status = ParamUtil.getInteger(
+			_httpServletRequest, "status", WorkflowConstants.STATUS_ANY);
+
+		return _status;
+	}
+
 	private final HttpServletRequest _httpServletRequest;
+	private String _keywords;
+	private Long _patcherFixComponentId;
 	private SearchContainer<PatcherFixPack>
 		_patcherPatcherFixPackSearchContainer;
+	private Long _patcherProjectVersionId;
+	private PortletURL _portletURL;
 	private final RenderRequest _renderRequest;
 	private final RenderResponse _renderResponse;
+	private Integer _status;
 
 }
