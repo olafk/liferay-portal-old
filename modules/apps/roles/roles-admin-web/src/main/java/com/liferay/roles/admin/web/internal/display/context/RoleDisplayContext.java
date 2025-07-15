brbalazs/liferay-portal

LPD-58202 Added duplicate role feature

modules/apps/roles/roles-admin-web/src/main/java/com/liferay/roles/admin/web/internal/display/context/RoleDisplayContext.java
@@ -9,9 +9,12 @@ import com.liferay.application.list.constants.ApplicationListWebKeys;
 import com.liferay.application.list.constants.PanelCategoryKeys;
 import com.liferay.application.list.display.context.logic.PanelCategoryHelper;
 import com.liferay.application.list.display.context.logic.PersonalMenuEntryHelper;
+import com.liferay.frontend.taglib.clay.servlet.taglib.util.DropdownItem;
+import com.liferay.frontend.taglib.clay.servlet.taglib.util.DropdownItemListBuilder;
 import com.liferay.frontend.taglib.clay.servlet.taglib.util.NavigationItem;
 import com.liferay.frontend.taglib.clay.servlet.taglib.util.NavigationItemList;
 import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Permission;
@@ -20,6 +23,7 @@ import com.liferay.portal.kernel.model.ResourceConstants;
 import com.liferay.portal.kernel.model.Role;
 import com.liferay.portal.kernel.model.role.RoleConstants;
 import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
+import com.liferay.portal.kernel.portlet.LiferayWindowState;
 import com.liferay.portal.kernel.portlet.PortletURLUtil;
 import com.liferay.portal.kernel.portlet.url.builder.PortletURLBuilder;
 import com.liferay.portal.kernel.security.permission.ActionKeys;
@@ -28,6 +32,7 @@ import com.liferay.portal.kernel.security.permission.ResourceActionsUtil;
 import com.liferay.portal.kernel.service.PortletLocalServiceUtil;
 import com.liferay.portal.kernel.service.RoleLocalServiceUtil;
 import com.liferay.portal.kernel.service.RoleServiceUtil;
+import com.liferay.portal.kernel.service.permission.PortalPermissionUtil;
 import com.liferay.portal.kernel.service.permission.RolePermissionUtil;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.ArrayUtil;
@@ -35,6 +40,7 @@ import com.liferay.portal.kernel.util.Constants;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.PortletKeys;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
@@ -42,6 +48,7 @@ import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.roles.admin.role.type.contributor.RoleTypeContributor;
 import com.liferay.roles.admin.web.internal.role.type.contributor.util.RoleTypeContributorRetrieverUtil;
 import com.liferay.segments.service.SegmentsEntryRoleLocalServiceUtil;
+import com.liferay.taglib.security.PermissionsURLTag;
 
 import jakarta.portlet.PortletURL;
 import jakarta.portlet.RenderResponse;
@@ -68,8 +75,131 @@ public class RoleDisplayContext {
 		_currentRoleTypeContributor =
 			RoleTypeContributorRetrieverUtil.getCurrentRoleTypeContributor(
 				httpServletRequest);
+
 		_themeDisplay = (ThemeDisplay)httpServletRequest.getAttribute(
 			WebKeys.THEME_DISPLAY);
+	}
+
+	public List<DropdownItem> getActionDropdownItems(Role role)
+		throws Exception {
+
+		PermissionChecker permissionChecker =
+			_themeDisplay.getPermissionChecker();
+
+		String currentURL = PortalUtil.getCurrentURL(_httpServletRequest);
+
+		return DropdownItemListBuilder.add(
+			() -> RolePermissionUtil.contains(
+				permissionChecker, role.getRoleId(), ActionKeys.UPDATE),
+			dropdownItem -> {
+				dropdownItem.setHref(
+					PortletURLBuilder.createRenderURL(
+						_renderResponse
+					).setMVCPath(
+						"/edit_role.jsp"
+					).setBackURL(
+						currentURL
+					).setTabs1(
+						"details"
+					).setParameter(
+						"roleId", role.getRoleId()
+					).buildString());
+				dropdownItem.setIcon("pencil");
+				dropdownItem.setLabel(
+					LanguageUtil.get(_httpServletRequest, "edit"));
+			}
+		).add(
+			() ->
+				!Objects.equals(role.getName(), RoleConstants.OWNER) &&
+				RolePermissionUtil.contains(
+					permissionChecker, role.getRoleId(),
+					ActionKeys.PERMISSIONS),
+			dropdownItem -> {
+				int[] roleTypes = {role.getType()};
+
+				if (role.getType() != RoleConstants.TYPE_REGULAR) {
+					roleTypes = new int[] {
+						RoleConstants.TYPE_REGULAR, role.getType()
+					};
+				}
+
+				dropdownItem.setData(
+					HashMapBuilder.<String, Object>put(
+						"action", "permissions"
+					).put(
+						"permissionsURL",
+						PermissionsURLTag.doTag(
+							StringPool.BLANK, Role.class.getName(),
+							role.getTitle(_themeDisplay.getLocale()), null,
+							String.valueOf(role.getRoleId()),
+							LiferayWindowState.POP_UP.toString(), roleTypes,
+							_httpServletRequest)
+					).build());
+				dropdownItem.setIcon("cog");
+				dropdownItem.setLabel(
+					LanguageUtil.get(_httpServletRequest, "permissions"));
+			}
+		).add(
+			() ->
+				_currentRoleTypeContributor.isAllowDelete(role) &&
+				RolePermissionUtil.contains(
+					permissionChecker, role.getRoleId(), ActionKeys.DELETE),
+			dropdownItem -> {
+				dropdownItem.setData(
+					HashMapBuilder.<String, Object>put(
+						"action", "delete"
+					).put(
+						"confirmationMessage",
+						LanguageUtil.get(
+							_httpServletRequest,
+							"are-you-sure-you-want-to-delete-this-role?-task-" +
+								"assignments-may-be-deleted")
+					).put(
+						"deleteRoleURL",
+						PortletURLBuilder.createActionURL(
+							_renderResponse
+						).setActionName(
+							"deleteRole"
+						).setMVCPath(
+							"/view.jsp"
+						).setRedirect(
+							currentURL
+						).setParameter(
+							"roleId", role.getRoleId()
+						).buildString()
+					).build());
+				dropdownItem.setIcon("trash");
+				dropdownItem.setLabel(
+					LanguageUtil.get(_httpServletRequest, "delete"));
+			}
+		).add(
+			() -> PortalPermissionUtil.contains(
+				permissionChecker, ActionKeys.ADD_ROLE),
+			dropdownItem -> {
+				dropdownItem.setData(
+					HashMapBuilder.<String, Object>put(
+						"action", "duplicate"
+					).put(
+						"duplicateRoleURL",
+						PortletURLBuilder.createRenderURL(
+							_renderResponse
+						).setMVCPath(
+							"/duplicate_role.jsp"
+						).setBackURL(
+							currentURL
+						).setParameter(
+							"roleId", role.getRoleId()
+						).setWindowState(
+							LiferayWindowState.POP_UP
+						).buildString()
+					).put(
+						"roleName", role.getName()
+					).build());
+				dropdownItem.setIcon("copy");
+				dropdownItem.setLabel(
+					LanguageUtil.get(_httpServletRequest, "duplicate"));
+			}
+		).build();
 	}
 
 	public String getActionLabel(String resourceName, String actionId) {

modules/apps/roles/roles-admin-web/src/main/java/com/liferay/roles/admin/web/internal/portlet/RolesAdminPortlet.java
@@ -30,6 +30,7 @@ import com.liferay.portal.kernel.model.ResourceConstants;
 import com.liferay.portal.kernel.model.Role;
 import com.liferay.portal.kernel.model.role.RoleConstants;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+import com.liferay.portal.kernel.portlet.url.builder.PortletURLBuilder;
 import com.liferay.portal.kernel.security.auth.PrincipalException;
 import com.liferay.portal.kernel.security.permission.ActionKeys;
 import com.liferay.portal.kernel.security.permission.ResourceActionsUtil;
@@ -52,6 +53,7 @@ import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.Localization;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.PortletKeys;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
@@ -196,6 +198,47 @@ public class RolesAdminPortlet extends MVCPortlet {
 		catch (ModelListenerException modelListenerException) {
 			throw (Exception)modelListenerException.getCause();
 		}
+	}
+
+	public void duplicateRole(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
+
+		long roleId = ParamUtil.getLong(actionRequest, "roleId");
+
+		if (roleId <= 0) {
+			return;
+		}
+
+		Role targetRole = null;
+
+		try {
+			targetRole = _roleLocalService.copyRole(
+				_portal.getUserId(actionRequest),
+				ParamUtil.getString(actionRequest, "roleName"), roleId,
+				ServiceContextFactory.getInstance(
+					Role.class.getName(), actionRequest));
+		}
+		catch (DuplicateRoleException | RoleNameException exception) {
+			actionResponse.setRenderParameter("mvcPath", "/duplicate_role.jsp");
+
+			throw exception;
+		}
+
+		actionResponse.setRenderParameter("mvcPath", "/duplicate_role.jsp");
+		actionResponse.setRenderParameter(
+			"redirectURL",
+			PortletURLBuilder.create(
+				PortalUtil.getControlPanelPortletURL(
+					actionRequest, RolesAdminPortletKeys.ROLES_ADMIN,
+					PortletRequest.RENDER_PHASE)
+			).setMVCPath(
+				"/edit_role.jsp"
+			).setBackURL(
+				ParamUtil.getString(actionRequest, "backURL")
+			).setParameter(
+				"roleId", targetRole.getRoleId()
+			).buildString());
 	}
 
 	public Role editRole(
@@ -555,7 +598,12 @@ public class RolesAdminPortlet extends MVCPortlet {
 				 SessionErrors.contains(
 					 renderRequest, RoleNameException.class.getName())) {
 
-			include("/edit_role.jsp", renderRequest, renderResponse);
+			if (mvcPath.equals("/duplicate_role.jsp")) {
+				include(mvcPath, renderRequest, renderResponse);
+			}
+			else {
+				include("/edit_role.jsp", renderRequest, renderResponse);
+			}
 		}
 		else if (SessionErrors.contains(
 					renderRequest,

modules/apps/roles/roles-admin-web/src/main/resources/META-INF/resources/js/RoleActionPropsTransformer.js
@@ -1,0 +1,63 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import {openConfirmModal, openModal} from 'frontend-js-components-web';
+import {sub} from 'frontend-js-web';
+
+const ACTIONS = {
+	delete(itemData) {
+		openConfirmModal({
+			message: itemData.confirmationMessage,
+			onConfirm: (isConfirmed) => {
+				if (isConfirmed) {
+					Liferay.Util.navigate(itemData.deleteRoleURL);
+				}
+			},
+		});
+	},
+	duplicate(itemData) {
+		openModal({
+			containerProps: {
+				className: 'modal-height-md',
+			},
+			size: 'lg',
+			title: sub(Liferay.Language.get('duplicate-x'), itemData.roleName),
+			url: itemData.duplicateRoleURL,
+		});
+	},
+	permissions(itemData) {
+		openModal({
+			title: Liferay.Language.get('permissions'),
+			url: itemData.permissionsURL,
+		});
+	},
+};
+
+export default function RoleActionPropsTransformer({items, ...props}) {
+	const bindAction = (item) => {
+		const action = ACTIONS[item.data?.action];
+
+		const transformedItem = {...item};
+
+		if (typeof action === 'function') {
+			transformedItem.onClick = (event) => {
+				event.preventDefault();
+
+				action.call(ACTIONS, item.data);
+			};
+		}
+
+		if (Array.isArray(item.items)) {
+			transformedItem.items = item.items.map(bindAction);
+		}
+
+		return transformedItem;
+	};
+
+	return {
+		...props,
+		items: items?.map(bindAction),
+	};
+}

modules/apps/roles/roles-admin-web/src/main/resources/META-INF/resources/js/index.js
@@ -6,7 +6,9 @@
 export {default as EditRoleAssignmentsManagementToolbarPropsTransformer} from './EditRoleAssignmentsManagementToolbarPropsTransformer';
 export {default as EditRolePermissionsNavigation} from './EditRolePermissionsNavigation';
 export {default as GroupLabels} from './GroupLabels';
+export {default as RoleActionPropsTransformer} from './RoleActionPropsTransformer';
 export {default as SelectAssigneesManagementToolbarPropsTransformer} from './SelectAssigneesManagementToolbarPropsTransformer';
 export {default as ViewRolesManagementToolbarPropsTransformer} from './ViewRolesManagementToolbarPropsTransformer';
 export {default as addAssignees} from './addAssignees';
+export {default as duplicateRoleRedirect} from './duplicateRoleRedirect';
 export {default as editRoleAssignmentsMain} from './editRoleAssignmentsMain';

olafk/liferay-portal-old

modules/apps/roles/roles-admin-web/src/main/java/com/liferay/roles/admin/web/internal/display/context/RoleDisplayContext.java
@@ -9,9 +9,12 @@ import com.liferay.application.list.constants.ApplicationListWebKeys;
 import com.liferay.application.list.constants.PanelCategoryKeys;
 import com.liferay.application.list.display.context.logic.PanelCategoryHelper;
 import com.liferay.application.list.display.context.logic.PersonalMenuEntryHelper;
+import com.liferay.frontend.taglib.clay.servlet.taglib.util.DropdownItem;
+import com.liferay.frontend.taglib.clay.servlet.taglib.util.DropdownItemListBuilder;
 import com.liferay.frontend.taglib.clay.servlet.taglib.util.NavigationItem;
 import com.liferay.frontend.taglib.clay.servlet.taglib.util.NavigationItemList;
 import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Permission;
@@ -20,6 +23,7 @@ import com.liferay.portal.kernel.model.ResourceConstants;
 import com.liferay.portal.kernel.model.Role;
 import com.liferay.portal.kernel.model.role.RoleConstants;
 import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
+import com.liferay.portal.kernel.portlet.LiferayWindowState;
 import com.liferay.portal.kernel.portlet.PortletURLUtil;
 import com.liferay.portal.kernel.portlet.url.builder.PortletURLBuilder;
 import com.liferay.portal.kernel.security.permission.ActionKeys;
@@ -28,6 +32,7 @@ import com.liferay.portal.kernel.security.permission.ResourceActionsUtil;
 import com.liferay.portal.kernel.service.PortletLocalServiceUtil;
 import com.liferay.portal.kernel.service.RoleLocalServiceUtil;
 import com.liferay.portal.kernel.service.RoleServiceUtil;
+import com.liferay.portal.kernel.service.permission.PortalPermissionUtil;
 import com.liferay.portal.kernel.service.permission.RolePermissionUtil;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.ArrayUtil;
@@ -35,6 +40,7 @@ import com.liferay.portal.kernel.util.Constants;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.PortletKeys;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
@@ -42,6 +48,7 @@ import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.roles.admin.role.type.contributor.RoleTypeContributor;
 import com.liferay.roles.admin.web.internal.role.type.contributor.util.RoleTypeContributorRetrieverUtil;
 import com.liferay.segments.service.SegmentsEntryRoleLocalServiceUtil;
+import com.liferay.taglib.security.PermissionsURLTag;
 
 import jakarta.portlet.PortletURL;
 import jakarta.portlet.RenderResponse;
@@ -70,6 +77,128 @@ public class RoleDisplayContext {
 				httpServletRequest);
 		_themeDisplay = (ThemeDisplay)httpServletRequest.getAttribute(
 			WebKeys.THEME_DISPLAY);
+	}
+
+	public List<DropdownItem> getActionDropdownItems(Role role)
+		throws Exception {
+
+		PermissionChecker permissionChecker =
+			_themeDisplay.getPermissionChecker();
+
+		String currentURL = PortalUtil.getCurrentURL(_httpServletRequest);
+
+		return DropdownItemListBuilder.add(
+			() -> RolePermissionUtil.contains(
+				permissionChecker, role.getRoleId(), ActionKeys.UPDATE),
+			dropdownItem -> {
+				dropdownItem.setHref(
+					PortletURLBuilder.createRenderURL(
+						_renderResponse
+					).setMVCPath(
+						"/edit_role.jsp"
+					).setBackURL(
+						currentURL
+					).setTabs1(
+						"details"
+					).setParameter(
+						"roleId", role.getRoleId()
+					).buildString());
+				dropdownItem.setIcon("pencil");
+				dropdownItem.setLabel(
+					LanguageUtil.get(_httpServletRequest, "edit"));
+			}
+		).add(
+			() ->
+				!Objects.equals(role.getName(), RoleConstants.OWNER) &&
+				RolePermissionUtil.contains(
+					permissionChecker, role.getRoleId(),
+					ActionKeys.PERMISSIONS),
+			dropdownItem -> {
+				int[] roleTypes = {role.getType()};
+
+				if (role.getType() != RoleConstants.TYPE_REGULAR) {
+					roleTypes = new int[] {
+						RoleConstants.TYPE_REGULAR, role.getType()
+					};
+				}
+
+				dropdownItem.setData(
+					HashMapBuilder.<String, Object>put(
+						"action", "permissions"
+					).put(
+						"permissionsURL",
+						PermissionsURLTag.doTag(
+							StringPool.BLANK, Role.class.getName(),
+							role.getTitle(_themeDisplay.getLocale()), null,
+							String.valueOf(role.getRoleId()),
+							LiferayWindowState.POP_UP.toString(), roleTypes,
+							_httpServletRequest)
+					).build());
+				dropdownItem.setIcon("cog");
+				dropdownItem.setLabel(
+					LanguageUtil.get(_httpServletRequest, "permissions"));
+			}
+		).add(
+			() ->
+				_currentRoleTypeContributor.isAllowDelete(role) &&
+				RolePermissionUtil.contains(
+					permissionChecker, role.getRoleId(), ActionKeys.DELETE),
+			dropdownItem -> {
+				dropdownItem.setData(
+					HashMapBuilder.<String, Object>put(
+						"action", "delete"
+					).put(
+						"confirmationMessage",
+						LanguageUtil.get(
+							_httpServletRequest,
+							"are-you-sure-you-want-to-delete-this-role?-task-" +
+								"assignments-may-be-deleted")
+					).put(
+						"deleteRoleURL",
+						PortletURLBuilder.createActionURL(
+							_renderResponse
+						).setActionName(
+							"deleteRole"
+						).setMVCPath(
+							"/view.jsp"
+						).setRedirect(
+							currentURL
+						).setParameter(
+							"roleId", role.getRoleId()
+						).buildString()
+					).build());
+				dropdownItem.setIcon("trash");
+				dropdownItem.setLabel(
+					LanguageUtil.get(_httpServletRequest, "delete"));
+			}
+		).add(
+			() -> PortalPermissionUtil.contains(
+				permissionChecker, ActionKeys.ADD_ROLE),
+			dropdownItem -> {
+				dropdownItem.setData(
+					HashMapBuilder.<String, Object>put(
+						"action", "copy"
+					).put(
+						"copyRoleURL",
+						PortletURLBuilder.createRenderURL(
+							_renderResponse
+						).setMVCPath(
+							"/copy_role.jsp"
+						).setBackURL(
+							currentURL
+						).setParameter(
+							"roleId", role.getRoleId()
+						).setWindowState(
+							LiferayWindowState.POP_UP
+						).buildString()
+					).put(
+						"roleName", role.getName()
+					).build());
+				dropdownItem.setIcon("copy");
+				dropdownItem.setLabel(
+					LanguageUtil.get(_httpServletRequest, "duplicate"));
+			}
+		).build();
 	}
 
 	public String getActionLabel(String resourceName, String actionId) {

modules/apps/roles/roles-admin-web/src/main/java/com/liferay/roles/admin/web/internal/portlet/RolesAdminPortlet.java
@@ -30,6 +30,7 @@ import com.liferay.portal.kernel.model.ResourceConstants;
 import com.liferay.portal.kernel.model.Role;
 import com.liferay.portal.kernel.model.role.RoleConstants;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+import com.liferay.portal.kernel.portlet.url.builder.PortletURLBuilder;
 import com.liferay.portal.kernel.security.auth.PrincipalException;
 import com.liferay.portal.kernel.security.permission.ActionKeys;
 import com.liferay.portal.kernel.security.permission.ResourceActionsUtil;
@@ -52,6 +53,7 @@ import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.Localization;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.PortletKeys;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
@@ -119,6 +121,47 @@ import org.osgi.service.component.annotations.Reference;
 	service = Portlet.class
 )
 public class RolesAdminPortlet extends MVCPortlet {
+
+	public void copyRole(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
+
+		long roleId = ParamUtil.getLong(actionRequest, "roleId");
+
+		if (roleId <= 0) {
+			return;
+		}
+
+		Role targetRole = null;
+
+		try {
+			targetRole = _roleLocalService.copyRole(
+				_portal.getUserId(actionRequest),
+				ParamUtil.getString(actionRequest, "roleName"), roleId,
+				ServiceContextFactory.getInstance(
+					Role.class.getName(), actionRequest));
+		}
+		catch (DuplicateRoleException | RoleNameException exception) {
+			actionResponse.setRenderParameter("mvcPath", "/copy_role.jsp");
+
+			throw exception;
+		}
+
+		actionResponse.setRenderParameter("mvcPath", "/copy_role.jsp");
+		actionResponse.setRenderParameter(
+			"redirectURL",
+			PortletURLBuilder.create(
+				PortalUtil.getControlPanelPortletURL(
+					actionRequest, RolesAdminPortletKeys.ROLES_ADMIN,
+					PortletRequest.RENDER_PHASE)
+			).setMVCPath(
+				"/edit_role.jsp"
+			).setBackURL(
+				ParamUtil.getString(actionRequest, "backURL")
+			).setParameter(
+				"roleId", targetRole.getRoleId()
+			).buildString());
+	}
 
 	public void deletePermission(
 			ActionRequest actionRequest, ActionResponse actionResponse)
@@ -555,7 +598,12 @@ public class RolesAdminPortlet extends MVCPortlet {
 				 SessionErrors.contains(
 					 renderRequest, RoleNameException.class.getName())) {
 
-			include("/edit_role.jsp", renderRequest, renderResponse);
+			if (mvcPath.equals("/copy_role.jsp")) {
+				include(mvcPath, renderRequest, renderResponse);
+			}
+			else {
+				include("/edit_role.jsp", renderRequest, renderResponse);
+			}
 		}
 		else if (SessionErrors.contains(
 					renderRequest,

modules/apps/roles/roles-admin-web/src/main/resources/META-INF/resources/js/RoleActionPropsTransformer.js
@@ -1,0 +1,63 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import {openConfirmModal, openModal} from 'frontend-js-components-web';
+import {sub} from 'frontend-js-web';
+
+const ACTIONS = {
+	copy(itemData) {
+		openModal({
+			containerProps: {
+				className: 'modal-height-md',
+			},
+			size: 'lg',
+			title: sub(Liferay.Language.get('duplicate-x'), itemData.roleName),
+			url: itemData.copyRoleURL,
+		});
+	},
+	delete(itemData) {
+		openConfirmModal({
+			message: itemData.confirmationMessage,
+			onConfirm: (isConfirmed) => {
+				if (isConfirmed) {
+					Liferay.Util.navigate(itemData.deleteRoleURL);
+				}
+			},
+		});
+	},
+	permissions(itemData) {
+		openModal({
+			title: Liferay.Language.get('permissions'),
+			url: itemData.permissionsURL,
+		});
+	},
+};
+
+export default function RoleActionPropsTransformer({items, ...props}) {
+	const bindAction = (item) => {
+		const action = ACTIONS[item.data?.action];
+
+		const transformedItem = {...item};
+
+		if (typeof action === 'function') {
+			transformedItem.onClick = (event) => {
+				event.preventDefault();
+
+				action.call(ACTIONS, item.data);
+			};
+		}
+
+		if (Array.isArray(item.items)) {
+			transformedItem.items = item.items.map(bindAction);
+		}
+
+		return transformedItem;
+	};
+
+	return {
+		...props,
+		items: items?.map(bindAction),
+	};
+}

modules/apps/roles/roles-admin-web/src/main/resources/META-INF/resources/js/index.js
@@ -6,7 +6,9 @@
 export {default as EditRoleAssignmentsManagementToolbarPropsTransformer} from './EditRoleAssignmentsManagementToolbarPropsTransformer';
 export {default as EditRolePermissionsNavigation} from './EditRolePermissionsNavigation';
 export {default as GroupLabels} from './GroupLabels';
+export {default as RoleActionPropsTransformer} from './RoleActionPropsTransformer';
 export {default as SelectAssigneesManagementToolbarPropsTransformer} from './SelectAssigneesManagementToolbarPropsTransformer';
 export {default as ViewRolesManagementToolbarPropsTransformer} from './ViewRolesManagementToolbarPropsTransformer';
 export {default as addAssignees} from './addAssignees';
+export {default as copyRoleRedirect} from './copyRoleRedirect';
 export {default as editRoleAssignmentsMain} from './editRoleAssignmentsMain';