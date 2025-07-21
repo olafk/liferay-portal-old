4fab915467483cdb3299c7d14cfeb1433449054e
Carolina Barbosa <carolina.barbosa@liferay.com>
2025-07-21 10:09:05 -0300

    LPD-60998 Add new service method to add the subscription notification templates

--- modules/apps/notification/notification-service/src/main/java/com/liferay/notification/service/impl/NotificationRecipientSettingLocalServiceImpl.java
+++ modules/apps/notification/notification-service/src/main/java/com/liferay/notification/service/impl/NotificationRecipientSettingLocalServiceImpl.java
@@ -8,10 +8,18 @@ package com.liferay.notification.service.impl;
 import com.liferay.notification.model.NotificationRecipientSetting;
 import com.liferay.notification.service.base.NotificationRecipientSettingLocalServiceBaseImpl;
 import com.liferay.portal.aop.AopService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.search.Indexable;
+import com.liferay.portal.kernel.search.IndexableType;
+import com.liferay.portal.kernel.service.UserLocalService;
 
 import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Feliphe Marinho
@@ -22,6 +30,39 @@ import org.osgi.service.component.annotations.Component;
 )
 public class NotificationRecipientSettingLocalServiceImpl
 	extends NotificationRecipientSettingLocalServiceBaseImpl {
+
+	@Indexable(type = IndexableType.REINDEX)
+	@Override
+	public NotificationRecipientSetting addNotificationRecipientSetting(
+			long userId, long notificationRecipientId, String name,
+			Object value)
+		throws PortalException {
+
+		NotificationRecipientSetting notificationRecipientSetting =
+			notificationRecipientSettingPersistence.create(
+				counterLocalService.increment());
+
+		User user = _userLocalService.getUser(userId);
+
+		notificationRecipientSetting.setCompanyId(user.getCompanyId());
+		notificationRecipientSetting.setUserId(user.getUserId());
+		notificationRecipientSetting.setUserName(user.getFullName());
+
+		notificationRecipientSetting.setNotificationRecipientId(
+			notificationRecipientId);
+		notificationRecipientSetting.setName(name);
+
+		if (value instanceof String) {
+			notificationRecipientSetting.setValue(String.valueOf(value));
+		}
+		else {
+			notificationRecipientSetting.setValueMap(
+				(Map<Locale, String>)value);
+		}
+
+		return notificationRecipientSettingPersistence.update(
+			notificationRecipientSetting);
+	}
 
 	@Override
 	public NotificationRecipientSetting fetchNotificationRecipientSetting(
@@ -38,5 +79,8 @@ public class NotificationRecipientSettingLocalServiceImpl
 		return notificationRecipientSettingPersistence.
 			findByNotificationRecipientId(notificationRecipientId);
 	}
+
+	@Reference
+	private UserLocalService _userLocalService;
 
 }
