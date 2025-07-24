ac697db4336ffd9df1d5de11f15d8d6d9b9764a5
Jorge González <jorge.gonzalez@liferay.com>
2025-07-24 13:30:56 +0200

    LPD-61553 Pretty-print accessibility issues in playwright

--- modules/test/playwright/utils/checkAccessibility.ts
+++ modules/test/playwright/utils/checkAccessibility.ts
@@ -14,6 +14,65 @@ interface Params {
 	soft?: boolean;
 }
 
+function indentMultiline(text: string, spaces = 4): string {
+	return text
+		.split('\n')
+		.map((line) => ' '.repeat(spaces) + line)
+		.join('\n');
+}
+
+function formatAccessibility(violations: any[]): string {
+	const USE_COLORS = process.stdout.isTTY;
+
+	const RED = USE_COLORS ? '\x1b[31m' : '';
+	const YELLOW = USE_COLORS ? '\x1b[33m' : '';
+	const CYAN = USE_COLORS ? '\x1b[36m' : '';
+	const WHITE = USE_COLORS ? '\x1b[37m' : '';
+	const BOLD = USE_COLORS ? '\x1b[1m' : '';
+	const RESET = USE_COLORS ? '\x1b[0m' : '';
+
+	const output = [];
+
+	output.push('\n');
+	output.push(
+		`${RED}${BOLD}Accessibility issues found: ${RESET}${WHITE}${
+			violations.length
+		} rules affecting ${violations.reduce(
+			(acc, {nodes}) => acc + nodes.length,
+			0
+		)} node(s)${RESET}`
+	);
+	output.push(`${CYAN}${'='.repeat(80)}${RESET}`);
+
+	for (const violation of violations) {
+		output.push('');
+		output.push(
+			`${YELLOW}${BOLD}• RULE: ${RESET}${YELLOW}${violation.description}${RESET}`
+		);
+		output.push(`  Help   : ${violation.help}`);
+		output.push(`  Docs   : ${violation.helpUrl}`);
+		output.push(`  Impact : ${violation.impact}`);
+		output.push('');
+
+		for (let i = 0; i < violation.nodes.length; i++) {
+			const node = violation.nodes[i];
+
+			i > 0 && output.push(`${CYAN}    ${'-'.repeat(40)}${RESET}`);
+			output.push(`${CYAN}  - Affected element:${RESET}`);
+			output.push(`    ${node.html}`);
+			output.push(`${CYAN}  - Target selector(s):${RESET}`);
+			output.push(`    ${node.target.join(', ')}`);
+			output.push(`${CYAN}  - Summary:${RESET}`);
+			output.push(indentMultiline(node.failureSummary.trim()));
+			output.push('');
+		}
+
+		output.push(`${CYAN}${'-'.repeat(80)}${RESET}`);
+	}
+
+	return output.join('\n');
+}
+
 /**
  * Check accessibility on a page.
  *
@@ -54,10 +113,15 @@ export async function checkAccessibility({
 		}
 	}
 
-	const results = await axeBuilder.withTags(tags).analyze();
+	const {violations} = await axeBuilder.withTags(tags).analyze();
 
-	(soft ? expect.soft : expect)(
-		results.violations,
-		'Accessibility issues'
-	).toEqual([]);
+	if (!violations.length) {
+		(soft ? expect.soft : expect)(
+			false,
+			formatAccessibility(violations)
+		).toBe(true);
+	}
+	else {
+		expect(true, 'Accessibility issues').toBe(true);
+	}
 }
