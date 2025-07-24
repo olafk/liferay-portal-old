MarinhoFeliphe/liferay-portal

LPD-56969 generated: build service

modules/apps/object/object-api/src/main/java/com/liferay/object/model/ObjectDefinition.java
@@ -77,6 +77,8 @@ public interface ObjectDefinition
 
 	public long getRootObjectDefinitionId();
 
+	public long[] getRootObjectDefinitionIds();
+
 	public String getShortName();
 
 	public boolean isApproved();
@@ -86,6 +88,8 @@ public interface ObjectDefinition
 	public boolean isLinkedToObjectFolder(long objectFolderId);
 
 	public boolean isModifiableAndSystem();
+
+	public boolean isReachableNode(long rootObjectDefinitionId);
 
 	public boolean isRootDescendantNode();
 

modules/apps/object/object-api/src/main/java/com/liferay/object/model/ObjectDefinitionWrapper.java
@@ -901,6 +901,11 @@ public class ObjectDefinitionWrapper
 		return model.getRootObjectDefinitionId();
 	}
 
+	@Override
+	public long[] getRootObjectDefinitionIds() {
+		return model.getRootObjectDefinitionIds();
+	}
+
 	/**
 	 * Returns the scope of this object definition.
 	 *
@@ -1154,6 +1159,11 @@ public class ObjectDefinitionWrapper
 	@Override
 	public boolean isPortlet() {
 		return model.isPortlet();
+	}
+
+	@Override
+	public boolean isReachableNode(long rootObjectDefinitionId) {
+		return model.isReachableNode(rootObjectDefinitionId);
 	}
 
 	@Override

olafk/liferay-portal-old

modules/apps/object/object-api/src/main/java/com/liferay/object/model/ObjectDefinition.java
@@ -67,8 +67,6 @@ public interface ObjectDefinition
 
 	public String getPortletId();
 
-	public String getPreviousRESTContextPath();
-
 	public String getResourceName();
 
 	public String getRESTContextPath();
@@ -76,6 +74,8 @@ public interface ObjectDefinition
 	public String getRootObjectDefinitionExternalReferenceCode();
 
 	public long getRootObjectDefinitionId();
+
+	public long[] getRootObjectDefinitionIds();
 
 	public String getShortName();
 
@@ -89,13 +89,13 @@ public interface ObjectDefinition
 
 	public boolean isRootDescendantNode();
 
+	public boolean isRootDescendantNode(long rootObjectDefinitionId);
+
 	public boolean isRootNode();
 
 	public boolean isUnmodifiableSystemObject();
 
 	public void setObjectDefinitionSettings(
 		java.util.List<ObjectDefinitionSetting> objectDefinitionSettings);
-
-	public void setPreviousRESTContextPath(String previousRESTContextPath);
 
 }

modules/apps/object/object-api/src/main/java/com/liferay/object/model/ObjectDefinitionWrapper.java
@@ -866,11 +866,6 @@ public class ObjectDefinitionWrapper
 		return model.getPortletId();
 	}
 
-	@Override
-	public String getPreviousRESTContextPath() {
-		return model.getPreviousRESTContextPath();
-	}
-
 	/**
 	 * Returns the primary key of this object definition.
 	 *
@@ -899,6 +894,11 @@ public class ObjectDefinitionWrapper
 	@Override
 	public long getRootObjectDefinitionId() {
 		return model.getRootObjectDefinitionId();
+	}
+
+	@Override
+	public long[] getRootObjectDefinitionIds() {
+		return model.getRootObjectDefinitionIds();
 	}
 
 	/**
@@ -1159,6 +1159,11 @@ public class ObjectDefinitionWrapper
 	@Override
 	public boolean isRootDescendantNode() {
 		return model.isRootDescendantNode();
+	}
+
+	@Override
+	public boolean isRootDescendantNode(long rootObjectDefinitionId) {
+		return model.isRootDescendantNode(rootObjectDefinitionId);
 	}
 
 	@Override
@@ -1646,11 +1651,6 @@ public class ObjectDefinitionWrapper
 	@Override
 	public void setPortlet(boolean portlet) {
 		model.setPortlet(portlet);
-	}
-
-	@Override
-	public void setPreviousRESTContextPath(String previousRESTContextPath) {
-		model.setPreviousRESTContextPath(previousRESTContextPath);
 	}
 
 	/**