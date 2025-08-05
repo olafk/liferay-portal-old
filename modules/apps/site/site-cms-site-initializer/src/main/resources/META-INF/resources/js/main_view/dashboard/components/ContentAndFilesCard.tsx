rafaellajordao/liferay-portal

LPD-60567 Reuse utils from analytics reports js components web
Co-authored-by: Rafaella Jordao <rafaella.jordao@liferay.com>
Co-authored-by: Tiago Campelo <tiago.campelo@liferay.com>

modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/main_view/dashboard/components/ContentAndFilesCard.tsx
@@ -6,20 +6,19 @@
 import {Text} from '@clayui/core';
 import ClayIcon from '@clayui/icon';
 import ClayLoadingIndicator from '@clayui/loading-indicator';
+import {
+	TrendClassification,
+	buildQueryString,
+	getStatsColor,
+	getStatsIcon,
+	toThousands,
+} from '@liferay/analytics-reports-js-components-web';
 import {sub} from 'frontend-js-web';
 import React, {useContext, useEffect, useMemo, useState} from 'react';
 
 import ApiHelper from '../../../common/services/ApiHelper';
 import {ViewDashboardContext} from '../ViewDashboardContext';
-import {buildQueryString} from '../utils/buildQueryString';
-import {toThousands} from '../utils/number';
 import {RangeSelector, getSafeRangeSelector} from './RangeSelectorsDropdown';
-
-export enum TrendClassification {
-	Negative = 'NEGATIVE',
-	Neutral = 'NEUTRAL',
-	Positive = 'POSITIVE',
-}
 
 export interface IMetricsProps {
 	categoriesCount: number;
@@ -38,30 +37,6 @@ export interface IContentAndFilesCard {
 	title: (totalCount: number) => string;
 }
 
-function getStatsColor(
-	trendClassification: TrendClassification
-): 'danger' | 'success' | 'secondary' {
-	if (trendClassification === TrendClassification.Negative) {
-		return 'danger';
-	}
-	else if (trendClassification === TrendClassification.Positive) {
-		return 'success';
-	}
-
-	return 'secondary';
-}
-
-function getStatsIcon(trendPercentage: number) {
-	if (trendPercentage > 0) {
-		return 'caret-top';
-	}
-	else if (trendPercentage < 0) {
-		return 'caret-bottom';
-	}
-
-	return null;
-}
-
 const ContentAndFilesCard: React.FC<IContentAndFilesCard> = ({
 	endpointURL,
 	rangeSelector,
@@ -74,11 +49,16 @@ const ContentAndFilesCard: React.FC<IContentAndFilesCard> = ({
 	const [loading, setLoading] = useState(true);
 	const [metrics, setMetrics] = useState<IMetricsProps>();
 
-	const queryParams = buildQueryString({
-		languageId: language.value,
-		spaceId: space.value,
-		...getSafeRangeSelector(rangeSelector),
-	});
+	const queryParams = buildQueryString(
+		{
+			languageId: language.value,
+			spaceId: space.value,
+			...getSafeRangeSelector(rangeSelector),
+		},
+		{
+			shouldIgnoreParam: (value) => value === 'all',
+		}
+	);
 
 	useEffect(() => {
 		async function getMetrics() {

modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/main_view/dashboard/components/InventoryAnalysisCard.tsx
@@ -4,11 +4,11 @@
  */
 
 import {Text} from '@clayui/core';
+import {buildQueryString} from '@liferay/analytics-reports-js-components-web';
 import React, {useContext, useEffect, useState} from 'react';
 
 import ApiHelper from '../../../common/services/ApiHelper';
 import {ViewDashboardContext} from '../ViewDashboardContext';
-import {buildQueryString} from '../utils/buildQueryString';
 import {AllCategoriesDropdown} from './AllCategoriesDropdown';
 import {AllStructureTypesDropdown} from './AllStructureTypesDropdown';
 import {AllTagsDropdown} from './AllTagsDropdown';
@@ -66,15 +66,20 @@ async function fetchStructureData({
 	language: Item;
 	space: Item;
 }) {
-	const queryParams = buildQueryString({
-		categoryId: filters.category?.value,
-		groupBy: filters.structureType?.value,
-		languageId: language?.value,
-		spaceId: space?.value,
-		structureId: filters.structure?.value,
-		tagId: filters.tag?.value,
-		vocabularyId: filters.vocabulary?.value,
-	});
+	const queryParams = buildQueryString(
+		{
+			categoryId: filters.category?.value,
+			groupBy: filters.structureType?.value,
+			languageId: language?.value,
+			spaceId: space?.value,
+			structureId: filters.structure?.value,
+			tagId: filters.tag?.value,
+			vocabularyId: filters.vocabulary?.value,
+		},
+		{
+			shouldIgnoreParam: (value) => value === 'all',
+		}
+	);
 
 	const endpoint = `/o/analytics-cms-rest/v1.0/inventory-analysis${queryParams}`;
 

olafk/liferay-portal-old

modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/main_view/dashboard/components/ContentAndFilesCard.tsx
@@ -6,20 +6,19 @@
 import {Text} from '@clayui/core';
 import ClayIcon from '@clayui/icon';
 import ClayLoadingIndicator from '@clayui/loading-indicator';
+import {
+	TrendClassification,
+	buildQueryString,
+	getStatsColor,
+	getStatsIcon,
+	toThousands,
+} from '@liferay/analytics-reports-js-components-web';
 import {sub} from 'frontend-js-web';
 import React, {useContext, useEffect, useMemo, useState} from 'react';
 
 import ApiHelper from '../../../common/services/ApiHelper';
 import {ViewDashboardContext} from '../ViewDashboardContext';
-import {buildQueryString} from '../utils/buildQueryString';
-import {toThousands} from '../utils/number';
 import {RangeSelector, getSafeRangeSelector} from './RangeSelectorsDropdown';
-
-export enum TrendClassification {
-	Negative = 'NEGATIVE',
-	Neutral = 'NEUTRAL',
-	Positive = 'POSITIVE',
-}
 
 export interface IMetricsProps {
 	categoriesCount: number;
@@ -38,30 +37,6 @@ export interface IContentAndFilesCard {
 	title: (totalCount: number) => string;
 }
 
-function getStatsColor(
-	trendClassification: TrendClassification
-): 'danger' | 'success' | 'secondary' {
-	if (trendClassification === TrendClassification.Negative) {
-		return 'danger';
-	}
-	else if (trendClassification === TrendClassification.Positive) {
-		return 'success';
-	}
-
-	return 'secondary';
-}
-
-function getStatsIcon(trendPercentage: number) {
-	if (trendPercentage > 0) {
-		return 'caret-top';
-	}
-	else if (trendPercentage < 0) {
-		return 'caret-bottom';
-	}
-
-	return null;
-}
-
 const ContentAndFilesCard: React.FC<IContentAndFilesCard> = ({
 	endpointURL,
 	rangeSelector,
@@ -74,11 +49,16 @@ const ContentAndFilesCard: React.FC<IContentAndFilesCard> = ({
 	const [loading, setLoading] = useState(true);
 	const [metrics, setMetrics] = useState<IMetricsProps>();
 
-	const queryParams = buildQueryString({
-		depotEntryId: space.value,
-		languageId: language.value,
-		...getSafeRangeSelector(rangeSelector),
-	});
+	const queryParams = buildQueryString(
+		{
+			depotEntryId: space.value,
+			languageId: language.value,
+			...getSafeRangeSelector(rangeSelector),
+		},
+		{
+			shouldIgnoreParam: (value) => value === 'all',
+		}
+	);
 
 	useEffect(() => {
 		async function getMetrics() {

modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/main_view/dashboard/components/InventoryAnalysisCard.tsx
@@ -4,11 +4,11 @@
  */
 
 import {Text} from '@clayui/core';
+import {buildQueryString} from '@liferay/analytics-reports-js-components-web';
 import React, {useContext, useEffect, useState} from 'react';
 
 import ApiHelper from '../../../common/services/ApiHelper';
 import {ViewDashboardContext} from '../ViewDashboardContext';
-import {buildQueryString} from '../utils/buildQueryString';
 import {AllCategoriesDropdown} from './AllCategoriesDropdown';
 import {AllStructureTypesDropdown} from './AllStructureTypesDropdown';
 import {AllTagsDropdown} from './AllTagsDropdown';
@@ -66,15 +66,20 @@ async function fetchStructureData({
 	language: Item;
 	space: Item;
 }) {
-	const queryParams = buildQueryString({
-		categoryId: filters.category?.value,
-		depotEntryId: space?.value,
-		groupBy: filters.structureType?.value,
-		languageId: language?.value,
-		structureId: filters.structure?.value,
-		tagId: filters.tag?.value,
-		vocabularyId: filters.vocabulary?.value,
-	});
+	const queryParams = buildQueryString(
+		{
+			categoryId: filters.category?.value,
+			depotEntryId: space?.value,
+			groupBy: filters.structureType?.value,
+			languageId: language?.value,
+			structureId: filters.structure?.value,
+			tagId: filters.tag?.value,
+			vocabularyId: filters.vocabulary?.value,
+		},
+		{
+			shouldIgnoreParam: (value) => value === 'all',
+		}
+	);
 
 	const endpoint = `/o/analytics-cms-rest/v1.0/inventory-analysis${queryParams}`;
 