AliciaGarciaGarcia/liferay-portal

LPD-61279 add new filter to the api URL call on the cms

modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/display/context/ViewSharedWithMeSectionDisplayContext.java
@@ -25,7 +25,7 @@ public class ViewSharedWithMeSectionDisplayContext {
 
 	public String getAPIURL() {
 		return "/o/headless-admin-user/v1.0/my-user-account/shared-assets" +
-			"/shared-with-me";
+			"/shared-with-me?filter=cms eq true";
 	}
 
 	public Map<String, Object> getEmptyState() {

olafk/liferay-portal-old

modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/display/context/ViewSharedWithMeSectionDisplayContext.java
@@ -29,7 +29,7 @@ public class ViewSharedWithMeSectionDisplayContext {
 
 	public String getAPIURL() {
 		return "/o/headless-admin-user/v1.0/my-user-account/shared-assets" +
-			"/shared-with-me";
+			"/shared-with-me?filter=space eq true";
 	}
 
 	public Map<String, Object> getEmptyState() {